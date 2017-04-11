d92d8f4205775db3780386a6f0877df7444a235f
Niranjan Rajendran <niranjan94@yahoo.com>
2017-04-11 11:46:10 +0530

    Added dictionary with static info

--- app/utils/dictionary.js
+++ app/utils/dictionary.js
@@ -1,3 +1,42 @@
-/**
- * Created by niranjan on 10/04/17.
- */
+import moment from 'moment';
+
+export const licenses = [
+  {
+    name : 'Attribution-NonCommercial-NoDerivs',
+    link : 'https://creativecommons.org/licenses/by-nc-nd/4.0'
+  },
+  {
+    name : 'Attribution-NonCommercial',
+    link : 'https://creativecommons.org/licenses/by-nc/4.0'
+  },
+  {
+    name : 'Public Domain Dedication (CC0)',
+    link : 'https://creativecommons.org/publicdomain/zero/1.0/'
+  },
+  {
+    name : 'Attribution',
+    link : 'https://creativecommons.org/licenses/by/4.0'
+  },
+  {
+    name : 'All rights reserved',
+    link : 'https://en.wikipedia.org/wiki/All_rights_reserved'
+  },
+  {
+    name : 'Attribution-NoDerivs',
+    link : 'https://creativecommons.org/licenses/by-nd/4.0'
+  },
+  {
+    name : 'Public Domain Work',
+    link : 'https://creativecommons.org/publicdomain/mark/1.0/'
+  },
+  {
+    name : 'Attribution-NonCommercial-ShareAlike',
+    link : 'https://creativecommons.org/licenses/by-nc-sa/4.0'
+  },
+  {
+    name : 'Attribution-ShareAlike',
+    link : 'https://creativecommons.org/licenses/by-sa/4.0'
+  }
+];
+
+export const timezones = moment.tz.names();
