a1086d77a815df0bd4c57dc0959eca0aa23de6b7
Niranjan Rajendran <niranjan94@yahoo.com>
2017-04-16 12:03:10 +0530

    Remove liquid-fire and move config into js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,8 @@ const Funnel = require('broccoli-funnel');
 
 module.exports = function(defaults) {
   const app = new EmberApp(defaults, {
-    flexibility: {
+    storeConfigInMeta : false,
+    flexibility       : {
       enabled: true
     },
     minifyHTML: {
@@ -21,7 +22,7 @@ module.exports = function(defaults) {
   app.import('bower_components/Croppie/croppie.css');
   app.import('bower_components/Croppie/croppie.min.js');
 
-  const appTree = app.toTree();
+  const appTree = app.toTree([]);
   return new MergeTrees([appTree, new Funnel(appTree, {
     files: ['index.html'],
 
