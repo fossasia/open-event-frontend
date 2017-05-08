0cbd1aaa336152e629fe51ba886776236f8a46b6
Niranjan Rajendran <me@niranjan.io>
2017-05-08 21:34:25 +0530

    Use form helper mixin in login, register, and forgot-password forms

--- app/components/forms/forgot-password-form.js
+++ app/components/forms/forgot-password-form.js
@@ -1,60 +1,63 @@
 import Ember from 'ember';
+import FormMixin from 'open-event-frontend/mixins/form';
 
 const { Component } = Ember;
 
-export default Component.extend({
+export default Component.extend(FormMixin, {
 
   identification : '',
   password       : '',
   isLoading      : false,
 
+  getValidationRules() {
+    return {
+      inline : true,
+      delay  : false,
+      on     : 'blur',
+      fields : {
+        identification: {
+          identifier : 'email',
+          rules      : [
+            {
+              type   : 'empty',
+              prompt : this.i18n.t('Please enter your email ID')
+            },
+            {
+              type   : 'email',
+              prompt : this.i18n.t('Please enter a valid email ID')
+            }
+          ]
+        }
+      }
+    };
+  },
+
   actions: {
-
     submit() {
-      let payload = this.getProperties('identification');
+      this.onValid(() => {
+        let payload = this.getProperties('identification');
 
-      this.set('errorMessage', null);
-      this.set('isLoading', true);
+        this.set('errorMessage', null);
+        this.set('isLoading', true);
 
-      this.get('loader')
-        .post('/forgot-password', payload)
-        .then(() => {
+        this.get('loader')
+          .post('/forgot-password', payload)
+          .then(() => {
 
-        })
-        .catch(reason => {
-          if (reason.hasOwnProperty('code') && reason.code === 401) {
-            this.set('errorMessage', this.i18n.t('Your credentials were incorrect.'));
-          } else {
-            this.set('errorMessage', this.i18n.t('An unexpected error occurred.'));
-          }
-          this.set('isLoading', false);
-        });
+          })
+          .catch(reason => {
+            if (reason.hasOwnProperty('code') && reason.code === 401) {
+              this.set('errorMessage', this.i18n.t('Your credentials were incorrect.'));
+            } else {
+              this.set('errorMessage', this.i18n.t('An unexpected error occurred.'));
+            }
+            this.set('isLoading', false);
+          });
+      });
     }
   },
 
   didInsertElement() {
-    this.$('.ui.form')
-      .form({
-        inline : true,
-        delay  : false,
-        on     : 'blur',
-        fields : {
-          identification: {
-            identifier : 'email',
-            rules      : [
-              {
-                type   : 'empty',
-                prompt : this.i18n.t('Please enter your email ID')
-              },
-              {
-                type   : 'email',
-                prompt : this.i18n.t('Please enter a valid email ID')
-              }
-            ]
-          }
-        }
-      });
-
     if (this.get('session.newUser')) {
       this.set('newUser', this.get('session.newUser'));
       this.set('identification', this.get('session.newUser'));
