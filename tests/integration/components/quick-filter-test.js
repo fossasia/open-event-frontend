fe2d39bb3a617611ed85fe64032f80c262d4809e
utkarshnath <nath.utkarsh1@gmail.com>
2017-05-10 01:40:25 +0530

    added filter option

---
+++ tests/integration/components/quick-filter-test.js
@@ -0,0 +1,10 @@
+import { test } from 'ember-qunit';
+import moduleForComponent from 'open-event-frontend/tests/helpers/component-helper';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('quick-filter', 'Integration | Component | quick-filter');
+
+test('it renders', function(assert) {
+  this.render(hbs`{{quick-filter}}`);
+  assert.ok(this.$().html().trim().includes('All Dates'));
+});
