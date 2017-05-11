295528e0d5555e12b0d0887610d85680ec28571e
anu-007 <anubhutiv1@gmail.com>
2017-05-11 21:04:43 +0530

    improved location map

--- tests/integration/components/public/event-map-test.js
+++ tests/integration/components/public/event-map-test.js
@@ -1,19 +1,26 @@
-import { test } from 'ember-qunit';
-import moduleForComponent from 'open-event-frontend/tests/helpers/component-helper';
+import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('public/event-map', 'Integration | Component | public/event map', {
   integration: true
 });
 
-const location= { latitude: 37.7833, longitude: -122.4167, locationName: 'sample event location address' };
-
 test('it renders', function(assert) {
+  assert.expect(1);
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.render(hbs`{{public/event-map event=model.event}}`);
+  assert.ok(this.$());
 
-  this.render(hbs`{{public/event-map}}`);
+});
 
-  assert.equal(this.$().text().trim(), 'location');
+test('it includes the marker', function(assert) {
+  assert.expect(1);
+
+  this.render(hbs`
+    {{#g-map as |context|}}
+      {{g-map-marker context}}
+    {{/g-map}}
+  `);
+
+  assert.equal(this.get('markers.length', 1));
 });
