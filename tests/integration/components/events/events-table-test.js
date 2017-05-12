06259548990fbe9024eb7e26bf2b80e00b39c2b5
Niranjan Rajendran <me@niranjan.io>
2017-05-12 18:14:48 +0530

    Refactor component location

--- tests/integration/components/events/events-table-test.js
+++ tests/integration/components/events/events-table-test.js
@@ -2,10 +2,9 @@ import { test } from 'ember-qunit';
 import moduleForComponent from 'open-event-frontend/tests/helpers/component-helper';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('event-table', 'Integration | Component | event table');
+moduleForComponent('events/events-table', 'Integration | Component | events/events table');
 
 test('it renders', function(assert) {
-
-  this.render(hbs`{{event-table i18n=i18n}}`);
+  this.render(hbs`{{events/events-table i18n=i18n}}`);
   assert.ok(this.$().html().trim().includes('Roles'));
 });
