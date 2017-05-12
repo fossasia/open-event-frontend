f42f55aa1bd0f652d7bcbe8f0f7a6369f8d6375c
Abhishek Prasad <abhishek71994@gmail.com>
2017-05-12 10:13:35 +0530

    updating notification component files

--- app/components/app-notification.js
+++ app/components/app-notification.js
@@ -1,30 +1,20 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-  isShowing : false,
-  notifications : ["The first things left to do here","The second things left to do here","this is 3rd"],
+const { Component } = Ember;
+
+export default Component.extend({
+  notifications : ['The first things left to do here', 'The second things left to do here', 'this is 3rd'],
   countShow : true,
   actions : {
-    notification_show(){
-      if(this.isShowing==false){
-        this.set('isShowing',true);
-      }
-      else
-        this.set('isShowing',false);
+    setCountShow() {
+      this.set('countShow', false);
     },
-    setCountShow(){
-      this.set('countShow',false);
-    },
-    showPop(){
-      console.log("single");
-      $(' .par').popup({
+    showPop() {
+      console.log('hurr');
+      this.$(' .notif-i').popup({
         popup : '.popup',
         on    : 'click'
-      })
+      });
     }
-
   }
 });
-
-
-
