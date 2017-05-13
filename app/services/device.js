17260539eecab990cd3c8e7299f912dc01d0d398
Niranjan Rajendran <me@niranjan.io>
2017-05-13 13:42:09 +0530

    Add js visibility helper

--- app/services/device.js
+++ app/services/device.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
 
-const { Service, computed } = Ember;
+const { $, Service, computed, computed: { equal }, run: { next, debounce } } = Ember;
 
 export default Service.extend({
+
+  deviceType: 'computer',
+
+  isMobile       : equal('deviceType', 'mobile'),
+  isComputer     : equal('deviceType', 'computer'),
+  isTablet       : equal('deviceType', 'tablet'),
+  isLargeMonitor : equal('deviceType', 'largeMonitor'),
+  isWideScreen   : equal('deviceType', 'widescreen'),
 
   isInternetExplorer: computed(function() {
     let rv = -1;
@@ -20,6 +28,33 @@ export default Service.extend({
       }
     }
     return rv !== -1;
-  })
+  }),
+
+  getDeviceType() {
+    try {
+      return document.defaultView
+        .getComputedStyle(
+          document.querySelector('#device-type-identifier'), '::after'
+        )
+        .getPropertyValue('content')
+        .replace(/['"]+/g, '');
+    } catch (e) {
+      return 'computer';
+    }
+  },
+
+  init() {
+    this._super(...arguments);
+    next(this, () => {
+      console.log(this.getDeviceType());
+      this.set('deviceType', this.getDeviceType());
+    });
+    $(window).resize(() => {
+      debounce(this, () => {
+        console.log(this.getDeviceType());
+        this.set('deviceType', this.getDeviceType());
+      }, 200);
+    });
+  }
 
 });
