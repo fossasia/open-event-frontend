449e2554a72e06584e22a72b700c632d1a961935
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-05-15 11:17:21 +0530

    add settings/contact info  page

---
+++ tests/integration/components/settings/application-page-test.js
@@ -0,0 +1,10 @@
+import { test } from 'ember-qunit';
+import moduleForComponent from 'open-event-frontend/tests/helpers/component-helper';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('settings/application-section', 'Integration | Component | settings/application section');
+
+test('it renders', function(assert) {
+  this.render(hbs`{{settings/application-section}}`);
+  assert.ok(this.$().html().trim().includes('Applications'));
+});
