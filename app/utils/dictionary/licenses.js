29099187d701970bc6e0e86ebdc1968fb7654187
harshitagupta30 <harshitagupta310@gmail.com>
2017-05-22 19:58:00 +0530

    feat: License info added at the bottom

--- app/utils/dictionary/licenses.js
+++ app/utils/dictionary/licenses.js
@@ -1,38 +1,47 @@
 export const licenses = [
   {
-    name : 'Attribution-NonCommercial-NoDerivs',
-    link : 'https://creativecommons.org/licenses/by-nc-nd/4.0'
+    name     : 'Attribution-NonCommercial-NoDerivs',
+    link     : 'https://creativecommons.org/licenses/by-nc-nd/4.0',
+    imageSrc : '/images/copyright/attribution-nc-nd.png'
   },
   {
-    name : 'Attribution-NonCommercial',
-    link : 'https://creativecommons.org/licenses/by-nc/4.0'
+    name     : 'Attribution-NonCommercial',
+    link     : 'https://creativecommons.org/licenses/by-nc/4.0',
+    imageSrc : '/images/copyright/attribution-nc.png'
   },
   {
-    name : 'Public Domain Dedication (CC0)',
-    link : 'https://creativecommons.org/publicdomain/zero/1.0/'
+    name     : 'Public Domain Dedication (CC0)',
+    link     : 'https://creativecommons.org/publicdomain/zero/1.0/',
+    imageSrc : '/images/copyright/pdd.png'
   },
   {
-    name : 'Attribution',
-    link : 'https://creativecommons.org/licenses/by/4.0'
+    name     : 'Attribution',
+    link     : 'https://creativecommons.org/licenses/by/4.0',
+    imageSrc : '/images/copyright/attribution.png'
   },
   {
-    name : 'All rights reserved',
-    link : 'https://en.wikipedia.org/wiki/All_rights_reserved'
+    name     : 'All rights reserved',
+    link     : 'https://en.wikipedia.org/wiki/All_rights_reserved',
+    imageSrc : ''
   },
   {
-    name : 'Attribution-NoDerivs',
-    link : 'https://creativecommons.org/licenses/by-nd/4.0'
+    name     : 'Attribution-NoDerivs',
+    link     : 'https://creativecommons.org/licenses/by-nd/4.0',
+    imageSrc : '/images/copyright/attribution-nd.png'
   },
   {
-    name : 'Public Domain Work',
-    link : 'https://creativecommons.org/publicdomain/mark/1.0/'
+    name     : 'Public Domain Work',
+    link     : 'https://creativecommons.org/publicdomain/mark/1.0/',
+    imageSrc : '/imagescopyright/pdw.png'
   },
   {
-    name : 'Attribution-NonCommercial-ShareAlike',
-    link : 'https://creativecommons.org/licenses/by-nc-sa/4.0'
+    name     : 'Attribution-NonCommercial-ShareAlike',
+    link     : 'https://creativecommons.org/licenses/by-nc-sa/4.0',
+    imageSrc : '/imagescopyright/attribution-nc-sa.png'
   },
   {
-    name : 'Attribution-ShareAlike',
-    link : 'https://creativecommons.org/licenses/by-sa/4.0'
+    name     : 'Attribution-ShareAlike',
+    link     : 'https://creativecommons.org/licenses/by-sa/4.0',
+    imageSrc : '/imagescopyright/attribution-sa.png'
   }
 ];
