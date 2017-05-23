d004bf0c537f32bd8cae802b5a63ab0a41541197
Niranjan Rajendran <me@niranjan.io>
2017-05-23 19:55:47 +0530

    Enable tickets order button only when tickets selected

--- app/components/public/ticket-list.js
+++ app/components/public/ticket-list.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
+import { sumBy } from 'lodash';
 
 const { Component, computed } = Ember;
 
 export default Component.extend({
   classNames: ['ui', 'segments', 'ticket-list'],
+
+  hasTicketsInOrder: computed('tickets.@each.orderQuantity', function() {
+    return sumBy(this.get('tickets'), 'orderQuantity') > 0;
+  }),
 
   total: computed('tickets.@each.orderQuantity', function() {
     let sum = 0.0;
