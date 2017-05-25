da8e47cb8db66056a1be418c3c888638e8282188
harshitagupta30 <harshitagupta310@gmail.com>
2017-05-25 23:14:16 +0530

    feat: added sessions to event route (UI)

---
+++ tests/unit/routes/events/view/sessions-test.js
@@ -0,0 +1,9 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+moduleFor('route:events/view/sessions', 'Unit | Route | events/view/sessions', []);
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
