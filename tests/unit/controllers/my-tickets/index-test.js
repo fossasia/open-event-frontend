1c10006fdebe4c6bc2679d03a2e00321dc55adb0
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-05-31 12:20:40 +0530

    feat: UI for my-tickets and it's subroutes

---
+++ tests/unit/controllers/my-tickets/index-test.js
@@ -0,0 +1,8 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+moduleFor('controller:my-tickets/index', 'Unit | Controller | my tickets/index');
+
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
