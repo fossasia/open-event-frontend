3a9e1bff2dc2616a8de9377811b3e721c532ff9c
Abhinav Khare <abhinav.khare31@gmail.com>
2017-06-04 19:58:57 +0530

    Adds UI for tickets/order route (#123)

---
+++ app/routes/events/view/tickets/orders.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+
+const { Route } = Ember;
+
+export default Route.extend({
+  titleToken() {
+    return this.i18n.t('Orders');
+  },
+
+  model() {
+    return [
+      {
+        'status'         : 'Completed',
+        'completed_at'   : '2016-04-06',
+        'id'             : 1,
+        'quantity'       : 2,
+        'invoice_number' : '#O1496523209-306',
+        'amount'         : 0,
+        'paid_via'       : 'Free',
+        'buyer'          : 'sample@gmail.com',
+        'buyer_name'     : 'Sample Buyer'
+      },
+
+      {
+        'status'         : 'Completed',
+        'completed_at'   : '2017-06-02T11:22:33+05:30',
+        'id'             : 2,
+        'quantity'       : 1,
+        'invoice_number' : '#O1345883292-302',
+        'amount'         : 50,
+        'paid_via'       : 'Paypal',
+        'buyer'          : 'sample@gmail.com',
+        'buyer_name'     : 'Sample Buyer2'
+      }
+    ];
+  }
+});
