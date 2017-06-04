7cc107157f81b9969d9ba60dfc1f2482d54e3980
Abhinav Khare <abhinav.khare31@gmail.com>
2017-06-04 21:55:25 +0530

    Adds UI for settings/password route (#129)

--- app/components/settings/password-section.js
+++ app/components/settings/password-section.js
@@ -1,6 +1,55 @@
 import Ember from 'ember';
+import FormMixin from 'open-event-frontend/mixins/form';
 
 const { Component } = Ember;
 
-export default Component.extend({
+export default Component.extend(FormMixin, {
+  getValidationRules() {
+    return {
+      inline : true,
+      delay  : false,
+      on     : 'blur',
+      fields : {
+        password_current: {
+          identifier : 'password_current',
+          rules      : [
+            {
+              type   : 'empty',
+              prompt : this.i18n.t('Please enter the current password')
+            }
+          ]
+        },
+        password_new: {
+          identifier : 'password_new',
+          rules      : [
+            {
+              type   : 'empty',
+              prompt : this.i18n.t('Please enter a new password')
+            },
+            {
+              type   : 'minLength[6]',
+              prompt : this.i18n.t('Your password must have at least {ruleValue} characters')
+            }
+          ]
+        },
+        password_repeat: {
+          identifier : 'password_repeat',
+          rules      : [
+            {
+              type   : 'match[password_new]',
+              prompt : this.i18n.t('Passwords do not match')
+            }
+          ]
+        }
+      }
+    };
+  },
+
+  actions: {
+    submit() {
+      this.onValid(() => {
+
+      });
+    }
+  }
 });
