db53194e6b534763b098d6714a78410c72a3bc07
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-06-05 02:14:01 +0530

    add access-codes to events tickets route
    add status, icons to action
    
    use predefined math helpers, add popups

---
+++ app/routes/events/view/tickets/access.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+const { Route } = Ember;
+
+export default Route.extend({
+  titleToken() {
+    return this.i18n.t('Access codes');
+  },
+
+  model() {
+    return [
+      {
+        'accessCode'       : '#386',
+        'accessCodeUrl'    : 'https://github.com',
+        'accessibleTicket' : 'Early bird',
+        'validity'         : '3 months',
+        'isActive'         : true
+      }, {
+        'accessCode'       : '#386',
+        'accessCodeUrl'    : 'https://github.com',
+        'accessibleTicket' : 'Early bird',
+        'validity'         : '3 months',
+        'isActive'         : false
+      }
+    ];
+  }
+});
