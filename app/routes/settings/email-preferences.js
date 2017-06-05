abc745a91f17d47a56e5348d0e31f84533e6125e
geekyd <singhsince94@gmail.com>
2017-06-05 18:06:23 +0530

    Adds ui for settings/email-preferences

--- app/routes/settings/email-preferences.js
+++ app/routes/settings/email-preferences.js
@@ -5,5 +5,21 @@ const { Route } = Ember;
 export default Route.extend({
   titleToken() {
     return this.i18n.t('Email Preferences');
+  },
+  model() {
+    return [{
+      name             : 'Techtoma',
+      role             : 'Organiser',
+      isNewPaper       : true,
+      isChangeSchedule : true,
+      isNewEvent       : true
+    },
+    {
+      name             : 'Open Summit 2017',
+      role             : 'Organiser',
+      isNewPaper       : false,
+      isChangeSchedule : false,
+      isNewEvent       : false
+    }];
   }
 });
