1c3b01ecbbc22cb5e3ee5ea2d90d8a3dba1812d6
geekyd <singhsince94@gmail.com>
2017-06-08 08:56:06 +0530

    Adds ui for admin/modules

--- app/routes/admin/modules.js
+++ app/routes/admin/modules.js
@@ -5,5 +5,12 @@ const { Route } = Ember;
 export default Route.extend({
   titleToken() {
     return this.i18n.t('Modules');
+  },
+  model() {
+    return {
+      ticketing : false,
+      payments  : false,
+      donations : false
+    };
   }
 });
