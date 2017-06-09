ce678bdf3bb9a7219ee27d3b4da1e7dd3433bbcd
geekyd <singhsince94@gmail.com>
2017-06-09 20:17:02 +0530

    Adds ui for content/social-links & navigation in admin/content

---
+++ app/components/forms/admin/content/social-links-form.js
@@ -0,0 +1,84 @@
+import Ember from 'ember';
+import FormMixin from 'open-event-frontend/mixins/form';
+
+const { Component } = Ember;
+
+export default Component.extend(FormMixin, {
+
+  getValidationRules() {
+    return {
+      inline : true,
+      delay  : false,
+      on     : 'blur',
+      fields : {
+        support: {
+          identifier : 'support',
+          optional   : true,
+          rules      : [
+            {
+              type   : 'url',
+              prompt : this.i18n.t('Please enter a valid url')
+            }
+          ]
+        },
+        twitter: {
+          identifier : 'twitter',
+          optional   : true,
+          rules      : [
+            {
+              type   : 'url',
+              prompt : this.i18n.t('Please enter a valid url')
+            }
+          ]
+        },
+        facebook: {
+          identifier : 'facebook',
+          optional   : true,
+          rules      : [
+            {
+              type   : 'url',
+              prompt : this.i18n.t('Please enter a valid url')
+            }
+          ]
+        },
+        google_plus: {
+          identifier : 'google_plus',
+          optional   : true,
+          rules      : [
+            {
+              type   : 'url',
+              prompt : this.i18n.t('Please enter a valid url')
+            }
+          ]
+        },
+        youtube: {
+          identifier : 'youtube',
+          optional   : true,
+          rules      : [
+            {
+              type   : 'url',
+              prompt : this.i18n.t('Please enter a valid url')
+            }
+          ]
+        },
+        github: {
+          identifier : 'github',
+          optional   : true,
+          rules      : [
+            {
+              type   : 'url',
+              prompt : this.i18n.t('Please enter a valid url')
+            }
+          ]
+        }
+      }
+    };
+  },
+  actions: {
+    submit() {
+      this.onValid(() => {
+      });
+    }
+  }
+
+});
