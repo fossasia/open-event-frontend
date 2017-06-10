25ebc6dc803e92a317f4727c48e6d1d262308f09
geekyd <singhsince94@gmail.com>
2017-06-10 10:58:16 +0530

    Adds ui for admin/settings/images

--- app/routes/admin/settings/images.js
+++ app/routes/admin/settings/images.js
@@ -5,5 +5,39 @@ const { Route } = Ember;
 export default Route.extend({
   titleToken() {
     return this.i18n.t('Images');
+  },
+
+  model() {
+    return [
+      {
+        type             : 'event',
+        fullWidth        : 1300,
+        fullHeight       : 500,
+        fullAspect       : true,
+        fullQuality      : 80,
+        iconWidth        : 75,
+        iconHeight       : 30,
+        iconAspect       : false,
+        iconQuality      : 80,
+        thumbnailWidth   : 500,
+        thumbnailHeight  : 200,
+        thumbnailAspect  : false,
+        thumbnailQuality : 80,
+        logoWidth        : 75,
+        logoHeight       : 30
+      },
+      {
+        type             : 'profile',
+        iconWidth        : 35,
+        iconHeight       : 35,
+        iconQuality      : 80,
+        thumbnailWidth   : 150,
+        thumbnailHeight  : 150,
+        thumbnailQuality : 80,
+        smallWidth       : 50,
+        smallHeight      : 50,
+        smallQuality     : 80
+      }
+    ];
   }
 });
