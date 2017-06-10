4aa0edadfefd77a8c4168d848a9dbe9294cfa5fd
anu-007 <anubhutiv1@gmail.com>
2017-06-10 11:59:21 +0530

    feat:adds discounts sub route
    added inc

---
+++ app/routes/events/view/tickets/discount-codes.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+const { Route } = Ember;
+
+export default Route.extend({
+  titleToken() {
+    return this.i18n.t('Discount codes');
+  },
+  model() {
+    return [
+      {
+        'id'       : 1,
+        'code'     : 'ABC',
+        'url'      : 'https://eventyay.com/e/9ba7ea20/?code=ABC',
+        'value'    : 1,
+        'validity' : 'N/A',
+        'status'   : 'Active'
+      },
+      {
+        'id'       : 2,
+        'code'     : 'FIRST50',
+        'url'      : 'https://eventyay.com/e/9ba7ea20/?code=FIRST50',
+        'value'    : 3,
+        'validity' : 'N/A',
+        'status'   : 'Active'
+      }
+    ];
+  }
+});
