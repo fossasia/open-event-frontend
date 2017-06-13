50bc30a9b99ccf5db412de9cff4db7649db04be0
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-06-13 15:12:38 +0530

    Add UI for content/system-images

---
+++ tests/unit/routes/admin/content/system-images/list-test.js
@@ -0,0 +1,9 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+moduleFor('route:admin/content/system-images/list', 'Unit | Route | admin/content/system images/list', []);
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
