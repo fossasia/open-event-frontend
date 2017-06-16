1f7a4881e28a7f235ea6a0264683cb345b496393
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-06-17 01:00:02 +0530

    Creates and links all sub routes of admin/reports

---
+++ tests/unit/routes/admin/reports/system-logs-test.js
@@ -0,0 +1,10 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+
+moduleFor('route:admin/reports/system-logs', 'Unit | Route | admin/reports/system logs');
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
---
+++ tests/unit/routes/admin/reports/system-logs/notification-logs-test.js
@@ -0,0 +1,9 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+moduleFor('route:admin/reports/system-logs/notification-logs', 'Unit | Route | admin/reports/system logs/notification logs');
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
