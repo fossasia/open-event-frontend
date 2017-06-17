520279b3f31d3f0882b61d1bdbafc1ab38152c57
harshitagupta30 <harshitagupta310@gmail.com>
2017-06-18 00:19:02 +0530

    feat: UI for admin/sales/marketer page

--- app/routes/admin/sales/marketer.js
+++ app/routes/admin/sales/marketer.js
@@ -5,5 +5,18 @@ const { Route } = Ember;
 export default Route.extend({
   titleToken() {
     return this.i18n.t('Marketer');
+  },
+  model() {
+    return [{
+      marketers : 'Sample Marketer1',
+      tickets   : 100,
+      sales     : 50.00,
+      discounts : 500.00
+    }, {
+      marketers : 'Sample Marketer2',
+      tickets   : 500,
+      sales     : 1000.00,
+      discounts : 200.00
+    }];
   }
 });
---
+++ tests/unit/controllers/admin/sales/marketer-test.js
@@ -0,0 +1,9 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+moduleFor('controller:admin/sales/marketer', 'Unit | Controller | admin/sales/marketer', []);
+
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
