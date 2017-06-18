34622b1ed731ddeabb11345449cd5ab166b321e6
geekyd <singhsince94@gmail.com>
2017-06-19 03:03:14 +0530

    Fixes lint errors for cli-eslint v4.0

--- app/components/events/view/export/api-response.js
+++ app/components/events/view/export/api-response.js
@@ -11,7 +11,7 @@ export default Component.extend({
 
 function syntaxHighlight(json) {
   json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function(match) {
+  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][/+\-/]?\d+)?)/g, function(match) {
     var cls = 'number';
     if (/^"/.test(match)) {
       if (/:$/.test(match)) {
