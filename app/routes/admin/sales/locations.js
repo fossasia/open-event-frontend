68817b554f0ad55bbedfb887c3e6c6e8a60a3975
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-06-19 21:10:57 +0530

    add admin/sales/locations UI

--- app/routes/admin/sales/locations.js
+++ app/routes/admin/sales/locations.js
@@ -5,5 +5,24 @@ const { Route } = Ember;
 export default Route.extend({
   titleToken() {
     return this.i18n.t('Location');
+  },
+  model() {
+    return [{
+      location         : 'Delhi',
+      completedTickets : 1,
+      completedSales   : 0.00,
+      placedTickets    : 1,
+      placedSales      : 0.00,
+      pendingTickets   : 1,
+      pendingSales     : 0.00
+    }, {
+      location         : 'Bangalore',
+      completedTickets : 3,
+      completedSales   : 23.30,
+      placedTickets    : 4,
+      placedSales      : 32.00,
+      pendingTickets   : 4,
+      pendingSales     : 32.00
+    }];
   }
 });
