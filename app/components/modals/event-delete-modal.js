6207f1e4533cd1e4f195f56c485e4547ec37918b
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-06-21 05:50:44 +0530

    Adds confirm modal before deleting event

---
+++ app/components/modals/event-delete-modal.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import ModalBase from 'open-event-frontend/components/modals/modal-base';
+
+const { computed } = Ember;
+
+export default ModalBase.extend({
+  isSmall         : true,
+  confirmName     : '',
+  isNameDifferent : computed('confirmName', function() {
+    return this.get('confirmName').toLowerCase() !== this.get('eventName').toLowerCase();
+  })
+});
