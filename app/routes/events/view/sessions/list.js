a05eb364cad237f791437838c873e8285ec12abc
geekyd <singhsince94@gmail.com>
2017-06-21 18:20:15 +0530

    Adds correct date variable in events/view/sessions

--- app/routes/events/view/sessions/list.js
+++ app/routes/events/view/sessions/list.js
@@ -18,24 +18,24 @@ export default Route.extend({
   model(params) {
     this.set('params', params);
     return [{
-      title          : 'Test Session 1',
-      speakers       : [{ name: 'speaker 1', id: 1, organization: 'fossasia' }, { name: 'speaker 2', id: 1, organization: 'fossasia' }],
-      track          : 'sample track',
-      shortAbstract  : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      submissionDate : new Date(),
-      lastModified   : new Date(),
-      emailSent      : 'No',
-      state          : 'confirmed'
+      title         : 'Test Session 1',
+      speakers      : [{ name: 'speaker 1', id: 1, organization: 'fossasia' }, { name: 'speaker 2', id: 1, organization: 'fossasia' }],
+      track         : 'sample track',
+      shortAbstract : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      submittedAt   : new Date(),
+      modifiedAt    : new Date(),
+      emailSent     : 'No',
+      state         : 'confirmed'
     },
     {
-      title          : 'Test Session 2',
-      speakers       : [{ name: 'speaker 3', id: 1, organization: 'fossasia' }, { name: 'speaker 4', id: 1, organization: 'fossasia' }],
-      track          : 'sample track',
-      shortAbstract  : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      submissionDate : new Date(),
-      lastModified   : new Date(),
-      emailSent      : 'Yes',
-      state          : 'confirmed'
+      title         : 'Test Session 2',
+      speakers      : [{ name: 'speaker 3', id: 1, organization: 'fossasia' }, { name: 'speaker 4', id: 1, organization: 'fossasia' }],
+      track         : 'sample track',
+      shortAbstract : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      submittedAt   : new Date(),
+      modifiedAt    : new Date(),
+      emailSent     : 'Yes',
+      state         : 'confirmed'
     }];
   }
 });
