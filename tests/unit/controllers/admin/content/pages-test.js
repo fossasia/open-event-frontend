13f4f4241a97f70f601cb066f7cdd496b1a8055d
geekyd <singhsince94@gmail.com>
2017-06-23 00:23:18 +0530

    Adds ui for admin/content/pages

---
+++ tests/unit/controllers/admin/content/pages-test.js
@@ -0,0 +1,9 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+moduleFor('controller:admin/content/pages', 'Unit | Controller | admin/content/pages', []);
+
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
