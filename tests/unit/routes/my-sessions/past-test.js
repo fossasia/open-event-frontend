d3c1342e5536904a010f86fc71c00d17b53bb2b8
geekyd <singhsince94@gmail.com>
2017-06-23 15:10:04 +0530

    Adds my-sessions route & its sub-routes

---
+++ tests/unit/routes/my-sessions/past-test.js
@@ -0,0 +1,9 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+moduleFor('route:my-sessions/past', 'Unit | Route | my sessions/past', []);
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
