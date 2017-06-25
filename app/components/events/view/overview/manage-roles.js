697dbe68f26b8e2b40cd2080b1150939c3d399ac
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-06-25 17:05:40 +0530

    Adds User role modal to event overview

--- app/components/events/view/overview/manage-roles.js
+++ app/components/events/view/overview/manage-roles.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
-  classNames: ['ui', 'fluid', 'card']
+  classNames : ['ui', 'fluid', 'card'],
+  actions    : {
+    openAddUserRoleModal() {
+      this.set('isAddUserRoleModalOpen', true);
+    },
+
+    addUserRole() {
+      this.set('isAddUserRoleModalOpen', false);
+    }
+  }
 });
