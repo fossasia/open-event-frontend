86dd2a9816ed9cb54d5e46952225475eb6d70df3
harshitagupta30 <harshitagupta310@gmail.com>
2017-06-25 14:08:29 +0530

    feat: Split events/<event-id>/tickets/access-codes into proper pages

--- app/router.js
+++ app/router.js
@@ -53,7 +53,9 @@ router.map(function() {
         this.route('attendees');
         this.route('add-order');
         this.route('discount-codes');
-        this.route('access', { path: 'access-codes' });
+        this.route('access-codes', function() {
+          this.route('list', { path: '/:access_status' });
+        });
       });
       this.route('speakers', function() {
         this.route('list', { path: '/:speakers_status' });
--- tests/unit/routes/events/view/tickets/access-codes.js
+++ tests/unit/routes/events/view/tickets/access-codes.js
@@ -1,7 +1,7 @@
 import { test } from 'ember-qunit';
 import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
 
-moduleFor('route:events/view/tickets/access', 'Unit | Route | events/view/tickets/access', []);
+moduleFor('route:events/view/tickets/access-codes', 'Unit | Route | events/view/tickets/access-codes', []);
 
 test('it exists', function(assert) {
   let route = this.subject();
