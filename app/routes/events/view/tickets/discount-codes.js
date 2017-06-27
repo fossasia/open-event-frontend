b11934b02eb47509f76650c9b700445dff5834d6
anu-007 <anubhutiv1@gmail.com>
2017-06-28 02:39:15 +0530

    splits events/<event-id>/tickets/discount-codes into proper pages

--- app/routes/events/view/tickets/discount-codes.js
+++ app/routes/events/view/tickets/discount-codes.js
@@ -1,3 +1,4 @@
+
 import Ember from 'ember';
 
 const { Route } = Ember;
@@ -5,25 +6,5 @@ const { Route } = Ember;
 export default Route.extend({
   titleToken() {
     return this.l10n.t('Discount codes');
-  },
-  model() {
-    return [
-      {
-        'id'       : 1,
-        'code'     : 'ABC',
-        'url'      : 'https://eventyay.com/e/9ba7ea20/?code=ABC',
-        'value'    : 1,
-        'validity' : 'N/A',
-        'status'   : 'Active'
-      },
-      {
-        'id'       : 2,
-        'code'     : 'FIRST50',
-        'url'      : 'https://eventyay.com/e/9ba7ea20/?code=FIRST50',
-        'value'    : 3,
-        'validity' : 'N/A',
-        'status'   : 'Active'
-      }
-    ];
   }
 });
