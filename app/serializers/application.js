f4e1f0460bbb00a54398e8aafa73bbb033670175
geekyd <singhsince94@gmail.com>
2017-07-01 18:06:18 +0530

    Adds singularize to application serializer

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,5 +1,13 @@
 import JSONAPISerializer from 'ember-data/serializers/json-api';
+import DS from 'ember-data';
+const { normalizeModelName } = DS;
+import { singularize } from 'ember-inflector';
 
 export default JSONAPISerializer.extend({
-
+  modelNameFromPayloadKey(key) {
+    return singularize(normalizeModelName(key));
+  },
+  payloadKeyFromModelName(modelName) {
+    return singularize(modelName);
+  }
 });
