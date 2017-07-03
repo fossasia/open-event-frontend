637c5e1dc3d6e1eb53f2796a75c346a6297aef0b
geekyd <singhsince94@gmail.com>
2017-07-03 13:33:02 +0530

    Adds register feature using API

---
+++ tests/unit/controllers/register-test.js
@@ -0,0 +1,8 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:register', 'Unit | Controller | register', {});
+
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
