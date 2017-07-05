1ef7e77c3ab9fa7b2fa9c8e891c37679c441908e
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-07-05 11:40:09 +0530

    Refactor public event page to enable speakers call

--- app/serializers/event.js
+++ app/serializers/event.js
@@ -2,13 +2,12 @@ import ApplicationSerializer from 'open-event-frontend/serializers/application';
 import CustomPrimaryKeyMixin from 'open-event-frontend/mixins/custom-primary-key';
 
 export default ApplicationSerializer.extend(CustomPrimaryKeyMixin, {
-  primaryKey: 'attributes.identifier',
 
-  attrs: {
-    type             : 'event-type',
-    topic            : 'event-topic',
-    subTopic         : 'event-sub-topic',
-    copyright        : 'event-copyright',
-    externalEventUrl : 'event-url'
+  primaryKey : 'attributes.identifier',
+  attrs      : {
+    typev     : 'event-type',
+    topic     : 'event-topic',
+    subTopic  : 'event-sub-topic',
+    copyright : 'event-copyright'
   }
 });
