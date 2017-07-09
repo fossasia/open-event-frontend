993de07fed2ba5a78e7c4a8b1a4a6b50d5afff7f
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-07-09 21:53:47 +0530

    Add create access code UI

--- tests/helpers/component-helper.js
+++ tests/helpers/component-helper.js
@@ -5,13 +5,33 @@ import Ember from 'ember';
 import l10nTestHelper from 'ember-l10n/test-helpers';
 import fragmentTransformInitializer from 'open-event-frontend/initializers/model-fragments';
 import L10n from 'ember-l10n/services/l10n';
-const { run, getOwner } = Ember;
+const { run, getOwner, Service } = Ember;
+
+const routingStub = Service.extend({
+  router: {
+    router: {
+      state: {
+        params: {
+          'events.view': {
+            event_id: 1
+          }
+        }
+      }
+    },
+    generate() {
+      return 'http://dummy-url.com';
+    }
+  }
+});
+
 
 export default function(path, name, testCase = null) {
   moduleForComponent(path, name, {
     integration: true,
 
     beforeEach() {
+      this.register('service:routing', routingStub);
+      this.inject.service('routing', { as: 'routing' });
       this.register('service:l10n', L10n);
       this.inject.service('l10n', { as: 'l10n' });
       this.application = startApp();
