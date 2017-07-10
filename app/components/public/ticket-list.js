52d9c035f65c7e99c9265390d29d1fbe631daf41
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-07-10 06:12:01 +0530

    Add promotional code text field on event page

--- app/components/public/ticket-list.js
+++ app/components/public/ticket-list.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
+import FormMixin from 'open-event-frontend/mixins/form';
 import { sumBy } from 'lodash';
 
 const { Component, computed } = Ember;
 
-export default Component.extend({
+export default Component.extend(FormMixin, {
   classNames: ['ui', 'segments', 'ticket-list'],
 
   hasTicketsInOrder: computed('tickets.@each.orderQuantity', function() {
@@ -16,5 +17,32 @@ export default Component.extend({
       sum += (ticket.price * ticket.orderQuantity);
     });
     return sum;
-  })
+  }),
+  actions: {
+    togglePromotionalCode() {
+      this.toggleProperty('enterPromotionalCode');
+    },
+    applyPromotionalCode() {
+      this.onValid(() => {
+      });
+    }
+  },
+  getValidationRules() {
+    return {
+      inline : true,
+      delay  : false,
+      on     : 'blur',
+      fields : {
+        promotionalCode: {
+          identifier : 'promotional_code',
+          rules      : [
+            {
+              type   : 'empty',
+              prompt : this.l10n.t('Please enter the promotional Code')
+            }
+          ]
+        }
+      }
+    };
+  }
 });
