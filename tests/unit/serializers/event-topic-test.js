16c70cc6a9b0273d8af573db79fff12c0f5db3d3
Niranjan Rajendran <me@niranjan.io>
2017-07-12 02:15:59 +0530

    Resolve conflicts and fix tests

--- tests/unit/serializers/event-topic-test.js
+++ tests/unit/serializers/event-topic-test.js
@@ -1,11 +1,14 @@
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('event-topic', 'Unit | Serializer | event topic', {
-  // Specify the other units that are required for this test.
-  needs: ['serializer:event-topic']
+  needs: [
+    'serializer:event-topic',
+    'serializer:event',
+    'model:event',
+    'model:event-sub-topic'
+  ]
 });
 
-// Replace this with your real tests.
 test('it serializes records', function(assert) {
   let record = this.subject();
 
