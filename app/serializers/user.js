2be2c2e536196289c08ffa026bee5f8d7bb138df
Dilpreet Singh <singhsince94@gmail.com>
2017-07-19 18:34:35 +0530

    Integrates user API to profile route

--- app/serializers/user.js
+++ app/serializers/user.js
@@ -1,10 +1,16 @@
 import ApplicationSerializer from 'open-event-frontend/serializers/application';
-import { pick } from 'lodash';
+import { pick, omit } from 'lodash';
 
 export default ApplicationSerializer.extend({
   serialize(snapshot, options) {
     const json = this._super(...arguments);
-    if (options && options.includeId) {
+    if (snapshot.id) {
+      let attributesToOmit = ['email'];
+      if (!snapshot.adapterOptions || !snapshot.adapterOptions.includePassword) {
+        attributesToOmit.push('password');
+      }
+      json.data.attributes = omit(json.data.attributes, attributesToOmit);
+    } else if (options && options.includeId) {
       json.data.attributes = pick(json.data.attributes, ['email', 'password']);
     }
     return json;
