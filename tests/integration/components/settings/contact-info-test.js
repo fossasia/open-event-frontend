7378b761eeee1b5437542603db82b09f45eef604
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-07-20 14:33:05 +0530

    add user API for settings/contact-info

--- tests/integration/components/settings/contact-info-test.js
+++ tests/integration/components/settings/contact-info-test.js
@@ -1,10 +1,19 @@
 import { test } from 'ember-qunit';
 import moduleForComponent from 'open-event-frontend/tests/helpers/component-helper';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('settings/contact-info-section', 'Integration | Component | settings/contact info section');
 
+const { Object: EmberObject } = Ember;
+
+var user = EmberObject.create({
+  email   : 'xyz@xyz.com',
+  contact : '34893485843'
+});
+
 test('it renders', function(assert) {
-  this.render(hbs`{{settings/contact-info-section}}`);
+  this.set('user', user);
+  this.render(hbs`{{settings/contact-info-section user=user l10n=l10n}}`);
   assert.ok(this.$().html().trim().includes('Email'));
 });
