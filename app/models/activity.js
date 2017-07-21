8961e43a31d5a127c292c8cb1f6c9b0e43623876
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-07-22 04:21:43 +0530

    Add activity model

---
+++ app/models/activity.js
@@ -0,0 +1,11 @@
+import attr from 'ember-data/attr';
+import moment from 'moment';
+import ModelBase from 'open-event-frontend/models/base';
+
+const detectedTimezone = moment.tz.guess();
+
+export default ModelBase.extend({
+  actor  : attr('string'),
+  time   : attr('moment', { defaultValue: () => moment.tz(detectedTimezone) }),
+  action : attr('string')
+});
