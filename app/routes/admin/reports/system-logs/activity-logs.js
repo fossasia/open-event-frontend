046d5677e53c334fbc1cc89d50c5f7e146479f9d
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-07-22 04:22:23 +0530

    Implement activity api:admin report: activity logs

--- app/routes/admin/reports/system-logs/activity-logs.js
+++ app/routes/admin/reports/system-logs/activity-logs.js
@@ -8,37 +8,6 @@ export default Route.extend({
   },
 
   model() {
-    return [
-      {
-        actor  : 'sample@gmail.com',
-        time   : new Date(),
-        action : 'User "sample@gmail.com" (1) login from IP 10.28.0.1 using browser chrome on macos platform'
-      },
-
-      {
-        actor  : 'sample@gmail.com',
-        time   : new Date(),
-        action : 'Event Sample 1 created'
-      },
-
-      {
-        actor  : 'sample2@gmail.com',
-        time   : new Date(),
-        action : 'Speaker sample speaker (1) of event 1 updated'
-      },
-
-      {
-        actor  : 'sample@gmail.com',
-        time   : new Date(),
-        action : 'User "sample4@gmail.com" (1) login from IP 100.68.0.1 using browser firefox on linux platform'
-      },
-
-      {
-        actor  : 'sample@gmail.com',
-        time   : new Date(),
-        action : 'Event Sample1 deleted'
-      }
-
-    ];
+    return this.get('store').query('activity', {});
   }
 });
