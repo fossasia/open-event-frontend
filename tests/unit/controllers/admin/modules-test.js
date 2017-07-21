dd1496cc630e8f8d70aadad61185a1a11bd1f958
geekyd <singhsince94@gmail.com>
2017-07-22 00:31:43 +0530

    Adds modules api to admin/modules

---
+++ tests/unit/controllers/admin/modules-test.js
@@ -0,0 +1,9 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+moduleFor('controller:admin/modules', 'Unit | Controller | admin/modules', []);
+
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
