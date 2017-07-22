d52a2faa9931ccbb32adbc3b81021dc032d6e465
geekyd <singhsince94@gmail.com>
2017-07-22 14:50:58 +0530

    Integrates settings API to admin/content/index

--- app/routes/admin/content/index.js
+++ app/routes/admin/content/index.js
@@ -5,5 +5,8 @@ const { Route } = Ember;
 export default Route.extend({
   titleToken() {
     return this.l10n.t('Social Links');
+  },
+  model() {
+    return this.get('store').queryRecord('setting', {});
   }
 });
