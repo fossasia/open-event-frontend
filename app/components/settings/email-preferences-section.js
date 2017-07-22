106d913e7044759c78974acc11236b05360691c7
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-07-22 10:30:23 +0530

    Integrate email-notification API at 'settings/email-preferences' route

--- app/components/settings/email-preferences-section.js
+++ app/components/settings/email-preferences-section.js
@@ -3,4 +3,12 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
+  actions: {
+    savePreference(setting) {
+      setting.save()
+        .then(() => {
+          this.get('notify').success(this.l10n.t('Email notifications updated successfully'));
+        });
+    }
+  }
 });
