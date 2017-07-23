ea95adc5fc67b44d18f67bf1ce5a2023f8b23a6e
Abhinav Khare <abhinav.khare31@gmail.com>
2017-07-23 12:56:16 +0530

    Implement settings API on admin payment-gateway settings (#540)

--- tests/integration/components/forms/admin/settings/payment-gateway-form-test.js
+++ tests/integration/components/forms/admin/settings/payment-gateway-form-test.js
@@ -1,10 +1,27 @@
 import { test } from 'ember-qunit';
 import moduleForComponent from 'open-event-frontend/tests/helpers/component-helper';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+const { Object: EmberObject } = Ember;
 
 moduleForComponent('forms/admin/settings/payment-gateway-form', 'Integration | Component | forms/admin/settings/payment gateway form');
 
+const settings = EmberObject.create({
+  stripeClientId         : 'ca_8x1ebxrl8eOwOSqRTVLUJkWtcfP92YJE',
+  stripeSecretKey        : 'sk_test_SQPTVKtS8YvItGQuvHFvwve4',
+  stripePublishableKey   : 'pk_test_SQPTVKtS8YvItGQuvHFvwve4',
+  paypalMode             : 'live',
+  paypalSandboxUsername  : 'donate-facilitator_api1.fossasia.org',
+  paypalSandboxPassword  : '123456',
+  paypalSandboxSignature : 'signature-sandbox',
+  paypalLiveUsername     : 'donate-facilitator_api1.fossasia.org',
+  paypalLivePassword     : '123456',
+  paypalLiveSignature    : 'signature-sandbox'
+});
+
 test('it renders', function(assert) {
-  this.render(hbs`{{forms/admin/settings/payment-gateway-form}}`);
+  this.set('settings', settings);
+  this.render(hbs`{{forms/admin/settings/payment-gateway-form settings=settings}}`);
   assert.ok(this.$().html().trim().includes('Stripe'));
 });
