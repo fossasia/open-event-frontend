7ddc78fb56cc03b1c1cd3a0370d6b58f1b8ba5cf
Harshita Gupta <harshitagupta30@users.noreply.github.com>
2017-07-24 01:44:19 +0530

    refactor public event page to use sponsors API

--- app/components/public/sponsor-list.js
+++ app/components/public/sponsor-list.js
@@ -5,6 +5,12 @@ const { Component, computed } = Ember;
 
 export default Component.extend({
   sponsorsGrouped: computed('sponsors.[]', function() {
-    return groupBy(orderBy(this.get('sponsors'), 'level'), 'type');
+    return groupBy(
+      orderBy(
+        this.get('sponsors').toArray(),
+        sponsor => sponsor.get('level')
+      ),
+      sponsor => sponsor.get('type')
+    );
   })
 });
