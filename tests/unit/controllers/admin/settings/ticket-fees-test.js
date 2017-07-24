ac44b211bd37437ed6c0f41d420428077602d2ae
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-07-24 15:16:50 +0530

    Implement ticket-fees API in admin settings

---
+++ tests/unit/controllers/admin/settings/ticket-fees-test.js
@@ -0,0 +1,10 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+moduleFor('controller:admin/settings/ticket-fees', 'Unit | Controller | admin/settings/ticket fees', []);
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
