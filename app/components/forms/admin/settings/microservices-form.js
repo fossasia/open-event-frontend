8eb20b6f93d148ac5e6b1913e2ba73a9f723a61b
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-07-26 23:59:17 +0530

    Add validation via new validator in validator.js

--- app/components/forms/admin/settings/microservices-form.js
+++ app/components/forms/admin/settings/microservices-form.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import FormMixin from 'open-event-frontend/mixins/form';
+import { protocolLessValidUrlPattern } from 'open-event-frontend/utils/validators';
 
 const { Component } = Ember;
 
@@ -16,7 +17,7 @@ export default Component.extend(FormMixin, {
           rules      : [
             {
               type   : 'regExp',
-              value  : /^(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i,
+              value  : protocolLessValidUrlPattern,
               prompt : this.l10n.t('Please enter a valid URL for Android app')
             }
           ]
@@ -28,7 +29,7 @@ export default Component.extend(FormMixin, {
           rules      : [
             {
               type   : 'regExp',
-              value  : /^(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i,
+              value  : protocolLessValidUrlPattern,
               prompt : this.l10n.t('Please enter a valid URL for web app')
             }
           ]
