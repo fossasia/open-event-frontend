b373589b1e38d5fb2bf7db2a96cff47576e82106
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-07-26 13:11:02 +0530

    Implement settings API on admins settings microservices

--- app/components/forms/admin/settings/microservices-form.js
+++ app/components/forms/admin/settings/microservices-form.js
@@ -15,7 +15,8 @@ export default Component.extend(FormMixin, {
           identifier : 'android_app',
           rules      : [
             {
-              type   : 'url',
+              type   : 'regExp',
+              value  : /^(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i,
               prompt : this.l10n.t('Please enter a valid URL for Android app')
             }
           ]
@@ -26,7 +27,8 @@ export default Component.extend(FormMixin, {
           identifier : 'web_app',
           rules      : [
             {
-              type   : 'url',
+              type   : 'regExp',
+              value  : /^(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i,
               prompt : this.l10n.t('Please enter a valid URL for web app')
             }
           ]
@@ -34,10 +36,10 @@ export default Component.extend(FormMixin, {
       }
     };
   },
-
   actions: {
     submit() {
       this.onValid(() => {
+        this.sendAction('save');
       });
     }
   }
