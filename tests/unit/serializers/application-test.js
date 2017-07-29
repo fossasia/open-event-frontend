ac40d6c029b1e2a6029124c03560ddfd1952e323
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-07-29 12:24:42 +0530

    Add speaker model to application serializer

--- tests/unit/serializers/application-test.js
+++ tests/unit/serializers/application-test.js
@@ -13,6 +13,7 @@ moduleForModel('event', 'Unit | Serializer | application', {
     'model:track',
     'model:ticket',
     'model:social-link',
+    'model:speaker',
     'model:speakers-call',
     'model:tax',
     'model:event-copyright',
