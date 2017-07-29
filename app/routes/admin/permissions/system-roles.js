8737c4855735b005b3c1ab13c7ceffb402330872
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-07-29 15:17:53 +0530

    Implement user-permissions api on admin/permissions

--- app/routes/admin/permissions/system-roles.js
+++ app/routes/admin/permissions/system-roles.js
@@ -5,5 +5,15 @@ const { Route } = Ember;
 export default Route.extend({
   titleToken() {
     return this.l10n.t('System Roles');
+  },
+  model() {
+    return this.get('store').findAll('user-permission');
+  },
+  actions: {
+    willTransition() {
+      this.get('controller.model').forEach(userPermission => {
+        userPermission.rollbackAttributes();
+      });
+    }
   }
 });
