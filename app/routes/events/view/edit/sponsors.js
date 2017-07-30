ba500e487e3cf33aedecff9e0b04f332ddff1a72
geekyd <singhsince94@gmail.com>
2017-07-31 03:12:59 +0530

    Integrates sponsors API to sponsors route

--- app/routes/events/view/edit/sponsors.js
+++ app/routes/events/view/edit/sponsors.js
@@ -1,16 +1,55 @@
 import Ember from 'ember';
 import EventWizardMixin from 'open-event-frontend/mixins/event-wizard';
 
-const { Route } = Ember;
+const { Route, RSVP } = Ember;
 
 export default Route.extend(EventWizardMixin, {
   titleToken() {
     return this.l10n.t('Sponsors');
   },
   model() {
-    return {
-      parentData : this.modelFor('events.view.edit'),
-      sponsors   : this.getSponsors()
-    };
+    return this.modelFor('events.view.edit');
+  },
+  actions: {
+    save() {
+      this.get('currentModel.event').save()
+        .then(data => {
+          let savedSponsorsPromises = [];
+          if (this.get('currentModel.event.isSponsorsEnabled')) {
+            savedSponsorsPromises = this.get('currentModel.event.sponsors').toArray().map(sponsor => sponsor.save());
+          } else {
+            savedSponsorsPromises = this.get('currentModel.event.sponsors').toArray().map(sponsor => sponsor.destroyRecord());
+          }
+          RSVP.Promise.all(savedSponsorsPromises)
+            .then(() => {
+              this.transitionTo('events.view.edit.sessions-speakers', data.id);
+            }, function() {
+              this.get('notify').error(this.l10n.t('Sponsors data did not save. Please try again'));
+            });
+        })
+        .catch(() => {
+          this.get('notify').error(this.l10n.t('Sponsors data did not save. Please try again'));
+        });
+    },
+    move() {
+      this.get('currentModel.event').save()
+        .then(data => {
+          let savedSponsorsPromises = [];
+          if (this.get('currentModel.event.isSponsorsEnabled')) {
+            savedSponsorsPromises = this.get('currentModel.event.sponsors').toArray().map(sponsor => sponsor.save());
+          } else {
+            savedSponsorsPromises = this.get('currentModel.event.sponsors').toArray().map(sponsor => sponsor.destroyRecord());
+          }
+          RSVP.Promise.all(savedSponsorsPromises)
+            .then(() => {
+              this.transitionTo('events.view.edit.sessions-speakers', data.id);
+            }, function() {
+              this.get('notify').error(this.l10n.t('Sponsors data did not save. Please try again'));
+            });
+        })
+        .catch(() => {
+          this.get('notify').error(this.l10n.t('Sponsors data did not save. Please try again'));
+        });
+    }
   }
 });
