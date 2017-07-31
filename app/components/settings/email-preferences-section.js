d23938a2fdc82f004065cdfe41aa9a1c7422159a
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-07-31 12:43:31 +0530

    Rollback a single preference on request failing

--- app/components/settings/email-preferences-section.js
+++ app/components/settings/email-preferences-section.js
@@ -4,10 +4,14 @@ const { Component } = Ember;
 
 export default Component.extend({
   actions: {
-    savePreference(setting) {
-      setting.save()
+    savePreference(emailPreference) {
+      emailPreference.save()
         .then(() => {
           this.get('notify').success(this.l10n.t('Email notifications updated successfully'));
+        })
+        .catch(() => {
+          emailPreference.rollbackAttributes();
+          this.get('notify').error(this.l10n.t('An unexpected error occurred.'));
         });
     }
   }
