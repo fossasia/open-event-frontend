6ac4fd06b93106aef8c8015922b0f6a743f4d9fe
Harshita Gupta <harshitagupta310@gmail.com>
2017-08-01 01:29:05 +0530

    Refactor code to use speakers call API to the public event page

--- app/models/speakers-call.js
+++ app/models/speakers-call.js
@@ -2,7 +2,11 @@ import attr from 'ember-data/attr';
 import ModelBase from 'open-event-frontend/models/base';
 import { belongsTo } from 'ember-data/relationships';
 import { v4 } from 'ember-uuid';
+import Ember from 'ember';
 import { computedDateTimeSplit } from 'open-event-frontend/utils/computed-helpers';
+import moment from 'moment';
+
+const { computed } = Ember;
 
 export default ModelBase.extend({
   announcement : attr('string'),
@@ -16,5 +20,13 @@ export default ModelBase.extend({
   startsAtDate : computedDateTimeSplit.bind(this)('startsAt', 'date'),
   startsAtTime : computedDateTimeSplit.bind(this)('startsAt', 'time'),
   endsAtDate   : computedDateTimeSplit.bind(this)('endsAt', 'date'),
-  endsAtTime   : computedDateTimeSplit.bind(this)('endsAt', 'date')
+  endsAtTime   : computedDateTimeSplit.bind(this)('endsAt', 'date'),
+
+  isOpen: computed('startsAt', 'endsAt', function() {
+    return moment().isAfter(this.get('startsAt')) && moment().isBefore(this.get('endsAt'));
+  }),
+
+  isInFuture: computed('startsAt', function() {
+    return moment(this.get('startsAt')).isAfter();
+  })
 });
