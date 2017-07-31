bc844adce947474ab89ec9c72bf239d82f9ea02a
geekyd <singhsince94@gmail.com>
2017-07-31 23:44:11 +0530

    Adds sessionsByState to event model & actions to cell-buttons

--- tests/integration/components/ui-table/cell/cell-sessions-test.js
+++ tests/integration/components/ui-table/cell/cell-sessions-test.js
@@ -4,10 +4,10 @@ import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('ui-table/cell/cell-sessions', 'Integration | Component | ui table/cell/cell sessions');
 
-const record = { sessions: [{ type: 'Drafts', number: 1 }] };
+const record = { sessionsByState: {} };
 
 test('it renders', function(assert) {
   this.set('record', record);
   this.render(hbs `{{ui-table/cell/cell-sessions record=record}}`);
-  assert.ok(this.$().html().trim().includes('Drafts'));
+  assert.ok(this.$().html().trim().includes(''));
 });
