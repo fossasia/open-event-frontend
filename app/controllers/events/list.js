875af249dc380c641ed2c5c6f0f9089d5a501eca
geekyd <singhsince94@gmail.com>
2017-07-31 23:44:11 +0530

    Adds sessionsByState to event model & actions to cell-buttons

--- app/controllers/events/list.js
+++ app/controllers/events/list.js
@@ -14,32 +14,52 @@ export default Controller.extend({
       title        : 'Date'
     },
     {
-      propertyName : 'roles',
-      template     : 'components/ui-table/cell/cell-roles',
-      title        : 'Roles'
+      propertyName     : 'roles',
+      template         : 'components/ui-table/cell/cell-roles',
+      title            : 'Roles',
+      disableSorting   : true,
+      disableFiltering : true
     },
     {
-      propertyName : 'sessions',
-      template     : 'components/ui-table/cell/cell-sessions',
-      title        : 'Sessions'
+      propertyName     : 'sessionsByState',
+      template         : 'components/ui-table/cell/cell-sessions',
+      title            : 'Sessions',
+      disableSorting   : true,
+      disableFiltering : true
     },
     {
-      propertyName : 'speakers',
-      title        : 'Speakers'
+      propertyName     : 'speakers.length',
+      title            : 'Speakers',
+      disableSorting   : true,
+      disableFiltering : true
     },
     {
-      propertyName : 'tickets',
-      template     : 'components/ui-table/cell/cell-tickets',
-      title        : 'Tickets'
+      propertyName     : 'tickets',
+      template         : 'components/ui-table/cell/cell-tickets',
+      title            : 'Tickets',
+      disableSorting   : true,
+      disableFiltering : true
     },
     {
-      propertyName : 'url',
-      template     : 'components/ui-table/cell/cell-link',
-      title        : 'Public URL'
+      propertyName     : 'url',
+      template         : 'components/ui-table/cell/cell-link',
+      title            : 'Public URL',
+      disableSorting   : true,
+      disableFiltering : true
     },
     {
-      template : 'components/ui-table/cell/cell-buttons',
-      title    : ''
+      template         : 'components/ui-table/cell/cell-buttons',
+      title            : '',
+      disableSorting   : true,
+      disableFiltering : true
     }
-  ]
+  ],
+  actions: {
+    moveToDetails(id) {
+      this.transitionToRoute('events.view', id);
+    },
+    editEvent(id) {
+      this.transitionToRoute('events.view.edit.basic-details', id);
+    }
+  }
 });
