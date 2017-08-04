09aa8bc374279bbeb2be0ab5e4fa535c576b3d4b
geekyd <singhsince94@gmail.com>
2017-08-04 20:40:17 +0530

    Adds tracks,microlocations & session-types API to sessions-speakers

--- app/routes/events/view/edit/sessions-speakers.js
+++ app/routes/events/view/edit/sessions-speakers.js
@@ -1,16 +1,17 @@
 import Ember from 'ember';
 import EventWizardMixin from 'open-event-frontend/mixins/event-wizard';
 
-const { Route } = Ember;
+const { Route, RSVP } = Ember;
 
 export default Route.extend(EventWizardMixin, {
   titleToken() {
     return this.l10n.t('Sessions & Speakers');
   },
   model() {
-    return {
-      parentData       : this.modelFor('events.view.edit'),
-      sessionsSpeakers : this.getSessionSpeakers()
-    };
+    let data = this.modelFor('events.view.edit');
+    data.tracks = data.event.get('tracks');
+    data.microlocations = data.event.get('microlocations');
+    data.sessionTypes = data.event.get('sessionTypes');
+    return RSVP.hash(data);
   }
 });
