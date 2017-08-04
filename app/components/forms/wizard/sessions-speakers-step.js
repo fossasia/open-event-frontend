6f68818958acbd796c81ad05583ba059c2bce310
geekyd <singhsince94@gmail.com>
2017-08-04 20:40:17 +0530

    Adds tracks,microlocations & session-types API to sessions-speakers

--- app/components/forms/wizard/sessions-speakers-step.js
+++ app/components/forms/wizard/sessions-speakers-step.js
@@ -2,12 +2,9 @@ import Ember from 'ember';
 import FormMixin from 'open-event-frontend/mixins/form';
 import { timezones } from 'open-event-frontend/utils/dictionary/date-time';
 
-const { Component, computed, computed: { alias } } = Ember;
+const { Component, computed } = Ember;
 
 export default Component.extend(FormMixin, {
-
-  event            : alias('data.parentData.event'),
-  sessionsSpeakers : alias('data.sessionsSpeakers'),
 
   getValidationRules() {
     return {
@@ -24,6 +21,18 @@ export default Component.extend(FormMixin, {
     return timezones;
   }),
 
+  tracks: computed('data.tracks.@each.isDeleted', function() {
+    return this.get('data.tracks').filterBy('isDeleted', false);
+  }),
+
+  sessionTypes: computed('data.sessionTypes.@each.isDeleted', function() {
+    return this.get('data.sessionTypes').filterBy('isDeleted', false);
+  }),
+
+  microlocations: computed('data.microlocations.@each.isDeleted', function() {
+    return this.get('data.event.microlocations').filterBy('isDeleted', false);
+  }),
+
   fieldChanged(field) {
     if (!field.get('isIncluded')) {
       field.set('isRequired', false);
@@ -36,35 +45,53 @@ export default Component.extend(FormMixin, {
   actions: {
     saveDraft() {
       this.onValid(() => {
-        this.get('save')('draft');
+        this.set('data.event.state', 'draft');
+        this.sendAction('save');
       });
     },
     moveForward() {
       this.onValid(() => {
-        this.get('move')();
+        this.sendAction('save');
       });
     },
     publish() {
       this.onValid(() => {
-        this.get('save')('publish');
+        this.set('data.event.state', 'published');
+        this.sendAction('save');
       });
     },
     addItem(type) {
       switch (type) {
         case 'sessionType':
-          this.get('data.sessionsSpeakers.sessionTypes').addObject(this.store.createRecord('session-type'));
+          this.get('data.sessionTypes').addObject(this.store.createRecord('session-type'));
           break;
         case 'track':
-          this.get('data.sessionsSpeakers.tracks').addObject(this.store.createRecord('track'));
+          this.get('data.tracks').addObject(this.store.createRecord('track'));
           break;
         case 'microlocation':
-          this.get('data.sessionsSpeakers.microlocations').addObject(this.store.createRecord('microlocation'));
+          this.get('data.microlocations').addObject(this.store.createRecord('microlocation'));
           break;
       }
     },
-    removeItem(item, type) {
-      item.unloadRecord();
-      this.get(`data.sessionsSpeakers.${type}s`).removeObject(item);
+    removeItem(item) {
+      item.destroyRecord();
+    }
+  },
+  didInsertElement() {
+    if (this.get('data.event.speakersCall.content')) {
+      this.set('data.event.speakersCall', this.store.createRecord('speakers-call'));
+    }
+
+    if (this.get('data.event.sessionTypes') && !this.get('data.event.sessionTypes.length')) {
+      this.get('data.event.sessionTypes').addObject(this.store.createRecord('session-type'));
+    }
+
+    if (this.get('data.event.tracks') && !this.get('data.event.tracks.length')) {
+      this.get('data.event.tracks').addObject(this.store.createRecord('track'));
+    }
+
+    if (this.get('data.event.microlocations') && !this.get('data.event.microlocations.length')) {
+      this.get('data.event.microlocations').addObject(this.store.createRecord('microlocation'));
     }
   }
 });
