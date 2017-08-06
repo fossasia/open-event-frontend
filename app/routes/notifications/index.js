439a054e25131c84558c8b9552b02c2c86823c95
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-08-06 15:48:22 +0530

    Integrate the notification API at the 'index' route and '/notifications' route (#570)

--- app/routes/notifications/index.js
+++ app/routes/notifications/index.js
@@ -8,23 +8,6 @@ export default Route.extend({
   },
   templateName: 'notifications/all',
   model() {
-    return [{
-      title       : 'New Session Proposal for event1 by  user1',
-      description : 'Title of proposal',
-      createdAt   : new Date(),
-      isRead      : false
-    },
-    {
-      title       : 'New Session Proposal for event3 by  user3',
-      description : 'Title of proposal',
-      createdAt   : new Date(),
-      isRead      : false
-    },
-    {
-      title       : 'New Session Proposal for event5 by  user5',
-      description : 'Title of proposal',
-      createdAt   : new Date(),
-      isRead      : false
-    }];
+    return this.get('authManager.currentUser').get('notifications').filterBy('isRead', false);
   }
 });
