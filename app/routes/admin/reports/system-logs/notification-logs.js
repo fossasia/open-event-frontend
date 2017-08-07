33f50db877335756aec913a15865d794fb749524
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-07 11:02:42 +0530

    Integrate notification api via ember-model-table on notification-logs

--- app/routes/admin/reports/system-logs/notification-logs.js
+++ app/routes/admin/reports/system-logs/notification-logs.js
@@ -8,40 +8,9 @@ export default Route.extend({
   },
 
   model() {
-    return [
-      {
-        receivedAt : new Date(),
-        action     : 'Invitation For Papers',
-        title      : 'Invitation to Submit Papers for event1',
-        message    : 'You have been invited to submit papers for event1',
-        user       : {
-          name  : 'test user',
-          email : 'test@gmail.com'
-        }
-
-      },
-
-      {
-        receivedAt : new Date(),
-        action     : 'New Session Proposal',
-        title      : 'New session proposal for event1',
-        message    : 'The event event1 has received a new session proposal.',
-        user       : {
-          name  : 'event user',
-          email : 'event_user@gmail.com'
-        }
-      },
-
-      {
-        receivedAt : new Date(),
-        action     : 'Ticket(s) Purchased to Organizer',
-        title      : 'New ticket purchase for event2 by buyer@mail.com (O1497634451-1) ',
-        message    : 'The order has been processed successfully',
-        user       : {
-          name  : 'event user',
-          email : 'event_user@gmail.com'
-        }
-      }
-    ];
+    return this.get('store').query('notification', {
+      include      : 'user',
+      'page[size]' : 10
+    });
   }
 });
