c2f0dc4b013d1f811dd31c94c40b1ccf671cf6ac
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-09 23:51:10 +0530

    Implement event-invoice on admin/sales/status

--- app/routes/admin/sales/status.js
+++ app/routes/admin/sales/status.js
@@ -7,20 +7,6 @@ export default Route.extend({
     return this.l10n.t('Status');
   },
   model() {
-    return [{
-      invoice : 'Sample Invoice 1',
-      event   : 'Sample Event 1',
-      date    : new Date(),
-      amount  : 1000.0,
-      sentTo  : 'testuser1@gmail.com',
-      status  : 'Delivered'
-    }, {
-      invoice : 'Sample Invoice 2',
-      event   : 'Sample Event 2',
-      date    : new Date(),
-      amount  : 100000.0,
-      sentTo  : 'testuser2@gmail.com',
-      status  : 'Pending'
-    }];
+    return this.store.query('event-invoice', { include: 'event,user' });
   }
 });
---
+++ tests/unit/controllers/admin/sales/status-test.js
@@ -0,0 +1,10 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+moduleFor('controller:admin/sales/status', 'Unit | Controller | admin/sales/status', []);
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
