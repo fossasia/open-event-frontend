8bfd124ff9ba9eeb1c51c37d8e6104ba6745f835
geekyd <singhsince94@gmail.com>
2017-08-11 02:54:05 +0530

    Implements API to create & basic-details routes

---
+++ tests/unit/controllers/create-test.js
@@ -0,0 +1,9 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+moduleFor('controller:create', 'Unit | Controller | create', []);
+
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
