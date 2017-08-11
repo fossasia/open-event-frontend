19a15ae9369f7d7e767ffe6b34c7f7b969415819
geekyd <singhsince94@gmail.com>
2017-08-11 21:08:34 +0530

    Implements events & users admin API to admin/index route

--- app/models/admin-statistics-user.js
+++ app/models/admin-statistics-user.js
@@ -1,5 +1,8 @@
 import attr from 'ember-data/attr';
 import ModelBase from 'open-event-frontend/models/base';
+import Ember from 'ember';
+
+const { computed } = Ember;
 
 export default ModelBase.extend({
   superAdmin     : attr('number'),
@@ -9,5 +12,9 @@ export default ModelBase.extend({
   organizer      : attr('number'),
   coorganizer    : attr('number'),
   attendee       : attr('number'),
-  trackOrganizer : attr('number')
+  trackOrganizer : attr('number'),
+
+  total: computed('superAdmin', 'admin', 'verified', 'unverified', function() {
+    return this.get('superAdmin') + this.get('admin') + this.get('verified') + this.get('unverified');
+  })
 });
--- app/routes/admin/index.js
+++ app/routes/admin/index.js
@@ -1,6 +1,24 @@
 import Ember from 'ember';
 
-const { Route } = Ember;
+const { Route, RSVP } = Ember;
 
 export default Route.extend({
+  model() {
+    return RSVP.hash({
+      events: this.get('store').queryRecord('admin-statistics-event', {
+        filter: {
+          name : 'id',
+          op   : 'eq',
+          val  : 1
+        }
+      }),
+      users: this.get('store').queryRecord('admin-statistics-user', {
+        filter: {
+          name : 'id',
+          op   : 'eq',
+          val  : 1
+        }
+      })
+    });
+  }
 });
