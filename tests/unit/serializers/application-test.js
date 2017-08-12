c0287db407aad0c9cd5f870c884cd8f3ae91d571
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-12 10:05:59 +0530

    Add admin-statisitics-session model

--- tests/unit/serializers/application-test.js
+++ tests/unit/serializers/application-test.js
@@ -19,6 +19,7 @@ moduleForModel('event', 'Unit | Serializer | application', {
     'model:speakers-call',
     'model:email-notification',
     'model:admin-statistics-mail',
+    'model:admin-statistics-session',
     'model:tax',
     'model:event-copyright',
     'model:session-type',
