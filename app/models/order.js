98684b27c4e501f002b681c826fc5b46e3226446
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-13 23:34:06 +0530

    Add order model

---
+++ app/models/order.js
@@ -0,0 +1,20 @@
+import attr from 'ember-data/attr';
+import ModelBase from 'open-event-frontend/models/base';
+import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default ModelBase.extend({
+  amount         : attr('string'),
+  address        : attr('string'),
+  city           : attr('string'),
+  state          : attr('string'),
+  country        : attr('string'),
+  zipcode        : attr('string'),
+  paymentMode    : attr('string'),
+  status         : attr('string'),
+  discountCodeId : attr('string'),
+  user           : belongsTo('user'),
+  event          : belongsTo('event'),
+  discountCode   : belongsTo('discount-code'),
+  tickets        : hasMany('ticket')
+
+});
