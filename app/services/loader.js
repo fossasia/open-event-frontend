5b0f123582e0c913d19dcb96d9b73e063846315c
Niranjan Rajendran <me@niranjan.io>
2017-08-14 15:15:37 +0530

    Make loader config handling more sane

--- app/services/loader.js
+++ app/services/loader.js
@@ -5,16 +5,25 @@ const { stringify } = JSON;
 
 export default Service.extend({
 
-  makePromise(url, type, data = null, autoPrefix = true) {
+  makePromise(url, type, data = null, config = {}) {
     const adapter = getOwner(this).lookup('adapter:application');
-    const options = autoPrefix ? adapter.ajaxOptions() : {};
-    options.url = autoPrefix ? `${adapter.urlPrefix()}${url}` : url;
+    const options = !config.isExternal ? adapter.ajaxOptions() : {};
     options.type = type;
 
+    if (config.isExternal) {
+      options.url = url;
+    } else {
+      options.url = config.withoutPrefix ? `${adapter.host}/${url}` : `${adapter.urlPrefix()}${url}`;
+    }
+
     if (data) {
-      options.contentType = 'application/json';
-      options.dataType = 'json';
-      options.data = stringify(data);
+      if (config.isFormData) {
+        options.data = data;
+      } else {
+        options.contentType = 'application/json';
+        options.dataType = 'json';
+        options.data = stringify(data);
+      }
     }
 
     return new RSVPPromise((resolve, reject) => {
@@ -30,19 +39,19 @@ export default Service.extend({
     });
   },
 
-  load(url, autoPrefix = true) {
-    return this.makePromise(url, 'GET', null, autoPrefix);
+  load(url, config = {}) {
+    return this.makePromise(url, 'GET', null, config);
   },
 
-  post(url, data = null, autoPrefix = true) {
-    return this.makePromise(url, 'POST', data, autoPrefix);
+  post(url, data = null, config = {}) {
+    return this.makePromise(url, 'POST', data, config);
   },
 
-  put(url, data = null, autoPrefix = true) {
-    return this.makePromise(url, 'PUT', data, autoPrefix);
+  put(url, data = null, config = {}) {
+    return this.makePromise(url, 'PUT', data, config);
   },
 
-  delete(url, autoPrefix = true) {
-    return this.makePromise(url, 'DELETE', null, autoPrefix);
+  delete(url, config = {}) {
+    return this.makePromise(url, 'DELETE', null, config);
   }
 });
