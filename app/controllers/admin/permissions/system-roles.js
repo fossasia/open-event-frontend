8ea6ed31b16b554a36abe1746564d019b494893d
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-15 11:01:51 +0530

    Implement roles api for creating roles on admin system-roles

--- app/controllers/admin/permissions/system-roles.js
+++ app/controllers/admin/permissions/system-roles.js
@@ -7,11 +7,28 @@ export default Controller.extend({
       this.set('isAddSystemRoleModalOpen', true);
     },
     addSystemRole() {
-      this.set('isAddSystemRoleModalOpen', false);
+      this.set('isLoading', true);
+      this.get('store').createRecord('role', {
+        name      : this.get('name'),
+        titleName : this.get('titleName')
+      }).save()
+        .then(() => {
+          this.set('isLoading', false);
+          this.notify.success(this.l10n.t('User permissions have been saved successfully.'));
+          this.set('isAddSystemRoleModalOpen', false);
+          this.setProperties({
+            name          : null,
+            roleTitleName : null
+          });
+        })
+        .catch(()=> {
+          this.set('isLoading', false);
+          this.notify.error(this.l10n.t('An unexpected error has occurred. User permissions not saved.'));
+        });
     },
     updatePermissions() {
       this.set('isLoading', true);
-      this.get('model').save()
+      this.get('model.userPermissions').save()
         .then(() => {
           this.set('isLoading', false);
           this.notify.success(this.l10n.t('User permissions have been saved successfully.'));
