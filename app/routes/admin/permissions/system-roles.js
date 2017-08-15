faa0f22e5eb1f2c7f13f2282ca1b74d20d9f4c00
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-15 11:01:51 +0530

    Implement roles api for creating roles on admin system-roles

--- app/routes/admin/permissions/system-roles.js
+++ app/routes/admin/permissions/system-roles.js
@@ -1,17 +1,19 @@
 import Ember from 'ember';
 
-const { Route } = Ember;
+const { Route, RSVP } = Ember;
 
 export default Route.extend({
   titleToken() {
     return this.l10n.t('System Roles');
   },
   model() {
-    return this.get('store').findAll('user-permission');
+    return RSVP.hash({
+      userPermissions: this.get('store').findAll('user-permission')
+    });
   },
   actions: {
     willTransition() {
-      this.get('controller.model').forEach(userPermission => {
+      this.get('controller.model.userPermissions').forEach(userPermission => {
         userPermission.rollbackAttributes();
       });
     }
