a223159852cfe61eee7d8d0ec4584b98fc74d001
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-19 09:29:09 +0530

    Implement attendees API on tickets/attendee

--- app/routes/events/view/tickets/attendees/index.js
+++ app/routes/events/view/tickets/attendees/index.js
@@ -3,34 +3,8 @@ import Ember from 'ember';
 const { Route } = Ember;
 
 export default Route.extend({
-  templateName: 'events/view/tickets/attendees/list',
-
-  model() {
-    return [
-      {
-        'status'        : 'Completed',
-        'completedAt'   : '2016-04-06',
-        'id'            : 1,
-        'invoiceNumber' : '#O1496523209-306',
-        'amount'        : 0,
-        'paidVia'       : 'Free',
-        'buyerName'     : 'sample@gmail.com',
-        'firstName'     : 'john',
-        'lastName'      : 'Doe',
-        'ticketName'    : 'A'
-      },
-      {
-        'status'        : 'Completed',
-        'completedAt'   : '2016-04-06',
-        'id'            : 1,
-        'invoiceNumber' : '#O1496523209-306',
-        'amount'        : 0,
-        'paidVia'       : 'Free',
-        'buyerName'     : 'sample@gmail.com',
-        'firstName'     : 'aajohn',
-        'lastName'      : 'bbDoe',
-        'ticketName'    : 'A'
-      }
-    ];
+  beforeModel() {
+    this._super(...arguments);
+    this.transitionTo('events.view.tickets.attendees.list', 'all');
   }
 });
