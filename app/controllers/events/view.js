c10741ce7c68775fecb046ad92c2731879b12430
Abhinav Khare <abhinav.khare31@gmail.com>
2017-08-19 10:17:47 +0530

    Integrate api on delete event button in event overview (#753)

--- app/controllers/events/view.js
+++ app/controllers/events/view.js
@@ -9,6 +9,17 @@ export default Controller.extend({
     },
 
     deleteEvent() {
+      this.set('isLoading', true);
+      this.get('model').destroyRecord()
+        .then(() => {
+          this.set('isLoading', false);
+          this.transitionToRoute('events');
+          this.notify.success(this.l10n.t('Event has been deleted successfully.'));
+        })
+        .catch(()=> {
+          this.set('isLoading', false);
+          this.notify.error(this.l10n.t('An unexpected error has occurred.'));
+        });
       this.set('isEventDeleteModalOpen', false);
     }
   }
