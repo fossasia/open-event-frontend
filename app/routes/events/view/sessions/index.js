9da1bfed7dfaff9843d12095af7b49ca160a4b46
geekyd <singhsince94@gmail.com>
2017-08-20 08:37:29 +0530

    Adds sessions/create route & session-speaker form

--- app/routes/events/view/sessions/index.js
+++ app/routes/events/view/sessions/index.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 const { Route } = Ember;
 
 export default Route.extend({
-  templateName: 'events/view/sessions/list',
   beforeModel() {
     this._super(...arguments);
     this.transitionTo('events.view.sessions.list', 'all');
