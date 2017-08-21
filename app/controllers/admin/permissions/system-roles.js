40e669d6ef5eb2a30d3b740edfbfd80554fcec99
geekyd <singhsince94@gmail.com>
2017-08-21 14:51:35 +0530

    Adds delete & update feature to admin/system-roles route

--- app/controllers/admin/permissions/system-roles.js
+++ app/controllers/admin/permissions/system-roles.js
@@ -3,27 +3,37 @@ import Ember from 'ember';
 const { Controller } = Ember;
 export default Controller.extend({
   actions: {
-    openAddSystemrRoleModal() {
+    openAddSystemRoleModal(role) {
+      if (role) {
+        this.set('role', role);
+      } else {
+        this.set('role', this.store.createRecord('role'));
+      }
       this.set('isAddSystemRoleModalOpen', true);
     },
-    addSystemRole() {
+    deleteSystemRole(role) {
       this.set('isLoading', true);
-      this.get('store').createRecord('role', {
-        name      : this.get('name'),
-        titleName : this.get('titleName')
-      }).save()
+      role.destroyRecord()
         .then(() => {
           this.set('isLoading', false);
-          this.notify.success(this.l10n.t('User permissions have been saved successfully.'));
-          this.set('isAddSystemRoleModalOpen', false);
-          this.setProperties({
-            name          : null,
-            roleTitleName : null
-          });
+          this.notify.success(this.l10n.t('System role has been deleted successfully.'));
         })
         .catch(()=> {
           this.set('isLoading', false);
-          this.notify.error(this.l10n.t('An unexpected error has occurred. User permissions not saved.'));
+          this.notify.error(this.l10n.t('An unexpected error has occurred. System role was not deleted.'));
+        });
+    },
+    addSystemRole() {
+      this.set('isLoading', true);
+      this.get('role').save()
+        .then(() => {
+          this.set('isAddSystemRoleModalOpen', false);
+          this.set('isLoading', false);
+          this.notify.success(this.l10n.t('System role have been saved successfully.'));
+        })
+        .catch(()=> {
+          this.set('isLoading', false);
+          this.notify.error(this.l10n.t('An unexpected error has occurred. System role not saved.'));
         });
     },
     updatePermissions() {
