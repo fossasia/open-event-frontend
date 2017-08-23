07677f1006bfe877ad5464048f74c75be34e8e04
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-23 14:43:38 +0530

    List role invites in manage roles in event/overview

--- app/components/events/view/overview/manage-roles.js
+++ app/components/events/view/overview/manage-roles.js
@@ -1,16 +1,23 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { Component, computed } = Ember;
 
 export default Component.extend({
-  classNames : ['ui', 'fluid', 'card'],
-  actions    : {
+  classNames  : ['ui', 'fluid', 'card'],
+  roleType    : 'accepted',
+  roleInvites : computed('data', 'roleType', function() {
+    return this.get('data').filterBy('status', this.get('roleType'));
+  }),
+  actions: {
     openAddUserRoleModal() {
       this.set('isAddUserRoleModalOpen', true);
     },
 
     addUserRole() {
       this.set('isAddUserRoleModalOpen', false);
+    },
+    filter(type) {
+      this.set('roleType', type);
     }
   }
 });
