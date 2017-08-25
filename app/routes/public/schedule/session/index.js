9b427188dc48a1eccec8c232e9b65957ed003715
Harshita Gupta <harshitagupta310@gmail.com>
2017-08-26 01:00:04 +0530

    UI for /schedule/sessions_view route

--- app/routes/public/schedule/session/index.js
+++ app/routes/public/schedule/session/index.js
@@ -1,18 +1,16 @@
 import Ember from 'ember';
 
-const { Route,RSVP } = Ember;
+const { Route, RSVP } = Ember;
 
 export default Route.extend({
   titleToken() {
-    return this.l10n.t('Sessions View');
+    return this.l10n.t('Sessions');
   },
   model() {
     const eventDetails = this.modelFor('public');
     return RSVP.hash({
       event  : eventDetails,
-      tracks : eventDetails.query('tracks', {
-        'fields[track]': 'name,id'
-      })
+      tracks : eventDetails.query('tracks', { include: 'sessions' })
     });
   }
 });
---
+++ tests/unit/routes/public/schedule/session/list-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:public/schedule/session/list', 'Unit | Route | public/schedule/session/list', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
