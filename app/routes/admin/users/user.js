895ea57455c0de587694f8d78f0b68d620728b5c
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-31 14:25:36 +0530

    Add dynamic user routes in admin/users

---
+++ app/routes/admin/users/user.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+const { Route } = Ember;
+
+export default Route.extend({
+  templateName: 'profile'
+});
---
+++ tests/unit/routes/admin/users/user-test.js
@@ -0,0 +1,9 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+moduleFor('route:admin/users/user', 'Unit | Route | admin/users/user', []);
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
