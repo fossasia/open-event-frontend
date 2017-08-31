1e62b461ec062613b4bb9f3f05276bf7d602ac5c
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-31 14:27:59 +0530

    Move update action from profile controller to form for reuse

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -3,20 +3,4 @@ import Ember from 'ember';
 const { Controller } = Ember;
 
 export default Controller.extend({
-  actions: {
-    updateUser() {
-      let user = this.get('model');
-      user.save()
-        .then(() => {
-          this.get('notify').success(this.l10n.t('Your profile has been updated'));
-        })
-        .catch(() => {
-          this.get('authManager.currentUser').rollbackAttributes();
-          this.get('notify').error(this.l10n.t('An unexpected error occurred'));
-        })
-        .finally(() => {
-          this.set('isLoading', false);
-        });
-    }
-  }
 });
