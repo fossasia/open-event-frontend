fb19a54f1444b55963072f71af2e6a9cb0139524
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-31 14:27:59 +0530

    Move update action from profile controller to form for reuse

--- app/components/forms/user-profile-form.js
+++ app/components/forms/user-profile-form.js
@@ -38,7 +38,18 @@ export default Component.extend(FormMixin, {
     submit() {
       this.onValid(() => {
         this.set('isLoading', true);
-        this.sendAction('submit');
+        let user = this.get('user');
+        user.save()
+          .then(() => {
+            this.get('notify').success(this.l10n.t('Your profile has been updated'));
+          })
+          .catch(() => {
+            this.get('authManager.currentUser').rollbackAttributes();
+            this.get('notify').error(this.l10n.t('An unexpected error occurred'));
+          })
+          .finally(() => {
+            this.set('isLoading', false);
+          });
       });
     }
   }
