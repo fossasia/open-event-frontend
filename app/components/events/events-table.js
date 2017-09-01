3c04cf3f01ba1febfed153ebe0cb699d32dd8f95
Abhinav Khare <abhinav.khare31@gmail.com>
2017-09-01 10:49:42 +0530

    Switch to closure actions for events ember model table (#845)

--- app/components/events/events-table.js
+++ app/components/events/events-table.js
@@ -1,16 +1,5 @@
 import UiTable from 'open-event-frontend/components/ui-table-server';
 
 export default UiTable.extend({
-  classNames : ['ui', 'main-container'],
-  actions    : {
-    moveToDetails(id) {
-      this.sendAction('moveToDetails', id);
-    },
-    editEvent(id) {
-      this.sendAction('editEvent', id);
-    },
-    openDeleteEventModal(id, name) {
-      this.sendAction('openDeleteEventModal', id, name);
-    }
-  }
+  classNames: ['ui', 'main-container']
 });
