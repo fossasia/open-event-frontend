a5f2c91ff26961f83c0c5b17bfa5cccd20008ae8
Dilpreet Singh <singhsince94@gmail.com>
2017-09-18 21:06:20 +0530

    Adds deletion of sessions from events panel

--- tests/unit/controllers/events/view/sessions/create-test.js
+++ tests/unit/controllers/events/view/sessions/create-test.js
@@ -1,9 +1,7 @@
-import { moduleFor, test } from 'ember-qunit';
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
 
-moduleFor('controller:events/view/sessions/create', 'Unit | Controller | events/view/sessions/create', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
-});
+moduleFor('controller:events/view/sessions/create', 'Unit | Controller | events/view/sessions/create', []);
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
