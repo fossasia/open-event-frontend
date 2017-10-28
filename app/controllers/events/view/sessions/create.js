a06683aa1fe0c088439fc33e02b7547b45fc515a
Dilpreet Singh <singhsince94@gmail.com>
2017-10-28 07:01:48 +0530

    Fixes session & speaker relationships in session-speaker form (#932)

--- app/controllers/events/view/sessions/create.js
+++ app/controllers/events/view/sessions/create.js
@@ -5,11 +5,12 @@ export default Controller.extend({
   actions: {
     save() {
       this.set('isLoading', true);
+      this.get('model.speaker.sessions').addObject(this.get('model.session'));
+      this.get('model.session.speakers').addObject(this.get('model.speaker'));
+
       this.get('model.session').save()
-        .then(session => {
-          let speaker = this.get('model.speaker');
-          speaker.set('session', session);
-          speaker.save()
+        .then(() => {
+          this.get('model.speaker').save()
             .then(() => {
               this.get('notify').success(this.l10n.t('Your session has been saved'));
               this.transitionToRoute('events.view.sessions', this.get('model.event.id'));
