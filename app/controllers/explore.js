16629edb62047dc8b4f4f131caad8378a196e5cb
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-01-28 20:57:07 +0530

    Implement filter on category, sub category, type for explore route

--- app/controllers/explore.js
+++ app/controllers/explore.js
@@ -1,8 +1,94 @@
 import Ember from 'ember';
+import moment from 'moment';
 
-const { Controller } = Ember;
+const { Controller, computed, Object: EmberObject } = Ember;
 
 export default Controller.extend({
+  queryParams  : ['category', 'sub_category', 'event_type', 'date_range'],
+  category     : null,
+  sub_category : null,
+  event_type   : null,
+  date_range   : null,
+
+  categoryFilter: computed('category', function() {
+    return EmberObject.create(
+      {
+        name : 'event-topic',
+        op   : 'has',
+        val  : {
+          name : 'name',
+          op   : 'eq',
+          val  : this.get('category')
+        }
+      }
+    );
+  }),
+
+  subCategoryFilter: computed('sub_category', function() {
+    return EmberObject.create(
+      {
+        name : 'event-sub-topic',
+        op   : 'has',
+        val  : {
+          name : 'slug',
+          op   : 'eq',
+          val  : this.get('sub_category')
+        }
+      }
+    );
+  }),
+
+  typeFilter: computed('event_type', function() {
+    return EmberObject.create(
+      {
+        name : 'event-type',
+        op   : 'has',
+        val  : {
+          name : 'name',
+          op   : 'eq',
+          val  : this.get('event_type')
+        }
+      }
+    );
+  }),
+
+  filterOptions: [
+    {
+      name : 'starts-at',
+      op   : 'ge',
+      val  : moment().toISOString()
+    },
+    {
+      name : 'state',
+      op   : 'eq',
+      val  : 'published'
+    }
+  ],
+
+  filteredEvents: computed('category', 'sub_category', 'event_type', 'date_range',  function() {
+    let category = this.get('category');
+    let subCategory = this.get('sub_category');
+    let eventType = this.get('event_type');
+    let filterOptions = this.get('filterOptions').slice(0, 2);
+
+    if (category) {
+      filterOptions.pushObject(this.get('categoryFilter'));
+
+      if (subCategory) {
+        this.get('filterOptions').pushObject(this.get('subCategoryFilter'));
+      }
+    }
+
+    if (eventType) {
+      this.get('filterOptions').pushObject(this.get('typeFilter'));
+    }
+
+    return this.get('store').query('event', {
+      sort   : 'starts-at',
+      filter : filterOptions
+    });
+
+  }),
   actions: {
     shareEvent(event) {
       this.set('eventToShare', event);
