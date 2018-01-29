b3cdec7fb3c128647e40d5fbc04954e15a24c062
ritikamotwani <iit2015096@iiita.ac.in>
2018-01-29 22:22:46 +0530

    Implement Discount Code API

---
+++ app/controllers/events/view/tickets/discount-codes/create.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+const { Controller } = Ember;
+
+export default Controller.extend({
+  actions: {
+    saveCode(code) {
+      code.save()
+        .then(() => {
+          this.get('notify').success(this.l10n.t('Discount code has been successfully created.'));
+        })
+        .catch(()=> {
+          this.get('notify').error(this.l10n.t('An unexpected error has occured. Discount code cannot be created.'));
+        });
+    }
+  }
+});
