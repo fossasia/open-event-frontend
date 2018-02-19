0c3224fe1e3f7ae8eb0f2d594dce7d7b69623930
ritikamotwani <iit2015096@iiita.ac.in>
2018-02-19 22:15:09 +0530

    Add my-sessions/<session_id> route to view sessions

---
+++ tests/unit/routes/my-sessions/view-test.js
@@ -0,0 +1,9 @@
+import { test } from 'ember-qunit';
+import moduleFor from 'open-event-frontend/tests/helpers/unit-helper';
+
+moduleFor('route:my-sessions/view', 'Unit | Route | my sessions/view', []);
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
