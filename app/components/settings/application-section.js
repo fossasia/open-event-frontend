26a980f34dd73c6ab2d65f5d7635c0e98ac496e0
harsh_jain <harshjniitr@gmail.com>
2018-03-16 05:42:45 +0530

    Linted the code for fixing the build failure

--- app/components/settings/application-section.js
+++ app/components/settings/application-section.js
@@ -5,20 +5,24 @@ const { Component } = Ember;
 export default Component.extend({
   actions: {
     updateFacebook() {
-      if(this.get('facebook'))
+      if (this.get('facebook')) {
         this.sendAction('updateSocialLink', 'facebook', this.get('facebook'));
+      }
     },
     updateTwitter() {
-      if(this.get('twitter'))
+      if (this.get('twitter')) {
         this.sendAction('updateSocialLink', 'twitter', this.get('twitter'));
+      }
     },
     updateInstagram() {
-      if(this.get('instagram'))
+      if (this.get('instagram')) {
         this.sendAction('updateSocialLink', 'instagram', this.get('instagram'));
+      }
     },
     updateGoogle() {
-      if(this.get('google'))
+      if (this.get('google')) {
         this.sendAction('updateSocialLink', 'google', this.get('google'));
+      }
     }
   }
 });
