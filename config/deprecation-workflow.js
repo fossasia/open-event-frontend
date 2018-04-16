0afe0e14ec18ea27fe8542c406ff0baa6c0bb376
Niranjan Rajendran <me@niranjan.io>
2018-04-16 20:49:13 +0530

    Update to ember 3.1
    Using a direct link to ember-source release until https://github.com/emberjs/ember.js/pull/16493 gets tagged into an npm release

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,6 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-router.router' }
+    { handler: 'silence', matchId: 'ember-routing.route-router' }
   ]
 };
