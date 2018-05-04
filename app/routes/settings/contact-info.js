538d5180c0ed5849b1a753a8ab381bd762a5da9e
pradeepgangwar <pradeepgangwar39@gmail.com>
2018-05-05 02:37:54 +0530

    Add authentication security to /settings route

--- app/routes/settings/contact-info.js
+++ app/routes/settings/contact-info.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
   titleToken() {
     return this.get('l10n').t('Contact Info');
   },
