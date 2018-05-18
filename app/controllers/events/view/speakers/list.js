5233bec13284098f31ab0d40e1f3a5de76bda8cc
ritika <ritika.motwani0712@gmail.com>
2018-05-18 19:58:19 +0530

    Convert speakers table to ember table and integrate Speakers API on it

---
+++ app/controllers/events/view/speakers/list.js
@@ -0,0 +1,29 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  columns: [
+    {
+      propertyName : 'name',
+      title        : 'Name'
+    },
+    {
+      propertyName : 'email',
+      title        : 'Email'
+    },
+    {
+      propertyName : 'mobile',
+      title        : 'Phone'
+    },
+    {
+      propertyName   : 'sessions',
+      title          : 'Submitted Sessions',
+      template       : 'components/ui-table/cell/events/view/speakers/cell-simple-sessions',
+      disableSorting : true
+    },
+    {
+      propertyName : '',
+      title        : 'Actions',
+      template     : 'components/ui-table/cell/events/view/speakers/cell-buttons'
+    }
+  ]
+});
--- app/routes/events/view/speakers/list.js
+++ app/routes/events/view/speakers/list.js
@@ -13,45 +13,46 @@ export default Route.extend({
   },
   model(params) {
     this.set('params', params);
-    return [{
-      image             : '/images/placeholders/avatar.png',
-      name              : 'Test Name1',
-      email             : 'testname1@gmail.com',
-      phone             : '123456789',
-      submitted_session : ['session1', 'session2'],
-      state             : 'confirmed'
-    },
-    {
-      image             : '/images/placeholders/avatar.png',
-      name              : 'Test Name2',
-      email             : 'testname2@gmail.com',
-      phone             : '123456789',
-      submitted_session : ['session2'],
-      state             : 'canceled'
-    },
-    {
-      image             : '/images/placeholders/avatar.png',
-      name              : 'Test Name3',
-      email             : 'testname3@gmail.com',
-      phone             : '123456789',
-      submitted_session : ['session3'],
-      state             : 'confirmed'
-    },
-    {
-      image             : '/images/placeholders/avatar.png',
-      name              : 'Test Name4',
-      email             : 'testname4@gmail.com',
-      phone             : '123456789',
-      submitted_session : ['session4'],
-      state             : 'canceled'
-    },
-    {
-      image             : '/images/placeholders/avatar.png',
-      name              : 'Test Name5',
-      email             : 'testname5@gmail.com',
-      phone             : '123456789',
-      submitted_session : ['session5', 'session6'],
-      state             : 'confirmed'
-    }];
+    let filterOptions = [];
+    if (params.session_status === 'pending') {
+      filterOptions = [
+        {
+          name : 'state',
+          op   : 'eq',
+          val  : 'pending'
+        }
+      ];
+    } else if (params.session_status === 'accepted') {
+      filterOptions = [
+        {
+          name : 'state',
+          op   : 'eq',
+          val  : 'accepted'
+        }
+      ];
+    } else if (params.session_status === 'rejected') {
+      filterOptions = [
+        {
+          name : 'state',
+          op   : 'eq',
+          val  : 'rejected'
+        }
+      ];
+    } else if (params.session_status === 'confirmed') {
+      filterOptions = [
+        {
+          name : 'state',
+          op   : 'eq',
+          val  : 'confirmed'
+        }
+      ];
+    } else {
+      filterOptions = [];
+    }
+    return this.modelFor('events.view').query('speakers', {
+      include      : 'sessions',
+      filter       : filterOptions,
+      'page[size]' : 10
+    });
   }
 });
