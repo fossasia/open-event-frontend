4cbde5dee862c445f520bfd1e2b934d10c25c1fd
Mayank Vaidya <mayank8318@gmail.com>
2018-06-05 09:20:37 +0530

    fix: Display only public call for speakers on the event website

--- app/components/public/side-menu.js
+++ app/components/public/side-menu.js
@@ -1,4 +1,8 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  shouldShowCallforSpeakers: computed(function() {
+    return this.get('event.isSessionsSpeakersEnabled') && (this.get('event.speakersCall.privacy') === 'public');
+  })
 });
