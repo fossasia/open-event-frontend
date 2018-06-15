0e0d88be21aadb9ffc94f057ad6a3e75cc0222e2
ritika <ritika.motwani0712@gmail.com>
2018-06-16 04:44:00 +0530

    Add functionality to event user link in admin/users

---
+++ app/controllers/admin/users/view/events/list.js
@@ -0,0 +1,81 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  columns: [
+    {
+      propertyName : 'name',
+      template     : 'components/ui-table/cell/cell-event',
+      title        : 'Name'
+    },
+    {
+      propertyName : 'startsAt',
+      template     : 'components/ui-table/cell/cell-simple-date',
+      dateFormat   : 'MMMM DD, YYYY - HH:mm A',
+      title        : 'Date'
+    },
+    {
+      propertyName     : 'sessionsByState',
+      template         : 'components/ui-table/cell/cell-sessions',
+      title            : 'Sessions',
+      disableSorting   : true,
+      disableFiltering : true
+    },
+    {
+      propertyName     : 'speakers.length',
+      title            : 'Speakers',
+      disableSorting   : true,
+      disableFiltering : true
+    },
+    {
+      propertyName     : 'tickets',
+      template         : 'components/ui-table/cell/cell-tickets',
+      title            : 'Tickets',
+      disableSorting   : true,
+      disableFiltering : true
+    },
+    {
+      propertyName     : 'url',
+      template         : 'components/ui-table/cell/cell-link',
+      title            : 'Public URL',
+      disableSorting   : true,
+      disableFiltering : true
+    },
+    {
+      template         : 'components/ui-table/cell/cell-buttons',
+      title            : 'Actions',
+      disableSorting   : true,
+      disableFiltering : true
+    }
+  ],
+  actions: {
+    moveToDetails(id) {
+      this.transitionToRoute('events.view', id);
+    },
+    editEvent(id) {
+      this.transitionToRoute('events.view.edit.basic-details', id);
+    },
+    openDeleteEventModal(id, name) {
+      this.set('isEventDeleteModalOpen', true);
+      this.set('confirmName', '');
+      this.set('eventName', name);
+      this.set('eventId', id);
+    },
+    deleteEvent() {
+      this.set('isLoading', true);
+      this.store.findRecord('event', this.get('eventId'), { backgroundReload: false }).then(function(event) {
+        event.destroyRecord();
+      })
+        .then(() => {
+          this.notify.success(this.get('l10n').t('Event has been deleted successfully.'));
+        })
+        .catch(() => {
+          this.notify.error(this.get('l10n').t('An unexpected error has occurred.'));
+        })
+        .finally(() => {
+          this.set('isLoading', false);
+        });
+      this.set('isEventDeleteModalOpen', false);
+    }
+  }
+});
+
