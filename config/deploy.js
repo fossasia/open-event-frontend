ca50a4fb2ab708838e8d24d1be7d0585c48b88a9
Niranjan Rajendran <me@niranjan.io>
2018-06-16 16:16:23 +0530

    Make docker builds use fastboot

--- config/deploy.js
+++ config/deploy.js
@@ -31,32 +31,14 @@ module.exports = function(deployTarget) {
 
   if (deployTarget === 'development') {
     ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
   }
 
   if (deployTarget === 'staging') {
     ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
   }
 
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
-  }
-
-  if (deployTarget !== 'production-docker') {
-    ENV.build.environment = 'production';
-  }
-
-  if (deployTarget === 'gh-pages-with-domain' || deployTarget === 'gh-pages') {
-    ENV.pipeline = {
-      disabled: {}
-    };
-    ENV.git = {
-      repo          : `https://niranjan94:${process.env.GIT_ACCESS_KEY}@github.com/fossasia/open-event-frontend`,
-      branch        : 'gh-pages',
-      commitMessage : 'Deployed %@'
-    };
   }
 
   if (deployTarget === 'gh-pages') {
