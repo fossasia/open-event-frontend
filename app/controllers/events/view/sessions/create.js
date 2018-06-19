bdf58b000ca64045ca6ee7f8a86f6d954c1a0764
Mayank Vaidya <mayank8318@gmail.com>
2018-06-19 14:17:20 +0530

    fix: Speaker-Session not being linked

--- app/controllers/events/view/sessions/create.js
+++ app/controllers/events/view/sessions/create.js
@@ -1,28 +1,18 @@
 import Controller from '@ember/controller';
 export default Controller.extend({
   actions: {
-    save() {
-      this.set('isLoading', true);
-      this.get('model.speaker.sessions').addObject(this.get('model.session'));
-      this.get('model.session.speakers').addObject(this.get('model.speaker'));
-
-      this.get('model.session').save()
-        .then(() => {
-          this.get('model.speaker').save()
-            .then(() => {
-              this.get('notify').success(this.get('l10n').t('Your session has been saved'));
-              this.transitionToRoute('events.view.sessions', this.get('model.event.id'));
-            })
-            .catch(() => {
-              this.get('notify').error(this.get('l10n').t('Oops something went wrong. Please try again'));
-            });
-        })
-        .catch(() => {
-          this.get('notify').error(this.get('l10n').t('Oops something went wrong. Please try again'));
-        })
-        .finally(() => {
-          this.set('isLoading', false);
-        });
+    async save() {
+      try {
+        this.set('isLoading', true);
+        await this.get('model.speaker').save();
+        this.get('model.speaker.sessions').pushObject(this.get('model.session'));
+        await this.get('model.session').save();
+        await this.get('model.speaker').save();
+        this.get('notify').success(this.get('l10n').t('Your session has been saved'));
+        this.transitionToRoute('events.view.sessions', this.get('model.event.id'));
+      } catch (e) {
+        this.get('notify').error(this.get('l10n').t('Oops something went wrong. Please try again'));
+      }
     }
   }
 });
