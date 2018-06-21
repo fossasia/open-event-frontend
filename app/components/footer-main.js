f67303265f7db4333202bcdf1227a6b32bfada08
Rainb <elektrischrainbow@gmail.com>
2018-06-21 03:32:34 +0000

    Implementing categories and places on the footer
    Stop making it hard-coded but dynamically loaded

--- app/components/footer-main.js
+++ app/components/footer-main.js
@@ -2,13 +2,16 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
-  tagName    : 'footer',
-  classNames : ['ui', 'inverted', 'vertical', 'footer', 'segment'],
-
-  currentLocale: computed(function() {
+  tagName       : 'footer',
+  classNames    : ['ui', 'inverted', 'vertical', 'footer', 'segment'],
+  places        : ['India', 'Singapore', 'Berlin', 'New York', 'Hong Kong'], 
+  currentLocale : computed(function() {
     return this.get('l10n').getLocale();
   }),
-
+  init() {
+    this._super(...arguments);
+    this.set('events', this.store.findAll('event-type'));
+  },
   actions: {
     switchLanguage(locale) {
       this.get('l10n').switchLanguage(locale);
