958bbb14cd46c8613a3fbce16d7ae70f7acb3956
Niranjan Rajendran <me@niranjan.io>
2018-06-23 16:00:01 +0530

    Redirect to previous route on login via transition

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import { merge, values, cloneDeep } from 'lodash';
 
 export default Route.extend(ApplicationRouteMixin, {
   title(tokens) {
@@ -11,15 +12,16 @@ export default Route.extend(ApplicationRouteMixin, {
     return tokens.join(' | ');
   },
 
-  async beforeModel() {
+  async beforeModel(transition) {
     this._super(...arguments);
+    this._rememberTransition(transition);
     await this.get('authManager').initialize();
     await this.get('settings').initialize();
   },
 
   async model() {
     let notifications = [];
-    if (this.get('session.isAuthenticated')) {
+    if (this.get('session.isAuthenticated') && this.get('authManager.currentUser')) {
       notifications = await this.get('authManager.currentUser').query('notifications', {
         filter: [
           {
@@ -43,6 +45,50 @@ export default Route.extend(ApplicationRouteMixin, {
   sessionInvalidated() {
     if (!this.get('session.skipRedirectOnInvalidation')) {
       this._super(...arguments);
+    }
+  },
+
+  /**
+   * Merge all params into one param.
+   *
+   * @param params
+   * @return {*}
+   * @private
+   */
+  _mergeParams(params) {
+    return merge({}, ...values(params));
+  },
+
+  /**
+   * Fix transition object by adding missing properties to make everything play nice with nested routes
+   *
+   * @param transition
+   * @return {*}
+   * @private
+   */
+  _fixTransition(transition) {
+    for (const property of ['params', 'queryParams']) {
+      transition[property] = cloneDeep(transition[property]);
+      transition[property][transition.targetName] = this._mergeParams(transition[property]);
+      transition.intent[property] = cloneDeep(transition[property]);
+    }
+    return transition;
+  },
+
+  /**
+   * Remember the given transition for future use
+   * @param transition
+   * @private
+   */
+  _rememberTransition(transition) {
+    if (!['login', 'register'].includes(transition.targetName)) {
+      this.set('session.attemptedTransition', this._fixTransition(transition));
+    }
+  },
+
+  actions: {
+    willTransition(transition) {
+      this._rememberTransition(transition);
     }
   }
 });
