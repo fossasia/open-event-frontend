7c54fc804aad2e6dc38a442eedc9f0a209f59bee
Rainb <elektrischrainbow@gmail.com>
2018-07-10 03:49:59 +0000

    Add admin sales models

---
+++ app/models/admin-sales-by-event.js
@@ -0,0 +1,15 @@
+import attr from 'ember-data/attr';
+import ModelBase from 'open-event-frontend/models/base';
+import { hasMany } from 'ember-data/relationships';
+
+export default ModelBase.extend({
+      events           : attr('string'),
+      eventDate        : attr('string'),
+      completedTickets : attr('number'),--
+      completedSales   : attr('number'),
+      placedTickets    : attr('number'),
+      placedSales      : attr('number'),
+      pendingTickets   : attr('number'),
+      pendingSales     : attr('number')
+
+});
