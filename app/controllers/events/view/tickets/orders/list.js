9906662ee38841a7e33de3051e6cdc4b2bc9e23d
pradeepgangwar <pradeepgangwar39@gmail.com>
2018-07-12 02:17:40 +0530

    Make action buttons in orders list functional

--- app/controllers/events/view/tickets/orders/list.js
+++ app/controllers/events/view/tickets/orders/list.js
@@ -27,5 +27,36 @@ export default Controller.extend({
       template       : 'components/ui-table/cell/events/view/tickets/orders/cell-actions',
       disableSorting : true
     }
-  ]
+  ],
+
+  actions: {
+    deleteOrder(order) {
+      this.set('isLoading', true);
+      order.destroyRecord()
+        .then(() => {
+          this.get('model').reload();
+          this.notify.success(this.get('l10n').t('Order has been deleted successfully.'));
+        })
+        .catch(() => {
+          this.notify.error(this.get('l10n').t('An unexpected error has occurred.'));
+        })
+        .finally(() => {
+          this.set('isLoading', false);
+        });
+    },
+    cancelOrder(order) {
+      this.set('isLoading', true);
+      order.set('status', 'cancelled');
+      order.save()
+        .then(() => {
+          this.notify.success(this.get('l10n').t('Order has been cancelled successfully.'));
+        })
+        .catch(() => {
+          this.notify.error(this.get('l10n').t('An unexpected error has occurred.'));
+        })
+        .finally(() => {
+          this.set('isLoading', false);
+        });
+    }
+  }
 });
