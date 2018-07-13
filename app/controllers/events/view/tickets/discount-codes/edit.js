c4baed8a84dd97fb62b25ec0900c2a4b9db253fd
Pradeep Gangwar <pradeepgangwar39@gmail.com>
2018-07-14 03:16:08 +0530

    feat: Add edit route to discount codes and allow deactivation of order from list (#1398)

--- app/controllers/events/view/tickets/discount-codes/edit.js
+++ app/controllers/events/view/tickets/discount-codes/edit.js
@@ -5,11 +5,11 @@ export default Controller.extend({
     saveCode(discountCode) {
       discountCode.save()
         .then(() => {
-          this.get('notify').success(this.get('l10n').t('Discount code has been successfully created.'));
+          this.get('notify').success(this.get('l10n').t('Discount code has been successfully updated.'));
           this.transitionToRoute('events.view.tickets.discount-codes');
         })
         .catch(() => {
-          this.get('notify').error(this.get('l10n').t('An unexpected error has occured. Discount code cannot be created.'));
+          this.get('notify').error(this.get('l10n').t('An unexpected error has occured. Discount code cannot be updated.'));
         });
     }
   }
