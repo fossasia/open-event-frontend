96581616a3b2466113a8ed430ae8d9f00d98a271
Mayank Vaidya <mayank8318@gmail.com>
2018-07-14 17:17:37 +0530

    fix: Make date filters in explore route as v1 (#1396)

--- app/components/explore/side-bar.js
+++ app/components/explore/side-bar.js
@@ -1,9 +1,19 @@
 import Component from '@ember/component';
 import moment from 'moment';
+import { computed } from '@ember/object';
+import { getDateRanges } from 'open-event-frontend/utils/dictionary/filters';
 
 export default Component.extend({
 
   classNames: ['ui', 'fluid', 'explore', 'vertical', 'menu'],
+
+  customStartDate: moment().toISOString(),
+
+  customEndDate: null,
+
+  dateRanges: computed(function() {
+    return getDateRanges.bind(this)();
+  }),
 
   actions: {
     selectCategory(category, subCategory) {
@@ -16,9 +26,70 @@ export default Component.extend({
     },
 
     dateValidate(date) {
-      if (moment(date).isAfter(this.get('endDate'))) {
-        this.set('endDate', date);
+      if (moment(date).isAfter(this.get('customEndDate'))) {
+        this.set('customEndDate', date);
       }
+
+      this.send('selectDateFilter', 'custom_dates');
+    },
+
+    selectDateFilter(dateType) {
+      let newStartDate = null;
+      let newEndDate = null;
+
+      if (dateType === this.get('dateType') && dateType !== 'custom_dates') {
+        this.set('dateType', null);
+      } else {
+        this.set('dateType', dateType);
+        switch (dateType) {
+          case 'custom_dates':
+            newStartDate = this.get('customStartDate');
+            newEndDate = this.get('customEndDate');
+            break;
+
+          case 'all_dates':
+            break;
+
+          case 'today':
+            newStartDate = moment().toISOString();
+            newEndDate = moment().toISOString();
+            break;
+
+          case 'tomorrow':
+            newStartDate = moment().add(1, 'day').toISOString();
+            newEndDate = newStartDate;
+            break;
+
+          case 'this_week':
+            newStartDate = moment().startOf('week').toISOString();
+            newEndDate = moment().endOf('week').toISOString();
+            break;
+
+          case 'this_weekend':
+            newStartDate = moment().isoWeekday('Friday').toISOString();
+            newEndDate = moment().isoWeekday('Sunday').toISOString();
+            break;
+
+          case 'next_week':
+            newStartDate = moment().isoWeekday('Monday').add(1, 'week').toISOString();
+            newEndDate = moment().isoWeekday('Sunday').add(1, 'week').toISOString();
+            break;
+
+          case 'this_month':
+            newStartDate = moment().startOf('month').toISOString();
+            newEndDate = moment().endOf('month').toISOString();
+            break;
+
+          default:
+        }
+      }
+
+      this.set('startDate', newStartDate);
+      this.set('endDate', newEndDate);
+    },
+
+    onDateChange() {
+      this.send('selectDateFilter', 'custom_dates');
     }
   }
 });
