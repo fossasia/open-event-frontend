dad334ef35a0d8fe789dd401a8e64ba2c065edbe
Rainb <elektrischrainbow@gmail.com>
2018-07-23 07:32:36 +0000

    Add console statements

--- app/serializers/admin-sales-by-event.js
+++ app/serializers/admin-sales-by-event.js
@@ -1,14 +1,15 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
-    normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    
-    payload2 = this._super(...arguments);
+  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+    console.log(store, primaryModelClass, payload, id, requestType); /* eslint-disable-line no-console */
+    var payload2 = this._super(...arguments);
     return payload2;
   },
   normalize(store, primaryModelClass, payload, id, requestType) {
-      console.log("normalize")
-    payload2 = this._super(...arguments);
+    console.log('normalize');/* eslint-disable-line no-console */
+    console.log(store, primaryModelClass, payload, id, requestType);/* eslint-disable-line no-console */
+    var payload2 = this._super(...arguments);
     return payload2;
-  },
+  }
 });
