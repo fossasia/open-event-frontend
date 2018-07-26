b4f1c2a965c31085fa32bc1f8db46988181791e3
pradeepgangwar <pradeepgangwar39@gmail.com>
2018-07-26 23:37:01 +0530

    Stripe Payment: Charge user after receiving stripe token

--- app/controllers/orders/placed.js
+++ app/controllers/orders/placed.js
@@ -18,17 +18,40 @@ export default Controller.extend({
   }),
 
   actions: {
-    processStripeToken() {
+    processStripeToken(token) {
       // Send this token to server to process payment
+      let order = this.get('model');
+      let chargePayload = {
+        'data': {
+          'attributes': {
+            'stripe'            : token.id,
+            'paypal_payer_id'   : null,
+            'paypal_payment_id' : null
+          },
+          'type': 'charge'
+        }
+      };
+      let config = {
+        skipDataTransform: true
+      };
+      chargePayload = JSON.stringify(chargePayload);
+      return this.get('loader').post(`orders/${order.identifier}/charge`, chargePayload, config)
+        .then(charge => {
+          if (charge.data.attributes.status) {
+            this.get('notify').success(charge.data.attributes.message);
+            this.transitionToRoute('orders.view', order.identifier);
+          } else {
+            this.get('notify').error(charge.data.attributes.message);
+          }
+        });
     },
 
     checkoutClosed() {
-      // The callback invoked when Checkout is closed.
-      this.get('notify').success(this.get('l10n').t('Your transaction was cancelled'));
+      // The callback invoked when stripe Checkout is closed.
     },
 
     checkoutOpened() {
-      // The callback invoked when Checkout is opened.
+      // The callback invoked when stripe Checkout is opened.
     }
   }
 });
