546c12901170173dbe8a9790c075d7cee3a02587
Pradeep Gangwar <pradeepgangwar39@gmail.com>
2018-07-28 17:43:40 +0530

    Paypal Payment: Charge a user after successful payment (#1509)

--- app/components/paypal-button.js
+++ app/components/paypal-button.js
@@ -1,17 +1,22 @@
 import paypal from 'npm:paypal-checkout';
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+
+  router: service(),
 
   async didInsertElement() {
     this._super(...arguments);
     let order = this.get('data');
     let loader = this.get('loader');
+    let notify = this.get('notify');
+    let router = this.get('router');
     let createPayload = {
       'data': {
         'attributes': {
-          'return-url' : `http://localhost:4200/orders/${order.identifier}/placed`,
-          'cancel-url' : `http://localhost:4200/orders/${order.identifier}/placed`
+          'return-url' : `${window.location.origin}/orders/${order.identifier}/placed`,
+          'cancel-url' : `${window.location.origin}/orders/${order.identifier}/placed`
         },
         'type': 'paypal-payment'
       }
@@ -36,8 +41,31 @@ export default Component.extend({
           });
       },
 
-      onAuthorize() {
+      onAuthorize(data) {
         // this callback will be for authorizing the payments
+        let chargePayload = {
+          'data': {
+            'attributes': {
+              'stripe'            : null,
+              'paypal_payer_id'   : data.payerID,
+              'paypal_payment_id' : data.paymentID
+            },
+            'type': 'charge'
+          }
+        };
+        let config = {
+          skipDataTransform: true
+        };
+        chargePayload = JSON.stringify(chargePayload);
+        return loader.post(`orders/${order.identifier}/charge`, chargePayload, config)
+          .then(charge => {
+            if (charge.data.attributes.status) {
+              notify.success(charge.data.attributes.message);
+              router.transitionTo('orders.view', order.identifier);
+            } else {
+              notify.error(charge.data.attributes.message);
+            }
+          });
       }
 
     }, this.elementId);
