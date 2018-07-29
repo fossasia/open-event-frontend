c6fc90f119d444ebb756aa7d7eccff6881c4f8c0
Ritika <ritika.motwani0712@gmail.com>
2018-07-29 06:23:23 +0200

    Add a route for tickets user link in admin/users (#1515)

---
+++ app/routes/admin/users/view/tickets/index.js
@@ -0,0 +1,11 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  titleToken() {
+    return this.get('l10n').t('Upcoming');
+  },
+  beforeModel() {
+    this._super(...arguments);
+    this.transitionTo('admin.users.view.tickets.list', 'upcoming');
+  }
+});
