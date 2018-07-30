f60bf9e577c698900cf856e53f2018a8f2d3e488
Mayank Vaidya <mayank8318@gmail.com>
2018-07-30 19:11:59 +0530

    feat: Add feature to update options in Admin -> Messages (#1527)

--- app/controllers/admin/messages.js
+++ app/controllers/admin/messages.js
@@ -2,36 +2,12 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
 
-  columns: [
-    {
-      propertyName : 'recipient',
-      title        : 'Recipients'
-    },
-    {
-      propertyName : 'action',
-      title        : 'Trigger'
-    },
-    {
-      subject  : 'emailSubject',
-      message  : 'emailMessage',
-      title    : 'Email Message',
-      template : 'components/ui-table/cell/cell-title-message'
-    },
-    {
-      subject  : 'notificationTitle',
-      message  : 'notificationMessage',
-      title    : 'Notification Message',
-      template : 'components/ui-table/cell/cell-title-message'
-    },
-    {
-      title    : 'Options',
-      template : 'components/ui-table/cell/admin/messages/cell-options'
-    },
-    {
-      propertyName : 'sentAt',
-      title        : 'Time/Date sent out',
-      template     : 'components/ui-table/cell/cell-simple-date',
-      dateFormat   : 'MMMM DD, YYYY - HH:mm A'
+  actions: {
+    save() {
+      let systemMessages = this.get('model');
+      systemMessages.forEach(systemMessage => {
+        systemMessage.save();
+      });
     }
-  ]
+  }
 });
