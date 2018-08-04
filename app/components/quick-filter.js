cadb9e578c73f7f835f6105dd0e3b4c62a41ea7b
Mayank Vaidya <mayank8318@gmail.com>
2018-08-04 12:07:09 +0530

    feat: Implement search on the home page using flask filters

--- app/components/quick-filter.js
+++ app/components/quick-filter.js
@@ -1,5 +1,82 @@
 import Component from '@ember/component';
+import moment from 'moment';
 
 export default Component.extend({
-  classNames: ['ui', 'action', 'input', 'fluid']
+  classNames: ['ui', 'action', 'input', 'fluid'],
+
+  dummyName     : null,
+  dummyLocation : null,
+  disableClear  : true,
+
+  setDateFilter() {
+    let newStartDate = null;
+    let newEndDate = null;
+
+    switch (this.get('filterDate')) {
+
+      case 'all_dates':
+        break;
+
+      case 'today':
+        newStartDate = moment().toISOString();
+        newEndDate = moment().toISOString();
+        break;
+
+      case 'tomorrow':
+        newStartDate = moment().add(1, 'day').toISOString();
+        newEndDate = newStartDate;
+        break;
+
+      case 'this_week':
+        newStartDate = moment().startOf('week').toISOString();
+        newEndDate = moment().endOf('week').toISOString();
+        break;
+
+      case 'this_weekend':
+        newStartDate = moment().isoWeekday('Friday').toISOString();
+        newEndDate = moment().isoWeekday('Sunday').toISOString();
+        break;
+
+      case 'next_week':
+        newStartDate = moment().isoWeekday('Monday').add(1, 'week').toISOString();
+        newEndDate = moment().isoWeekday('Sunday').add(1, 'week').toISOString();
+        break;
+
+      case 'this_month':
+        newStartDate = moment().startOf('month').toISOString();
+        newEndDate = moment().endOf('month').toISOString();
+        break;
+
+      default:
+    }
+
+    this.set('startDate', newStartDate);
+    this.set('endDate', newEndDate);
+  },
+
+  init() {
+    this._super(...arguments);
+    this.set('dummyLocation', this.get('location'));
+    this.set('dummyName', this.get('eventName'));
+    if (this.get('dummyName') || this.get('dummyLocation')) {this.set('disableClear', false)}
+  },
+
+  actions: {
+    search() {
+      this.setDateFilter();
+      this.set('location', this.get('dummyLocation'));
+      this.set('eventName', this.get('dummyName'));
+      this.set('disableClear', false);
+    },
+
+    clearFilters() {
+      this.set('dummyLocation', null);
+      this.set('dummyName', null);
+      this.set('filterDate', null);
+      this.setDateFilter();
+      this.set('location', this.get('dummyLocation'));
+      this.set('eventName', this.get('dummyName'));
+      this.set('disableClear', true);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,40 +2,129 @@ import Route from '@ember/routing/route';
 import moment from 'moment';
 
 export default Route.extend({
-  model() {
-    return this.store.query('event', {
-      include : 'event-topic,event-sub-topic,event-type,speakers-call',
-      filter  : [
-        {
-          or: [
+
+  /**
+   * Load filtered events based on the given params
+   *
+   * @param params
+   * @return {*}
+   * @private
+   */
+  _loadEvents(params) {
+    let filterOptions = [
+      {
+        name : 'state',
+        op   : 'eq',
+        val  : 'published'
+      }
+    ];
+
+    if (params.location) {
+      filterOptions.push({
+        name : 'location_name',
+        op   : 'ilike',
+        val  : `%${params.location}%`
+      });
+    }
+
+    if (params.event_name) {
+      filterOptions.push({
+        name : 'name',
+        op   : 'ilike',
+        val  : `%${params.event_name}%`
+      });
+    }
+
+    if (params.start_date && params.end_date) {
+      filterOptions.push({
+        or:
+          [
             {
-              name : 'starts-at',
-              op   : 'ge',
-              val  : moment().toISOString()
+              and: [
+                {
+                  name : 'starts-at',
+                  op   : 'ge',
+                  val  : params.start_date
+                },
+                {
+                  name : 'starts-at',
+                  op   : 'le',
+                  val  : params.end_date
+                }
+              ]
+            },
+            {
+              and: [
+                {
+                  name : 'ends-at',
+                  op   : 'ge',
+                  val  : params.start_date
+                },
+                {
+                  name : 'ends-at',
+                  op   : 'le',
+                  val  : params.end_date
+                }
+              ]
             },
             {
               and: [
                 {
                   name : 'starts-at',
                   op   : 'le',
-                  val  : moment().toISOString()
+                  val  : params.start_date
                 },
                 {
                   name : 'ends-at',
-                  op   : 'gt',
-                  val  : moment().toISOString()
+                  op   : 'ge',
+                  val  : params.end_date
                 }
               ]
             }
           ]
-        },
-        {
-          name : 'state',
-          op   : 'eq',
-          val  : 'published'
-        }
-      ],
-      sort: 'starts-at'
+      });
+    } else {
+      params.start_date = moment().toISOString();
+      filterOptions.push({
+        or: [
+          {
+            name : 'starts-at',
+            op   : 'ge',
+            val  : params.start_date
+          },
+          {
+            name : 'ends-at',
+            op   : 'ge',
+            val  : params.start_date
+          }
+        ]
+      });
+    }
+
+    return this.store.query('event', {
+      sort    : 'starts-at',
+      include : 'event-topic,event-sub-topic,event-type,speakers-call',
+      filter  : filterOptions
     });
+  },
+
+  async model(params) {
+    return {
+      filteredEvents: await this._loadEvents(params)
+    };
+  },
+
+  setupController(controller, model) {
+    this._super(...arguments);
+    controller.set('filteredEvents', model.filteredEvents);
+    this.set('controller', controller);
+  },
+
+  actions: {
+    async queryParamsDidChange(change, params) {
+      if (this.get('controller')) {
+        this.get('controller').set('filteredEvents', await this._loadEvents(params));
+      }
+    }
   }
 });
