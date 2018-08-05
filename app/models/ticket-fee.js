de6e4ee5cbe5e3094e1aff82245cd34acee931c9
mishuvs <vaibhavmishu@gmail.com>
2018-08-05 16:42:13 +0530

    feat: Add Admin Ticket Fee settings (#1551)

--- app/models/ticket-fee.js
+++ app/models/ticket-fee.js
@@ -2,6 +2,7 @@ import attr from 'ember-data/attr';
 import ModelBase from 'open-event-frontend/models/base';
 
 export default ModelBase.extend({
+  country    : attr('string'),
   currency   : attr('string'),
   serviceFee : attr('number'),
   maximumFee : attr('number')
