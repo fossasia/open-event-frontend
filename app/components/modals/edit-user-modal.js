169f1f026851de608174c481a2099e59ee26b21a
Ritika <ritika.motwani0712@gmail.com>
2018-08-10 08:43:49 +0200

    Integrate API to the edit user modal (#1609)

--- app/components/modals/edit-user-modal.js
+++ app/components/modals/edit-user-modal.js
@@ -2,12 +2,20 @@ import ModalBase from 'open-event-frontend/components/modals/modal-base';
 
 export default ModalBase.extend({
   actions: {
-    saveRole(id, isAdmin) {
+    saveRole(id) {
       this.get('store').findRecord('user', id).then(function(user) {
-        user.set('isAdmin', isAdmin);
         user.save();
       });
       this.set('isOpen', false);
+    },
+    toggleSalesAdmin(user) {
+      user.toggleProperty('isSalesAdmin');
+    },
+    toggleMarketer(user) {
+      user.toggleProperty('isMarketer');
+    },
+    createAdmin(user, isAdmin) {
+      user.set('isAdmin', isAdmin);
     }
   }
 });
