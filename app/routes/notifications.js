543e290453acb22f8980d00233c107bac8344b17
Saurav Tiwary <srv.twry@gmail.com>
2018-08-11 13:16:35 +0530

    fix: Major issues with the notification panel (#1607)
    * fix: Major issues in Notifications
    
    * test: update notification acceptance test

--- app/routes/notifications.js
+++ app/routes/notifications.js
@@ -4,32 +4,5 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Route.extend(AuthenticatedRouteMixin, {
   titleToken() {
     return this.get('l10n').t('Notifications');
-  },
-  async model() {
-    let filterOptions = [
-      {
-        name : 'is-read',
-        op   : 'eq',
-        val  : false
-      }
-    ];
-
-    let data = {};
-
-    data.notifications = await this.get('authManager.currentUser').query('notifications', {
-      include : 'notification_actions',
-      filter  : filterOptions,
-      reload  : true
-    });
-
-    let actions = await this.get('store').peekAll('notification-action');
-
-    for (let notification of data.notifications ? data.notifications.toArray() : []) {
-      if (!notification.actions) {
-        notification.actions = actions.filter(action => action.notificationId === notification.id);
-      }
-    }
-
-    return data;
   }
 });
--- app/routes/notifications/index.js
+++ app/routes/notifications/index.js
@@ -1,11 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  titleToken() {
-    return this.get('l10n').t('Unread');
-  },
-  templateName: 'notifications/all',
-  model() {
-    return this.modelFor('notifications');
+  beforeModel() {
+    this._super(...arguments);
+    this.transitionTo('notifications.all', 'unread');
   }
 });
