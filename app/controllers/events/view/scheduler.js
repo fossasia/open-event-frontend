f31010e56bf01e916b4fe9376f646440a2855f27
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-10-23 06:57:15 +0530

    Scheduler version 1.0

--- app/controllers/events/view/scheduler.js
+++ app/controllers/events/view/scheduler.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import $ from 'jquery';
 
 export default Controller.extend({
   header: {
@@ -12,15 +13,51 @@ export default Controller.extend({
       duration : { days: 3 }
     }
   },
+  updateSession(start, end, microlocationId, sessionId) {
+    let payload = {
+      data: {
+        attributes: {
+          'starts-at' : start ? start.toISOString() : null,
+          'ends-at'   : end ? end.toISOString() : null
+        },
+        relationships: {
+          microlocation: {
+            data: {
+              type : 'microlocation',
+              id   : microlocationId
+            }
+          }
+        },
+        type : 'session',
+        id   : sessionId
+      }
+    };
+    let config = {
+      skipDataTransform: true
+    };
+    return this.get('loader')
+      .patch(`sessions/${sessionId}`, JSON.stringify(payload), config)
+      .then(() => {
+        this.get('notify').success('Changes have been made successfully');
+      })
+      .catch(reason => {
+        this.set('error', reason);
+        this.get('notify').error(`Error: ${reason}`);
+      });
+  },
   actions: {
-    drop() {
+    drop(date, jsEvent, ui, resourceId) {
+      let start = date;
+      let duration = this.get('model.defaultDuration').split(':');
+      let end = start.clone().add(duration[0], 'hours').add(duration[1], 'minutes');
+      this.updateSession(start, end, resourceId, $(ui.helper).data('serverId'));
+      $(ui.helper).remove();
     },
-    eventReceive(event) {
-      event.preventDefault();
+    eventDrop(session) {
+      this.updateSession(session.start, session.end, session.resourceId, session.serverId);
     },
-    eventDrop(event) {
-      event.preventDefault();
+    eventResize(session) {
+      this.updateSession(session.start, session.end, session.resourceId, session.serverId);
     }
-    // All these callbacks work with jquery UI (get triggered), will be handled later.
   }
 });
