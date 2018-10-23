978edc923122eadc6df17262fc2545ac6ccb67cc
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-10-23 06:52:19 +0530

    Customise the model according to fullcalendar

--- app/routes/events/view/scheduler.js
+++ app/routes/events/view/scheduler.js
@@ -48,7 +48,7 @@ export default Route.extend({
 
     let durationDays = eventDetails.endsAt.diff(eventDetails.startsAt, 'days') + 1;
     let views = {
-      agendaEvent: {
+      timelineThreeDays: {
         type       : 'agenda',
         duration   : { days: durationDays },
         buttonText : `${durationDays} day`
@@ -56,13 +56,13 @@ export default Route.extend({
     };
 
     let header = {
-      left   : 'prev,next',
+      left   : 'today,prev,next',
       center : 'title',
-      right  : 'agendaDay,agendaEvent'
+      right  : 'timelineDay,timelineThreeDays,agendaWeek,month'
     };
 
     let scheduledSessions = await eventDetails.query('sessions', {
-      include : 'speakers,microlocation',
+      include : 'speakers,microlocation,track',
       filter  : scheduledFilterOptions
     });
 
@@ -77,12 +77,14 @@ export default Route.extend({
         title      : `${session.title} | ${speakerNames.join(', ')}`,
         start      : session.startsAt.format('YYYY-MM-DDTHH:mm:SS'),
         end        : session.endsAt.format('YYYY-MM-DDTHH:mm:SS'),
-        resourceId : session.microlocation.get('id')
+        resourceId : session.microlocation.get('id'),
+        color      : session.track.get('color'),
+        serverId   : session.get('id') // id of the session on BE
       });
     });
 
     let unscheduledSessions = await eventDetails.query('sessions', {
-      include : 'speakers',
+      include : 'speakers,track',
       filter  : unscheduledFilterOptions
     });
 
@@ -107,7 +109,8 @@ export default Route.extend({
       minTime         : eventDetails.startsAt.format('HH:mm:ss'),
       maxTime         : eventDetails.endsAt.format('HH:mm:ss'),
       validRange,
-      views
+      views,
+      defaultDuration : '01:00'
     };
   }
 });
