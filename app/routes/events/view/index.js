2a0598491e250d72121926296180f51b692abf2e
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-12-24 01:02:24 +0530

    Pagination: Modify model of event overview sponsors table

--- app/routes/events/view/index.js
+++ app/routes/events/view/index.js
@@ -3,18 +3,22 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   async model() {
     let eventDetails = this.modelFor('events.view');
+    let sponsorQueryObject = {
+      'page[size]': 10
+    };
+
     return {
-      event    : await eventDetails,
-      sponsors : await eventDetails.query('sponsors', {
-        'page[size]': 10
-      }),
-      roleInvites  : await eventDetails.query('roleInvites', {}),
-      sessionTypes : await eventDetails.query('sessionTypes', {}),
-      socialLinks  : await eventDetails.query('socialLinks', {}),
-      statistics   : await eventDetails.query('eventStatisticsGeneral', {}),
-      orderStat    : await eventDetails.query('orderStatistics', {}),
-      tickets      : await eventDetails.query('tickets', {}),
-      roles        : await this.store.findAll('role')
+      event             : await eventDetails,
+      sponsors          : await eventDetails.query('sponsors', sponsorQueryObject),
+      query             : sponsorQueryObject,
+      sponsorObjectType : 'sponsors',
+      roleInvites       : await eventDetails.query('roleInvites', {}),
+      sessionTypes      : await eventDetails.query('sessionTypes', {}),
+      socialLinks       : await eventDetails.query('socialLinks', {}),
+      statistics        : await eventDetails.query('eventStatisticsGeneral', {}),
+      orderStat         : await eventDetails.query('orderStatistics', {}),
+      tickets           : await eventDetails.query('tickets', {}),
+      roles             : await this.store.findAll('role')
     };
   }
 });
