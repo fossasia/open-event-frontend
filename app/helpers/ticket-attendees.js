188c5050b34a78bca5008f4b4fdbf7890466de09
Abhinav Khare <abhinav.khare31@gmail.com>
2019-01-12 15:32:43 +0800

    Fix: Inconsistent order summary values (#1790)

---
+++ app/helpers/ticket-attendees.js
@@ -0,0 +1,13 @@
+import { helper } from '@ember/component/helper';
+import { intersection } from 'lodash';
+
+export  function ticketAttendees(params/* , hash*/) {
+
+  let allTicketAttendees =  params[1].toArray();
+  let orderAttendees = params[0].toArray();
+  let commonAttendees =  intersection(orderAttendees, allTicketAttendees);
+  return commonAttendees.length;
+
+}
+
+export default helper(ticketAttendees);
