b0c13151654719969cbe8431e9aa9c2cf60af172
Yogesh Sharma <yogesh02011998@gmail.com>
2019-01-20 23:20:51 +0530

    fix admin sales

--- app/controllers/admin/sales/index.js
+++ app/controllers/admin/sales/index.js
@@ -1,24 +1,4 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
-import { sumBy } from 'lodash';
+import AdminSalesMixin from 'open-event-frontend/mixins/admin-sales';
 
-export default Controller.extend({
-  totalCompletedTickets: computed(function() {
-    return sumBy(this.get('model'), 'completedTickets');
-  }),
-  totalCompletedSales: computed(function() {
-    return sumBy(this.get('model'), 'completedSales');
-  }),
-  totalPlacedTickets: computed(function() {
-    return sumBy(this.get('model'), 'placedTickets');
-  }),
-  totalPlacedSales: computed(function() {
-    return sumBy(this.get('model'), 'placedSales');
-  }),
-  totalPendingTickets: computed(function() {
-    return sumBy(this.get('model'), 'pendingTickets');
-  }),
-  totalPendingSales: computed(function() {
-    return sumBy(this.get('model'), 'pendingSales');
-  })
-});
+export default Controller.extend(AdminSalesMixin);
