1fd5ef3459806092eaefe106518319fed857da3e
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-01-23 13:26:03 +0800

    Add fastboot server

---
+++ fastboot/server.js
@@ -0,0 +1,11 @@
+const FastBootAppServer = require('fastboot-app-server');
+
+let server = new FastBootAppServer({
+  distPath: 'dist',
+  gzip: true,
+  host: '0.0.0.0',
+  port: 4000,
+  chunkedResponse: true
+});
+
+server.start();
