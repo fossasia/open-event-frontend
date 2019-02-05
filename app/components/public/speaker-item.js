c525a96970813d214b42356ebc2d6537f688ec47
shubham <kumarsinghshubham32111@gmail.com>
2019-02-06 05:16:28 +0530

    popup box made

--- app/components/public/speaker-item.js
+++ app/components/public/speaker-item.js
@@ -1,9 +1,15 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import $ from 'jquery';
 
 export default Component.extend({
   classNames  : ['four wide  speaker column'],
   socialLinks : computed(function() {
     return this.get('speaker').getProperties('twitter', 'facebook', 'github', 'linkedin');
-  })
+  }),
+  didInsertElement() {
+    $('.title').popup({
+      position: 'bottom center'
+    });
+  }
 });
