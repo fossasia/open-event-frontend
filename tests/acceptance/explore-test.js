856a04621d3241bef7c3b233806fab10452ec717
saicharan <saicharan.reddy1@gmail.com>
2019-02-11 06:45:55 +0530

    Acceptance test for explore route

--- tests/acceptance/explore-test.js
+++ tests/acceptance/explore-test.js
@@ -1,9 +1,8 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
-import { render } from '@ember/test-helpers';
-import { login, logout } from 'open-event-frontend/tests/helpers/custom-helpers';
+import { currentSession } from 'ember-simple-auth/test-support';
+import { login } from 'open-event-frontend/tests/helpers/custom-helpers';
 
 module('Acceptance | explore', function(hooks) {
   setupApplicationTest(hooks);
@@ -25,10 +24,10 @@ module('Acceptance | explore', function(hooks) {
   test('side bar loading test', async function(assert) {
     await visit('/explore');
     assert.equal(currentURL(), '/explore');
-    await render(hbs`{{explore/side-bar l10n=l10n}}`);
     assert.ok(this.element.innerHTML.trim().includes('Categories'));
     assert.ok(this.element.innerHTML.trim().includes('Event Type'));
     assert.ok(this.element.innerHTML.trim().includes('Date'));
+    assert.ok(this.element.innerHTML.trim().includes('Clear Filters'));
   });
 
 });
