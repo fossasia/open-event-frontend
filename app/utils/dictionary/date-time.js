0e438365a148088cdcde32f4ce05474e60d805ca
jaynilshah <jaynil.shah84@gmail.com>
2019-02-22 15:26:21 +0530

    Singapore only once in Timezone

--- app/utils/dictionary/date-time.js
+++ app/utils/dictionary/date-time.js
@@ -1,6 +1,8 @@
 import moment from 'moment';
 
-export const timezones = moment.tz.names();
+export const timezones = moment.tz.names().filter(function(timezone) {
+  return timezone !== 'Singapore';
+});
 
 export const FORM_DATE_FORMAT = 'MM/DD/YYYY';
 export const FORM_TIME_FORMAT = 'HH:mm';
