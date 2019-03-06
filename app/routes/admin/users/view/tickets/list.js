e7c71e17d8ea2730e027670b2f7ef79826db1f16
shubham <kumarsinghshubham32111@gmail.com>
2019-03-06 10:24:59 +0530

    saved events column removed from user tickets page

--- app/routes/admin/users/view/tickets/list.js
+++ app/routes/admin/users/view/tickets/list.js
@@ -8,8 +8,6 @@ export default Route.extend(AuthenticatedRouteMixin, {
         return this.get('l10n').t('Upcoming');
       case 'past':
         return this.get('l10n').t('Past');
-      case 'saved':
-        return this.get('l10n').t('Saved');
     }
   },
   model() {
