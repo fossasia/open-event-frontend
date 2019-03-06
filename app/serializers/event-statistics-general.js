a0047f2c65f3db85d3bd2f0d66d2dd4fc23f9a1e
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-03-06 17:50:25 +0530

    implement serializer for parsing speaker dict

---
+++ app/serializers/event-statistics-general.js
@@ -0,0 +1,11 @@
+import ApplicationSerializer from 'open-event-frontend/serializers/application';
+
+export default ApplicationSerializer.extend({
+  attrs: {
+    speakersAccepted: 'speakers.accepted',
+    speakersConfirmed: 'speakers.confirmed',
+    speakersPending: 'speakers.pending',
+    speakersRejected: 'speakers.rejected',
+    speakers: 'speakers.total'
+  }
+});
