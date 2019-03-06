0dfa4e8877891262b3c7875a865b74276026c5e1
Anupam Dagar <dagaranupam@gmail.com>
2019-03-06 20:59:23 +0530

    Create UI for events revision dialog modal.
    Fixes: #2192

--- app/components/events/view/overview/general-info.js
+++ app/components/events/view/overview/general-info.js
@@ -1,5 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['ui', 'fluid', 'card']
+  classNames : ['ui', 'fluid', 'card'],
+  actions    : {
+    openModal() {
+      this.set('isEventRevisionModalOpen', true);
+    }
+  }
 });
