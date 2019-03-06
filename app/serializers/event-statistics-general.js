0b844a9d2660c68a29cce586bd9839fa594e201a
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-03-06 18:47:07 +0530

    fix : lint issues

--- app/serializers/event-statistics-general.js
+++ app/serializers/event-statistics-general.js
@@ -2,10 +2,10 @@ import ApplicationSerializer from 'open-event-frontend/serializers/application';
 
 export default ApplicationSerializer.extend({
   attrs: {
-    speakersAccepted: 'speakers.accepted',
-    speakersConfirmed: 'speakers.confirmed',
-    speakersPending: 'speakers.pending',
-    speakersRejected: 'speakers.rejected',
-    speakers: 'speakers.total'
+    speakersAccepted  : 'speakers.accepted',
+    speakersConfirmed : 'speakers.confirmed',
+    speakersPending   : 'speakers.pending',
+    speakersRejected  : 'speakers.rejected',
+    speakers          : 'speakers.total'
   }
 });
