00c680fcfd094427993ec431945d1560735fe37e
shubham3211 <kumarsinghshubham32111@gmail.com>
2019-03-12 08:05:53 +0530

    filter for events added (#2291)

--- app/routes/admin/users/view/events/list.js
+++ app/routes/admin/users/view/events/list.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import moment from 'moment';
 
 export default Route.extend(AuthenticatedRouteMixin, {
   titleToken() {
@@ -12,10 +13,80 @@ export default Route.extend(AuthenticatedRouteMixin, {
         return this.get('l10n').t('Past');
     }
   },
-  model() {
-    const userDetails = this.modelFor('admin.users.view');
-    return this.store.findRecord('user', userDetails.id, {
-      include: 'events'
-    });
+  async model(params) {
+    this.set('params', params);
+
+    let filterOptions = [];
+    if (params.event_status === 'live') {
+      filterOptions = [
+        {
+          name : 'state',
+          op   : 'eq',
+          val  : 'published'
+        },
+        {
+          or: [
+            {
+              name : 'starts-at',
+              op   : 'ge',
+              val  : moment().toISOString()
+            },
+            {
+              and: [
+                {
+                  name : 'starts-at',
+                  op   : 'le',
+                  val  : moment().toISOString()
+                },
+                {
+                  name : 'ends-at',
+                  op   : 'gt',
+                  val  : moment().toISOString()
+                }
+              ]
+            }
+          ]
+        }
+      ];
+    } else if (params.event_status === 'past') {
+      filterOptions = [
+        {
+          name : 'ends-at',
+          op   : 'lt',
+          val  : moment().toISOString()
+        },
+        {
+          name : 'state',
+          op   : 'eq',
+          val  : 'published'
+        }
+      ];
+    } else {
+      filterOptions = [
+        {
+          name : 'state',
+          op   : 'eq',
+          val  : 'draft'
+        }
+      ];
+    }
+
+    let queryObject =  {
+      include      : 'tickets,sessions,speakers',
+      filter       : filterOptions,
+      'page[size]' : 10
+    };
+
+    const store = this.modelFor('admin.users.view');
+
+    const data = await store.query('events', queryObject);
+
+    return {
+      data,
+      store,
+      query      : queryObject,
+      objectType : 'events'
+    };
+
   }
 });
