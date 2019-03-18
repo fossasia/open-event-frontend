7ab8d26c287f2a548d25cc329ec3659ff64a97b8
prateekj117 <prateekj1171998@gmail.com>
2019-03-18 09:18:38 +0530

    Correct implementation of Facebook-OAuth

--- app/components/forms/login-form.js
+++ app/components/forms/login-form.js
@@ -84,27 +84,11 @@ export default Component.extend(FormMixin, {
     async auth(provider) {
       try {
         if (provider === 'facebook') {
-          this.get('torii').open('facebook').then(authData => {
-            this.get('loader').load(`/auth/oauth/login/${  provider  }/${  authData.authorizationCode  }/?redirect_uri=${  authData.redirectUri}`)
-              .then(async response => {
-                let credentials = {
-                  'identification' : response.email,
-                  'password'       : response.facebook_login_hash
-                };
-
-                let authenticator = 'authenticator:jwt';
-                this.get('session')
-                  .authenticate(authenticator, credentials)
-                  .then(async() => {
-                    const tokenPayload = this.get('authManager').getTokenPayload();
-                    if (tokenPayload) {
-                      this.get('authManager').persistCurrentUser(
-                        await this.get('store').findRecord('user', tokenPayload.identity)
-                      );
-                    }
-                  });
-              });
-          });
+          this.get('loader').load('/auth/oauth/facebook')
+            .then(async response => {
+              console.log(response.url)
+              window.location.replace(response.url)
+            })
         }
       } catch (error) {
         this.get('notify').error(this.get('l10n').t(error.message));
