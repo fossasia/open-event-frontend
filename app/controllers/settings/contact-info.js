61ece3c35aa791a2928197cf435dbcddfdaee39c
Haider Ali <haider.lee23@gmail.com>
2019-03-20 15:53:46 +0530

    Validate phone number in contact info

--- app/controllers/settings/contact-info.js
+++ app/controllers/settings/contact-info.js
@@ -5,16 +5,29 @@ export default Controller.extend({
     updateContactInfo() {
       this.set('isLoading', true);
       let currentUser = this.get('model');
-      currentUser.save()
-        .then(() => {
-          this.get('notify').success(this.get('l10n').t('Your Contact Info has been updated'));
-        })
-        .catch(() => {
-          this.get('notify').error(this.get('l10n').t('An unexpected error occurred'));
-        })
-        .finally(() => {
-          this.set('isLoading', false);
-        });
+      if (verifyPhoneNumber(currentUser.contact)) {
+        currentUser.save()
+          .then(() => {
+            this.get('notify').success(this.get('l10n').t('Your Contact Info has been updated'));
+          })
+          .catch(() => {
+            this.get('notify').error(this.get('l10n').t('An unexpected error occurred'));
+          })
+          .finally(() => {
+            this.set('isLoading', false);
+          });
+      } else {
+        this.get('notify').error(this.get('l10n').t('Please enter the valid phone number'));
+        this.set('isLoading', false);
+      }
     }
   }
 });
+
+function verifyPhoneNumber(number) {
+  if (!isNaN(number) && number.length === 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
