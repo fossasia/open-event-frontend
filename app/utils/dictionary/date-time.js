ebb5c49071c351c496d8351bbe0444388438fd76
Kush <kushthedude@gmail.com>
2019-03-25 04:00:50 +0530

    [WIP]Making TimeZone on GMT basis

--- app/utils/dictionary/date-time.js
+++ app/utils/dictionary/date-time.js
@@ -1,6 +1,12 @@
 import moment from 'moment';
 
-export const timezones = moment.tz.names();
+const timeZones = moment.tz.names();
+let offsetTmz = [];
+for (let i in timeZones) {
+  offsetTmz.push(`(GMT${moment.tz(timeZones[i]).format('Z')}) ${timeZones[i]}`);
+}
+
+export const timezones = offsetTmz.sort();
 
 export const FORM_DATE_FORMAT = 'MM/DD/YYYY';
 export const FORM_TIME_FORMAT = 'HH:mm';
