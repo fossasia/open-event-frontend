2078737d78f30f6c627ee61c2ddec71ab5ae20f6
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-03-28 04:17:27 +0530

    add attributes in the payload (#2381)

--- app/serializers/event-statistics-general.js
+++ app/serializers/event-statistics-general.js
@@ -1,11 +1,19 @@
 import ApplicationSerializer from 'open-event-frontend/serializers/application';
 
 export default ApplicationSerializer.extend({
+  normalizeResponse(store, primaryModelClass, payload) {
+    payload.data.attributes.speakersAccepted = payload.data.attributes.speakers.accepted;
+    payload.data.attributes.speakersConfirmed = payload.data.attributes.speakers.confirmed;
+    payload.data.attributes.speakersPending = payload.data.attributes.speakers.pending;
+    payload.data.attributes.speakersRejected = payload.data.attributes.speakers.rejected;
+    payload.data.attributes.speakersTotal = payload.data.attributes.speakers.total;
+    return this._super(...arguments);
+  },
   attrs: {
-    speakersAccepted  : 'speakers.accepted',
-    speakersConfirmed : 'speakers.confirmed',
-    speakersPending   : 'speakers.pending',
-    speakersRejected  : 'speakers.rejected',
-    speakers          : 'speakers.total'
+    speakersAccepted  : 'speakersAccepted',
+    speakersConfirmed : 'speakersConfirmed',
+    speakersPending   : 'speakersPending',
+    speakersRejected  : 'speakersRejected',
+    speakers          : 'speakersTotal'
   }
 });
