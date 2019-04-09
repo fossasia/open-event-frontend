96459dfcfe967a08e649adcbb0503d72915dcd0d
saicharan <saicharan.reddy1@gmail.com>
2019-04-09 18:26:51 +0530

    Used downloader service of loader for translations

--- app/controllers/admin/content/translations.js
+++ app/controllers/admin/content/translations.js
@@ -7,7 +7,10 @@ export default Controller.extend({
     translationsDownload() {
       this.set('isLoading', true);
       this.get('loader')
-        .load('/admin/content/translations/all')
+        .downloadFile('/admin/content/translations/all')
+        .then(() => {
+          this.get('notify').success(this.get('l10n').t('Translations Zip generated successfully.'));
+        })
         .catch(e => {
           console.warn(e);
           this.get('notify').error(this.get('l10n').t('Unexpected error occurred.'));
