3e14fa03a14a1df4f2d708329f1c0a70c33a3344
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-04-19 21:04:31 +0530

    Check for content before applying first divider

--- app/components/public/speaker-item.js
+++ app/components/public/speaker-item.js
@@ -5,5 +5,13 @@ export default Component.extend({
   classNames  : ['four wide  speaker column'],
   socialLinks : computed(function() {
     return this.get('speaker').getProperties('twitter', 'facebook', 'github', 'linkedin');
+  }),
+  hasSocialLinks: computed(function() {
+    var currentSpeaker = this.speaker;
+    if (currentSpeaker.twitter || currentSpeaker.facebook || currentSpeaker.github || currentSpeaker.linkedin || currentSpeaker.shortBiography || currentSpeaker.longBiography || currentSpeaker.speakingExperience) {
+      return true;
+    } else {
+      return false;
+    }
   })
 });
