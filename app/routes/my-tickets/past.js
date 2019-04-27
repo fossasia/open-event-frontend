9b5c3b53b6d0288eda5176bc46f48cdf62096854
Shreyansh Dwivedi <shreyanshdwivedi1997@gmail.com>
2019-04-27 16:40:52 +0530

    my-tickets: unfinished ticket orders show up as open orders

---
+++ app/routes/my-tickets/past.js
@@ -0,0 +1,53 @@
+import Route from '@ember/routing/route';
+import moment from 'moment';
+
+export default Route.extend({
+  titleToken() {
+    return this.get('l10n').t('Past'); 
+  },
+  model() {
+    let filterOptions = [];
+    filterOptions.push(
+      {
+        and: [
+          {
+            name : 'event',
+            op   : 'has',
+            val  : {
+              name : 'starts-at',
+              op   : 'lt',
+              val  : moment().toISOString()
+            }
+          },
+          {
+            or: [
+              {
+                name : 'status',
+                op   : 'eq',
+                val  : 'completed'
+              },
+              {
+                name : 'status',
+                op   : 'eq',
+                val  : 'placed'
+              }
+            ]
+          },
+          {
+            name : 'event',
+            op   : 'has',
+            val  : {
+              name : 'deleted-at',
+              op   : 'eq',
+              val  : null
+            }
+          }
+        ]
+      }
+    );
+    return this.get('authManager.currentUser').query('orders', {
+      include : 'event',
+      filter  : filterOptions
+    });
+  }
+});
