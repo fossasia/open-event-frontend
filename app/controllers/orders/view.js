b5b4310599c594805fcdb9f458c313dac59213ff
Abhinav Khare <abhinav.khare31@gmail.com>
2019-04-30 04:22:52 +0800

    Enhance order invoice print output (#2837)
    * Add ember-print-this
    
    * Enhance order invoice print output

--- app/controllers/orders/view.js
+++ app/controllers/orders/view.js
@@ -1,9 +1,17 @@
 import Controller from '@ember/controller';
-
+import { inject as service } from '@ember/service';
 export default Controller.extend({
+  printThis: service(),
+
+
   actions: {
     downloadInvoice() {
-      window.print();
+      const selector = '.print';
+      const options = {
+        header: 'Order Invoice'
+      };
+      this.get('printThis').print(selector, options);
+
     }
   }
 });
