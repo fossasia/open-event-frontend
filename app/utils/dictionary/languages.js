8d9aecab4ffc6e3b5cfbfc64074e5edbe563b348
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-05-11 12:59:17 +0530

    Sorting the languages

--- app/utils/dictionary/languages.js
+++ app/utils/dictionary/languages.js
@@ -39,46 +39,5 @@ export const languages = [
     name: 'Russian'
   }
 ];
-export const languages = [
-  {
-    name: 'English'
-  },
-  {
-    name: 'Chinese'
-  },
-  {
-    name: 'French'
-  },
-  {
-    name: 'German'
-  },
-  {
-    name: 'Indonesian'
-  },
-  {
-    name: 'Korean'
-  },
-  {
-    name: 'Polish'
-  },
-  {
-    name: 'Spanish'
-  },
-  {
-    name: 'Thai'
-  },
-  {
-    name: 'Viatnamese'
-  },
-  {
-    name: 'Hindi'
-  },
-  {
-    name: 'Japanese'
-  },
-  {
-    name: 'Russian'
-  }
-];
 languages.sort('name');
 
