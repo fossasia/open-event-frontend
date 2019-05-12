f475459a51055f620347f5b7189f12304f0de471
Saicharan Reddy <saicharan.reddy1@gmail.com>
2019-05-12 09:07:53 +0530

    Ticket Downloads fix (#2662)

--- app/controllers/orders/view.js
+++ app/controllers/orders/view.js
@@ -1,17 +1,33 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+
 export default Controller.extend({
-  printThis: service(),
-
-
-  actions: {
+  isLoading : false,
+  printThis : service(),
+  actions   : {
     downloadInvoice() {
       const selector = '.print';
       const options = {
         header: 'Order Invoice'
       };
       this.get('printThis').print(selector, options);
-
     }
+  },
+
+  downloadTickets() {
+    this.set('isLoading', true);
+    this.get('loader')
+      .downloadFile(`/tickets/${this.get('model.order.identifier')}`)
+      .then(() => {
+        this.get('notify').success(this.get('l10n').t('Here are your tickets'));
+      })
+      .catch(e => {
+        console.warn(e);
+        this.get('notify').error(this.get('l10n').t('An unexpected Error occurred'));
+      })
+      .finally(() => {
+        this.set('isLoading', false);
+      });
   }
+
 });
