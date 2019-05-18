24b841a9344363f0f5b282a817d2911433f9d6ea
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-05-19 01:36:10 +0530

    change action alignment and remove the column

--- tests/integration/components/ui-table/cell/events/view/tickets/orders/cell-order-test.js
+++ tests/integration/components/ui-table/cell/events/view/tickets/orders/cell-order-test.js
@@ -8,12 +8,15 @@ module('Integration | Component | ui table/cell/events/view/tickets/orders/cell 
   setupIntegrationTest(hooks);
 
   test('it renders', async function(assert) {
-
+    this.set('deleteOrder', () => {});
+    this.set('cancelOrder', () => {});
     let record = EmberObject.create({
+      amount    : 20,
+      status    : 'cancelled',
       identifier: 'identifier_order'
     });
     this.set('record', record);
-    await render(hbs`{{ui-table/cell/events/view/tickets/orders/cell-order record=record}}`);
+    await render(hbs`{{ui-table/cell/events/view/tickets/orders/cell-order deleteOrder=(action deleteOrder) cancelOrder=(action cancelOrder) record=record }}`);
     assert.ok(this.element.textContent.trim().includes(''));
   });
 });
