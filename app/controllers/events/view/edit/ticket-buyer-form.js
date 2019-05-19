e33bdd7a7bb306aa7b8edc2ca27a4b369326c85e
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-05-20 01:57:11 +0530

    intermediate order form showing option

--- app/controllers/events/view/edit/ticket-buyer-form.js
+++ app/controllers/events/view/edit/ticket-buyer-form.js
@@ -2,14 +2,23 @@ import Controller from '@ember/controller';
 import EventWizardMixin from 'open-event-frontend/mixins/event-wizard';
 
 export default Controller.extend(EventWizardMixin, {
+  async saveForms(data) {
+    for (const customForm of data.customForms ? data.customForms.toArray() : []) {
+      await customForm.save();
+    }
+    return data;
+  },
+
   actions: {
     save() {
+      this.saveForms(data);
       this.saveEventDataAndRedirectTo(
         'events.view.index',
         []
       );
     },
     move(direction) {
+      this.saveForms(data);
       this.saveEventDataAndRedirectTo(
         direction === 'forwards' ? 'events.view.edit.sponsors' : 'events.view.edit.basic-details',
         []
--- app/routes/events/view/edit/ticket-buyer-form.js
+++ app/routes/events/view/edit/ticket-buyer-form.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import EventWizardMixin from 'open-event-frontend/mixins/event-wizard';
+import { A } from '@ember/array';
 
 export default Route.extend(EventWizardMixin, {
 
@@ -7,7 +8,41 @@ export default Route.extend(EventWizardMixin, {
     return this.get('l10n').t('Ticket Buyer Form');
   },
 
-  model() {
-    return this.modelFor('events.view.edit');
+  async model() {
+    let filterOptions = [{
+      name : 'form',
+      op   : 'eq',
+      val  : 'attendee'
+    }];
+
+    let data = {
+      event: this.modelFor('events.view')
+    };
+    data.customForms = await data.event.query('customForms', {
+      filter       : filterOptions,
+      sort         : 'id',
+      'page[size]' : 50
+    });
+
+    return data;
+  },
+  afterModel(data) {
+    /**
+     * Create the additional custom forms if only the compulsory forms exist.
+     */
+    if (data.customForms.length === 3) {
+      let customForms = A();
+      for (const customForm of data.customForms ? data.customForms.toArray() : []) {
+        customForms.pushObject(customForm);
+      }
+
+      const createdCustomForms = this.getCustomAttendeeForm(data.event);
+
+      for (const customForm of createdCustomForms ? createdCustomForms : []) {
+        customForms.pushObject(customForm);
+      }
+
+      data.customForms = customForms;
+    }
   }
 });
