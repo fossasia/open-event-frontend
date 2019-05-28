ecd5fb7b9a5af854c4b3055f5def8c0fdaa6173c
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-05-28 14:28:43 +0530

    fix ESLint errors

--- app/components/create-session-message.js
+++ app/components/create-session-message.js
@@ -3,9 +3,8 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
 
-  isMessageVisible : true,
-
-  shouldShowMessage: computed('session.isAuthenticated', 'isMessageVisible', 'isNewSpeaker', 'isNewSession', function() {
+  isMessageVisible  : true,
+  shouldShowMessage : computed('session.isAuthenticated', 'isMessageVisible', 'isNewSpeaker', 'isNewSession', function() {
     let speakerIDlength = this.data.userSpeaker.toArray().length;
     return this.session.isAuthenticated
           && this.isMessageVisible
