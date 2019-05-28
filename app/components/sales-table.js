6e08df4adcfd85481b8fb9711768f4ff250a2381
Kush Trivedi <kush@Kushs-MacBook-Pro.local>
2019-05-28 06:06:45 +0530

    Update query

--- app/components/sales-table.js
+++ app/components/sales-table.js
@@ -1,4 +1,9 @@
 import UiTable from 'open-event-frontend/components/ui-table-server';
+import Semantic from 'open-event-frontend/themes/semantic';
 
 export default UiTable.extend({
+  'pageSize'    : 10,
+  themeInstance : Semantic.create(),
+
+  useFilteringByColumns: false
 });
