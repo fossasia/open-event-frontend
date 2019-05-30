c8c4db6e89570c77a18f92abb7e2bd960487c52d
Saicharan Reddy <saicharan.reddy1@gmail.com>
2019-05-30 00:58:48 -0500

    Added logic for speakers length to fix rejected promise (#3040)

--- app/components/create-session-message.js
+++ app/components/create-session-message.js
@@ -5,7 +5,7 @@ export default Component.extend({
 
   isMessageVisible  : true,
   shouldShowMessage : computed('session.isAuthenticated', 'isMessageVisible', 'isNewSpeaker', 'isNewSession', function() {
-    let speakerIDlength = this.data.userSpeaker.toArray().length;
+    let speakerIDlength =  this.data.userSpeaker ? this.data.userSpeaker.toArray().length : 0;
     return this.session.isAuthenticated
           && this.isMessageVisible
           && !this.isNewSpeaker
