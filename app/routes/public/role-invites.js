987ca60bd23a3031278820f9e033727fdf85707d
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-06-11 12:48:42 +0530

    refactor the role acceptance flow

--- app/routes/public/role-invites.js
+++ app/routes/public/role-invites.js
@@ -1,51 +1,34 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  beforeModel(transition) {
-    let payload = {
-      data: {
-        token: transition.queryParams.token
-      }
+  async beforeModel(transition) {
+    const { token } = transition.to.queryParams;
+    const originalEventId = transition.resolvedModels.public.originalId;
+    const payload = {
+      data: { token }
     };
-    this.loader
-      .post('/role_invites/user', payload)
-      .then(user => {
-        if (this.get('session.isAuthenticated')) {
-          if (this.get('authManager.currentUser.email') === user.email) {
-            this.loader
-              .post('/role_invites/accept-invite', payload)
-              .then(invite => {
-                this.transitionTo('events.view', invite.event);
-              })
-              .catch(e => {
-                this.notify.error(this.l10n.t('An unexpected error has occurred'));
-                console.warn(e);
-              });
-          } else {
-            this.set('session.skipRedirectOnInvalidation', true);
-            this.session.invalidate();
-            this.transitionTo('register', {
-              queryParams: {
-                event       : `${transition.params.public.event_id}`,
-                inviteToken : `${transition.queryParams.token}`,
-                inviteEmail : `${user.email}`
-              }
-            });
 
-          }
-        } else {
-          this.transitionTo('register', {
-            queryParams: {
-              event       : `${transition.params.public.event_id}`,
-              inviteToken : `${transition.queryParams.token}`,
-              inviteEmail : `${user.email}`
-            }
-          });
-        }
-      })
-      .catch(e => {
-        this.notify.error(this.l10n.t('An unexpected error has occurred'));
-        console.warn(e);
-      });
+    const user = await this.loader.post('/role_invites/user', payload);
+
+    if (this.session.isAuthenticated) {
+
+      if (this.authManager.currentUser.email === user.email) {
+        const invite = await this.loader.post('/role_invites/accept-invite', payload);
+        return this.transitionTo(
+          ['organiser', 'coorganizer'].includes(invite.role) ? 'events.view' : 'public', invite.event
+        );
+      }
+
+      this.set('session.skipRedirectOnInvalidation', true);
+      this.session.invalidate();
+    }
+
+    this.transitionTo('register', {
+      queryParams: {
+        event       : originalEventId,
+        inviteToken : token,
+        inviteEmail : user.email
+      }
+    });
   }
 });
