a2c2b95f365bb58ff9b17f0eeeec6971fb95bb3e
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-06-12 23:29:07 +0530

    fix tests according to new routes

--- tests/acceptance/profile-test.js
+++ tests/acceptance/profile-test.js
@@ -7,13 +7,13 @@ module('Acceptance | profile', function(hooks) {
   setupApplicationTest(hooks);
 
   test('visiting settings/profile without login', async function(assert) {
-    await visit('/settings/profile');
+    await visit('/account/profile');
     assert.equal(currentURL(), '/login');
   });
 
   test('visiting settings/profile with login', async function(assert) {
     await login(assert);
-    await visit('/settings/profile');
-    assert.equal(currentURL(), '/settings/profile');
+    await visit('/account/profile');
+    assert.equal(currentURL(), '/account/profile');
   });
 });
