bf9adaf9cfafd95846248cfbd6d77430183c9383
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-06-23 13:50:57 +0530

    Reopen ember tables component

---
+++ app/ext/ember-table.js
@@ -0,0 +1,5 @@
+import component from 'ember-table/components/ember-table/component';
+import layout from 'open-event-frontend/templates/components/ember-table';
+component.reopen({
+  layout
+});
---
+++ app/initializers/ember-table.js
@@ -0,0 +1,9 @@
+import '../ext/ember-table';
+export function initialize(/* application */) {
+  // application.inject('route', 'foo', 'service:foo');
+}
+
+export default {
+  name: 'ember-table',
+  initialize
+};
