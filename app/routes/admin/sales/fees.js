59d61c062eb3f883a1aa0c218b718c65e240683f
Anupam Dagar <dagaranupam@gmail.com>
2019-06-28 01:42:16 +0530

    enh: Rename Fees to Revenue and add Event Date in Admin Sales Fees. Reference: #3173

--- app/routes/admin/sales/fees.js
+++ app/routes/admin/sales/fees.js
@@ -1,11 +1,23 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
   titleToken() {
-    return this.l10n.t('Fees');
+    return this.l10n.t('Revenue');
   },
 
   model() {
-    return this.store.findAll('admin-sales-fee');
+    return RSVP.hash({
+      orders : this.store.findAll('admin-sales-fee', { reload: true }).then(orders => orders.sortBy('name')),
+      events : this.store.findAll('event', { reload: true }).then(events => events.sortBy('name'))
+    });
+  },
+
+  afterModel(model) {
+    model.orders.forEach((data, index) => data.set('eventDate', model.events[index].startsAtDate));
+  },
+
+  setupController(controller, model) {
+    controller.set('orders', model.orders);
   }
 });
