d8b24599b5682cc827d5fcec84781a77f795d27d
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-06-30 10:12:48 +0530

    Search module

--- tests/integration/components/tables/headers/sort-test.js
+++ tests/integration/components/tables/headers/sort-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | tables/base', function(hooks) {
+module('Integration | Component | tables/headers/sort', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{tables/base}}`);
+    await render(hbs`{{tables/headers/sort}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#tables/base}}
+      {{#tables/headers/sort}}
         template block text
-      {{/tables/base}}
+      {{/tables/headers/sort}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
--- tests/integration/components/tables/utilities/search-box-test.js
+++ tests/integration/components/tables/utilities/search-box-test.js
@@ -7,20 +7,8 @@ module('Integration | Component | tables/utilities/search-box', function(hooks) 
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`{{tables/utilities/search-box}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#tables/utilities/search-box}}
-        template block text
-      {{/tables/utilities/search-box}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.ok(this.element.innerHTML.trim().includes('Search'));
   });
 });
