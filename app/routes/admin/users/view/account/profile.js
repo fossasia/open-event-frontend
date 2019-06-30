81033e82a999b7ad264fd788dfd8b0afc9fc0ed3
Shreyansh Dwivedi <shreyanshdwivedi1997@gmail.com>
2019-07-01 00:29:59 +0530

    fix: routes linked to admin/users

--- app/routes/admin/users/view/account/profile.js
+++ app/routes/admin/users/view/account/profile.js
@@ -1,5 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  templateName: 'admin/users/view/account/profile'
+  model() {
+    return this.modelFor('admin.users.view');
+  }
 });
