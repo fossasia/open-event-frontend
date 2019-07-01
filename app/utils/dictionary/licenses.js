40bf88ab1564fc718f70368dcfd7979bfcff74fe
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-07-02 02:47:03 +0530

    fix: Avoid using external links for license images

--- app/utils/dictionary/licenses.js
+++ app/utils/dictionary/licenses.js
@@ -3,25 +3,25 @@ export const licenses = [
     name     : 'Attribution-NonCommercial-NoDerivs',
     link     : 'https://creativecommons.org/licenses/by-nc-nd/4.0',
     imageSrc : '/images/copyright/attribution-nc-nd.png',
-    logoUrl  : 'https://licensebuttons.net/l/by-nc-nd/4.0/80x15.png'
+    logoUrl  : '/images/copyright/attribution-nc-nd-logo.png'
   },
   {
     name     : 'Attribution-NonCommercial',
     link     : 'https://creativecommons.org/licenses/by-nc/4.0',
     imageSrc : '/images/copyright/attribution-nc.png',
-    logoUrl  : 'https://licensebuttons.net/l/by-nc/4.0/80x15.png'
+    logoUrl  : '/images/copyright/attribution-nc-logo.png'
   },
   {
     name     : 'Public Domain Dedication (CC0)',
     link     : 'https://creativecommons.org/publicdomain/zero/1.0/',
     imageSrc : '/images/copyright/pdd.png',
-    logoUrl  : 'https://licensebuttons.net/p/zero/1.0/80x15.png'
+    logoUrl  : '/images/copyright/pdd-logo.png'
   },
   {
     name     : 'Attribution',
     link     : 'https://creativecommons.org/licenses/by/4.0',
     imageSrc : '/images/copyright/attribution.png',
-    logoUrl  : 'https://licensebuttons.net/l/by/4.0/80x15.png'
+    logoUrl  : '/images/copyright/attribution-logo.png'
   },
   {
     name     : 'All rights reserved',
@@ -33,24 +33,24 @@ export const licenses = [
     name     : 'Attribution-NoDerivs',
     link     : 'https://creativecommons.org/licenses/by-nd/4.0',
     imageSrc : '/images/copyright/attribution-nd.png',
-    logoUrl  : 'https://licensebuttons.net/l/by-nd/4.0/80x15.png'
+    logoUrl  : '/images/copyright/attribution-nd-logo.png'
   },
   {
     name     : 'Public Domain Work',
     link     : 'https://creativecommons.org/publicdomain/mark/1.0/',
     imageSrc : '/imagescopyright/pdw.png',
-    logoUrl  : 'https://licensebuttons.net/p/mark/1.0/80x15.png'
+    logoUrl  : '/images/copyright/pdw-logo.png'
   },
   {
     name     : 'Attribution-NonCommercial-ShareAlike',
     link     : 'https://creativecommons.org/licenses/by-nc-sa/4.0',
     imageSrc : '/imagescopyright/attribution-nc-sa.png',
-    logoUrl  : 'https://licensebuttons.net/l/by-nc-sa/4.0/80x15.png'
+    logoUrl  : '/images/copyright/attribution-nc-sa.png'
   },
   {
     name     : 'Attribution-ShareAlike',
     link     : 'https://creativecommons.org/licenses/by-sa/4.0',
     imageSrc : '/imagescopyright/attribution-sa.png',
-    logoUrl  : 'https://licensebuttons.net/l/by-sa/4.0/80x15.png'
+    logoUrl  : '/images/copyright/attribution-sa-logo.png'
   }
 ];
