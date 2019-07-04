5e9df87a8f2aef6f757277c1f3dc869e77350cb2
Shreyansh Dwivedi <shreyanshdwivedi1997@gmail.com>
2019-07-04 12:06:54 +0530

    enh: rearranges tabs in order and attendee view
    include pending tickets for admin/superadmin
    
    updates the condition

--- app/routes/events/view/tickets/attendees/index.js
+++ app/routes/events/view/tickets/attendees/index.js
@@ -3,6 +3,6 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   beforeModel() {
     this._super(...arguments);
-    this.transitionTo('events.view.tickets.attendees.list', 'all');
+    this.transitionTo('events.view.tickets.attendees.list', 'completed');
   }
 });
