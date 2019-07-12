35bc55a71a33ced97b79fdd4a5e80ec95e0bec2c
mrsaicharan1 <saicharan.reddy1@gmail.com>
2019-07-12 12:58:30 +0530

    Fixed maps on events & explore page

--- app/components/public/event-map.js
+++ app/components/public/event-map.js
@@ -1,5 +1,14 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['ui', 'stackable', 'grid']
+  classNames : ['ui', 'stackable', 'grid'],
+  actions    : {
+    onLocationChangeHandler(lat, lng) {
+      this.setProperties({
+        zoom: 17,
+        lat,
+        lng
+      });
+    }
+  }
 });
