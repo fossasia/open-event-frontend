5b29d39575327326ed399ecb408190a5a10c343b
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-07-12 20:27:47 +0530

    feat: Create user payment information form (#3215)
    * Create user payment information form
    
    * default reroute billing info to payment info
    
    * change route name to billing
    
    * use authManager.currentUser
    
    * follow ES6 patterns and work on suggestions
    
    * add loader in the form

--- tests/acceptance/payment-info-test.js
+++ tests/acceptance/payment-info-test.js
@@ -4,18 +4,18 @@ import { currentURL, visit } from '@ember/test-helpers';
 import { login } from 'open-event-frontend/tests/helpers/custom-helpers';
 
 
-module('Acceptance | account/billing-info/payment-info', function(hooks) {
+module('Acceptance | account/billing/payment-info', function(hooks) {
   setupApplicationTest(hooks);
 
 
-  test('visiting account/billing-info/payment-info without login', async function(assert) {
-    await visit('account/billing-info/payment-info');
+  test('visiting account/billing/payment-info without login', async function(assert) {
+    await visit('account/billing/payment-info');
     assert.equal(currentURL(), '/login');
   });
 
-  test('visiting account/billing-info/payment-info with login', async function(assert) {
+  test('visiting account/billing/payment-info with login', async function(assert) {
     await login(assert);
-    await visit('/account/billing-info/payment-info');
-    assert.equal(currentURL(), '/account/billing-info/payment-info');
+    await visit('/account/billing/payment-info');
+    assert.equal(currentURL(), '/account/billing/payment-info');
   });
 });
