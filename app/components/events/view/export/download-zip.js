99f3bca4fe6f021fed3150fec93c1df174798370
mrsaicharan1 <saicharan.reddy1@gmail.com>
2019-07-19 10:54:09 +0530

    Fix export events module(URL fetch logic)

--- app/components/events/view/export/download-zip.js
+++ app/components/events/view/export/download-zip.js
@@ -1,4 +1,27 @@
 import Component from '@ember/component';
+import { action } from '@ember/object';
 
-export default Component.extend({
-});
+export default class extends Component {
+    @action
+  exportEventDownload(eventDownloadUrl) {
+    this.set('isLoading', true);
+    this.loader
+      .load(`${eventDownloadUrl}`, { withoutPrefix: true })
+      .then(res => {
+        const anchor = document.createElement('a');
+        anchor.style.display = 'none';
+        anchor.href = URL.createObjectURL(new Blob([res], { type: 'octet/stream' }));
+        anchor.download = 'EventExport.zip';
+        anchor.click();
+        this.notify.success(this.l10n.t('Exported Event Downloaded successfully.'));
+      })
+      .catch(e => {
+        console.warn(e);
+        this.notify.error(this.l10n.t('Unexpected error occurred.'));
+      })
+      .finally(() => {
+        this.set('isLoading', false);
+      });
+  }
+
+}
