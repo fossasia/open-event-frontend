4c6aa2d52371beb99be60195382c44850abb0971
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-07-21 07:49:58 +0800

    feat: ember table for admin notificaition logs

--- app/routes/admin/reports/system-logs/notification-logs.js
+++ app/routes/admin/reports/system-logs/notification-logs.js
@@ -1,15 +1,23 @@
 import Route from '@ember/routing/route';
+import EmberTableRouteMixin from 'open-event-frontend/mixins/ember-table-route';
 
-export default Route.extend({
+export default class extends Route.extend(EmberTableRouteMixin) {
   titleToken() {
     return this.l10n.t('Notification Logs');
-  },
-
-  model() {
-    return this.store.query('notification', {
-      include      : 'user',
-      'page[size]' : 100,
-      sort         : '-received-at'
-    });
   }
-});
+
+
+  async  model(params) {
+    const searchField = 'title';
+    let filterOptions = this.applySearchFilters([], params, searchField);
+
+    let queryString = {
+      include        : 'user',
+      filter         : filterOptions,
+      'page[size]'   : params.per_page || 100,
+      'page[number]' : params.page || 1
+    };
+    queryString = this.applySortFilters(queryString, params);
+    return this.asArray(this.store.query('notification', queryString));
+  }
+}
