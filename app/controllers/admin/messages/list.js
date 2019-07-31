83d8284d5af0ee64ef195069e370f28238291ecd
kushthedude <kushthedude@gmail.com>
2019-07-31 23:24:45 +0530

    Introducing Ember Tables in Admin/Messages

--- app/controllers/admin/messages/list.js
+++ app/controllers/admin/messages/list.js
@@ -1,36 +1,54 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import EmberTableControllerMixin from 'open-event-frontend/mixins/ember-table-controller';
 
-export default Controller.extend({
-  columns: [
-    {
-      propertyName : 'recipient',
-      title        : 'Recipients'
-    },
-    {
-      propertyName : 'action',
-      title        : 'Trigger'
-    },
-    {
-      subject  : 'emailSubject',
-      message  : 'emailMessage',
-      title    : 'Email Message',
-      template : 'components/ui-table/cell/cell-title-message'
-    },
-    {
-      subject  : 'notificationTitle',
-      message  : 'notificationMessage',
-      title    : 'Notification Message',
-      template : 'components/ui-table/cell/cell-title-message'
-    },
-    {
-      title    : 'Options',
-      template : 'components/ui-table/cell/admin/messages/cell-options'
-    },
-    {
-      propertyName : 'sentAt',
-      title        : 'Time/Date sent out',
-      template     : 'components/ui-table/cell/cell-simple-date',
-      dateFormat   : 'MMMM DD, YYYY - HH:mm A'
-    }
-  ]
-});
+export default class extends Controller.extend(EmberTableControllerMixin) {
+	@computed()
+	get columns() {
+		return [
+		{
+			name : 'Recipients',
+			valuePath : 'recipient'
+		},
+		{
+			name : 'Trigger',
+			valuePath : 'action'
+		},
+		{
+			name : 'Email Message',
+			valuePath : 'emailMessage',
+		
+			cellComponent : 'ui-table/cell/cell-title-message',
+			options : {
+				subject : 'emailSubject',
+				message : 'emailMessage'
+			}
+		},
+		{
+			name : 'Notification Message',
+			valuePath : 'notificationMessage',
+      cellComponent : 'ui-table/cell/cell-title-message',
+      options : {
+      	subject  : 'notificationSubject',
+      	message  : 'notificationMessage'
+      }
+		},
+		{
+			name : 'Options',
+			valuePath : 'option',
+			cellComponent : 'ui-table/cell/admin/messages/cell-options'
+		},
+		{
+			name : 'Time/Date Sent Out',
+			valuePath : 'sentAt',
+			cellComponent : 'ui-table/cell/cell-simple-date',
+			options  :  {
+				dateFormat: 'MMMM DD, YYYY - HH:mm A'
+			}
+		}
+
+		];
+	}
+}
+  
+  
--- app/routes/admin/messages/list.js
+++ app/routes/admin/messages/list.js
@@ -1,7 +1,32 @@
 import Route from '@ember/routing/route';
+import EmberTableRouteMixin from 'open-event-frontend/mixins/ember-table-route';
 
-export default Route.extend({
-  model() {
-    return this.modelFor('admin.messages');
+export default class extends Route.extend(EmberTableRouteMixin) {
+
+  async model(params) {
+    return this.asArray(this.modelFor('admin.messages'));
   }
-});
+
+}
+// }
+// import Route from '@ember/routing/route';
+// import EmberTableRouteMixin from 'open-event-frontend/mixins/ember-table-route';
+
+// export default class extends Route.extend(EmberTableRouteMixin) {
+//   titleToken() {
+//     return this.l10n.t('Mail Logs');
+//   }
+
+//   async model(params) {
+//     const searchField = 'recipient';
+//     let filterOptions = this.applySearchFilters([], params, searchField);
+
+//     let queryString = {
+//       filter         : filterOptions,
+//       'page[size]'   : params.per_page || 100,
+//       'page[number]' : params.page || 1
+//     };
+//     queryString = this.applySortFilters(queryString, params);
+//     return this.asArray(this.store.query('mail', queryString));
+//   }
+// }
