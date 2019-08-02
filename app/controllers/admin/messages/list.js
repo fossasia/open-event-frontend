4cc28905c2910fcdc6ad3a63d41e0e346f267eff
kushthedude <kushthedude@gmail.com>
2019-08-02 18:09:37 +0530

    Final Changes

--- app/controllers/admin/messages/list.js
+++ app/controllers/admin/messages/list.js
@@ -3,52 +3,63 @@ import { computed } from '@ember/object';
 import EmberTableControllerMixin from 'open-event-frontend/mixins/ember-table-controller';
 
 export default class extends Controller.extend(EmberTableControllerMixin) {
-	@computed()
-	get columns() {
-		return [
-		{
-			name : 'Recipients',
-			valuePath : 'recipient'
-		},
-		{
-			name : 'Trigger',
-			valuePath : 'action'
-		},
-		{
-			name : 'Email Message',
-			valuePath : 'emailMessage',
-		
-			cellComponent : 'ui-table/cell/cell-title-message',
-			options : {
-				subject : 'emailSubject',
-				message : 'emailMessage'
-			}
-		},
-		{
-			name : 'Notification Message',
-			valuePath : 'notificationMessage',
-      cellComponent : 'ui-table/cell/cell-title-message',
-      options : {
-      	subject  : 'notificationSubject',
-      	message  : 'notificationMessage'
-      }
-		},
-		{
-			name : 'Options',
-			valuePath : 'option',
-			cellComponent : 'ui-table/cell/admin/messages/cell-options'
-		},
-		{
-			name : 'Time/Date Sent Out',
-			valuePath : 'sentAt',
-			cellComponent : 'ui-table/cell/cell-simple-date',
-			options  :  {
-				dateFormat: 'MMMM DD, YYYY - HH:mm A'
-			}
-		}
+per_page = 100;
 
-		];
-	}
+sort_by = 'time';
+
+sort_dir = 'ASC';
+
+@computed()
+get columns() {
+  return [
+    {
+      name            : 'Recipients',
+      valuePath       : 'recipient',
+      headerComponent : 'tables/headers/sort',
+      isSortable      : true
+    },
+    {
+      name      : 'Trigger',
+      valuePath : 'action'
+    },
+    {
+      name      : 'Email Message',
+      valuePath : 'emailMessage',
+
+      cellComponent   : 'ui-table/cell/cell-title-message',
+      extraValuePaths : ['emailSubject', 'emailMessage'],
+      options         : {
+        subject : 'emailSubject',
+        message : 'emailMessage'
+      }
+    },
+    {
+      name            : 'Notification Message',
+      valuePath       : 'notificationMessage',
+      cellComponent   : 'ui-table/cell/cell-title-message',
+      extraValuePaths : ['notificationTitle', 'notificationMessage'],
+      options         : {
+        subject : 'notificationTitle',
+        message : 'notificationMessage'
+      }
+    },
+    {
+      name          : 'Options',
+      valuePath     : 'option',
+      cellComponent : 'ui-table/cell/admin/messages/cell-options'
+    },
+    {
+      name            : 'Time/Date Sent Out',
+      valuePath       : 'sentAt',
+      headerComponent : 'tables/headers/sort',
+      isSortable      : true,
+      cellComponent   : 'ui-table/cell/cell-simple-date',
+      options         : {
+        dateFormat: 'MMMM DD, YYYY - HH:mm A'
+      }
+    }
+
+  ];
 }
-  
-  
+}
+
--- app/routes/admin/messages/list.js
+++ app/routes/admin/messages/list.js
@@ -4,29 +4,15 @@ import EmberTableRouteMixin from 'open-event-frontend/mixins/ember-table-route';
 export default class extends Route.extend(EmberTableRouteMixin) {
 
   async model(params) {
-    return this.asArray(this.modelFor('admin.messages'));
+    const searchField = 'recipient';
+    let filterOptions = this.applySearchFilters([], params, searchField);
+    let queryString = {
+      filter        : filterOptions,
+      'page[size]'  : params.per_page || 100,
+      'page[number' : params.page || 1
+    };
+    queryString = this.applySortFilters(queryString, params);
+    return this.asArray(this.modelFor('admin.messages', queryString));
   }
 
 }
-// }
-// import Route from '@ember/routing/route';
-// import EmberTableRouteMixin from 'open-event-frontend/mixins/ember-table-route';
-
-// export default class extends Route.extend(EmberTableRouteMixin) {
-//   titleToken() {
-//     return this.l10n.t('Mail Logs');
-//   }
-
-//   async model(params) {
-//     const searchField = 'recipient';
-//     let filterOptions = this.applySearchFilters([], params, searchField);
-
-//     let queryString = {
-//       filter         : filterOptions,
-//       'page[size]'   : params.per_page || 100,
-//       'page[number]' : params.page || 1
-//     };
-//     queryString = this.applySortFilters(queryString, params);
-//     return this.asArray(this.store.query('mail', queryString));
-//   }
-// }
