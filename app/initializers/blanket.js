17d201c4d815a02e3e9862234e27393136c51c62
Abhinav Khare <abhinav.khare31@gmail.com>
2019-08-13 16:44:44 -0400

    feat: implement a method for reloading model for tables (#3403)

--- app/initializers/blanket.js
+++ app/initializers/blanket.js
@@ -20,6 +20,7 @@ export function initialize(application) {
   inject('settings', 'service:settings');
   inject('fastboot', 'service:fastboot');
   inject('routing', 'service:-routing');
+  inject('router', 'service:router');
   inject('cookies', 'service:cookies');
   inject('infinity', 'service:infinity');
   application.inject('component', 'router', 'service:router');
