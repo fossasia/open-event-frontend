f51983a366b838797646b4e83c81af5d547ccbfe
kushthedude <kushthedude@gmail.com>
2019-08-17 04:14:13 +0530

    Using ember tables for Admin/Sales/Invoices

--- app/controllers/admin/sales/invoices.js
+++ app/controllers/admin/sales/invoices.js
@@ -1,37 +1,47 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import EmberTableControllerMixin from 'open-event-frontend/mixins/ember-table-controller';
 
-export default Controller.extend({
-  columns: [
-    {
-      propertyName : 'identifier',
-      title        : 'Invoice #'
-    },
-    {
-      propertyName     : 'event.name',
-      title            : 'Event',
-      disableSorting   : true,
-      disableFiltering : true
 
-    },
-    {
-      propertyName : 'created-at',
-      template     : 'components/ui-table/cell/admin/sales/status/cell-dated',
-      title        : 'Dated'
-    },
-    {
-      propertyName : 'amount',
-      template     : 'components/ui-table/cell/admin/sales/status/cell-amount',
-      title        : 'Amount'
-    },
-    {
-      propertyName     : 'user.email',
-      title            : 'Sent To',
-      disableSorting   : true,
-      disableFiltering : true
-    },
-    {
-      propertyName : 'status',
-      title        : 'Status'
-    }
-  ]
-});
+export default class extends Controller.extend(EmberTableControllerMixin) {
+
+  @computed()
+  get columns() {
+    return [
+      {
+        name            : 'Invoice #',
+        headerComponent : 'tables/headers/sort',
+        isSortable      : true,
+        valuePath       : 'identifier'
+      },
+      {
+        name      : 'Event',
+        valuePath : 'event.name'
+      },
+      {
+        name            : 'Dated',
+        valuePath       : 'created-at',
+        headerComponent : 'tables/headers/sort',
+        isSortable      : true,
+        cellComponent   : 'ui-table/cell/cell-simple-date',
+        options         : {
+          dateFormat: 'MMMM DD, YYYY - HH:mm A'
+        }
+      },
+      {
+        name          : 'Amount',
+        valuePath     : 'amount',
+        cellComponent : 'ui-table/cell/admin/sales/status/cell-amount'
+      },
+      {
+        name      : 'Sent To',
+        valuePath : 'user.email'
+      },
+      {
+        name      : 'Status',
+        valuePath : 'status'
+      }
+    ];
+  }
+}
+
