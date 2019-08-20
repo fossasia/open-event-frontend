478a48e5b035bd395d802f5a7e50ff00d2185bf4
kushthedude <kushthedude@gmail.com>
2019-08-20 13:39:26 +0530

    Using ember table for order

--- app/controllers/events/view/tickets/add-order.js
+++ app/controllers/events/view/tickets/add-order.js
@@ -1,98 +1,120 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
+import { computed, action } from '@ember/object';
 import { sumBy } from 'lodash-es';
+import EmberTableControllerMixin from 'open-event-frontend/mixins/ember-table-controller';
 
-export default Controller.extend({
-  hasTicketsInOrder: computed('model.tickets.@each.orderQuantity', function() {
+export default class extends Controller.extend(EmberTableControllerMixin) {
+
+  @computed('model.tickets.@each.orderQuantity')
+  get hasTicketsInOrder() {
     return sumBy(this.get('model.tickets').toArray(),
       ticket => ticket.getWithDefault('orderQuantity', 0)
     ) > 0;
-  }),
-  total: computed('model.tickets.@each.orderQuantity', function() {
+  }
+
+
+  @computed('model.tickets.@each.orderQuantity')
+  get total() {
     let sum = 0.0;
     this.get('model.tickets').forEach(ticket => {
       sum += ticket.get('orderQuantity') * ticket.get('price');
     });
     return sum;
-  }),
-  columns: [
-    {
-      propertyName : 'name',
-      title        : 'Ticket Type'
-    },
-    {
-      propertyName : 'price',
-      title        : 'Price',
-      template     : 'components/ui-table/cell/events/view/tickets/cell-add-order-price'
-    },
-    {
-      propertyName : 'quantity',
-      title        : 'Quantity',
-      template     : 'components/ui-table/cell/events/view/tickets/cell-add-order-quantity'
-    },
-    {
-      propertyName : 'itemTotal',
-      title        : 'Item Total',
-      template     : 'components/ui-table/cell/events/view/tickets/cell-add-order-total'
-    }
-  ],
+  }
 
-  actions: {
-    updateOrder(ticket, count) {
-      let order = this.get('model.order');
-      ticket.set('orderQuantity', count);
-      order.set('amount', this.total);
-      if (!this.total) {
-        order.set('amount', 0);
+  @computed()
+  get columns() {
+    return [
+      {
+        name      : 'Ticket Type',
+        valuePath : 'name'
+      },
+      {
+        name            : 'Price',
+        valuePath       : 'price',
+        extraValuePaths : ['event'],
+        cellComponent   : 'ui-table/cell/events/view/tickets/cell-add-order-price'
+      },
+      {
+        name            : 'Quantity',
+        valuePath       : 'quantity',
+        extraValuePaths : ['maxOrder', 'minOrder', 'orderQuantity', 'id'],
+        cellComponent   : 'ui-table/cell/events/view/tickets/cell-add-order-quantity',
+        actions         : {
+          updateOrder: this.updateOrder.bind(this)
+        }
+      },
+      {
+        name            : 'Item Total',
+        valuePath       : 'orderQuantity',
+        extraValuePaths : ['price', 'event'],
+        cellComponent   : 'ui-table/cell/events/view/tickets/cell-add-order-total'
       }
-      if (count > 0) {
-        order.tickets.addObject(ticket);
-      } else {
-        if (order.tickets.includes(ticket)) {
-          order.tickets.removeObject(ticket);
-        }
-      }
-    },
-    async placeOrder() {
-      this.set('isLoading', true);
-      let order = this.get('model.order');
-      let event = order.get('event');
-      order.tickets.forEach(ticket => {
-        let numberOfAttendees = ticket.orderQuantity;
-        while (numberOfAttendees--) {
-          this.get('model.attendees').addObject(this.store.createRecord('attendee', {
-            firstname : 'John',
-            lastname  : 'Doe',
-            email     : 'johndoe@example.com',
-            event,
-            ticket
-          }));
-        }
-      });
-      try {
-        let order = this.get('model.order');
-        let attendees = this.get('model.attendees');
-        for (const attendee of attendees ? attendees.toArray() : []) {
-          await attendee.save();
-        }
-        order.set('attendees', attendees.slice());
-        await order.save()
-          .then(order => {
-            this.notify.success(this.l10n.t('Order details saved. Please fill further details within 10 minutes.'));
-            this.transitionToRoute('orders.new', order.identifier);
-          })
-          .catch(async() => {
-            for (const attendee of attendees ? attendees.toArray() : []) {
-              await attendee.destroyRecord();
-            }
-            this.notify.error(this.l10n.t('Oops something went wrong. Please try again'));
-          })
-          .finally(() => {
-            this.set('isLoading', false);
-          });
-      } catch (e) {
-        this.notify.error(this.l10n.t('Oops something went wrong. Please try again'));
+
+    ];
+  }
+
+  @action
+  updateOrder(ticket, count) {
+    let order = this.get('model.order');
+    ticket.set('orderQuantity', count);
+    order.set('amount', this.total);
+    if (!this.total) {
+      order.set('amount', 0);
+    }
+
+    if (count > 0) {
+      order.tickets.addObject(ticket);
+    } else {
+      if (order.tickets.includes(ticket)) {
+        order.tickets.removeObject(ticket);
       }
     }
   }
-});
+
+    @action
+  async placeOrder() {
+    this.set('isLoading', true);
+    let order = this.get('model.order');
+    let event = order.get('event');
+    order.tickets.forEach(ticket => {
+      let numberOfAttendees = ticket.orderQuantity;
+      while (numberOfAttendees--) {
+        this.get('model.attendees').addObject(this.store.createRecord('attendee', {
+          firstname : 'John',
+          lastname  : 'Doe',
+          email     : 'johndoe@example.com',
+          event,
+          ticket
+        }));
+      }
+    });
+    try {
+      let order = this.get('model.order');
+      let attendees = this.get('model.attendees');
+      for (const attendee of attendees ? attendees.toArray() : []) {
+        await attendee.save();
+      }
+
+      order.set('attendees', attendees.slice());
+      await order.save()
+        .then(order => {
+          this.notify.success(this.l10n.t('Order details saved. Please fill further details within 10 minutes.'));
+          this.transitionToRoute('orders.new', order.identifier);
+        })
+        .catch(async() => {
+          for (const attendee of attendees ? attendees.toArray() : []) {
+            await attendee.destroyRecord();
+          }
+
+          this.notify.error(this.l10n.t('Oops something went wrong. Please try again'));
+        })
+        .finally(() => {
+          this.set('isLoading', false);
+        });
+    } catch (e) {
+      this.notify.error(this.l10n.t('Oops something went wrong. Please try again'));
+    }
+  }
+}
+
--- app/routes/events/view/tickets/add-order.js
+++ app/routes/events/view/tickets/add-order.js
@@ -1,15 +1,25 @@
 import Route from '@ember/routing/route';
+import EmberTableRouteMixin from 'open-event-frontend/mixins/ember-table-route';
 
-export default Route.extend({
+export default class extends Route.extend(EmberTableRouteMixin) {
   titleToken() {
     return this.l10n.t('Add Order');
-  },
-  async model() {
+  }
+
+  async model(params) {
     const eventDetails = this.modelFor('events.view');
-    let queryObject = {};
+    const searchField = 'name';
+    let filterOptions = [];
+    filterOptions = this.applySearchFilters(filterOptions, params, searchField);
+    let queryString = {
+      filter         : filterOptions,
+      'page[size]'   : params.per_page || 10,
+      'page[number]' : params.per_page || 1
+    };
+    queryString = this.applySortFilters(queryString, params);
     return {
-      tickets    : await eventDetails.query('tickets', queryObject),
-      query      : queryObject,
+      tickets    : await this.asArray(eventDetails.query('tickets', queryString)),
+      // query      : queryObject,
       store      : eventDetails,
       objectType : 'tickets',
 
@@ -22,13 +32,15 @@ export default Route.extend({
 
       attendees: []
     };
-  },
+  }
+
   afterModel(model) {
     let { tickets } = model;
     tickets.forEach(ticket => {
       ticket.set('orderQuantity', 0);
     });
-  },
+  }
+
   resetController(controller) {
     this._super(...arguments);
     const model = controller.get('model.order');
@@ -36,4 +48,4 @@ export default Route.extend({
       model.unloadRecord();
     }
   }
-});
+}
