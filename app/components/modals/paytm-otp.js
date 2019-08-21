708f85cfc645730b5b7b87e0129ca5023d1fbb5f
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-08-21 09:06:20 +0530

    feat: add paytm checkout modals

---
+++ app/components/modals/paytm-otp.js
@@ -0,0 +1,5 @@
+import ModalBase from 'open-event-frontend/components/modals/modal-base';
+
+export default class extends ModalBase {
+  isSmall = true;
+}
---
+++ app/components/modals/paytm-payment-options.js
@@ -0,0 +1,7 @@
+import ModalBase from 'open-event-frontend/components/modals/modal-base';
+
+export default class extends ModalBase {
+  isSmall = true;
+  isWalletSelected = false;
+  mobileNumber = '';
+}
