9035566a18dd1705063d04305d91a7ae0bf0fd10
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-08-21 19:13:45 +0530

    feat: add validations to modals

--- app/components/modals/paytm-payment-options.js
+++ app/components/modals/paytm-payment-options.js
@@ -1,7 +1,33 @@
 import ModalBase from 'open-event-frontend/components/modals/modal-base';
+import FormMixin from 'open-event-frontend/mixins/form';
+import { validPhoneNumber } from 'open-event-frontend/utils/validators';
 
-export default class extends ModalBase {
+export default class extends ModalBase.extend(FormMixin) {
   isSmall = true;
   isWalletSelected = false;
-  mobileNumber = '';
+
+  getValidationRules() {
+    return {
+      inline : true,
+      delay  : false,
+      on     : 'blur',
+      fields : {
+        mobileNumber: {
+          identifier : 'mobile_number',
+          rules      : [
+            {
+              type   : 'empty',
+              prompt : this.l10n.t('Please enter the Mobile Number')
+            },
+            {
+              type   : 'regExp',
+              value  : validPhoneNumber,
+              prompt : this.l10n.t('Please enter a valid mobile number')
+            }
+          ]
+        }
+      }
+    };
+  }
+
 }
