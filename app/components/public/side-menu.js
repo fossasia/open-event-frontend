39cfd8de4fd5ab8e2fe10c4518e663084139220d
Geet Choudhary <geetchoudhary@gmail.com>
2019-10-16 15:15:43 +0530

    fix highlighting issue

--- app/components/public/side-menu.js
+++ app/components/public/side-menu.js
@@ -3,11 +3,17 @@ import { computed } from '@ember/object';
 import moment from 'moment';
 
 export default Component.extend({
+  selectedIndex: undefined,
   async didInsertElement() {
     this._super(...arguments);
     let speakersCall = await this.get('event.speakersCall');
     this.set('shouldShowCallforSpeakers',
       speakersCall && speakersCall.announcement && (speakersCall.privacy === 'public'));
+  },
+  actions: {
+    changeSelectedIndex(index) {
+      this.set('selectedIndex', index);
+    }
   },
   isSchedulePublished: computed('event.schedulePublishedOn', function() {
     return this.get('event.schedulePublishedOn') && this.get('event.schedulePublishedOn').toISOString() !== moment(0).toISOString();
