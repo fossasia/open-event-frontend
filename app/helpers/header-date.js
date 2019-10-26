845a26648dd8b17c5ab5e8baee8cb894f34da906
Sai Rithwik <rithwiksaim@gmail.com>
2019-10-26 11:42:25 +0530

    Updated timezone format from +hhmm to +hh:mm UTC

--- app/helpers/header-date.js
+++ app/helpers/header-date.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 
 export function headerDate(params) {
   let timezone = params[1] ? params[1] : moment.tz.guess();
-  return `${moment(params[0]).tz(timezone).format('dddd, MMMM Do YYYY, h:mm A')} (${moment.tz(params[0], timezone).zoneAbbr()})`;
+  return `${moment(params[0]).tz(timezone).format('dddd, MMMM Do YYYY, hh:mm')} (${moment.tz(params[0], timezone).format('Z')} UTC)`;
 }
 
 export default Helper.helper(headerDate);
