3e6faf34465bce003223874ae129e8df64247509
kushthedude <kushthedude@gmail.com>
2019-11-06 04:59:34 +0530

    test travis

--- tests/helpers/custom-helpers.js
+++ tests/helpers/custom-helpers.js
@@ -2,9 +2,7 @@ import { click, fillIn, currentURL, visit, settled } from '@ember/test-helpers';
 import { currentSession } from 'ember-simple-auth/test-support';
 
 export async function login(assert, email = null, password = null, gotoLoginPage = true) {
-  if (gotoLoginPage) {
-    await visit('/login');
-  }
+  await visit('/login');
   assert.equal(currentURL(), '/login');
   await fillIn('input[name=email]', email !== null ? email : 'opev-fe@test.com');
   await fillIn('input[name=password]', password !== null ? password : 'test-fe-user');
