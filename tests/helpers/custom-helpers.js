dfb8f9d6c86b296cd859985d391ef102c78413ed
Kush Trivedi <kushthedude@gmail.com>
2019-11-06 10:59:56 +0530

    Update custom-helpers.js

--- tests/helpers/custom-helpers.js
+++ tests/helpers/custom-helpers.js
@@ -4,9 +4,9 @@ import { currentSession } from 'ember-simple-auth/test-support';
 export async function login(assert, email = null, password = null, gotoLoginPage = true) {
 
   await settled();
-  if (gotoLoginPage) {
-    await visit('/login');
-  }
+
+  await visit('/login');
+  
   assert.equal(currentURL(), '/login');
   await fillIn('input[name=email]', email !== null ? email : 'opev-fe@test.com');
   await fillIn('input[name=password]', password !== null ? password : 'test-fe-user');
