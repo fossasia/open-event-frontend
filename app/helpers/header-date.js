66bc366544484af9e199a98f6f32b7c5db0f9917
kushthedude <kushthedude@gmail.com>
2019-11-08 15:25:04 +0530

    Test fixing

--- app/helpers/header-date.js
+++ app/helpers/header-date.js
@@ -2,6 +2,7 @@ import Helper from '@ember/component/helper';
 import moment from 'moment';
 
 export function headerDate(params) {
+  let [ timezone ] = params;
   let timezone = params[1];
   return `${moment(params[0]).format('dddd, MMMM Do YYYY, h:mm A')} (${moment.tz(params[0], timezone).zoneAbbr()})`;
 }
