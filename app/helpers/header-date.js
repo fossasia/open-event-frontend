c34c0bee1913c4cd1c309c2039883b68bf1ebee1
kushthedude <kushthedude@gmail.com>
2019-11-08 14:45:11 +0530

    Fixing TimeZone issues on Wizard

--- app/helpers/header-date.js
+++ app/helpers/header-date.js
@@ -2,8 +2,8 @@ import Helper from '@ember/component/helper';
 import moment from 'moment';
 
 export function headerDate(params) {
-  let timezone = params[1] ? params[1] : moment.tz.guess();
-  return `${moment(params[0]).tz(timezone).format('dddd, MMMM Do YYYY, h:mm A')} (${moment.tz(params[0], timezone).zoneAbbr()})`;
+  let timezone = params[1];
+  return `${moment(params[0]).format('dddd, MMMM Do YYYY, h:mm A')} (${moment.tz(params[0], timezone).zoneAbbr()})`;
 }
 
 export default Helper.helper(headerDate);
