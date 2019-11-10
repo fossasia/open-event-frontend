dbe378e960575832fdd15827b0cb4807d15dc7e3
kushthedude <kushthedude@gmail.com>
2019-11-10 16:55:09 +0530

    Reverting ES6 migration

--- app/controllers/my-sessions/view.js
+++ app/controllers/my-sessions/view.js
@@ -1,38 +1,34 @@
 import Controller from '@ember/controller';
-import { computed, action } from '@ember/object';
+import { computed } from '@ember/object';
 import moment from 'moment';
 
-export default class extends Controller {
-
-  @computed('model.endsAt')
-  get isUpcoming() {
+export default Controller.extend({
+  isUpcoming: computed('model.endsAt', function() {
     let endAt = this.get('model.endsAt');
     if (endAt < moment()) {
       return false;
     }
     return true;
-  }
+  }),
 
-  @action
-  openProposalDeleteModal() {
-    this.set('isProposalDeleteModalOpen', true);
+  actions: {
+    openProposalDeleteModal() {
+      this.set('isProposalDeleteModalOpen', true);
+    },
+    deleteProposal() {
+      this.set('isLoading', true);
+      this.model.destroyRecord()
+        .then(() => {
+          this.transitionToRoute('my-sessions.index');
+          this.notify.success(this.l10n.t('Proposal has been deleted successfully.'));
+        })
+        .catch(() => {
+          this.notify.error(this.l10n.t('An unexpected error has occurred.'));
+        })
+        .finally(() => {
+          this.set('isLoading', false);
+          this.set('isProposalDeleteModalOpen', false);
+        });
+    }
   }
-
-  @action
-  deleteProposal() {
-    this.set('isLoading', true);
-    this.model.destroyRecord()
-      .then(() => {
-        this.transitionToRoute('my-sessions.index');
-        this.notify.success(this.l10n.t('Proposal has been deleted successfully.'));
-      })
-      .catch(() => {
-        this.notify.error(this.l10n.t('An unexpected error has occurred.'));
-      })
-      .finally(() => {
-        this.set('isLoading', false);
-        this.set('isProposalDeleteModalOpen', false);
-      });
-  }
-}
-
+});
