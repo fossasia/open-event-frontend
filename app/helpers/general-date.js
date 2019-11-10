baa455b898966e711861f3927eba2fe37b284bd4
kushthedude <kushthedude@gmail.com>
2019-11-10 20:34:27 +0530

    Fixing Date and TimeZone Issues in Events

---
+++ app/helpers/general-date.js
@@ -0,0 +1,9 @@
+import Helper from '@ember/component/helper';
+import moment from 'moment';
+
+export function headerDate(params) {
+  let timezone = params[1] ? params[1] : moment.tz.guess();
+  return `${moment(params[0]).tz(timezone).format('dddd, MMMM Do YYYY, h:mm A')}`;
+}
+
+export default Helper.helper(headerDate);
