c10247bf72a16fd15957525e8f869247ec9c4752
kushthedude <kushthedude@gmail.com>
2019-11-20 13:37:01 +0530

    Updating date format and fixing tests

--- app/helpers/general-date.js
+++ app/helpers/general-date.js
@@ -1,9 +1,9 @@
 import Helper from '@ember/component/helper';
 import moment from 'moment';
 
-export function headerDate(params) {
+export function generalDate(params) {
   let timezone = params[1] ? params[1] : moment.tz.guess();
-  return `${moment(params[0]).tz(timezone).format('dddd, MMMM Do YYYY, h:mm A')}`;
+  return `${moment(params[0]).tz(timezone).format('h:mm A , MMMM Do YYYY')} (${moment.tz(params[0], timezone).zoneAbbr()})`;
 }
 
-export default Helper.helper(headerDate);
+export default Helper.helper(generalDate);
--- tests/integration/helpers/general-date-test.js
+++ tests/integration/helpers/general-date-test.js
@@ -4,13 +4,13 @@ import hbs from 'htmlbars-inline-precompile';
 import { render } from '@ember/test-helpers';
 import moment from 'moment';
 
-module('Integration | Helper | header date', function(hooks) {
+module('Integration | Helper | general date', function(hooks) {
   setupIntegrationTest(hooks);
 
   test('it renders', async function(assert) {
-    this.set('inputMomentValue', moment('2019-04-30T18:30:00+00:00'));
+    this.set('inputMomentValue', moment('2019-05-01T03:30:00+09:00'));
     this.set('inputTimezoneValue', 'Japan');
-    await render(hbs`{{header-date inputMomentValue inputTimezoneValue}}`);
-    assert.equal(this.element.innerHTML.trim(), 'Wednesday, May 1st 2019, 3:30 AM');
+    await render(hbs`{{general-date inputMomentValue inputTimezoneValue}}`);
+    assert.equal(this.element.innerHTML.trim(), '3:30 AM , May 1st 2019 (JST)');
   });
 });
