a291cbe6e56e490f301de573d347417d6099dea6
Kush Trivedi <kushthedude@gmail.com>
2019-11-21 00:10:27 +0530

    fix: TimeZone issues on Wizard (#3611)
    * Fixing TimeZone issues on Wizard
    
    * Test fixing
    
    * Test fixing
    
    * Test fixing
    
    * Fixing Date and TimeZone Issues in Events
    
    * Fixing Date and TimeZone Issues in Events
    
    * Updating date format and fixing tests

--- app/controllers/admin/events/list.js
+++ app/controllers/admin/events/list.js
@@ -35,22 +35,18 @@ export default class extends Controller.extend(EmberTableControllerMixin) {
          valuePath       : 'startsAt',
          isSortable      : true,
          headerComponent : 'tables/headers/sort',
+         extraValuePaths : ['timezone'],
          cellComponent   : 'ui-table/cell/cell-simple-date',
-         width           : 65,
-         options         : {
-           dateFormat: 'MMMM DD, YYYY - hh:mm A'
-         }
+         width           : 75
        },
        {
          name            : 'Ends At',
          valuePath       : 'endsAt',
          isSortable      : true,
+         extraValuePaths : ['timezone'],
          headerComponent : 'tables/headers/sort',
          cellComponent   : 'ui-table/cell/cell-simple-date',
-         width           : 65,
-         options         : {
-           dateFormat: 'MMMM DD, YYYY - hh:mm A'
-         }
+         width           : 75
        },
        {
          name            : 'State',
--- app/controllers/events/list.js
+++ app/controllers/events/list.js
@@ -27,8 +27,9 @@ export default class extends Controller.extend(EmberTableControllerMixin) {
       {
         name            : 'Date',
         valuePath       : 'startsAt',
-        extraValuePaths : ['endsAt'],
+        extraValuePaths : ['endsAt', 'timezone'],
         isSortable      : true,
+        width           : 180,
         headerComponent : 'tables/headers/sort',
         cellComponent   : 'ui-table/cell/cell-event-date'
 
