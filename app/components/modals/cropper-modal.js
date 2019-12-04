c0385b19e33e0cc991691f9bf8603e122a0d98f1
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2019-12-04 20:46:22 +0530

    reverting viewport

--- app/components/modals/cropper-modal.js
+++ app/components/modals/cropper-modal.js
@@ -3,7 +3,7 @@ import { action } from '@ember/object';
 export default class extends ModalBase { 
   onVisible() {
     let viewport = {};
-    let factor = 750;
+    let factor = 150;
     const aspectRatio = this.getWithDefault('aspectRatio', [2, 1]);
     viewport.width = aspectRatio[0] * factor;
     viewport.height = aspectRatio[1] * factor;
@@ -33,7 +33,7 @@ export default class extends ModalBase {
   }
   @action
   cropImage() {
-    this.$('img').croppie('result', 'base64', 'original', 'jpeg', 1).then(result => {
+    this.$('img').croppie('result', 'base64', 'original', 'jpeg').then(result => {
       if (this.onImageCrop) {
         this.onImageCrop(result);
       }
