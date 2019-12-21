963a1abca0061fcc8b8fd5e361ca0bc59422cdaf
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2019-12-21 14:07:19 +0530

    Update cropper-modal.js

--- app/components/modals/cropper-modal.js
+++ app/components/modals/cropper-modal.js
@@ -3,7 +3,7 @@ import ModalBase from 'open-event-frontend/components/modals/modal-base';
 export default ModalBase.extend({
   onVisible() {
     let viewport = {};
-    let factor = 750;
+    let factor = 150;
     const aspectRatio = this.getWithDefault('aspectRatio', [2, 1]);
     viewport.width = aspectRatio[0] * factor;
     viewport.height = aspectRatio[1] * factor;
@@ -32,7 +32,7 @@ export default ModalBase.extend({
       this.onVisible();
     },
     cropImage() {
-      this.$('img').croppie('result', 'base64', 'original', 'jpeg').then(result => {
+      this.$('img').croppie('result', { type: 'base64', size: 'original', quality:1 }).then(result => {
         if (this.onImageCrop) {
           this.onImageCrop(result);
         }
