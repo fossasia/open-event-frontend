c7ddd9c2f36083c59c71c52c06ae54f5033df020
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2019-12-22 19:28:07 +0530

    fix: side menu icon should not be visible in mobile when there… (#3665)
    * side menu icon should not be visible
    
    * adding displaySideMenu computed property
    
    * using displaySideMenu computed property in public.hbs
    
    * removing es5 getter
    
    Co-authored-by: Kush Trivedi <kushthedude@gmail.com>

--- app/controllers/public.js
+++ app/controllers/public.js
@@ -11,6 +11,11 @@ export default Controller.extend({
   displayEndDate: computed('model.startsAtDate', 'model.endsAtDate', function() {
     return !moment(this.model.startsAtDate).isSame(this.model.endsAtDate, 'minute');
   }),
+  displaySideMenu: computed('session.currentRouteName', function() {
+    if (this.session.currentRouteName) {
+      return this.session.currentRouteName !== 'public.cfs.new-session' && this.session.currentRouteName !== 'public.cfs.new-speaker' && this.session.currentRouteName !== 'public.cfs.edit-speaker' && this.session.currentRouteName !== 'public.cfs.edit-session';
+    }
+  }),
   actions: {
     toggleMenu() {
       this.toggleProperty('isMenuOpen');
