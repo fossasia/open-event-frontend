a175ccf337967a2b0a4873dd02ac7b3dec59b639
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2019-12-31 13:40:55 +0530

    refactor: fix hasOwnProperty and format code (#3749)

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -13,7 +13,7 @@ import FastbootAdapter from 'ember-data-storefront/mixins/fastboot-adapter';
  * @return {*}
  */
 export const fixFilterQuery = query  => {
-  if (query.hasOwnProperty('filter')) {
+  if (Object.prototype.hasOwnProperty.call(query, 'filter')) {
     query.filter = JSON.stringify(query.filter);
   }
 
--- scripts/replace-config.js
+++ scripts/replace-config.js
@@ -1,22 +1,22 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const fs = require('fs');
-const promisify = require('util').promisify
-const { merge } = require('lodash')
-const safeEval = require('safe-eval')
+const promisify = require('util').promisify;
+const { merge } = require('lodash');
+const safeEval = require('safe-eval');
 
-const env = require('../config/environment')
-const appName = 'open-event-frontend'
+const env = require('../config/environment');
+const appName = 'open-event-frontend';
 
-const environment = env('production')
+const environment = env('production');
 
 async function replaceFastbootConfig() {
-  const packagePath = './dist/package.json'
-  const packageInfo = require('.' + packagePath)
+  const packagePath = './dist/package.json';
+  const packageInfo = require('.' + packagePath);
 
-  const old = packageInfo.fastboot.config[appName]
+  const old = packageInfo.fastboot.config[appName];
   packageInfo.fastboot.config[appName] = merge(old, environment);
-  await promisify(fs.writeFile)(packagePath, JSON.stringify(packageInfo))
+  await promisify(fs.writeFile)(packagePath, JSON.stringify(packageInfo));
 
   console.log('Transformed package.json with new environment')
 }
@@ -24,7 +24,7 @@ async function replaceFastbootConfig() {
 function findObject(js, objectStart) {
   let braceCount = 1;
 
-  const start = js.indexOf("{", objectStart);
+  const start = js.indexOf('{', objectStart);
 
   if (start < 0) {
     return null;
@@ -38,15 +38,15 @@ function findObject(js, objectStart) {
     const c = js[index];
 
     if (!parseBlocker && !parseBlockerBuffer) {
-      if (c == "{") {
+      if (c === '{') {
         braceCount += 1;
       } else if (c == "}") {
         braceCount -= 1;
       } else if (c === '"' || c === "'" || c === "`") {
         parseBlocker = c;
-      } else if (js[index - 1] == "/") {
-        if (c == "/") parseBlocker = "\n";
-        else if (c == "*") parseBlockerBuffer = "*/";
+      } else if (js[index - 1] === '/') {
+        if (c === '/') { parseBlocker = "\n"; }
+        else if (c === "*") { parseBlockerBuffer = "*/"; }
       }
 
       if (braceCount === 0) {
@@ -55,7 +55,7 @@ function findObject(js, objectStart) {
     } else {
       if (c === parseBlocker) {
         parseBlocker = null;
-      } else if (c == '/' && js[index - 1] == '*') {
+      } else if (c === '/' && js[index - 1] == '*') {
         parseBlockerBuffer = null;
       }
     }
@@ -72,43 +72,43 @@ function findObject(js, objectStart) {
 }
 
 function replace(str, start, end, replacement) {
-  return str.substring(0, start) + replacement + str.substring(end + 1, str.length)
+  return str.substring(0, start) + replacement + str.substring(end + 1, str.length);
 }
 
 async function replaceWebConfig() {
-  const pattern = new RegExp(`^${appName}.*\.js$`)
-  const basePath = './dist/assets/'
-  const appJs = (await promisify(fs.readdir)(basePath)).filter(name => name.match(pattern))
+  const pattern = new RegExp(`^${appName}.*\.js$`);
+  const basePath = './dist/assets/';
+  const appJs = (await promisify(fs.readdir)(basePath)).filter(name => name.match(pattern));
 
-  const envDefinition = 'define("open-event-frontend/config/environment"'
+  const envDefinition = 'define("open-event-frontend/config/environment"';
 
   for (const js of appJs) {
     const jsPath = basePath + js;
     const code = (await promisify(fs.readFile)(jsPath)).toString()
-    let defineIndex = code.indexOf(envDefinition)
+    let defineIndex = code.indexOf(envDefinition);
 
     if (defineIndex < 0) {
-      defineIndex = code.indexOf(envDefinition.replace(/"/g, "'"))
+      defineIndex = code.indexOf(envDefinition.replace(/"/g, "'"));
       if (defineIndex < 0)
         continue;
     }
 
-    const defaultIndex = code.indexOf('default', defineIndex + 1)
+    const defaultIndex = code.indexOf('default', defineIndex + 1);
 
-    if (defaultIndex < 0)
+    if (defaultIndex < 0) {
       continue;
-    
+    }
     // File with environment definition found
 
     console.log('Transforming ' + js)
-    
-    const object = findObject(code, defaultIndex)
+
+    const object = findObject(code, defaultIndex);
 
     console.log('Environment Object Found', object)
 
     const webEnvJson = code.substring(object.start, object.end + 1)
     const old = safeEval('(' + webEnvJson + ')')
-    
+
     const newEnv = JSON.stringify(merge(old, environment))
     const newCode = replace(code, object.start, object.end, newEnv)
 
