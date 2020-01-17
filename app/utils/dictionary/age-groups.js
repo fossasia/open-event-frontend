25b5de57b133393811273eced872f2b9fb1f079e
Nitin Kumar <snitin315@gmail.com>
2020-01-17 21:14:24 +0530

    add ageGroup field in attendee form

---
+++ app/utils/dictionary/age-groups.js
@@ -0,0 +1,17 @@
+export const ageGroups = [
+  {
+    name: '19 or less'
+  },
+  {
+    name: '20 to 29'
+  },
+  {
+    name: '30 to 39'
+  },
+  {
+    name: '40 to 49'
+  },
+  {
+    name: '50 or above'
+  }
+];
