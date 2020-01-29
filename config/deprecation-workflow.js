be616b94bb3c799d59e80531a55aa79255c89869
kushthedude <kushthedude@gmail.com>
2020-01-29 15:13:12 +0530

    chore: Depreciate jquery related warnings

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,7 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-routing.route-router' }
+    { handler: 'silence', matchId: 'ember-routing.route-router' },
+    { handler: 'silence', matchId: 'ember-views.curly-components.jquery-element'}
   ]
 };
