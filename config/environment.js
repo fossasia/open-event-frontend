66d7f990f4eadea3e70fd9d2d527c3bb524c8c63
iamareebjamal <jamal.areeb@gmail.com>
2020-02-01 11:27:49 +0530

    fix

--- config/environment.js
+++ config/environment.js
@@ -102,12 +102,10 @@ module.exports = function(environment) {
   };
 
   ENV['ember-simple-auth-token'] = {
-    serverTokenEndpoint              : `${ENV.APP.apiHost}/auth/session`,
-    tokenPropertyName                : 'access_token',
-
-    tokenExpirationInvalidateSession : false,
-    authorizationPrefix              : 'JWT ',
-    authorizationHeaderName          : 'Authorization'
+    refreshAccessTokens  : false,
+    serverTokenEndpoint  : `${ENV.APP.apiHost}/auth/session`,
+    tokenPropertyName    : 'access_token',
+    authorizationPrefix  : 'JWT ',
   };
 
   ENV['g-map'] = {
@@ -208,6 +206,8 @@ module.exports = function(environment) {
     ENV['simple-auth'] = {
       store: 'simple-auth-session-store:ephemeral'
     };
+
+    ENV['ember-simple-auth-token'].tokenExpirationInvalidateSession = false;
   }
 
   if (environment === 'production') {
