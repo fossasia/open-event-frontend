a7a58d73f14fb846aa4dd9ca3bad63f59f07a0b2
Nitin Kumar <snitin315@gmail.com>
2020-02-06 20:34:55 +0530

    add custom scroll action for side-menu

--- app/components/public/side-menu.js
+++ app/components/public/side-menu.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import moment from 'moment';
@@ -8,6 +9,22 @@ export default Component.extend({
     let speakersCall = await this.get('event.speakersCall');
     this.set('shouldShowCallforSpeakers',
       speakersCall && speakersCall.announcement && (speakersCall.privacy === 'public'));
+  },
+  actions: {
+    scrollToTarget() {
+      document.querySelectorAll('.scroll').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+          });
+          $('.scroll').each(function() {
+            $(this).removeClass('active');
+          });
+          $(this).addClass('active');
+        });
+      });
+    }
   },
   isSchedulePublished: computed('event.schedulePublishedOn', function() {
     return this.get('event.schedulePublishedOn') && this.get('event.schedulePublishedOn').toISOString() !== moment(0).toISOString();
