3be3b1057a1f5a6f17ab464a177c35ddc54a75cf
kushthedude <kushthedude@gmail.com>
2020-02-10 19:56:35 +0530

    fix: event URL generated by frontend

--- app/components/events/view/export/download-zip.js
+++ app/components/events/view/export/download-zip.js
@@ -1,27 +1,5 @@
 import Component from '@ember/component';
-import { action } from '@ember/object';
 
 export default class extends Component {
-  @action
-  async exportEventDownload(eventDownloadUrl) {
-    this.set('isLoading', true);
-    try {
-      const res = await this.loader.downloadFile(`${eventDownloadUrl}`);
-      const anchor = document.createElement('a');
-      anchor.style.display = 'none';
-      anchor.href = URL.createObjectURL(new Blob([res], { type: 'octet/stream' }));
-      anchor.download = 'EventExport.zip';
-      anchor.click();
-      this.notify.success(this.l10n.t('Exported Event Downloaded successfully.'), {
-        id: 'export_succ'
-      });
-    } catch (e) {
-      console.error(e);
-      this.notify.error(this.l10n.t(e), {
-        id: 'err_down'
-      });
-    }
-    this.set('isLoading', false);
-  }
 
 }
