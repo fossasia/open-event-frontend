84a7306849a1b8bfb539b5aee3406c6865544bb7
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-02-15 22:42:03 +0530

    Update test-helper.js

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,19 +1,9 @@
-import Application from '@ember/application';
+import Application from 'open-event-frontend/app';
 import QUnit from 'qunit';
 import config from 'open-event-frontend/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import loadEmberExam from 'ember-exam/test-support/load';
-import Resolver from 'ember-resolver';
-import loadInitializers from 'ember-load-initializers';
-
-const App = Application.extend({
-  modulePrefix    : config.modulePrefix,
-  podModulePrefix : config.podModulePrefix,
-  Resolver
-});
-
-loadInitializers(App, config.modulePrefix);
 
 QUnit.config.testTimeout = 300000;
 
