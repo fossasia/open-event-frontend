c739619ea5076f1b5f2148fac51476713041af0f
Areeb Jamal <jamal.areeb@gmail.com>
2020-03-29 06:22:10 +0530

    chore: Remove ready event listener (#4285)

--- app/models/event.js
+++ app/models/event.js
@@ -1,6 +1,5 @@
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { on } from '@ember/object/evented';
 import moment from 'moment';
 import attr from 'ember-data/attr';
 import ModelBase from 'open-event-frontend/models/base';
@@ -176,9 +175,6 @@ export default ModelBase.extend(CustomPrimaryKeyMixin, {
 
   sessionsByState: computed('sessions', function() {
     return groupBy(this.sessions.toArray(), 'data.state');
-  }),
-
-  _ready: on('ready', function() {
-
   })
+
 });
