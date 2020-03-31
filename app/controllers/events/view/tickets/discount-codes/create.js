b13614fdc933fecfff2f31674e404029a9eb404f
Nitin Kumar <46647141+snitin315@users.noreply.github.com>
2020-03-31 21:04:30 +0530

    chore: improve error reporting and fix typos (#4290)
    * chore: fix typos
    
    * chore(access-codes): improve error reporting
    
    * chore(discount-codes): improve error reporting

--- app/controllers/events/view/tickets/discount-codes/create.js
+++ app/controllers/events/view/tickets/discount-codes/create.js
@@ -8,8 +8,9 @@ export default Controller.extend({
           this.notify.success(this.l10n.t('Discount code has been successfully created.'));
           this.transitionToRoute('events.view.tickets.discount-codes');
         })
-        .catch(() => {
-          this.notify.error(this.l10n.t('An unexpected error has occured. Discount code cannot be created.'));
+        .catch(e => {
+          console.error('Error while creating discount code', e);
+          this.notify.error(this.l10n.t('An unexpected error has occurred. Discount code cannot be created.'));
         });
     }
   }
