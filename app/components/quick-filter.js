ee51a050d2c65fe15d5ee3140ff67c299e76127d
Naman Shankhydhar <naman.geek@gmail.com>
2020-05-10 00:19:21 +0530

    fix: Add condition check on Clear Filter (#4370)

--- app/components/quick-filter.js
+++ app/components/quick-filter.js
@@ -74,7 +74,7 @@ export default class QuickFilter extends Component {
     this.setDateFilter();
     this.set('location', this.dummyLocation);
     this.set('eventName', this.dummyName);
-    this.set('disableClear', false);
+    this.set('disableClear', !(this.dummyName || this.dummyLocation || this.startDate !== null));
   }
 
   @action
