6a974f76ce4ab3ad9ec51c864cb3fdfc1c7b289b
Naman Shankhydhar <naman.geek@gmail.com>
2020-05-09 15:08:30 +0530

    Fix: Added condition check for ClearFilter to get highlighted

--- app/components/quick-filter.js
+++ app/components/quick-filter.js
@@ -74,11 +74,7 @@ export default class QuickFilter extends Component {
     this.setDateFilter();
     this.set('location', this.dummyLocation);
     this.set('eventName', this.dummyName);
-    if (this.dummyName || this.dummyLocation || this.startDate !== null) {
-      this.set('disableClear', false);
-    } else {
-      this.set('disableClear', true);
-    }
+    this.set('disableClear', (this.dummyName || this.dummyLocation || this.startDate !== null) ? false : true);
   }
 
   @action
