3e1de9eb5009e006e70179c65d6f864c8d1aafac
Areeb Jamal <jamal.areeb@gmail.com>
2020-05-20 03:28:57 +0530

    chore: Replace html-sanitizer with dompurify (#4390)

--- app/services/sanitizer.js
+++ app/services/sanitizer.js
@@ -1,35 +1,46 @@
 import Service from '@ember/service';
-import sanitizeHtml from 'sanitize-html';
+import createDOMPurify from 'dompurify';
 
 export default Service.extend({
 
   sanitize: null,
 
+  _purify: null,
+
+  async init() {
+    this._super(...arguments);
+    if (typeof window !== 'undefined') {
+      this._purify = createDOMPurify(self);
+    } else {
+      const { JSDOM } = await import('jsdom');
+
+      this._purify = createDOMPurify(new JSDOM('').window);
+    }
+    this._purify.addHook('beforeSanitizeElements', function(node) {
+      if ('href' in node) {
+        node.setAttribute('target', '_blank');
+        node.setAttribute('rel', 'nofollow noopener');
+      }
+    });
+  },
+
   // Ensure any changes to the sanitizer rules are set in the rich text editor @ components/widgets/forms/rich-text-editor.js
   options: {
     allowedTags       : ['b', 'strong', 'i', 'em', 'u', 'ol', 'ul', 'li', 'a', 'p'],
-    allowedAttributes : {
-      'a': ['href', 'rel', 'target']
-    },
-    selfClosing           : ['br'],
-    allowedSchemes        : ['http', 'https', 'ftp', 'mailto'],
-    allowedSchemesByTag   : {},
-    allowProtocolRelative : false,
-    transformTags         : {
-      'i' : 'em',
-      'b' : 'strong',
-      'a' : sanitizeHtml.simpleTransform('a', { rel: 'nofollow', target: '_blank' })
-    }
+    allowedAttributes : ['href', 'rel', 'target']
   },
 
   purify(string) {
-    return sanitizeHtml(string, this.options);
+    return this._purify.sanitize(string, {
+      ALLOWED_TAGS : this.options.allowedTags,
+      ALLOWED_ATTR : this.options.allowedAttributes
+    });
   },
 
   strip(string) {
-    return sanitizeHtml(string, {
-      allowedTags       : [],
-      allowedAttributes : []
+    return this._purify.sanitize(string, {
+      ALLOWED_TAGS : [],
+      ALLOWED_ATTR : []
     });
   }
 });
