c49bfa63261c9f1232e48b5a710803579ca0751c
Haider Ali <haider.lee23@gmail.com>
2020-07-28 00:03:00 +0530

    New session route transition if speaker detail exists

--- app/components/public/call-for-speakers.js
+++ app/components/public/call-for-speakers.js
@@ -1,9 +1,13 @@
 import classic from 'ember-classic-decorator';
 import { action, computed } from '@ember/object';
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 @classic
 export default class CallForSpeakers extends Component {
+  @service
+  router;
+
   @action
   openModal() {
     if (this.session.isAuthenticated) {
@@ -17,6 +21,7 @@ export default class CallForSpeakers extends Component {
   addSession() {
     if (this.data.userSpeaker && this.data.userSpeaker.toArray().length) {
       // speaker detail exists
+      this.router.transitionTo('public.cfs.new-session');
     } else {
       this.notify.error(this.l10n.t('You need to add your speaker details first before submitting a session'),
         {
