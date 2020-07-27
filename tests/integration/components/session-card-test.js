71afd7fccc961083554cc34764afb180063822ea
Haider Ali <haider.lee23@gmail.com>
2020-07-27 13:47:54 +0530

    Change route for session-card

--- tests/integration/components/session-card-test.js
+++ tests/integration/components/session-card-test.js
@@ -7,7 +7,7 @@ import { render } from '@ember/test-helpers';
 module('Integration | Component | session card', function(hooks) {
   setupIntegrationTest(hooks);
 
-  const session = EmberObject.create({ id: 123, title: 'Super cool JS', state: 'rejected', event: 'OS Tech', startsAt: new Date(), endsAt: new Date() });
+  const session = EmberObject.create({ id: 123, title: 'Super cool JS', state: 'rejected', event: {name: 'OS Tech', id: 123}, startsAt: new Date(), endsAt: new Date() });
 
   test('it renders', async function(assert) {
     this.set('session', session);
