edee89d0f831b1a3687e61ef3da9b290805219f7
ruhci28 <59759735+ruhci28@users.noreply.github.com>
2020-07-28 21:24:04 +0530

    add helper

--- app/helpers/identify-social-link.js
+++ app/helpers/identify-social-link.js
@@ -1,6 +1,6 @@
 import Helper from '@ember/component/helper';
 
-export function identifySocialLink(main_string, sub_string) {
+export function identifySocialLink([main_string, sub_string]) {
   if (main_string.includes(sub_string)) {
     return true;
   } else {
