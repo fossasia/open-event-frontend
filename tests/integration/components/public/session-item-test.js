935c2320048f65da384de98b311a28f7e1089421
Haider Ali <haider.lee23@gmail.com>
2020-08-05 21:29:16 +0530

    fix: Session times in public session list (#4705)

--- tests/integration/components/public/session-item-test.js
+++ tests/integration/components/public/session-item-test.js
@@ -1,4 +1,5 @@
 import { hbs } from 'ember-cli-htmlbars';
+import EmberObject from '@ember/object';
 import { module, test } from 'qunit';
 import { setupIntegrationTest } from 'open-event-frontend/tests/helpers/setup-integration-test';
 import { render } from '@ember/test-helpers';
@@ -6,7 +7,7 @@ import { render } from '@ember/test-helpers';
 module('Integration | Component | public/session item', function(hooks) {
   setupIntegrationTest(hooks);
 
-  const session = { session_type: { length: '11:00', id: 12, name: 'Talk' }, shortAbstract: 'An introduction to the event', id: '1', title: 'Welcome to FOSSASIA', startsAt: '10:35 AM / 17-03-2017', track: { color: 'green', fontColor: 'green', id: 0, name: 'Track 1' }, microlocation: { id: 0, name: 'Room 1' }, speakers: [{ shortBiography: 'Works for ORG 1', id: 0, city: 'Delhi', name: 'Arnold Singh', speakingExperience: 'GSOC 2015', organisation: 'ORG 1', longBiography: '', photo: { 'iconImageUrl': '' } }] };
+  const session = { session_type: { length: '11:00', id: 12, name: 'Talk' }, shortAbstract: 'An introduction to the event', id: '1', title: 'Welcome to FOSSASIA', startsAt: '10:35 AM / 17-03-2017', event: EmberObject.create({ timezone: 'Asia/Kolkata' }), track: { color: 'green', fontColor: 'green', id: 0, name: 'Track 1' }, microlocation: { id: 0, name: 'Room 1' }, speakers: [{ shortBiography: 'Works for ORG 1', id: 0, city: 'Delhi', name: 'Arnold Singh', speakingExperience: 'GSOC 2015', organisation: 'ORG 1', longBiography: '', photo: { 'iconImageUrl': '' } }] };
 
   test('it renders', async function(assert) {
     this.set('session', session);
