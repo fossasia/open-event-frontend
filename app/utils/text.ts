0fe8d1f58021e1e387c876db7056606b6363d7d8
Areeb Jamal <jamal.areeb@gmail.com>
2020-08-14 07:22:20 +0530

    feat: Add filtering option to unscheduled sessions (#4766)

--- app/utils/text.ts
+++ app/utils/text.ts
@@ -4,3 +4,16 @@ export function slugify(string: string, sep = '-'): string {
     .replace(/[\s_-]+/g, sep) // swap any length of whitespace, underscore, hyphen characters with a single -
     .replace(/^-+|-+$/g, ''); // remove leading, trailing -
 }
+
+interface EmberObject {
+  get(key: string): string
+}
+
+export function matchPropertyIn(obj: EmberObject, filter: string, properties: string[]): boolean {
+  filter = filter.toLowerCase();
+  for (const property of properties) {
+    if (obj.get(property).toLowerCase().includes(filter)) {return true}
+  }
+
+  return false;
+}
