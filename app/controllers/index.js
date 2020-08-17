25423c6ca7a5cec185c72672f501efe166b46512
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-08-17 10:32:01 +0530

    Standardize Logic and Admin Control to list events on index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,6 +3,7 @@ import { action, computed } from '@ember/object';
 import Controller from '@ember/controller';
 import moment from 'moment';
 
+let upcoming_events_count = 0;
 @classic
 export default class IndexController extends Controller {
   queryParams = ['event_name', 'start_date', 'end_date', 'location'];
@@ -17,17 +18,17 @@ export default class IndexController extends Controller {
     const filteredEventsCfs = this.filteredEvents.filter(isEventCfsOpen);
     const featuredEventsCfs = this.featuredEvents.filter(isEventCfsOpen);
     const combinedCfsEvents = new Set(filteredEventsCfs.concat(featuredEventsCfs));
-    return Array.from(combinedCfsEvents);
+    return Array.from(combinedCfsEvents).slice(0, 12);
   }
 
   @computed('filteredEvents.[]')
   get promotedEvents() {
-    return this.filteredEvents ? this.filteredEvents.filter(event => {return event.isPromoted}) : null;
+    return this.filteredEvents ? this.filteredEvents.filter(isPromotedEvent) : null;
   }
 
   @computed('filteredEvents.[]')
   get upcomingEvents() {
-    return this.filteredEvents ? this.filteredEvents.filter(event => {return !event.isPromoted}) : null;
+    return this.filteredEvents ? this.filteredEvents.filter(validUpcomingEvent) : null;
   }
 
   @action
@@ -47,4 +48,21 @@ function isEventCfsOpen(event) {
   const endDateTime = callForPapers.get('endsAt');
   const privacyState = callForPapers.get('privacy');
   return (moment().isBetween(startDateTime, endDateTime) && (sessionEnabled) && (privacyState === 'public'));
+}
+
+function isPromotedEvent(event) {
+  if (upcoming_events_count < 21 && event.isPromoted) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validUpcomingEvent(event) {
+  if (upcoming_events_count < 21 && event.thumbnailImageUrl && event.location.name && !event.isPromoted) {
+    upcoming_events_count += 1;
+    return true;
+  } else {
+    return false;
+  }
 }
