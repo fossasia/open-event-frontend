95eb214d4f72b30728622f01d93cb1d2e429f09a
Areeb Jamal <jamal.areeb@gmail.com>
2020-08-18 07:47:48 +0530

    feat: Change text color based on session color lightness (#4790)

--- app/components/scheduler/unscheduled-session.js
+++ app/components/scheduler/unscheduled-session.js
@@ -2,6 +2,7 @@ import classic from 'ember-classic-decorator';
 import { classNames, classNameBindings } from '@ember-decorators/component';
 import $ from 'jquery';
 import Component from '@ember/component';
+import { isLight } from 'open-event-frontend/utils/color';
 
 @classic
 @classNames('unscheduled-session')
@@ -11,14 +12,15 @@ export default class UnscheduledSession extends Component {
 
   didInsertElement() {
     super.didInsertElement(...arguments);
+    const color = this.session.get('track.color');
     const $element = $(this.element);
     $element.data('event', {
-      title    : $element.text().replace(/\s\s+/g, ' '), // use the element's text as the event title
-      stick    : true, // maintain when user navigates (see docs on the renderEvent method)
-      id       : $element.attr('id'),
-      serverId : this.session.id, // id of the session on the server
-      color    : this.session.get('track.color')
-
+      title     : $element.text().replace(/\s\s+/g, ' '), // use the element's text as the event title
+      stick     : true, // maintain when user navigates (see docs on the renderEvent method)
+      id        : $element.attr('id'),
+      serverId  : this.session.id, // id of the session on the server
+      color,
+      textColor : isLight(color) ? '#000' : '#fff'
     });
     $element.draggable({
       zIndex         : 999,
@@ -26,7 +28,7 @@ export default class UnscheduledSession extends Component {
       revertDuration : 0  //  original position after the drag
     });
     $element.data('serverId', this.session.id);
-    $element.css('border-color', this.session.get('track.color'));
+    $element.css('border-color', color);
 
   }
 
---
+++ app/utils/color.ts
@@ -0,0 +1,18 @@
+export function getLuma(hex: string): number {
+  // https://stackoverflow.com/a/12043228/3309666
+  const c = hex.substring(1);    // strip #
+  const rgb = parseInt(c, 16);   // convert rrggbb to decimal
+  const r = (rgb >> 16) & 0xff;  // extract red
+  const g = (rgb >>  8) & 0xff;  // extract green
+  const b = (rgb >>  0) & 0xff;  // extract blue
+
+  return 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+}
+
+export function isLight(hex: string, threshold = 128): boolean {
+  return getLuma(hex) > threshold;
+}
+
+export function isDark(hex: string, threshold = 128): boolean {
+  return !isLight(hex, threshold);
+}
