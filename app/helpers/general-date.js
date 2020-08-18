e8df3732551aa417f0e7652dcfc4a053f93b4c67
iamareebjamal <jamal.areeb@gmail.com>
2020-08-18 11:47:48 +0530

    fix: Session list date format with timezone

--- app/helpers/general-date.js
+++ app/helpers/general-date.js
@@ -2,8 +2,9 @@ import Helper from '@ember/component/helper';
 import moment from 'moment';
 
 export function generalDate(params) {
-  const timezone = params[1] ? params[1] : moment.tz.guess();
-  return `${moment(params[0]).tz(timezone).format('h:mm A , MMMM Do YYYY')} (${moment.tz(params[0], timezone).zoneAbbr()})`;
+  const timezone = params[1] || moment.tz.guess();
+  const format = params[2] || 'h:mm A , MMMM Do YYYY (z)';
+  return moment(params[0]).tz(timezone).format(format);
 }
 
 export default Helper.helper(generalDate);
