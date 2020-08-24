e3d5cfef476f8c7c8063feef979479c3870141df
iamareebjamal <jamal.areeb@gmail.com>
2020-08-25 05:23:43 +0530

    fix

--- app/controllers/public/sessions.js
+++ app/controllers/public/sessions.js
@@ -2,7 +2,6 @@ import classic from 'ember-classic-decorator';
 import { computed } from '@ember/object';
 import Controller from '@ember/controller';
 import moment from 'moment';
-import { timezones } from 'open-event-frontend/utils/dictionary/date-time';
 
 @classic
 export default class SessionsController extends Controller {
@@ -10,24 +9,14 @@ export default class SessionsController extends Controller {
   queryParams = ['sort'];
   sort = 'starts-at';
   isTrackVisible = false;
-  currentTimezone = moment.tz.guess();
-  localTimezone = moment.tz.guess();
+  timezone = null;
 
-
-  @computed
-  get timezones() {
-    let mytimezones = []
-    mytimezones = timezones.filter(item => item !== moment.tz.guess());
-    mytimezones.unshift(moment.tz.guess());
-    return mytimezones;
-  }
-
-  @computed('model.event.startsAt', 'model.event.endsAt')
+  @computed('model.event.startsAt', 'model.event.endsAt', 'timezone')
   get allDates() {
     const arr = [];
     const difference = (this.model.event.endsAt).diff(this.model.event.startsAt, 'days');
     for (let i = 0; i <= Math.abs(difference); i++) {
-      arr.push(moment.tz(this.model.event.startsAt, this.model.event.timezone).add(i, 'days').toISOString());
+      arr.push(moment.tz(this.model.event.startsAt, this.timezone).add(i, 'days').toISOString());
     }
     return arr;
   }
--- app/utils/dictionary/date-time.ts
+++ app/utils/dictionary/date-time.ts
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import 'moment-timezone';
 
 export const timezones = moment.tz.names();
 
