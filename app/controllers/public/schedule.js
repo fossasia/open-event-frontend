d1fa33fca01210f1ef34ba27e8bbd84c0a7091ab
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-08-27 10:12:15 +0530

    Update schedule.js

--- app/controllers/public/schedule.js
+++ app/controllers/public/schedule.js
@@ -1,5 +1,5 @@
 import classic from 'ember-classic-decorator';
-import { computed } from '@ember/object';
+import { computed, action } from '@ember/object';
 import Controller from '@ember/controller';
 import moment from 'moment';
 
@@ -8,5 +8,44 @@ export default class ScheduleController extends Controller {
   @computed('model.event.schedulePublishedOn')
   get isSchedulePublished() {
     return this.model.event.schedulePublishedOn.toISOString() !== moment(0).toISOString();
+  }
+
+   hideSchedule = true;
+   hideTrack = false;
+   hideRooms = false;
+  @action
+  showSchedule() {
+    this.toggleProperty('hideSchedule');
+    this.toggleProperty('hideTrack');
+    this.toggleProperty('hideRooms');
+
+	  this.set('hideSchedule', true);
+	  this.set('hideTrack', false);
+	  this.set('hideRooms', false);
+
+  }
+
+  @action
+  showTrack() {
+    this.toggleProperty('hideSchedule');
+    this.toggleProperty('hideTrack');
+    this.toggleProperty('hideRooms');
+    
+    this.set('hideSchedule', false);
+    this.set('hideTrack', true);
+    this.set('hideRooms', false);
+  
+  }
+
+  @action
+  showRoom() {
+    this.toggleProperty('hideSchedule');
+    this.toggleProperty('hideTrack');
+    this.toggleProperty('hideRooms');
+   
+    this.set('hideSchedule', false);
+    this.set('hideTrack', false);
+    this.set('hideRooms', true);
+    
   }
 }
