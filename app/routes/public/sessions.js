83ab8e91d073bea1e74efdbf38c8c13e964d3bf8
iamareebjamal <jamal.areeb@gmail.com>
2020-08-27 11:46:34 +0530

    fin

--- app/routes/public/sessions.js
+++ app/routes/public/sessions.js
@@ -75,27 +75,26 @@ export default class SessionsRoute extends Route {
     if (params.track) {
       filterOptions.push({
         name : 'track',
-        op : 'has',
-        val : {
-          name: 'name',
-          op: 'eq',
-          val: params.track
+        op   : 'has',
+        val  : {
+          name : 'name',
+          op   : 'eq',
+          val  : params.track
         }
-      })
+      });
     }
 
     if (params.room) {
       filterOptions.push({
         name : 'microlocation',
-        op : 'has',
-        val : {
-          name: 'name',
-          op: 'eq',
-          val: params.room
+        op   : 'has',
+        val  : {
+          name : 'name',
+          op   : 'eq',
+          val  : params.room
         }
-      })
+      });
     }
-  
 
     return {
       event   : eventDetails,
