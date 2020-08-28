40efc222b924b1be00dd6d3e1285ecf6d02d38f8
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-08-28 21:11:51 +0530

    Update other-details.js

--- app/controllers/events/view/edit/other-details.js
+++ app/controllers/events/view/edit/other-details.js
@@ -1,1 +1,31 @@
-<Forms::Wizard::OtherDetailsStep @data={{this.model}} @move="move" @save="save" @isLoading={{this.isLoading}} />
+import classic from 'ember-classic-decorator';
+import { action } from '@ember/object';
+import Controller from '@ember/controller';
+import EventWizardMixin from 'open-event-frontend/mixins/event-wizard';
+
+@classic
+export default class OtherDetailsController extends Controller.extend(EventWizardMixin) {
+  @action
+  save() {
+    this.saveEventDataAndRedirectTo(
+      'events.view.index',
+      ['tickets', 'socialLinks', 'copyright', 'tax', 'stripeAuthorization']
+    );
+  }
+
+  @action
+  async move(direction, data) {
+    try {
+      this.saveEventDataAndRedirectTo(
+        direction === 'forwards' ? 'events.view.edit.attendee' : 'events.view.edit.basic-details',
+        ['tickets', 'socialLinks', 'copyright', 'tax', 'stripeAuthorization']
+      );
+    } catch (error) {
+      console.error('Error while moving attendee', error);
+      this.notify.error(this.l10n.t(error.message),
+        {
+          id: 'attendee_move_error'
+        });
+    }
+  }
+}
