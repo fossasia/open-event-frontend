211c7a0523ee5a75a8ec03c183e67f3ff1118e8f
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-08-29 16:34:49 +0530

    removing validations that are not imp here

--- app/components/forms/wizard/other-details-step.js
+++ app/components/forms/wizard/other-details-step.js
@@ -84,56 +84,12 @@ export default Component.extend(FormMixin, EventWizardMixin, {
       delay  : false,
       on     : 'blur',
       fields : {
-        name: {
-          identifier : 'name',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Please give your event a name')
-            }
-          ]
-        },
-        location: {
-          identifier : 'location',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Location is required to save an event')
-            }
-          ]
-        },
         timezone: {
           identifier : 'timezone',
           rules      : [
             {
               type   : 'empty',
               prompt : this.l10n.t('Choose a timezone for your event')
-            }
-          ]
-        },
-        startDate: {
-          identifier : 'start_date',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Please tell us when your event starts')
-            },
-            {
-              type   : 'date',
-              prompt : this.l10n.t('Please give a valid start date')
-            }
-          ]
-        },
-        endDate: {
-          identifier : 'end_date',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Please tell us when your event ends')
-            },
-            {
-              type   : 'date',
-              prompt : this.l10n.t('Please give a valid end date')
             }
           ]
         },
@@ -157,167 +113,6 @@ export default Component.extend(FormMixin, EventWizardMixin, {
             }
           ]
         },
-        ticketName: {
-          identifier : 'ticket_name',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Please give your ticket a name')
-            }
-          ]
-        },
-        ticketDescription: {
-          identifier : 'ticket_description',
-          optional   : true,
-          rules      : [
-            {
-              type   : 'maxLength[160]',
-              prompt : this.l10n.t('Ticket description shouldn\'t contain more than {ruleValue} characters')
-            }
-          ]
-        },
-        ticketPrice: {
-          identifier : 'ticket_price',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Please give your ticket a price')
-            },
-            {
-              type   : 'number',
-              prompt : this.l10n.t('Please give a proper price for you ticket')
-            },
-            {
-              type   : 'decimal[0..]',
-              prompt : this.l10n.t('Ticket price should be greater than 0')
-            }
-          ]
-        },
-        ticketQuantity: {
-          identifier : 'ticket_quantity',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Please specify how many tickets of this type are available')
-            },
-            {
-              type   : 'number',
-              prompt : this.l10n.t('Please give a proper quantity for you ticket')
-            }
-          ]
-        },
-        ticketMinOrder: {
-          identifier : 'ticket_min_order',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Minimum tickets per order required')
-            },
-            {
-              type   : 'number',
-              prompt : this.l10n.t('Invalid number')
-            },
-            {
-              type   : 'checkMaxMinOrder',
-              prompt : this.l10n.t('Minimum order should not be greater than maximum')
-            }
-          ]
-        },
-        ticketMaxOrder: {
-          identifier : 'ticket_max_order',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Maximum tickets per order required')
-            },
-            {
-              type   : 'number',
-              prompt : this.l10n.t('Invalid number')
-            },
-            {
-              type   : 'integer[1..]',
-              prompt : this.l10n.t('Maximum tickets per order should be greater than 0')
-            },
-            {
-              type   : 'checkMaxMinOrder',
-              prompt : this.l10n.t('Maximum order should not be less than minimum')
-            }
-          ]
-        },
-        minPrice: {
-          identifier : 'min_price',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Minimum price for donation tickets required')
-            },
-            {
-              type   : 'integer[1..]',
-              prompt : this.l10n.t('Minimum price needs to be greater than zero')
-            },
-            {
-              type   : 'checkMaxMinPrice',
-              prompt : this.l10n.t('Minimum price should not be greater than maximum')
-            }
-          ]
-        },
-        maxPrice: {
-          identifier : 'max_price',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Maximum price for donation tickets required')
-            },
-            {
-              type   : 'integer[1..]',
-              prompt : this.l10n.t('Maximum price needs to be greater than zero')
-            },
-            {
-              type   : 'checkMaxMinPrice',
-              prompt : this.l10n.t('Maximum price should not be less than minimum')
-            }
-          ]
-        },
-        paypalEmail: {
-          identifier : 'paypal_email',
-          rules      : [
-            {
-              type   : 'email',
-              prompt : this.l10n.t('Please enter a valid email')
-            },
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Please fill your paypal email for payment of tickets.')
-            }
-          ]
-        },
-        onSiteDetails: {
-          identifier : 'on_site_details',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Please fill the details for payment of tickets.')
-            }
-          ]
-        },
-        bankDetails: {
-          identifier : 'bank_details',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Please fill the bank details for payment of tickets.')
-            }
-          ]
-        },
-        chequeDetails: {
-          identifier : 'cheque_details',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Please fill the cheque details for payment of tickets.')
-            }
-          ]
-        },
         externalEventIdentifier: {
           identifier : 'external_event_url',
           optional   : true,
@@ -329,15 +124,6 @@ export default Component.extend(FormMixin, EventWizardMixin, {
             }
           ]
         },
-        paymentCountry: {
-          identifier : 'payment_country',
-          rules      : [
-            {
-              type   : 'empty',
-              prompt : this.l10n.t('Please select your country')
-            }
-          ]
-        }
       }
     };
     // Merging the predetermined rules with the rules for social links.
@@ -356,10 +142,7 @@ export default Component.extend(FormMixin, EventWizardMixin, {
         licenceUrl : license.link
       });
     },
-    onChange() {
-      this.onValid(() => {});
-    },
-    saveDraft() {
+     saveDraft() {
       this.onValid(() => {
         this.set('data.event.state', 'draft');
         this.sendAction('save');
@@ -375,6 +158,9 @@ export default Component.extend(FormMixin, EventWizardMixin, {
         this.set('data.event.state', 'published');
         this.sendAction('save');
       });
+    },
+    onChange() {
+      this.onValid(() => {});
     }
   }
 });
