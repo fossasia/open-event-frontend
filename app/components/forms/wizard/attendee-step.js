e232aa790f24004f56f0239a2bf31130049cc0d1
iamareebjamal <jamal.areeb@gmail.com>
2020-08-30 07:26:22 +0530

    feat: Separate required and optional attendee fields

--- app/components/forms/wizard/attendee-step.js
+++ app/components/forms/wizard/attendee-step.js
@@ -1,7 +1,17 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import FormMixin from 'open-event-frontend/mixins/form';
 
 export default Component.extend(FormMixin, {
+  
+  fixedFields: computed('data.customForms', function() {
+    return this.data.customForms?.filter(field => field.isFixed);
+  }),
+
+  editableFields: computed('data.customForms', function() {
+    return this.data.customForms?.filter(field => !field.isFixed);
+  }),
+
   actions: {
     saveDraft() {
       this.onValid(() => {
