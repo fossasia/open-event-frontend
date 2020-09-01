b373e4e9a861e8809f226eee6686399f76e061c7
Nitin Kumar <snitin315@gmail.com>
2020-09-01 21:30:22 +0530

    feat: implement dropdown for social media in other-details step

--- app/components/forms/wizard/other-details-step.js
+++ app/components/forms/wizard/other-details-step.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import moment from 'moment';
-import { merge, orderBy, find } from 'lodash-es';
+import { orderBy, find } from 'lodash-es';
 import { licenses } from 'open-event-frontend/utils/dictionary/licenses';
 import { timezones } from 'open-event-frontend/utils/dictionary/date-time';
 import FormMixin from 'open-event-frontend/mixins/form';
@@ -23,7 +23,6 @@ export default Component.extend(FormMixin, EventWizardMixin, {
     return orderBy(licenses, 'name');
   }),
 
-
   socialLinks: computed('data.event.socialLinks.@each.isDeleted', function() {
     return this.data.event.socialLinks.filterBy('isDeleted', false);
   }),
@@ -31,31 +30,7 @@ export default Component.extend(FormMixin, EventWizardMixin, {
   isUserUnverified: computed('authManager.currentUser.isVerified', function() {
     return !this.authManager.currentUser.isVerified;
   }),
-  /**
-   * returns the validation rules for the social links.
-   */
-  socialLinksValidationRules: computed('socialLinks', function() {
-    let validationRules = {};
-    for (let i = 0; i < this.socialLinks.length; i++) {
-      validationRules = merge(validationRules, {
-        [this.socialLinks.get(i).identifier]: {
-          identifier : this.socialLinks.get(i).identifier,
-          optional   : true,
-          rules      : [
-            {
-              type   : 'regExp',
-              value  : protocolLessValidUrlPattern,
-              prompt : this.l10n.t('Please enter a valid url')
-            }
-          ]
-        }
-      });
-    }
-
-    return validationRules;
-  }),
-
-
+  
   showDraftButton: computed('data.event.state', function() {
     return this.data.event.state !== 'published';
   }),
@@ -126,8 +101,6 @@ export default Component.extend(FormMixin, EventWizardMixin, {
         }
       }
     };
-    // Merging the predetermined rules with the rules for social links.
-    validationRules.fields = merge(validationRules.fields, this.socialLinksValidationRules);
     return validationRules;
   },
 
