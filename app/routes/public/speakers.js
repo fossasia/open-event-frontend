b1e1734ad61c034e56432a096250f54922fe58d4
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-09-02 11:24:57 +0530

    fix: increase page size to 21 for speakers (#4932)

--- app/routes/public/speakers.js
+++ app/routes/public/speakers.js
@@ -47,7 +47,7 @@ export default class SpeakersRoute extends Route {
       event    : eventDetails,
       speakers : await this.infinity.model('speakers', {
         filter       : SPEAKERS_FILTER,
-        perPage      : 12,
+        perPage      : 21,
         startingPage : 1,
         perPageParam : 'page[size]',
         pageParam    : 'page[number]',
