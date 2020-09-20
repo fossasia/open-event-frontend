d03920e6ca4bb6dd887ac5e29e61589c7eb83651
sundaram <dubeysundaram057@gmail.com>
2020-09-20 17:14:33 +0530

    completing modal and line break

--- app/helpers/confirm.js
+++ app/helpers/confirm.js
@@ -10,7 +10,7 @@ export default Helper.extend({
   compute(params) {
     return () => {
       if (params.length >= 6) {
-        this.confirm.prompt(params[0], { 'denyText': params[2], 'approveText': params[3], 'denyColor': params[4], 'approveColor': params[5] })
+        this.confirm.prompt("Are You Sure?", { 'denyText': params[2], 'approveText': params[3], 'denyColor': params[4], 'approveColor': params[5], 'extra': params[0] })
           .then(() => {
             params[1]();
           });
