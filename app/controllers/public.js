5ff36be4bdfe524995af585ada58997282ab51a1
HADES-01 <adityavats122017@gmail.com>
2020-09-30 17:47:46 +0530

    Fixed Public Event Page Location Issue #5161

--- app/controllers/public.js
+++ app/controllers/public.js
@@ -20,6 +20,19 @@ export default class PublicController extends Controller {
     return this.session.currentRouteName && this.session.currentRouteName !== 'public.cfs.new-session' && this.session.currentRouteName !== 'public.cfs.new-speaker' && this.session.currentRouteName !== 'public.cfs.edit-speaker' && this.session.currentRouteName !== 'public.cfs.edit-session';
   }
 
+  @computed('locationName', 'online')
+  get headerLocation() {
+    if (this.model.locationName && this.model.online) {
+      return `In-Person Event and Online Event ${this.model.locationName}`;
+    } else if (this.model.online) {
+      return 'Online Event';
+    } else if (this.model.locationName) {
+      return this.model.locationName;
+    } else {
+      return 'Location to be announced';
+    }
+  }
+  
   @action
   toggleMenu() {
     this.toggleProperty('isMenuOpen');
