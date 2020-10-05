754f106e936bfb6c14a8d3e3520fddfe08d0b731
sundaram <dubeysundaram057@gmail.com>
2020-10-05 20:57:38 +0530

    indentation error

--- app/controllers/admin/sales/index.js
+++ app/controllers/admin/sales/index.js
@@ -1,6 +1,52 @@
-import classic from 'ember-classic-decorator';
 import Controller from '@ember/controller';
 import AdminSalesMixin from 'open-event-frontend/mixins/admin-sales';
+import { or } from '@ember/object/computed';
+import EmberTableControllerMixin from 'open-event-frontend/mixins/ember-table-controller';
 
-@classic
-export default class IndexController extends Controller.extend(AdminSalesMixin) {}
+export default class IndexController extends Controller.extend(AdminSalesMixin, EmberTableControllerMixin) {
+
+@or('authManager.currentUser.isSuperAdmin', 'authManager.currentUser.isAdmin') hasRestorePrivileges;
+
+get columns() {
+  return [
+    {
+      name            : 'Events',
+      valuePath       : 'name',
+      isSortable      : true,
+      headerComponent : 'tables/headers/sort'
+    },
+    {
+      name            : 'Event Date',
+      valuePath       : 'startsAt',
+      isSortable      : true,
+      headerComponent : 'tables/headers/sort'
+    },
+    {
+      name      : 'Completed Tickets',
+      valuePath : 'sales.completed.ticket_count',
+      width     : 75
+    },
+    {
+      name      : 'Completed Sales',
+      valuePath : 'sales.completed.sales_total'
+    },
+    {
+      name      : 'Placed Tickets',
+      valuePath : 'sales.placed.ticket_count'
+    },
+    {
+      name      : 'Placed Sales',
+      valuePath : 'sales.placed.sales_total'
+    },
+    {
+      name      : 'Pending Tickets',
+      valuePath : 'sales.pending.ticket_count'
+    },
+    {
+      name      : 'Pending Sales',
+      valuePath : 'sales.pending.sales_total'
+    }
+  ];
+}
+
+}
