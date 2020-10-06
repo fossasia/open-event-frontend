16e514593344e2f1e8e1fe6d015bc427dc07c174
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-10-06 07:35:04 +0530

    fix: adding table for admin-sales-by-event (#5243)
    Co-authored-by: iamareebjamal <jamal.areeb@gmail.com>

--- app/controllers/admin/sales/index.js
+++ app/controllers/admin/sales/index.js
@@ -1,6 +1,83 @@
-import classic from 'ember-classic-decorator';
 import Controller from '@ember/controller';
 import AdminSalesMixin from 'open-event-frontend/mixins/admin-sales';
+import { or } from '@ember/object/computed';
+import EmberTableControllerMixin from 'open-event-frontend/mixins/ember-table-controller';
 
-@classic
-export default class IndexController extends Controller.extend(AdminSalesMixin) {}
+export default class IndexController extends Controller.extend(AdminSalesMixin, EmberTableControllerMixin) {
+
+@or('authManager.currentUser.isSuperAdmin', 'authManager.currentUser.isAdmin') hasRestorePrivileges;
+
+get columns() {
+  return [
+    {
+      name            : 'Events',
+      valuePath       : 'name',
+      isSortable      : true,
+      headerComponent : 'tables/headers/sort'
+    },
+    {
+      name            : 'Event Date',
+      valuePath       : 'startsAt',
+      isSortable      : true,
+      headerComponent : 'tables/headers/sort',
+      extraValuePaths : ['timezone'],
+      cellComponent   : 'ui-table/cell/cell-simple-date'
+    },
+    {
+      name            : 'Completed Orders',
+      headerComponent : 'tables/headers/sort',
+      color           : 'green',
+      subcolumns      : [
+        {
+          name      : 'Tickets',
+          valuePath : 'sales.completed.ticket_count',
+          width     : 30
+        },
+        {
+          name            : 'Sales',
+          valuePath       : 'sales.completed.sales_total',
+          extraValuePaths : ['paymentCurrency'],
+          cellComponent   : 'ui-table/cell/admin/sales/cell-amount'
+        }
+      ]
+    },
+    {
+      name            : 'Placed Orders',
+      color           : 'blue',
+      headerComponent : 'tables/headers/sort',
+      subcolumns      : [
+        {
+          name      : 'Tickets',
+          valuePath : 'sales.placed.ticket_count',
+          width     : 30
+        },
+        {
+          name            : 'Sales',
+          valuePath       : 'sales.placed.sales_total',
+          extraValuePaths : ['paymentCurrency'],
+          cellComponent   : 'ui-table/cell/admin/sales/cell-amount'
+        }
+      ]
+    },
+    {
+      name            : 'Pending Orders',
+      color           : 'yellow',
+      headerComponent : 'tables/headers/sort',
+      subcolumns      : [
+        {
+          name      : 'Tickets',
+          valuePath : 'sales.pending.ticket_count',
+          width     : 30
+        },
+        {
+          name            : 'Sales',
+          valuePath       : 'sales.pending.sales_total',
+          extraValuePaths : ['paymentCurrency'],
+          cellComponent   : 'ui-table/cell/admin/sales/cell-amount'
+        }
+      ]
+    }
+  ];
+}
+
+}
