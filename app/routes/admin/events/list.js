de7d62dd5d30de92f13bf03d14922dc2c2aa4bfa
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2020-10-25 12:17:07 +0530

    default sorting of events by start date

--- app/routes/admin/events/list.js
+++ app/routes/admin/events/list.js
@@ -124,6 +124,8 @@ export default class extends Route.extend(EmberTableRouteMixin) {
       'page[size]'   : params.per_page || 10,
       'page[number]' : params.page || 1
     };
+    params.sort_by = 'starts-at';
+    params.sort_dir = 'DSC';
     queryString = this.applySortFilters(queryString, params);
     return  this.asArray(this.store.query('event', queryString));
   }
--- app/routes/events/list.js
+++ app/routes/events/list.js
@@ -88,6 +88,8 @@ export default class extends Route.extend(EmberTableRouteMixin) {
       'page[size]'   : params.per_page || 10,
       'page[number]' : params.page || 4
     };
+    params.sort_by = 'starts-at';
+    params.sort_dir = 'DSC';
     queryString = this.applySortFilters(queryString, params);
     return this.asArray(this.authManager.currentUser.query('events', queryString));
   }
