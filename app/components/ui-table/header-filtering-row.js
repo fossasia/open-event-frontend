90a4255623245de5d06e2a075e67dcb00ba8e3be
Areeb Jamal <jamal.areeb@gmail.com>
2020-10-26 00:08:48 +0530

    chore: Remove ember-models-table (#5379)

--- app/components/ui-table/header-filtering-row.js
+++
@@ -1,8 +0,0 @@
-import classic from 'ember-classic-decorator';
-import { layout as templateLayout } from '@ember-decorators/component';
-import DefaultPagination from 'ember-models-table/components/models-table/row-filtering';
-import layout from 'open-event-frontend/components/ui-table/header-row-filtering';
-
-@classic
-@templateLayout(layout)
-export default class HeaderFilteringRow extends DefaultPagination {}
