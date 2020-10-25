794277727a6c5c1ce6948a57f764265f656c7240
iamareebjamal <jamal.areeb@gmail.com>
2020-10-25 23:58:47 +0530

    chore: Remove ember-models-table

--- app/components/ui-table/row.js
+++
@@ -1,8 +0,0 @@
-import classic from 'ember-classic-decorator';
-import { layout as templateLayout } from '@ember-decorators/component';
-import DefaultPagination from 'ember-models-table/components/models-table/row';
-import layout from 'open-event-frontend/components/ui-table/row';
-
-@classic
-@templateLayout(layout)
-export default class Row extends DefaultPagination {}
