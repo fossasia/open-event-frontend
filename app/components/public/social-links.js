5c8a4d4bf6057d2b4543bfd3ac52f31e51298611
akash2408 <aakash20012408@gmail.com>
2020-10-26 17:15:26 +0530

    add to calender

--- app/components/public/social-links.js
+++ app/components/public/social-links.js
@@ -11,12 +11,13 @@ export default Component.extend({
   twitterLink: computed('socialLinks.[]', function() {
     return this.socialLinks.findBy('isTwitter', true);
   }),
-   calender:computed('eventtime','eventtimeends','eventname','eventlocation',function(){
-   let startparams=this.get('eventtime');
-   let endparams=this.get('eventtimeends');
-   const starttime=moment(startparams).tz("Asia/Kolkata").utc().format('YYYYMMDD[T]HHmmSS[Z]');
-   const endtime=moment(endparams).tz("Asia/Kolkata").utc().format('YYYYMMDD[T]HHmmSS[Z]');
-   return `https://calendar.google.com/calendar/u/0/r/eventedit?dates=${starttime}/${endtime}&text=${this.get('eventname')}&location=${this.get('eventlocation')}&sf=true`;
 
+  calender: computed( function() {
+    const startparams = this.get('eventtime');
+    const endparams = this.get('eventtimeends');
+    const starttime = moment(startparams).tz('Asia/Kolkata').utc().format('YYYYMMDD[T]HHmmSS[Z]');
+    const endtime = moment(endparams).tz('Asia/Kolkata').utc().format('YYYYMMDD[T]HHmmSS[Z]');
+    return `https://calendar.google.com/calendar/u/0/r/eventedit?dates=${starttime}/${endtime}&text=${this.get('eventname')}&location=${this.get('eventlocation')}&sf=true`;
   })
+
 });
