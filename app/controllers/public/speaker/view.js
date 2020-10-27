26192bcc021d9d141d9ebeb34a56d75791492f28
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-10-27 16:47:20 +0530

    Improved Code

--- app/controllers/public/speaker/view.js
+++ app/controllers/public/speaker/view.js
@@ -1,9 +1,9 @@
 import Controller from '@ember/controller';
-import { action, computed } from '@ember/object';
 
 export default class extends Controller {
-    @computed('speaker.isAuthenticated', 'authManager.currentUser.email', 'authManager.currentUser.isAdmin', 'model.speakers')
-    get showEditSection() {
-      return this.session.isAuthenticated && (this.authManager.currentUser.isAdmin || this.model.email === this.authManager.currentUser.email);
-    }
+
+  @computed('speaker.isAuthenticated', 'authManager.currentUser.email', 'authManager.currentUser.isAdmin', 'model.speakers')
+  get showEditSection() {
+    return this.session.isAuthenticated && (this.authManager.currentUser.isAdmin || this.model.email === this.authManager.currentUser.email);
+  }
 }
