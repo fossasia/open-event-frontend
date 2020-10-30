b6864fc5037fccb57a30e2b99df629e289bd0e47
sundaram <dubeysundaram057@gmail.com>
2020-10-30 18:37:31 +0530

    adding ember leaflet

--- app/components/public/event-map.js
+++ app/components/public/event-map.js
@@ -1,7 +1,20 @@
 import classic from 'ember-classic-decorator';
 import { classNames } from '@ember-decorators/component';
 import Component from '@ember/component';
+import { tracked } from '@glimmer/tracking';
 
 @classic
 @classNames('ui', 'stackable', 'grid')
-export default class EventMap extends Component {}
+export default class EventMap extends Component {
+    @tracked
+    lat = this.event.latitude;
+
+    @tracked
+    lng = this.event.longitude;
+    
+    @tracked
+    zoom = 4;
+
+    @tracked
+    emberConfLocation = [this.event.latitude, this.event.longitude];
+}
