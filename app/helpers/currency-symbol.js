f07e69fd4af06d97b83fc1698fe77ec9bdb9f331
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-11-03 13:09:42 +0530

    Do not $ sign when cost is 0-2

--- app/helpers/currency-symbol.js
+++ app/helpers/currency-symbol.js
@@ -4,7 +4,10 @@ import { paymentCurrencies } from 'open-event-frontend/utils/dictionary/payment'
 
 export function currencySymbol(params) {
   const currency = find(paymentCurrencies, ['code', params[0]]);
-  return currency ? currency.symbol : params[0];
+  if(params[1])
+    return currency ? currency.symbol : params[0];
+  else
+    return null;
 }
 
 export default Helper.helper(currencySymbol);
