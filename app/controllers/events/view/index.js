1b0c4979110fb4fb764c827f6c62464480d91f96
sundaram <dubeysundaram057@gmail.com>
2020-11-03 11:11:07 +0530

    simplify sponsor box

--- app/controllers/events/view/index.js
+++ app/controllers/events/view/index.js
@@ -1,5 +1,4 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import EmberTableControllerMixin from 'open-event-frontend/mixins/ember-table-controller';
 
 export default class extends Controller.extend(EmberTableControllerMixin) {
@@ -27,45 +26,36 @@ export default class extends Controller.extend(EmberTableControllerMixin) {
         name          : 'Level',
         valuePath     : 'level',
         cellComponent : 'ui-table/cell/cell-sponsor-sanitize'
-      },
-      {
-        name          : 'Options',
-        valuePath     : 'id',
-        cellComponent : 'ui-table/cell/cell-sponsor-options',
-        actions       : {
-          editSponsor   : this.editSponsor.bind(this),
-          deleteSponsor : this.deleteSponsor.bind(this)
-        }
       }
     ];
   }
 
-  @action
-  deleteSponsor(sponsor_id) {
-    this.set('isLoading', true);
-    const sponsor = this.store.peekRecord('sponsor', sponsor_id, { backgroundReload: false });
-    sponsor.destroyRecord()
-      .then(() => {
-        this.notify.success(this.l10n.t('Sponsor has been deleted successfully.'),
-          {
-            id: 'spons_deleted'
-          });
-        this.refreshModel.bind(this)();
-      })
-      .catch(() => {
-        this.notify.error(this.l10n.t('An unexpected error has occurred.'),
-          {
-            id: 'spons_deleted_error'
-          });
-      })
-      .finally(() => {
-        this.set('isLoading', false);
-      });
-  }
+  // @action
+  // deleteSponsor(sponsor_id) {
+  //   this.set('isLoading', true);
+  //   const sponsor = this.store.peekRecord('sponsor', sponsor_id, { backgroundReload: false });
+  //   sponsor.destroyRecord()
+  //     .then(() => {
+  //       this.notify.success(this.l10n.t('Sponsor has been deleted successfully.'),
+  //         {
+  //           id: 'spons_deleted'
+  //         });
+  //       this.refreshModel.bind(this)();
+  //     })
+  //     .catch(() => {
+  //       this.notify.error(this.l10n.t('An unexpected error has occurred.'),
+  //         {
+  //           id: 'spons_deleted_error'
+  //         });
+  //     })
+  //     .finally(() => {
+  //       this.set('isLoading', false);
+  //     });
+  // }
 
-  @action
-  editSponsor() {
-    this.transitionToRoute('events.view.edit.sponsors');
-  }
+  // @action
+  // editSponsor() {
+  //   this.transitionToRoute('events.view.edit.sponsors');
+  // }
 }
 
