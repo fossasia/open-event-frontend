3f2edb3ff9e4e0a39d64c9de3edc3c194ca30f69
iamareebjamal <jamal.areeb@gmail.com>
2020-11-09 08:09:38 +0530

    fix: Display sponsor size according to their level

--- app/components/public/sponsor-list.js
+++ app/components/public/sponsor-list.js
@@ -1,18 +1,50 @@
-import classic from 'ember-classic-decorator';
 import { computed } from '@ember/object';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { orderBy, groupBy } from 'lodash-es';
 
-@classic
 export default class SponsorList extends Component {
+
+  getDistinctLevels(sponsors) {
+    const levels = new Set(sponsors.toArray().map(sponsor => sponsor.level).filter(level => level != null));
+    return [...levels].sort((a, b) => a - b);
+  }
+
+  @computed('sponsors.[]')
+  get distinctLevels() {
+    return this.getDistinctLevels(this.args.sponsors);
+  }
+
+  inferColumns(key, sponsors) {
+    const levels = this.distinctLevels;
+    const groupLevels = this.getDistinctLevels(sponsors);
+
+    const groupLevel = groupLevels.pop();
+    if (groupLevel !== null || groupLevel !== undefined) {
+      const index = levels.indexOf(groupLevel);
+      const columns = ['three', 'four', 'five', 'six', 'seven'];
+      return columns[Math.min(index, columns.length - 1)];
+    }
+
+    return 'three';
+  }
+
   @computed('sponsors.[]')
   get sponsorsGrouped() {
-    return groupBy(
+    const grouped = groupBy(
       orderBy(
-        this.sponsors.toArray(),
+        this.args.sponsors.toArray(),
         sponsor => sponsor.get('level')
       ),
       sponsor => sponsor.get('type')
     );
+
+    const finalGrouped = {};
+    for (const [key, sponsors] of Object.entries(grouped)) {
+      finalGrouped[key] = {
+        sponsors,
+        columns: this.inferColumns(key, sponsors)
+      };
+    }
+    return finalGrouped;
   }
 }
