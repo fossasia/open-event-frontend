0634905a0e7319d493a7d5a183d211a434d4997e
iamareebjamal <jamal.areeb@gmail.com>
2020-11-10 12:22:00 +0530

    fix

--- app/utils/dictionary/social-media.ts
+++ app/utils/dictionary/social-media.ts
@@ -1,4 +1,4 @@
-export const socialMediaNames = ['Twitter', 'Facebook', 'Instagram', 'Linkedin', 'YouTube', 'GitHub', 'GitLab', 'Patreon', 'Vimeo', 'Flicker', 'Google Groups', 'VK', 'Xing', 'Weibo'];
+export const socialMediaNames = ['Twitter', 'Facebook', 'Instagram', 'Linkedin', 'YouTube', 'GitHub', 'GitLab', 'Gitter', 'Telegram', 'Patreon', 'Vimeo', 'Flickr', 'Google Groups', 'VK', 'Xing', 'Weibo'];
 
 const identifierOverrides: { [key: string]: string } = {
   'Google Groups': 'groups.google'
@@ -8,7 +8,10 @@ export const socialMediaIdentifiers = socialMediaNames.map(name => {
   return identifierOverrides[name] ?? name.toLowerCase();
 });
 
-const prefixOverrides: { [key: string]: string } = {};
+const prefixOverrides: { [key: string]: string } = {
+  'gitter'   : 'https://gitter.im/',
+  'telegram' : 'https://t.me/'
+};
 
 export interface SocialMedia {
   identifier: string;
