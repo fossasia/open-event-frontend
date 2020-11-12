d76c0249af2e3ef332c8b7bba16f6d3cbeb172e3
akash2408 <aakash20012408@gmail.com>
2020-11-12 23:20:51 +0530

    add error message on profile image

--- app/components/widgets/forms/image-upload.js
+++ app/components/widgets/forms/image-upload.js
@@ -41,7 +41,9 @@ export default class ImageUpload extends Component {
       })
       .catch(e => {
         console.error('Error while uploading and setting image URL', e);
+        this.notify.error(this.l10n.t('Error while uploading and setting image URL'));
         this.set('uploadingImage', false);
+        this.set('selectedImage', null);
         this.set('errorMessage', this.i18n.t('An unexpected error has occurred.'));
       });
   }
