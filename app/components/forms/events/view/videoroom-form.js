689c6fccd90a63eddc852672ab78ff8952abe9b5
iamareebjamal <jamal.areeb@gmail.com>
2020-11-12 12:28:19 +0530

    fix

---
+++ app/components/forms/events/view/videoroom-form.js
@@ -0,0 +1,60 @@
+import Component from '@ember/component';
+import FormMixin from 'open-event-frontend/mixins/form';
+import { protocolLessValidUrlPattern } from 'open-event-frontend/utils/validators';
+
+
+export default Component.extend(FormMixin, {
+
+  getValidationRules() {
+    window.$.fn.form.settings.rules.checkVideoRoomsLength = () => {
+      return this.data.stream.rooms.length > 0;
+    };
+    const validationRules = {
+      inline : true,
+      delay  : false,
+      on     : 'blur',
+
+      fields: {
+        name: {
+          rules: [
+            {
+              type   : 'empty',
+              prompt : this.l10n.t('Please enter a name')
+            }
+          ]
+        },
+        url: {
+          rules: [
+            {
+              type   : 'empty',
+              prompt : this.l10n.t('Please enter a url')
+            },
+            {
+              type   : 'regExp',
+              value  : protocolLessValidUrlPattern,
+              prompt : this.l10n.t('Please enter a valid url')
+            }
+          ]
+        },
+        rooms: {
+          rules: [
+            {
+              type   : 'checkVideoRoomsLength',
+              prompt : this.l10n.t('Please select a room')
+            }
+          ]
+        }
+      }
+    };
+
+    return validationRules;
+  },
+
+  actions: {
+    submit() {
+      this.onValid(() => {
+        this.save();
+      });
+    }
+  }
+});
--- app/routes/events/view/videoroom/create.js
+++ app/routes/events/view/videoroom/create.js
@@ -6,4 +6,17 @@ export default class CreateRoute extends Route {
   titleToken() {
     return this.l10n.t('Create Video Room');
   }
+
+  async model() {
+    const eventDetails = this.modelFor('events.view');
+    return {
+      event : eventDetails,
+      rooms : await eventDetails.query('microlocations', {
+        'page[size]': 0
+      }),
+      stream: await this.store.createRecord('video-stream', {
+        rooms: []
+      })
+    };
+  }
 }
