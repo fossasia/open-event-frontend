71ec5f8243b0ac38a359ae6c7363efcf7be66222
Harsh Shrivastava <57146965+Draco9421@users.noreply.github.com>
2020-11-17 19:39:18 +0530

    Update test-helper.js

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,7 +3,6 @@ import QUnit from 'qunit';
 import config from 'open-event-frontend/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import './helpers/flash-message';
 
 import loadEmberExam from 'ember-exam/test-support/load';
 
