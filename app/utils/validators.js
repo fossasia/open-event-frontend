fbc22c72154a09499dee4df8dd27c26683126658
rahul bansal <rahulbansal@192.168.1.4>
2020-11-18 18:25:14 +0530

    fix: Invalid Email is no longer allowed

--- app/utils/validators.js
+++ app/utils/validators.js
@@ -23,7 +23,7 @@ export const protocolLessValidUrlPattern = new RegExp(
   // localhost inclusion
   + '(?:(?:localhost)(:\\d{2,5})?|'
   // user:pass authentication
-  + '(?:\\S+(?::\\S*)?@)?'
+  + '/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'
   + '(?:'
   // IP address exclusion
   // private & local networks
