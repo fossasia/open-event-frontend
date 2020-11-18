7d49c05d845a19496fa87f8d2055867b3bf8c15f
sundaram <dubeysundaram057@gmail.com>
2020-11-18 15:14:55 +0530

    when i will find a easy way to do things..

--- app/helpers/round-up.js
+++
@@ -1,8 +0,0 @@
-import Helper from '@ember/component/helper';
-import { ceil } from 'lodash-es';
-
-export function roundUp(params) {
-  return ceil(params[0], 2);
-}
-
-export default Helper.helper(roundUp);
