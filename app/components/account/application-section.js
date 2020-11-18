a2e9b858c863af12f48c5a2aa73335668cac1bbc
DIVYAM TAYAL <daretobedifferent10920@gmail.com>
2020-11-18 08:44:08 +0530

    Update application-section.js

--- app/components/account/application-section.js
+++ app/components/account/application-section.js
@@ -1,6 +1,6 @@
 import classic from 'ember-classic-decorator';
 import { action } from '@ember/object';
-import { inject  as service } from '@ember/service';
+import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
 @classic
