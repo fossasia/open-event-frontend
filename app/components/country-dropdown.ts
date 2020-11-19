7f735e740d7f6469b9a0ad979e4fd021f6bc9551
DIVYAM TAYAL <daretobedifferent10920@gmail.com>
2020-11-19 21:06:48 +0530

    Update country-dropdown.ts

--- app/components/country-dropdown.ts
+++ app/components/country-dropdown.ts
@@ -3,7 +3,7 @@ import { orderBy, filter } from 'lodash-es';
 import { paymentCountries } from 'open-event-frontend/utils/dictionary/payment';
 import { countries } from 'open-event-frontend/utils/dictionary/demography';
 
-export default class CountryDropdown extends Component<CountryDropdownArgs> {
+export default class CountryDropdown extends Component {
   get countries() { // eslint-disable-line @typescript-eslint/explicit-module-boundary-types
     return orderBy(countries, 'name');
   }
