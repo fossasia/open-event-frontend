4bcd9e880dff279c82c0c34c1920d7aead7812dd
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-11-19 21:03:09 +0530

    Improved code

---
+++ app/components/country-dropdown.ts
@@ -0,0 +1,14 @@
+import Component from '@glimmer/component';
+import { orderBy, filter } from 'lodash-es';
+import { paymentCountries } from 'open-event-frontend/utils/dictionary/payment';
+import { countries } from 'open-event-frontend/utils/dictionary/demography';
+
+export default class CountryDropdown extends Component<CountryDropdownArgs> {
+  get countries() { // eslint-disable-line @typescript-eslint/explicit-module-boundary-types
+    return orderBy(countries, 'name');
+  }
+
+  get paymentCountries() { // eslint-disable-line @typescript-eslint/explicit-module-boundary-types
+    return orderBy(filter(countries, country => paymentCountries.includes(country.code)), 'name');
+  }
+}
