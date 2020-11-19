5087262b26737ea09fa84e0d207d64d6e8f220d6
Harsh Shrivastava <Alexshri940@gmail.com>
2020-11-19 18:44:52 +0530

    Added alert message to Order button

--- app/controllers/public/index.js
+++ app/controllers/public/index.js
@@ -105,7 +105,12 @@ export default class IndexController extends Controller {
     }
     if (!this.session.isAuthenticated) {
       this.set('userExists', false);
-      this.set('isLoginModalOpen', true);
+      this.flashMessages.add({
+        message           : 'In order to buy tickets you need to login. If you have not registered yet, please create an account first. Thank you!',
+        type              : 'info',
+      });
+      this.transitionToRoute('login');
+      // this.set('isLoginModalOpen', true);
       return;
     }
     this.send('save');
