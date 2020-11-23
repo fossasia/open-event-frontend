35cb55c9d3c55c29c7174ab918b2278a4010ed5d
hb <hb@rm.com>
2020-11-24 02:50:00 +0900

    Fix Codacy errors

--- app/components/forms/speaker-section.js
+++ app/components/forms/speaker-section.js
@@ -1,12 +1,12 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 import FormMixin from 'open-event-frontend/mixins/form';
 import { countries } from 'open-event-frontend/utils/dictionary/demography';
 import { languages } from 'open-event-frontend/utils/dictionary/languages';
 import { genders } from 'open-event-frontend/utils/dictionary/genders';
+import { orderBy } from 'lodash-es';
 
 export default Component.extend(FormMixin, {
-  countries: orderBy(countries, 'name'),
-  languages: orderBy(languages, 'name'),
-  genders: orderBy(genders, 'name')
+  countries : orderBy(countries, 'name'),
+  languages : orderBy(languages, 'name'),
+  genders : orderBy(genders, 'name')
 });
