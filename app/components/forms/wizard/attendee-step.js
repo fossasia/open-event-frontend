68fc854690720f4dfbf42aeb8e894408c650f7ab
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-11-23 20:48:33 +0530

    Save Attedee Form

--- app/components/forms/wizard/attendee-step.js
+++ app/components/forms/wizard/attendee-step.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { computed, observer } from '@ember/object';
 import FormMixin from 'open-event-frontend/mixins/form';
 import EventWizardMixin from 'open-event-frontend/mixins/event-wizard';
 import { sortBy } from 'lodash-es';
@@ -13,6 +13,12 @@ export default Component.extend(FormMixin, EventWizardMixin, {
     const filteredFields = this.data.customForms?.filter(field => !field.isFixed);
     const fields = sortBy(filteredFields, ['isComplex', 'name']);
     return fields;
+  }),
+
+  revertChanges: observer('data.event.isTicketFormEnabled', function() {
+    if(!this.data.event.isTicketFormEnabled){
+      this.editableFields.forEach(field => field.set('isRequired', false))
+    }
   }),
 
   showEditColumn: computed('editableFields.@each', function() {
