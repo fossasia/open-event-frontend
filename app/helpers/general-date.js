8760c75eb622b64126152e6d37f4434e1b35ddfa
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-11-25 13:47:07 +0530

    Changed to 24h

--- app/helpers/general-date.js
+++ app/helpers/general-date.js
@@ -3,7 +3,19 @@ import moment from 'moment';
 
 export function generalDate(params) {
   const timezone = params[1] || moment.tz.guess();
-  const format = params[2] || 'h:mm A, MMMM Do YYYY (z)';
+  let changeTo24 = false;
+
+  let format = params[2] || 'h:mm A, MMMM Do YYYY (z)';
+
+  if (moment(params[0]).tz(timezone).format('A') === 'AM' || moment(params[0]).tz(timezone).format('A') === 'PM') {
+    changeTo24 = true;
+  }
+
+  if (changeTo24) {
+    format = format.replaceAll('h', 'H');
+    format = format.replace('A', '');
+    format = format.replace('a', '');
+  }
   return moment(params[0]).tz(timezone).format(format);
 }
 
