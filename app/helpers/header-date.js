f07c128fe8ed514102948554a438035ca7f8d539
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-11-25 14:08:31 +0530

    Changed to 24h-1

--- app/helpers/header-date.js
+++ app/helpers/header-date.js
@@ -3,7 +3,15 @@ import moment from 'moment';
 
 export function headerDate(params) {
   const timezone = params[1] ? params[1] : moment.tz.guess();
-  return `${moment(params[0]).tz(timezone).format('dddd, MMMM Do YYYY, h:mm A')} (${moment.tz(params[0], timezone).zoneAbbr()})`;
+
+  let format = 'dddd, MMMM Do YYYY, h:mm A';
+
+  if (moment(params[0]).tz(timezone).format('A') === 'AM' || moment(params[0]).tz(timezone).format('A') === 'PM') {
+    format = format.replaceAll('h', 'H');
+    format = format.replace('A', '');
+  }
+  
+  return `${moment(params[0]).tz(timezone).format(format)} (${moment.tz(params[0], timezone).zoneAbbr()})`;
 }
 
 export default Helper.helper(headerDate);
