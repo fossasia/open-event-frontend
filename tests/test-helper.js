18f775c50c3fe7a13b93b548c9f5f3ff6687fce8
Harsh Shrivastava <Alexshri940@gmail.com>
2020-11-28 01:03:37 +0530

    designing changes

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,7 +3,6 @@ import QUnit from 'qunit';
 import config from 'open-event-frontend/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-
 import loadEmberExam from 'ember-exam/test-support/load';
 
 QUnit.config.testTimeout = 300000;
