0808091295c5c8fd417456868e02ccb21e116957
sundaram <dubeysundaram057@gmail.com>
2020-11-29 07:48:14 +0530

    f

--- app/components/widgets/forms/link-field.ts
+++ app/components/widgets/forms/link-field.ts
@@ -63,11 +63,8 @@ export default class LinkField extends Component<Args> {
   setValue(event: { target: HTMLInputElement }): void {
     const text = event.target.value;
     this.value = this.fixValue(text);
-    if (this.value) {
-      this.args.onChange(this.finalValue);
-    } else {
-      this.args.onChange(null);
-    }
+    const finalUrl = this.value ? this.finalValue : null;
+    this.args.onChange(finalUrl);
   }
 
   @action
