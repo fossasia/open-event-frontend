5eda4c45928ad1945a9f78e9f88fd0dd0f340ac6
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-11-30 10:13:48 +0530

    Added date formats-1

--- app/helpers/general-date.js
+++ app/helpers/general-date.js
@@ -11,19 +11,18 @@ const dateFormats = {
 
 const locales12Hours = new Set(['en', 'bn', 'hi', 'id', 'ja', 'run', 'th', 'vi', 'ko']);
 
-export function generalDate(params, { tz }) {
+export function generalDate(params, { ft, tz }) {
   const timezone = tz || moment.tz.guess();
 
   const local = moment(params[0]).tz(timezone).locale();
 
-  let format = params[1] || 'h:mm A, MMMM Do YYYY (z)';
+  let format = (params[1] || dateFormats[ft]) || 'h:mm A, MMMM Do YYYY (z)';
 
   if (!locales12Hours.has(local)) {
     format = format.replaceAll('h', 'H');
     format = format.replace(' A', '');
     format = format.replace(' a', '');
   }
-  
   return moment(params[0]).tz(timezone).format(format);
 }
 
