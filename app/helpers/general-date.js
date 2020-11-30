5d96783f8d5cac8016fccb7609a97f95acd199db
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-11-30 09:51:11 +0530

    Added date formats

--- app/helpers/general-date.js
+++ app/helpers/general-date.js
@@ -1,6 +1,14 @@
 import Helper from '@ember/component/helper';
 import moment from 'moment';
 
+const dateFormats = {
+  'date-time-long'     : 'dddd, D MMMM, YYYY h:mm A',
+  'date-time-tz-long'  : 'dddd, D MMMM, YYYY h:mm A (z)',
+  'date-time-short'    : 'D MMM, YYYY h:mm A',
+  'date-time-tz-short' : 'D MMM, YYYY h:mm A (z)',
+  'date-short'         : 'D MMM, YYYY'
+};
+
 export function generalDate(params) {
   const timezone = params[1] || moment.tz.guess();
   const format = params[2] || 'h:mm A, MMMM Do YYYY (z)';
