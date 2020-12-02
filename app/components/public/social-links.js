297409a0e6449e3012d4fba7e0f080bd4c376005
Schin Chauhan <sachinchauhan2889@gmail.com>
2020-12-02 18:05:21 +0530

    code-cleanup

--- app/components/public/social-links.js
+++ app/components/public/social-links.js
@@ -1,6 +1,5 @@
 import { A } from '@ember/array';
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
 export default Component.extend({
   classNames: ['ui', 'basic', 'segment', 'm-0', 'pt-0'],
