fc0e28f86d094ca52a5913c94c5a36562b340dab
Meghal <mailtomeghu25@gmail.com>
2020-12-03 20:51:41 +0530

    Duration Picker added for WS-5

--- app/utils/dictionary/date-time.ts
+++ app/utils/dictionary/date-time.ts
@@ -5,5 +5,6 @@ export const timezones = moment.tz.names();
 
 export const FORM_DATE_FORMAT = 'MM-DD-YYYY';
 export const FORM_TIME_FORMAT = 'HH:mm';
+export const FORM_DURATION_FORMAT = 'H.mm';
 
 export const FORM_DATE_TIME_FORMAT = `${FORM_DATE_FORMAT} ${FORM_TIME_FORMAT}`;
