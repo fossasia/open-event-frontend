f353b2d4e4c2eecad6d54c3879061a90f7edc5cd
Harsh Shrivastava <Alexshri940@gmail.com>
2020-12-04 20:01:11 +0530

    fixed integration test for Cell-Ticket

--- tests/integration/components/ui-table/cell/cell-tickets-test.js
+++ tests/integration/components/ui-table/cell/cell-tickets-test.js
@@ -6,15 +6,15 @@ import { render } from '@ember/test-helpers';
 module('Integration | Component | ui table/cell/cell tickets', function(hooks) {
   setupIntegrationTest(hooks);
 
-  const record = [{ type: 'Premium', order: 12, total: 100 }];
+  const extraRecords = {tickets :[{ type: 'Premium', order: 12, total: 100 }] };
   test('it renders', async function(assert) {
-    this.set('record', record);
-    await render(hbs `{{ui-table/cell/cell-tickets record=record}}`);
+    this.set('extraRecords', extraRecords);
+    await render(hbs `{{ui-table/cell/cell-tickets extraRecords=extraRecords}}`);
     assert.ok(this.element.innerHTML.trim().includes('Premium'));
   });
 
   test('it renders', async function(assert) {
-    await render(hbs `{{ui-table/cell/cell-tickets record=record}}`);
+    await render(hbs `{{ui-table/cell/cell-tickets extraRecords=extraRecords}}`);
     assert.ok(this.element.innerHTML.trim().includes('No Ticket Information'));
   });
 });
