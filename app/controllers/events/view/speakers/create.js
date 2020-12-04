7288c94b6bf40a9768b5c4e79ff58fe383ccd553
sundaram <dubeysundaram057@gmail.com>
2020-12-04 13:47:06 +0530

    f

--- app/controllers/events/view/speakers/create.js
+++ app/controllers/events/view/speakers/create.js
@@ -4,7 +4,7 @@ import { inject as service } from '@ember/service';
 
 export default class extends Controller {
   @service errorHandler;
-  
+
   @action
   async save(sessionDetails) {
     try {
--- app/services/error-handler.js
+++ app/services/error-handler.js
@@ -4,26 +4,26 @@ import Service from '@ember/service';
 @classic
 export default class ErrorHandlerService extends Service {
 
-genericError(error) {
+  genericError(error) {
     const errorStatus =  parseInt(error.errors[0].status);
-    if(errorStatus === 500) {
-        return "There is an error error in server.";
+    if (errorStatus === 500) {
+      return 'There is an error error in server.';
     } else if (errorStatus === 403) {
-        return "You are not authorized to access the page.";
+      return 'You are not authorized to access the page.';
     } else if (errorStatus === 400) {
-        return "The server could not understand the request due to invalid syntax.";
-    } else if(errorStatus === 401) {
-        return "The client must authenticate itself to get the requested response."
-    } else if(errorStatus === 405) {
-        return "The request method is known by the server but has been disabled and cannot be used."
-    } else if(errorStatus === 409) {
-        return "This response is sent when a request conflicts with the current state of the server."
-    } else if(errorStatus === 422) {
-        return "Unprocessable Entity";
-    } else if(errorStatus === 429) {
-        return "Too many reuqests.";
+      return 'The server could not understand the request due to invalid syntax.';
+    } else if (errorStatus === 401) {
+      return 'The client must authenticate itself to get the requested response.';
+    } else if (errorStatus === 405) {
+      return 'The request method is known by the server but has been disabled and cannot be used.';
+    } else if (errorStatus === 409) {
+      return 'This response is sent when a request conflicts with the current state of the server.';
+    } else if (errorStatus === 422) {
+      return 'Unprocessable Entity';
+    } else if (errorStatus === 429) {
+      return 'Too many reuqests.';
     } else {
-        return "Opps Something went wrong.";
+      return 'Opps Something went wrong.';
     }
-}
+  }
 }
