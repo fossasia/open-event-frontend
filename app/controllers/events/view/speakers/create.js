a4b39bb669c3f586af34f11ecf3bf04ca35571cb
sundaram <dubeysundaram057@gmail.com>
2020-12-05 13:48:27 +0530

    upgrading..

--- app/controllers/events/view/speakers/create.js
+++ app/controllers/events/view/speakers/create.js
@@ -29,7 +29,7 @@ export default class extends Controller {
       this.transitionToRoute('events.view.speakers', this.model.event.id);
     } catch (e) {
       console.error('Error while saving session', e);
-      this.notify.error(this.l10n.t(this.errorHandler.genericError(e)));
+      this.errorHandler.genericError(e);
     } finally {
       this.set('isLoading', false);
     }
