2a7b29f11e2e7b0eac08e2279c53199cfdc29d46
Schin Chauhan <sachinchauhan2889@gmail.com>
2020-12-08 20:34:32 +0530

    fixs

--- app/routes/explore.js
+++ app/routes/explore.js
@@ -79,14 +79,14 @@ export default class ExploreRoute extends Route {
         val  : params.is_online
       });
     }
-    if (!params.has_image) {
+    if (params.has_image) {
       filterOptions.push({
         name : 'original-image-url',
         op   : 'ne',
         val  : null
       });
     }
-    if (!params.has_logo) {
+    if (params.has_logo) {
       filterOptions.push({
         name : 'logo-url',
         op   : 'ne',
