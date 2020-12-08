4362e224bea456c41816ba836fb4cfc640326cf7
Schin Chauhan <sachinchauhan2889@gmail.com>
2020-12-08 21:31:38 +0530

    reimplementation

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,27 @@
 import classic from 'ember-classic-decorator';
-import { action } from '@ember/object';
+import { action, computed } from '@ember/object';
 import Controller from '@ember/controller';
+import moment from 'moment';
 
 @classic
 export default class IndexController extends Controller {
+  @computed('filteredEvents.[]', 'featuredEvents.[]')
+  get callForSpeakersEvents() {
+    const filteredEventsCfs = this.filteredEvents.filter(isEventCfsOpen);
+    const featuredEventsCfs = this.featuredEvents.filter(isEventCfsOpen);
+    const combinedCfsEvents = new Set(filteredEventsCfs.concat(featuredEventsCfs));
+    return Array.from(combinedCfsEvents);
+  }
+
+  @computed('filteredEvents.[]')
+  get promotedEvents() {
+    return this.filteredEvents?.filter(event => event.isPromoted);
+  }
+
+  @computed('filteredEvents.[]')
+  get upcomingEvents() {
+    return this.filteredEvents?.filter(event => !event.isPromoted);
+  }
 
   @action
   handleKeyPress() {
@@ -22,4 +40,16 @@ export default class IndexController extends Controller {
     this.set('eventToShare', event);
     this.set('isShareModalOpen', true);
   }
+}
+
+function isEventCfsOpen(event) {
+  const callForPapers = event.get('speakersCall');
+  const sessionEnabled = event.isSessionsSpeakersEnabled;
+  if (!callForPapers || !callForPapers.get('startsAt')  || !callForPapers.get('endsAt')) {
+    return false;
+  }
+  const startDateTime = callForPapers.get('startsAt');
+  const endDateTime = callForPapers.get('endsAt');
+  const privacyState = callForPapers.get('privacy');
+  return (moment().isBetween(startDateTime, endDateTime) && (sessionEnabled) && (privacyState === 'public'));
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,18 +1,72 @@
 import classic from 'ember-classic-decorator';
+import { action } from '@ember/object';
 import Route from '@ember/routing/route';
 import moment from 'moment';
 import { hash } from 'rsvp';
 
 @classic
 export default class IndexRoute extends Route {
-  async model() {
-    const featuredEventsFilter = [
+  /**
+   * Load filtered events based on the given params
+   *
+   * @param params
+   * @param mode
+   * @return {*}
+   * @private
+   */
+  _loadEvents(mode) {
+    const filterOptions = [
       {
-        name : 'is-featured',
-        op   : 'eq',
-        val  : true
+        and:
+        [
+          {
+            name : 'state',
+            op   : 'eq',
+            val  : 'published'
+          },
+          {
+            name : 'privacy',
+            op   : 'eq',
+            val  : 'public'
+          }
+        ]
       }
     ];
+
+    filterOptions.push({
+      or: [
+        {
+          name : 'starts-at',
+          op   : 'ge',
+          val  : moment().toISOString()
+        },
+        {
+          name : 'ends-at',
+          op   : 'ge',
+          val  : moment().toISOString()
+        }
+      ]
+    });
+
+    if (mode === 'filterOptions') {
+      return filterOptions;
+    } else {
+      return this.store.query('event', {
+        sort    : 'starts-at',
+        include : 'event-topic,event-sub-topic,event-type,speakers-call',
+        filter  : filterOptions
+      });
+    }
+
+  }
+
+  async model() {
+    const filterOptions =  this._loadEvents('filterOptions');
+    filterOptions[0].and.push({
+      name : 'is-featured',
+      op   : 'eq',
+      val  : true
+    });
 
     const upcomingEventsFilter = [
       {
@@ -60,7 +114,9 @@ export default class IndexRoute extends Route {
       }
     ];
 
-    const callForSpeakersFilter = [
+    const callForSpeakersFilter = this._loadEvents('filterOptions');
+    callForSpeakersFilter[0].and = [
+      ...callForSpeakersFilter[0].and,
       ...upcomingEventsFilter,
       {
         name : 'is-sessions-speakers-enabled',
@@ -74,6 +130,15 @@ export default class IndexRoute extends Route {
           name : 'privacy',
           op   : 'eq',
           val  : 'public'
+        }
+      },
+      {
+        name : 'speakers-call',
+        op   : 'has',
+        val  : {
+          name : 'starts-at',
+          op   : 'le',
+          val  : moment().toISOString()
         }
       },
       {
@@ -97,21 +162,20 @@ export default class IndexRoute extends Route {
     ];
 
     return hash({
-      featuredEvents: this.store.query('event', {
-        sort         : 'starts-at',
-        include      : 'event-topic,event-sub-topic,event-type,speakers-call',
-        filter       : featuredEventsFilter,
-        cache        : true,
-        public       : true,
-        'page[size]' : 6
-      }),
-      upcomingEvents: this.store.query('event', {
+      filteredEvents: this.store.query('event', {
         upcoming     : true,
         include      : 'event-topic,event-sub-topic,event-type,speakers-call',
-        filter       : upcomingEventsFilter,
         cache        : true,
         public       : true,
         'page[size]' : 12
+      }),
+      featuredEvents: this.store.query('event', {
+        sort         : 'starts-at',
+        include      : 'event-topic,event-sub-topic,event-type,speakers-call',
+        filter       : filterOptions,
+        cache        : true,
+        public       : true,
+        'page[size]' : 6
       }),
       callForSpeakersEvents: this.store.query('event', {
         sort         : 'starts-at',
@@ -126,10 +190,25 @@ export default class IndexRoute extends Route {
 
   setupController(controller, model) {
     super.setupController(...arguments);
+    controller.set('filteredEvents', model.filteredEvents);
     controller.set('featuredEvents', model.featuredEvents);
-    controller.set('upcomingEvents', model.upcomingEvents);
-    controller.set('callForSpeakersEvents', model.callForSpeakersEvents);
     this.set('controller', controller);
   }
 
+  @action
+  async queryParamsDidChange(change) {
+    if (this.controller) {
+      this.controller.set('filteredEvents', await this._loadEvents());
+    }
+  }
+
+  @action
+  loading(transition) {
+    transition.promise.finally(() => {
+      if (this.controller) {
+        this.controller.set('finishedLoading', true);
+      }
+    });
+    return false;
+  }
 }
