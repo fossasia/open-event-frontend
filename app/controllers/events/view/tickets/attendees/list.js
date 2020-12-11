aab2c34bf269ca6baf3a2fb716a29b6863624b3c
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-12-12 02:48:18 +0530

    Enhance Table heading

--- app/controllers/events/view/tickets/attendees/list.js
+++ app/controllers/events/view/tickets/attendees/list.js
@@ -3,7 +3,7 @@ import { action } from '@ember/object';
 import EmberTableControllerMixin from 'open-event-frontend/mixins/ember-table-controller';
 import moment from 'moment';
 
-
+// Start
 export default class extends Controller.extend(EmberTableControllerMixin) {
   get columns() {
     return [
