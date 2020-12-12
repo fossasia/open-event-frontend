c52a53c8d3b1f3aab59b23d59f0f990aa772d89b
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-12-13 02:39:14 +0530

    Fix Test

--- tests/integration/components/ui-table/cell/events/view/videoroom/cell-stream-url-test.ts
+++ tests/integration/components/ui-table/cell/events/view/videoroom/cell-stream-url-test.ts
@@ -7,11 +7,30 @@ module('Integration | Component | ui-table/cell/events/view/videoroom/cell-strea
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    this.set('record', { id: 23, name: 'Main Hall', slugName: 'main-hall' });
-    this.set('extraRecords', { event: { identifier: 'abcd' } });
+    this.set('record', { get(dataType: string) {
+      switch (dataType) {
+        case 'id':
+          return 23
+        case 'name':
+          return 'Main Hall'
+        case 'slugName':
+          return 'main-hall'
+        default:
+          return
+      }
+    } });
+    this.set('extraRecords', {
+      identifier : 'ds5fe45s',
+      event      : { get(dataType: string) {
+        switch (dataType) {
+          case 'identifier':
+            return 'd2e1d5626'
+          default: return;
+        }
+      } } });
     await render(hbs`{{ui-table/cell/events/view/videoroom/cell-stream-url record=record extraRecords=extraRecords}}`);
-    assert.ok(this.element.querySelector('a'));
+    assert.ok(this.element.querySelector('div'));
     this.set('record', null);
-    assert.notOk(this.element.querySelector('a'));
+    assert.notOk(this.element.querySelector('div'));
   });
 });
