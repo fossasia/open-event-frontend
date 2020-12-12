17bd877dcabd8aa872f2eed9afe1a9b8c136df43
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-12-12 23:59:57 +0530

    Fix test-1

--- tests/integration/components/nav-bar-test.js
+++ tests/integration/components/nav-bar-test.js
@@ -8,6 +8,6 @@ module('Integration | Component | nav bar', function(hooks) {
 
   test('it renders', async function(assert) {
     await render(hbs`{{nav-bar}}`);
-    assert.ok(this.element.innerHTML.trim().includes('Search for events'));
+    assert.ok(this.element.innerHTML.trim().includes('Search Events'));
   });
 });
