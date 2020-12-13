bc946a2b29e9923318d373d2b83532ba0defc2f2
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-12-13 17:27:38 +0530

    Fix Test-1

--- tests/integration/components/ui-table/cell/events/view/videoroom/cell-stream-url-test.ts
+++ tests/integration/components/ui-table/cell/events/view/videoroom/cell-stream-url-test.ts
@@ -2,35 +2,19 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import EmberObject from '@ember/object';
 
 module('Integration | Component | ui-table/cell/events/view/videoroom/cell-stream-url', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    this.set('record', { get(dataType: string) {
-      switch (dataType) {
-        case 'id':
-          return 23
-        case 'name':
-          return 'Main Hall'
-        case 'slugName':
-          return 'main-hall'
-        default:
-          return
-      }
-    } });
-    this.set('extraRecords', {
-      identifier : 'ds5fe45s',
-      event      : { get(dataType: string) {
-        switch (dataType) {
-          case 'identifier':
-            return 'd2e1d5626'
-          default: return;
-        }
-      } } });
+    const record = EmberObject.create({ id: 23, name: 'Main Hall', slugName: 'main-hall' })
+    const extraRecords = EmberObject.create({ identifier: 45, event: { identifier: 'abcd' } });
+    this.set('record', record);
+    this.set('extraRecords', extraRecords);
     await render(hbs`{{ui-table/cell/events/view/videoroom/cell-stream-url record=record extraRecords=extraRecords}}`);
-    assert.ok(this.element.querySelector('div'));
+    assert.ok(this.element.querySelector('a'));
     this.set('record', null);
-    assert.notOk(this.element.querySelector('div'));
+    assert.notOk(this.element.querySelector('a'));
   });
 });
