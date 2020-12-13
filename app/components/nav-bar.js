3083eef67d39a7a59517e81a10f40c7716831238
daretobedifferent18 <daretobedifferent10920@gmail.com>
2020-12-13 23:48:07 +0530

    Add Search bar in Navbar in mobile view

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -18,7 +18,13 @@ export default class NavBar extends Component {
   handleKeyPress() {
     if (event.code === 'Enter') {
       this.sendAction('search');
+      document.querySelector('#mobile-bar').classList.remove('show-bar');
     }
+  }
+
+  @action
+  toggleSearchBar() {
+    document.querySelector('#mobile-bar').classList.toggle('show-bar');
   }
 
   @action
