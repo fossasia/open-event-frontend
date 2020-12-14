bc00692d862999d115598a5c1230327e4eedd5af
Creator5923 <nlokhande5923@gmail.com>
2020-12-14 19:12:43 +0530

    removed limit from start date and time

--- app/components/forms/wizard/basic-details-step.js
+++ app/components/forms/wizard/basic-details-step.js
@@ -464,9 +464,9 @@ export default Component.extend(FormMixin, EventWizardMixin, {
     updateSalesEndDate(eventStartDate) {
       eventStartDate = moment(new Date(eventStartDate));
       this.data.event.tickets.forEach(ticket => {
-        if (moment(eventStartDate).isBefore(ticket.get('salesEndsAt'))) {
+        // if (moment(eventStartDate).isBefore(ticket.get('salesEndsAt'))) {
           ticket.set('salesEndsAt', moment(eventStartDate, 'MM/DD/YYYY').toDate());
-        }
+        // }
       });
     },
 
