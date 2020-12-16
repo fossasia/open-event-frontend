c420383e6eb95f0ee1f48a176e773bd3fc957471
sundaram <dubeysundaram057@gmail.com>
2020-12-16 23:18:34 +0530

    f

---
+++ app/helpers/format-money.js
@@ -0,0 +1,33 @@
+import Helper from '@ember/component/helper';
+import { inject as service } from '@ember/service';
+
+const locales = {
+  'en'      : 'en',
+  'hi'      : 'hi',
+  'bn'      : 'bn',
+  'de'      : 'de-DE',
+  'es'      : 'es',
+  'id'      : 'id',
+  'pl'      : 'pl',
+  'ja'      : 'ja',
+  'ru'      : 'ru',
+  'th'      : 'th',
+  'vi'      : 'vi',
+  'zh_Hans' : 'zh-CN',
+  'zh_Hant' : 'zh-TW',
+  'ko'      : 'ko'
+};
+
+export default Helper.extend({
+  l10n: service(),
+
+  compute(params) {
+    const x = new Intl.NumberFormat().resolvedOptions().locale;
+    let currentLocale = 'de';
+    if (x === 'en-US') {
+      currentLocale = this.l10n.getLocale();
+    }
+
+    return (params[0])?.toLocaleString(locales[currentLocale]);
+  }
+});
