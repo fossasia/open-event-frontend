869fc1e8ff0d09cf1354c20ee7edf523b44daf24
sundaram <dubeysundaram057@gmail.com>
2020-12-16 08:40:09 +0530

    f

--- app/components/public/event-map.js
+++ app/components/public/event-map.js
@@ -2,14 +2,14 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 import ENV from 'open-event-frontend/config/environment';
 
-export default Component.extend ({
-    mapTile :  ENV.mapboxToken ? `https://api.mapbox.com/styles/v1/mapbox/emerald-v8/tiles/{z}/{x}/{y}?access_token=${ENV.mapboxToken}` : 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+export default Component.extend({
+  mapTile: ENV.mapboxToken ? `https://api.mapbox.com/styles/v1/mapbox/emerald-v8/tiles/{z}/{x}/{y}?access_token=${ENV.mapboxToken}` : 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 
-    latitude: computed('event', function() {
-        return this.event?.latitude ? this.event.latitude : 20;
-    }),
+  latitude: computed('event', function() {
+    return this.event?.latitude ? this.event.latitude : 20;
+  }),
 
-    longitude: computed('event', function() {
-        return this.event?.longitude ? this.event.longitude : 80;
-    }),
+  longitude: computed('event', function() {
+    return this.event?.longitude ? this.event.longitude : 80;
+  })
 });
