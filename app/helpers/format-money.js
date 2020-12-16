d93f6fa84f8fee9e3c294dc3e2335fcbd820f522
sundaram <dubeysundaram057@gmail.com>
2020-12-16 23:47:49 +0530

    key-value map

--- app/helpers/format-money.js
+++ app/helpers/format-money.js
@@ -2,32 +2,20 @@ import Helper from '@ember/component/helper';
 import { inject as service } from '@ember/service';
 
 const locales = {
-  'en'      : 'en',
-  'hi'      : 'hi',
-  'bn'      : 'bn',
-  'de'      : 'de-DE',
-  'es'      : 'es',
-  'id'      : 'id',
-  'pl'      : 'pl',
-  'ja'      : 'ja',
-  'ru'      : 'ru',
-  'th'      : 'th',
-  'vi'      : 'vi',
-  'zh_Hans' : 'zh-CN',
-  'zh_Hant' : 'zh-TW',
-  'ko'      : 'ko'
+    "de": "de-DE",
+    "zh_Hans": "zh-CN",
+    "zh_Hant": "zh-TW",
 };
 
 export default Helper.extend({
-  l10n: service(),
-
-  compute(params) {
-    const x = new Intl.NumberFormat().resolvedOptions().locale;
-    let currentLocale = 'en';
-    if (x === 'en-US') {
-      currentLocale = this.l10n.getLocale();
+    l10n: service(),
+  
+    compute(params) {
+        const x = new Intl.NumberFormat().resolvedOptions().locale;
+        let currentLocale = "en";
+        if(x === "en-US") {
+            currentLocale = this.l10n.getLocale();
+        }    
+        return (params[0])?.toLocaleString(locales[currentLocale] ? locales[currentLocale] : x);
     }
-
-    return (params[0])?.toLocaleString(locales[currentLocale]);
-  }
 });
