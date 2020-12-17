b0f0b7b8b87a56f54b22b2d1b95e758944c7612f
sundaram <dubeysundaram057@gmail.com>
2020-12-17 11:49:21 +0530

    f

--- app/components/currency-amount.js
+++ app/components/currency-amount.js
@@ -3,22 +3,22 @@ import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 
 const locales = {
-    "de": "de-DE",
-    "zh_Hans": "zh-CN",
-    "zh_Hant": "zh-TW",
+  'de'      : 'de-DE',
+  'zh_Hans' : 'zh-CN',
+  'zh_Hant' : 'zh-TW'
 };
 
 export default class CurrencyAmount extends Component {
- 
+
     @service l10n;
-    
+
     @computed('amount', 'currency')
     get modifiedAmount() {
-        const resolvedLocale = new Intl.NumberFormat().resolvedOptions().locale;
-        let currentLocale = "en";
-        if(resolvedLocale === "en-US") {
-            currentLocale = this.l10n.getLocale();
-        }    
-        return (this.amount)?.toLocaleString(locales[currentLocale] ?? resolvedLocale, {style: "currency", currency: this.currency, minimumFractionDigits: 2});
+      const resolvedLocale = new Intl.NumberFormat().resolvedOptions().locale;
+      let currentLocale = 'en';
+      if (resolvedLocale === 'en-US') {
+        currentLocale = this.l10n.getLocale();
+      }
+      return (this.amount)?.toLocaleString(locales[currentLocale] ?? resolvedLocale, { style: 'currency', currency: this.currency, minimumFractionDigits: 2 });
     }
 }
