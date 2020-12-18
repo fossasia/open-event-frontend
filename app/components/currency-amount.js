c735db0abe34fd2665e97ec928a24ca2c03e0e96
sundaram <dubeysundaram057@gmail.com>
2020-12-18 10:04:28 +0530

    -

--- app/components/currency-amount.js
+++ app/components/currency-amount.js
@@ -10,12 +10,15 @@ const locales = {
 export default class CurrencyAmount extends Component {
 
     @service l10n;
-
+   
     get modifiedAmount() {
       const resolvedLocale = new Intl.NumberFormat().resolvedOptions().locale;
       let currentLocale = 'en';
       if (resolvedLocale === 'en-US') {
         currentLocale = this.l10n.getLocale();
+      }
+      if(this.args.amount <= 0){
+        return "0.00";
       }
       return (this.args.amount)?.toLocaleString(locales[currentLocale] ?? resolvedLocale, { style: 'currency', currency: this.args.currency, minimumFractionDigits: 2 });
     }
