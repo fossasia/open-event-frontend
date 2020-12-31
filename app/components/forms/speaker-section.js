174b87104a17bfea1f18e60ba5cd8e262e283620
Schin Chauhan <sachinchauhan2889@gmail.com>
2021-01-01 01:55:37 +0530

    add-session-speaker-form

---
+++ app/components/forms/speaker-section.js
@@ -0,0 +1,10 @@
+import Component from '@ember/component';
+import FormMixin from 'open-event-frontend/mixins/form';
+import { countries } from 'open-event-frontend/utils/dictionary/demography';
+import { genders } from 'open-event-frontend/utils/dictionary/genders';
+import { orderBy } from 'lodash-es';
+
+export default Component.extend(FormMixin, {
+  countries : orderBy(countries, 'name'),
+  genders   : orderBy(genders, 'name')
+});
