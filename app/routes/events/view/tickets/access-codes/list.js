e2df533902f3d8f6cdac5752a7e3f43ca8ee960d
Saumya Chitransh <Saumyachitransh3001@gmail.com>
2021-01-05 20:49:24 +0530

    Update list.js

--- app/routes/events/view/tickets/access-codes/list.js
+++ app/routes/events/view/tickets/access-codes/list.js
@@ -15,6 +15,7 @@ export default class extends Route.extend(EmberTableRouteMixin) {
   }
 
   async model(params) {
+    this.set('params', params);
     let filterOptions = [];
     const searchField = 'code';
     if (params.access_status === 'active') {
