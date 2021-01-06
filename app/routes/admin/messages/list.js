edb3ec1b6bc869df78d98ace4a39ddd144d91a97
sundaram <dubeysundaram057@gmail.com>
2021-01-06 22:25:27 +0530

    just

--- app/routes/admin/messages/list.js
+++ app/routes/admin/messages/list.js
@@ -9,7 +9,7 @@ export default class extends Route.extend(EmberTableRouteMixin) {
     let queryString = {
       filter        : filterOptions,
       'page[size]'  : params.per_page || 100,
-      'page[number' : params.page || 1
+      'page[number]' : params.page || 1
     };
     queryString = this.applySortFilters(queryString, params);
     return this.asArray(this.modelFor('admin.messages', queryString));
