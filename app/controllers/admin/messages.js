34a5d369bf569e03cd924b5fc97cbff13306ca3e
sundaram <dubeysundaram057@gmail.com>
2021-01-08 16:31:39 +0530

    reverting with modification

--- app/controllers/admin/messages.js
+++ app/controllers/admin/messages.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import EmberTableControllerMixin from 'open-event-frontend/mixins/ember-table-controller';
 
 export default class extends Controller.extend(EmberTableControllerMixin) {
@@ -52,5 +53,25 @@ get columns() {
     }
 
   ];
+};
+
+@action
+save() {
+  try {
+    const systemMessages = this.model;
+    Array.from(systemMessages).forEach(systemMessage => {
+      systemMessage.save();
+    });
+    this.notify.success(this.l10n.t('Changes have been saved successfully'),
+      {
+        id: 'message_success'
+      });
+  } catch (e) {
+    console.error('Error while saving system messages', e);
+    this.notify.error(e.errors[0].detail,
+      {
+        id: 'change_error_message'
+      });
+  }
 }
 }
