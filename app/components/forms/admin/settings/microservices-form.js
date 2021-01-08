19b74b734271ebfe9fb6c7f2349ee5bd3d0de2ec
Saumya Chitransh <saumyachitransh3001@gmail.com>
2021-01-08 19:59:56 +0530

    removing sendAction()

--- app/components/forms/admin/settings/microservices-form.js
+++ app/components/forms/admin/settings/microservices-form.js
@@ -38,7 +38,7 @@ export default Component.extend(FormMixin, {
   actions: {
     submit() {
       this.onValid(() => {
-        this.sendAction('save');
+        this.save();
       });
     }
   }
