f35918325ab8f29667008d8e7890e0e080a0f634
sundaram <dubeysundaram057@gmail.com>
2021-01-08 15:51:06 +0530

    fix

--- app/controllers/admin/messages.js
+++ app/controllers/admin/messages.js
@@ -1,5 +1,4 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import EmberTableControllerMixin from 'open-event-frontend/mixins/ember-table-controller';
 
 export default class extends Controller.extend(EmberTableControllerMixin) {
@@ -53,25 +52,5 @@ get columns() {
     }
 
   ];
-}
-
-@action
-save() {
-  try {
-    const systemMessages = this.model;
-    systemMessages.forEach(systemMessage => {
-      systemMessage.save();
-    });
-    this.notify.success(this.l10n.t('Changes have been saved successfully'),
-      {
-        id: 'message_success'
-      });
-  } catch (e) {
-    console.error('Error while saving system messages', e);
-    this.notify.error(e.errors[0].detail,
-      {
-        id: 'change_error_message'
-      });
-  }
 }
 }
