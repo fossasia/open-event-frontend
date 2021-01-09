523f31682252f2b6f9bbf71311583c36cbd82643
sundaram <dubeysundaram057@gmail.com>
2021-01-09 20:43:32 +0530

    here

--- app/components/ui-table/cell/admin/events/event-no-front.js
+++ app/components/ui-table/cell/admin/events/event-no-front.js
@@ -1,5 +1,3 @@
-import classic from 'ember-classic-decorator';
 import Component from '@ember/component';
 
-@classic
-export default class EventNoFront extends Component {}
+export default Component.extend({});
