03f36d7158a3e9942deddff887f26cab65168052
Schin Chauhan <sachinchauhan2889@gmail.com>
2021-01-13 01:51:21 +0530

    fix

--- app/models/users-events-role.js
+++ app/models/users-events-role.js
@@ -1,5 +1,5 @@
 import ModelBase from 'open-event-frontend/models/base';
-import { belongsTo} from 'ember-data/relationships';
+import { belongsTo } from 'ember-data/relationships';
 
 export default ModelBase.extend({
   event : belongsTo('event'),
