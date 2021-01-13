d6965cb049f6ac56cce793550b78661bc401c369
Creatoon <yasharyan1099@gmail.com>
2021-01-13 05:54:58 +0530

    updated

--- tests/integration/components/orders/company-info-test.js
+++ tests/integration/components/orders/company-info-test.js
@@ -6,21 +6,8 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | orders/company-info', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('it renders', async function (assert) {
     await render(hbs`{{orders/company-info}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#orders/company-info}}
-        template block text
-      {{/orders/company-info}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.textContent.trim(), 'Company Information');
   });
 });
