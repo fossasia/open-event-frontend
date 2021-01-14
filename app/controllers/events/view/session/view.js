2c6f36d6833ad32a975bbbd3ea9754f42238cb2d
Schin Chauhan <sachinchauhan2889@gmail.com>
2021-01-14 19:02:59 +0530

    changes

--- app/controllers/events/view/session/view.js
+++ app/controllers/events/view/session/view.js
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 import { SESSION_FORM_ORDER } from 'open-event-frontend/models/custom-form';
 import FormMixin from 'open-event-frontend/mixins/form';
 import { groupBy } from 'lodash-es';
-import { computed } from '@ember/object';
+import { computed, action } from '@ember/object';
 import { sortCustomFormFields } from 'open-event-frontend/utils/sort';
 
 export default class extends Controller.extend(FormMixin) {
@@ -12,5 +12,25 @@ export default class extends Controller.extend(FormMixin) {
     const grouped = groupBy(this.model.form.toArray(), field => field.get('form'));
     const fields = sortCustomFormFields(grouped.session, SESSION_FORM_ORDER);
     return fields;
+  }
+
+  @action
+  async WithdrawnSession() {
+    const oldState = this.model.session.state;
+    this.model.session.set('state', 'withdrawn');
+    this.set('isLoading', true);
+
+    try {
+      await this.model.session.save();
+      this.notify.success(this.l10n.t('Session has been withdrawn successfully.'), {
+        id: 'session_state'
+      });
+      this.refreshModel.bind(this)();
+    } catch (e) {
+      this.model.session.set('state', oldState);
+      // this.errorHandler.handle(e);
+    } finally {
+      this.set('isLoading', false);
+    }
   }
 }
