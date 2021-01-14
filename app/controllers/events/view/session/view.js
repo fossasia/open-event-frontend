527bf6fb1f7afeab0cb7ae5974f06c85a820bb23
Schin Chauhan <sachinchauhan2889@gmail.com>
2021-01-15 01:36:05 +0530

    photo-speaker

--- app/controllers/events/view/session/view.js
+++ app/controllers/events/view/session/view.js
@@ -2,13 +2,11 @@ import Controller from '@ember/controller';
 import { SESSION_FORM_ORDER } from 'open-event-frontend/models/custom-form';
 import FormMixin from 'open-event-frontend/mixins/form';
 import { groupBy } from 'lodash-es';
-import { computed, action } from '@ember/object';
+import { computed} from '@ember/object';
 import { sortCustomFormFields } from 'open-event-frontend/utils/sort';
-import { inject as service } from '@ember/service';
 
 
 export default class extends Controller.extend(FormMixin) {
-  @service errorHandler;
 
   @computed('model.form')
   get allFields() {
@@ -20,25 +18,5 @@ export default class extends Controller.extend(FormMixin) {
   @computed('model.session')
   get allSpeakers() {
     return this.model.session.get('speakers').map(speaker => speaker.name).join(', ');
-  }
-
-  @action
-  async withdrawnSession(session_id) {
-    const session =  this.store.peekRecord('session', session_id);
-    const oldState = session.state;
-    session.set('state', 'withdrawn');
-    this.set('isLoading', true);
-
-    try {
-      await session.save();
-      this.notify.success(this.l10n.t('Session has been withdrawn successfully.'), {
-        id: 'session_state'
-      });
-    } catch (e) {
-      session.set('state', oldState);
-      this.errorHandler.handle(e);
-    } finally {
-      this.set('isLoading', false);
-    }
   }
 }
