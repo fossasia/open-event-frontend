f34d3ccf380df73eaf07035a32c74c67bdd83ba8
Creatoon <yasharyan1099@gmail.com>
2021-01-14 22:42:05 +0530

    updated

--- app/components/orders/company-info.js
+++ app/components/orders/company-info.js
@@ -1,7 +1,3 @@
-import classic from 'ember-classic-decorator';
 import Component from '@ember/component';
 
-@classic
-export default class EventInfo extends Component {
-  showBanner = true;
-}
+export default Component.extend({});
