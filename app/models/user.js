5ef8ecf2be0e1809be85efcead10596c033313c0
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2021-01-20 12:50:14 +0000

    server issue there was

--- app/models/user.js
+++ app/models/user.js
@@ -81,7 +81,7 @@ export default ModelBase.extend({
   notifications        : hasMany('notification'),
   orders               : hasMany('order'),
   events               : hasMany('event', { inverse: 'user' }),
-  groups               : hasMany('group', { inverse: 'user' }),
+  groups               : hasMany('group'),
   sessions             : hasMany('session'),
   feedbacks            : hasMany('feedback'),
   eventInvoices        : hasMany('event-invoice'),
--- app/routes/groups/list.js
+++ app/routes/groups/list.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 import { action } from '@ember/object';
 import EmberTableRouteMixin from 'open-event-frontend/mixins/ember-table-route';
 
-
 export default class extends Route.extend(EmberTableRouteMixin) {
   titleToken() {
     return this.l10n.t('Groups');
@@ -11,22 +10,14 @@ export default class extends Route.extend(EmberTableRouteMixin) {
   async model(params) {
     this.set('params', params);
     const searchField = 'name';
-    let filterOptions = [];
-    filterOptions = this.applySearchFilters(filterOptions, params, searchField);
+    const filterOptions = this.applySearchFilters([], params, searchField);
     let queryString = {
-      include        : 'speakers,feedbacks,session-type,track',
       filter         : filterOptions,
-      'page[size]'   : params.per_page || 25,
-      'page[number]' : params.page || 1
+      'page[size]'   : params.per_page || 10,
+      'page[number]' : params.page || 4
     };
     queryString = this.applySortFilters(queryString, params);
-
-    const groups =  this.asArray(this.authManager.currentUser.query('groups', queryString));
-
-
-    return {
-      groups
-    };
+    return this.asArray(this.authManager.currentUser.query('groups', queryString));
   }
 
   @action
