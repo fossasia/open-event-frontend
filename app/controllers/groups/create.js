0d53b41eae5da43f4014d6acd39cfaedb081138f
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2021-01-21 03:03:03 +0000

    it never shuld have worked but it does

--- app/controllers/groups/create.js
+++ app/controllers/groups/create.js
@@ -6,7 +6,7 @@ export default class extends Controller {
 
   @service errorHandler;
 
-  GroupName ='';
+  groupName ='';
 
   @action
   async submit() {
@@ -14,7 +14,7 @@ export default class extends Controller {
       this.loading = true;
       const myGroup = this.store.createRecord('group', {
         user : this.authManager.currentUser,
-        name : this.GroupName
+        name : this.groupName
       });
       await myGroup.save();
       this.notify.success(this.l10n.t('Your group has been saved'),
@@ -24,14 +24,10 @@ export default class extends Controller {
       this.router.transitionTo('groups.list');
     } catch (e) {
       console.error('Error while saving group', e);
-      const message = e.errors?.[0]?.detail ?? this.l10n.t('Oops something went wrong. Please try again');
-      this.notify.error(message,
-        {
-          id: 'group_save_error'
-        });
+      this.errorHandler.handle(e);
     } finally {
       this.loading = false;
-      this.set('GroupName', '');
+      this.set('groupName', '');
     }
   }
 }
