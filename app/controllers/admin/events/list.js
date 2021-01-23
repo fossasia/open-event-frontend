7fa4193c1948d28ef3c7be62d78f042f6f405e62
Areeb Jamal <jamal.areeb@gmail.com>
2021-01-23 08:24:08 +0530

    fix: Sort events by descending in admin (#6465)

--- app/controllers/admin/events/list.js
+++ app/controllers/admin/events/list.js
@@ -7,8 +7,6 @@ import EmberTableControllerMixin from 'open-event-frontend/mixins/ember-table-co
 export default class extends Controller.extend(EmberTableControllerMixin) {
   sort_by = 'starts-at';
 
-  sort_dir = 'DSC';
-
    @or('authManager.currentUser.isSuperAdmin', 'authManager.currentUser.isAdmin') hasRestorePrivileges;
 
    get columns() {
