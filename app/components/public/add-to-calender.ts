5b1984db8b333ee8ce9bee1736623a2e06b6eb35
Harsh Shrivastava <Alexshri940@gmail.com>
2021-01-24 16:53:57 +0530

    event description

--- app/components/public/add-to-calender.ts
+++ app/components/public/add-to-calender.ts
@@ -13,6 +13,24 @@ export default class AddToCalender extends Component<Args> {
 
   @service loader: any;
 
+  get description(): string {
+    const { event } = this.args;
+    let desc = `Join the event at ${event.url} \n `;
+    if (event.description) {
+      desc = desc + `<section>
+                        <h3>Event Description </h3>\n
+                        ${event.description}\n
+                    </section>`;
+    }
+    if (event.ownerDescription) {
+      desc = desc +  `<section>
+                          <h3>Organizer Message</h3>\n
+                           ${event.ownerDescription}\n
+                      </section>`;
+    }
+    return desc;
+  }
+
   get startsAt(): Moment {
     const { event } = this.args;
     return moment(event.startsAt).tz(event.timezone);
@@ -27,21 +45,21 @@ export default class AddToCalender extends Component<Args> {
     const { event } = this.args;
     const startTime = this.startsAt.utc().format('YYYYMMDD[T]HHmmSS[Z]');
     const endTime = this.endsAt.utc().format('YYYYMMDD[T]HHmmSS[Z]');
-    return `https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${startTime}/${endTime}&text=${event.name}&location=${this.args.location}&ctz=${event.timezone}&details=${event.description}`;
+    return `https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${startTime}/${endTime}&text=${event.name}&location=${this.args.location}&ctz=${event.timezone}&details=${this.description}`;
   }
 
   get yahooUrl(): string {
     const { event } = this.args;
     const startTime = this.startsAt.format('YYYYMMDD[T]HHmmSS');
     const endTime = this.endsAt.format('YYYYMMDD[T]HHmmSS');
-    return `https://calendar.yahoo.com/?v=60&title=${event.name}&st=${startTime}&et=${endTime}&desc=${event.description}&in_loc=${this.args.location}`;
+    return `https://calendar.yahoo.com/?v=60&title=${event.name}&st=${startTime}&et=${endTime}&desc=${this.description}&in_loc=${this.args.location}`;
   }
 
   get outlookUrl(): string {
     const { event } = this.args;
     const startTime = this.startsAt.utc().format('YYYY[-]MM[-]DDTHH[:]mm[:]SS[Z]');
     const endTime = this.endsAt.utc().format('YYYY[-]MM[-]DDTHH[:]mm[:]SS[Z]');
-    return `https://outlook.live.com/calendar/0/deeplink/compose?subject=${event.name}&startdt=${startTime}&enddt=${endTime}&body=${(event.description).substring(0, 1000)}&location=${this.args.location}`;
+    return `https://outlook.live.com/calendar/0/deeplink/compose?subject=${event.name}&startdt=${startTime}&enddt=${endTime}&body=${(this.description).substring(0, 1000)}&location=${this.args.location}`;
   }
 
   get iCalUrl(): string {
