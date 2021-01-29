cbeb853a80a7d1002a012525cd3fbb4375530968
Creatoon <yasharyan1099@gmail.com>
2021-01-30 04:03:12 +0530

    updated

--- app/controllers/events/view/sessions/list.js
+++ app/controllers/events/view/sessions/list.js
@@ -63,10 +63,10 @@ export default class extends Controller.extend(EmberTableControllerMixin) {
         }
       },
       {
-        name          : this.l10n.t('Average Rating / No. of ratings'),
-        width         : 120,
-        extraValuePaths     : ['averageRating', 'feedbacks'],
-        cellComponent : 'ui-table/cell/events/view/sessions/cell-rating-details'
+        name            : this.l10n.t('Average Rating / No. of ratings'),
+        width           : 120,
+        extraValuePaths : ['averageRating', 'feedbacks'],
+        cellComponent   : 'ui-table/cell/events/view/sessions/cell-rating-details'
       },
       {
         name      : this.l10n.t('Track'),
