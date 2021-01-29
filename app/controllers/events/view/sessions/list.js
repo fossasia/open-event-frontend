56e2caf7b340c0e34cfed7afb4e944780d12be4a
Creatoon <yasharyan1099@gmail.com>
2021-01-30 03:26:43 +0530

    updated

--- app/controllers/events/view/sessions/list.js
+++ app/controllers/events/view/sessions/list.js
@@ -63,16 +63,16 @@ export default class extends Controller.extend(EmberTableControllerMixin) {
         }
       },
       {
-        name            : this.l10n.t('Average Rating'),
-        width           : 90,
-        valuePath       : 'averageRating',
-        cellComponent   : 'ui-table/cell/events/view/sessions/cell-rating-details',
+        name          : this.l10n.t('Average Rating'),
+        width         : 90,
+        valuePath     : 'averageRating',
+        cellComponent : 'ui-table/cell/events/view/sessions/cell-rating-details'
       },
       {
-        name            : this.l10n.t('No. of ratings'),
-        width           : 90,
-        valuePath       : 'feedbacks.length',
-        cellComponent   : 'ui-table/cell/events/view/sessions/cell-rating-details',
+        name          : this.l10n.t('No. of ratings'),
+        width         : 90,
+        valuePath     : 'feedbacks.length',
+        cellComponent : 'ui-table/cell/events/view/sessions/cell-rating-details'
       },
       {
         name      : this.l10n.t('Track'),
--- tests/integration/components/ui-table/cell/events/view/sessions/cell-rating-details-test.js
+++ tests/integration/components/ui-table/cell/events/view/sessions/cell-rating-details-test.js
@@ -6,7 +6,7 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | ui-table/cell/events/view/sessions/cell-rating-details', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
+  test('it renders', async function(assert) {
     await render(hbs`{{ui-table/cell/events/view/sessions/cell-rating-details}}`);
     assert.equal(this.element.textContent.trim(), '');
   });
