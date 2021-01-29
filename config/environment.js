9b6c5757cac328218fb6f1acd076f74ffa98f323
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2021-01-30 00:06:25 +0530

    feat: Mapbox and osm implementation (#6026)
    Co-authored-by: Areeb Jamal <jamal.areeb@gmail.com>

--- config/environment.js
+++ config/environment.js
@@ -16,8 +16,10 @@ module.exports = function(environment) {
     rootURL                  : process.env.ROOT_URL || '/',
     locationType             : 'router-scroll',
     historySupportMiddleware : true,
+    mapboxToken              : process.env.MAPBOX_ACCESS_TOKEN,
     hcaptchaKey              : process.env.HCAPTCHA_SITE_KEY,
-    EmberENV                 : {
+
+    EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
