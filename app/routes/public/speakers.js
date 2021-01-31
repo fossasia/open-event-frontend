935501a1efb6ab32bd6322fc8b57d2c9775a309b
Areeb Jamal <jamal.areeb@gmail.com>
2021-02-01 01:22:58 +0530

    fix: Load less speakers in one go

--- app/routes/public/speakers.js
+++ app/routes/public/speakers.js
@@ -47,12 +47,15 @@ export default class SpeakersRoute extends Route {
       event    : eventDetails,
       speakers : await this.infinity.model('speakers', {
         filter       : SPEAKERS_FILTER,
-        perPage      : 21,
+        perPage      : 9,
         startingPage : 1,
         perPageParam : 'page[size]',
         pageParam    : 'page[number]',
         store        : eventDetails,
-        include      : 'sessions.track'
+        include      : 'sessions.track',
+        cache: true,
+        public: true,
+        infinityCache: 36000
       })
     };
 
