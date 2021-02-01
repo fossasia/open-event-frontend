3b53c30f7990f78e5974760940a89156886a82e4
Creatoon <yasharyan1099@gmail.com>
2021-02-01 13:30:17 +0530

    updated

--- app/helpers/session-color.js
+++ app/helpers/session-color.js
@@ -2,16 +2,7 @@ import Helper from '@ember/component/helper';
 import { stateColorMap } from 'open-event-frontend/utils/dictionary/sessions';
 
 export function sessionColor(params) {
-  switch (params[0]) {
-    case 'accepted':
-      return stateColorMap.accepted;
-    case 'pending':
-      return stateColorMap.pending;
-    case 'confirmed':
-      return stateColorMap.confirmed;
-    default:
-      return 'red';
-  }
+  return stateColorMap[`${params[0]}`];
 }
 
 export default Helper.helper(sessionColor);
