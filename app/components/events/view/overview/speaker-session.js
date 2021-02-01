b8ad3e80e69aa3a8e563b0ab273aa2dbbdd0e26a
maze-runner <dubesundaram99@gmail.com>
2021-02-01 18:47:45 +0530

    point 1 of issue done, some doubts regarding 2nd point

--- app/components/events/view/overview/speaker-session.js
+++ app/components/events/view/overview/speaker-session.js
@@ -1,18 +1,7 @@
 import classic from 'ember-classic-decorator';
 import { classNames } from '@ember-decorators/component';
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
 @classic
 @classNames('ui', 'fluid', 'card')
-export default class SpeakerSession extends Component {
-
-	@computed('data.event.speakers.[]')
-  get noSessionSpeaker() {
-    let count = 0;
-    this.data.event.speakers.map(x => {
-      if (x.get('sessions').length === 0) {count += 1}
-    });
-    return count;
-  }
-}
+export default class SpeakerSession extends Component {}
