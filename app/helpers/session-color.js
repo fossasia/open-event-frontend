aa93b65d13786b576167483b092310679e71d2f3
Creatoon <yasharyan1099@gmail.com>
2021-02-01 12:33:26 +0530

    updated

--- app/helpers/session-color.js
+++ app/helpers/session-color.js
@@ -4,11 +4,11 @@ import { stateColorMap } from 'open-event-frontend/utils/dictionary/sessions';
 export function sessionColor(params) {
   switch (params[0]) {
     case 'accepted':
-      return `${stateColorMap['accepted']}`;
+      return stateColorMap.accepted;
     case 'pending':
-      return `${stateColorMap['pending']}`;
+      return stateColorMap.pending;
     case 'confirmed':
-      return `${stateColorMap['confirmed']}`;
+      return stateColorMap.confirmed;
     default:
       return 'red';
   }
