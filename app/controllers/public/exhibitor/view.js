1d3f7859316b8dad0651e2f6f8c464109b5999ba
Schin Chauhan <sachinchauhan2889@gmail.com>
2021-02-10 14:23:48 +0530

    new enhancement

--- app/controllers/public/exhibitor/view.js
+++ app/controllers/public/exhibitor/view.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 import { extractYoutubeUrl } from 'open-event-frontend/utils/url';
+import { buttonColor } from 'open-event-frontend/utils/dictionary/social-media';
 
 export default class extends Controller {
   @computed('model.videoUrl')
@@ -17,5 +18,16 @@ export default class extends Controller {
   get pptLink() {
     const { slidesUrl } = this.model;
     return slidesUrl?.indexOf('.pptx') > -1 || slidesUrl?.indexOf('.ppt') > -1;
+  }
+
+  @computed('model.socailLinks')
+  get links() {
+    return this.model.socialLinks.map(socialLink => {
+      let newLink = {};
+      newLink['name'] = socialLink.name;
+      newLink['link'] = socialLink.link;
+      newLink['color'] = buttonColor[socialLink.name];
+      return newLink;
+    });
   }
 }
