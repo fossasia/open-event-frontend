f89cea040e5c4df3090ae0b2534fe0bc4f18b241
Areeb Jamal <jamal.areeb@gmail.com>
2021-02-10 23:17:26 +0530

    wip: Session Fav

--- app/models/session.js
+++ app/models/session.js
@@ -48,6 +48,7 @@ export default class Session extends ModelBase.extend({
   feedbacks          : hasMany('feedback'),
   event              : belongsTo('event'), // temporary
   creator            : belongsTo('user'),
+  favourite          : belongsTo('user-favourite-session', { async: false }),
 
   status: computed('state', 'deletedAt', function() {
     if (this.deletedAt !== null) {
