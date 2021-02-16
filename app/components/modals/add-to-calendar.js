faecc189ffaa6dbc13a454904998fbd43a923aa8
Schin Chauhan <sachinchauhan2889@gmail.com>
2021-02-16 17:51:53 +0530

    codacy

--- app/components/modals/add-to-calendar.js
+++ app/components/modals/add-to-calendar.js
@@ -5,28 +5,28 @@ import ModalBase from 'open-event-frontend/components/modals/modal-base';
 @classic
 export default class AddToCalendarModal extends ModalBase {
     calendarIcons = {
-        'Google Calendar' : 'google',
-        'iCal' : 'calendar',
-        'Yahoo' : 'yahoo',
-        'Outlook' : 'microsoft',
+      'Google Calendar' : 'google',
+      'iCal'            : 'calendar',
+      'Yahoo'           : 'yahoo',
+      'Outlook'         : 'microsoft'
     }
 
     calendarColors = {
-        'Google Calendar' : 'blue',
-        'iCal' : 'red',
-        'Yahoo' : 'purple',
-        'Outlook' : 'teal',
+      'Google Calendar' : 'blue',
+      'iCal'            : 'red',
+      'Yahoo'           : 'purple',
+      'Outlook'         : 'teal'
     }
 
     @computed('calendarUrls')
     get calendars() {
-        return this.calendarUrls.map(calendar => {
-        const newCalendar = {};
-        newCalendar.name = calendar.name;
-        newCalendar.url = calendar.url;
-        newCalendar.icon = this.calendarIcons[calendar.name];
-        newCalendar.color = this.calendarColors[calendar.name];
-        return newCalendar;
-        });
+      return this.calendarUrls.map(calendar => {
+      const newCalendar = {};
+      newCalendar.name = calendar.name;
+      newCalendar.url = calendar.url;
+      newCalendar.icon = this.calendarIcons[calendar.name];
+      newCalendar.color = this.calendarColors[calendar.name];
+      return newCalendar;
+      });
     }
 }
