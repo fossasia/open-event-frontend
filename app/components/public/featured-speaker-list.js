6324e10c5b301a0175e6ae08bb441b6644694179
maze-runner <dubeysundaram057@gmail.com>
2021-02-17 18:23:20 +0530

    logic++

--- app/components/public/featured-speaker-list.js
+++ app/components/public/featured-speaker-list.js
@@ -2,4 +2,18 @@ import classic from 'ember-classic-decorator';
 import Component from '@ember/component';
 
 @classic
-export default class FeaturedSpeakerList extends Component {}
+export default class FeaturedSpeakerList extends Component {
+    
+    moreSpeakers = false;
+    
+    async loadSpeakerAvailability() {
+        const moreSpeaker = await this.loader.load(`/events/${this.event.identifier}/more-speakers`);
+        if(moreSpeaker > 0){
+            this.set('moreSpeakers', true);
+        }
+    }
+    didInsertElement() {
+        this._super(...arguments);
+        this.loadSpeakerAvailability();
+    }
+}
