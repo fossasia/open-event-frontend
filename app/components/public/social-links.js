b3156eccdefe027cab9f926a953a06f8c39d5e75
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-02-20 13:11:10 +0530

    Changes requested done

--- app/components/public/social-links.js
+++ app/components/public/social-links.js
@@ -2,7 +2,7 @@ import { A } from '@ember/array';
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['ui', 'basic', 'segment', 'm-0', 'pt-0'],
+  classNames: ['ui', 'basic', 'm-0', 'pt-0','pl-0','pr-0'],
 
   socialLinks: A()
 });
