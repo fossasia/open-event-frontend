17402561272ce852d6937819e7fd0ee3e2bd639f
maze-runner <dubeysundaram057@gmail.com>
2021-02-22 08:03:31 +0530

    wip

--- app/components/forms/register-form.js
+++ app/components/forms/register-form.js
@@ -4,12 +4,12 @@ import ENV from 'open-event-frontend/config/environment';
 
 export default Component.extend(FormMixin, {
 
-  email        : '',
-  password     : '',
-  isLoading    : false,
-  captcha      : false,
-  showHcaptcha : !!ENV.hcaptchaKey,
-  captchaRendered: false,
+  email           : '',
+  password        : '',
+  isLoading       : false,
+  captcha         : false,
+  showHcaptcha    : !!ENV.hcaptchaKey,
+  captchaRendered : false,
 
   getValidationRules() {
     return {
