6d0c54edbec6eeb7e96017dba1043b01c3fd7fe7
maze-runner <dubeysundaram057@gmail.com>
2021-02-24 11:20:59 +0530

    complete

--- app/components/public/stream/join-video.ts
+++ app/components/public/stream/join-video.ts
@@ -36,7 +36,7 @@ export default class JoinVideo extends Component<Args> {
   @action
   openPanel(): void {
     if (this.canAccess) {
-      this.args.showSidePanel();
+      this.args.showSidePanel?.();
       this.router.transitionTo('public', this.args.event, { queryParams: { side_panel: true } })
     } else {
       this.router.transitionTo('public', this.args.event, { queryParams: { video_dialog: true } })
