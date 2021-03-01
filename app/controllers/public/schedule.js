813e8a8c1fa3c4d7f53b27640ac127e17e926d7e
maze-runner <dubeysundaram057@gmail.com>
2021-03-01 23:39:47 +0530

    complete

--- app/controllers/public/schedule.js
+++ app/controllers/public/schedule.js
@@ -6,13 +6,9 @@ export default class ScheduleController extends Controller {
 
     @action
     fullScreen() {
-        let elem = document.getElementById("fullscreen");
+        let calendar = document.getElementById("fullscreen");
         if (!document.fullscreenElement) {
-            elem.requestFullscreen().catch(err => {
-              alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-            });
-          } else {
-            document.exitFullscreen();
-          }
+            calendar.requestFullscreen();
+        }
     }
 }
