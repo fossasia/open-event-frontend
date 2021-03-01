1e4430ff02323bcb38ef8f0b696df50644ebef1f
maze-runner <dubeysundaram057@gmail.com>
2021-03-01 23:17:56 +0530

    working full screen

--- app/controllers/public/schedule.js
+++ app/controllers/public/schedule.js
@@ -1,5 +1,18 @@
 import classic from 'ember-classic-decorator';
 import Controller from '@ember/controller';
-
+import { action } from '@ember/object';
 @classic
-export default class ScheduleController extends Controller {}
+export default class ScheduleController extends Controller {
+
+    @action
+    fullScreen() {
+        let elem = document.getElementById("fullscreen");
+        if (!document.fullscreenElement) {
+            elem.requestFullscreen().catch(err => {
+              alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+            });
+          } else {
+            document.exitFullscreen();
+          }
+    }
+}
