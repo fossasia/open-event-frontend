23d47d13969e14f04a0c0ee7cc4aa3e68406d4e5
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2021-03-02 00:26:52 +0530

    fix: full screen for calendar view (#6868)

--- app/controllers/public/schedule.js
+++ app/controllers/public/schedule.js
@@ -1,5 +1,14 @@
 import classic from 'ember-classic-decorator';
 import Controller from '@ember/controller';
-
+import { action } from '@ember/object';
 @classic
-export default class ScheduleController extends Controller {}
+export default class ScheduleController extends Controller {
+
+  @action
+  fullScreen() {
+    const calendar = document.getElementById('fullscreen');
+    if (!document.fullscreenElement) {
+      calendar.requestFullscreen();
+    }
+  }
+}
