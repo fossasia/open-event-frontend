4d41efe02abb723729c30d4e8ef3030ed671510d
MeghalBisht <mailtomeghu25@gmail.com>
2021-03-03 05:32:58 +0530

    f

--- app/routes/public/exhibition/view.js
+++ app/routes/public/exhibition/view.js
@@ -1,6 +1,7 @@
 import classic from 'ember-classic-decorator';
 import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
+import { get, set } from '@ember/object';
 
 @classic
 export default class ViewRoute extends Route {
@@ -12,17 +13,30 @@ export default class ViewRoute extends Route {
     const event = this.modelFor('public');
     return hash({
       event,
-      exhibitors: event.query('exhibitors', {
-        sort: 'position'
-      }),
       exhibitor: this.store.findRecord('exhibitor', params.exhibitor_id)
     });
   }
 
-  afterModel(model) {
+  async afterModel(model) {
+    const event = this.modelFor('public');
     super.afterModel(...arguments);
     if (model.exhibitor.status !== 'accepted') {
       this.transitionTo('not-found');
     }
+    const exhibitors = await event.query('exhibitors', {
+      sort         : 'position',
+      'page[size]' : 0
+    })
+    if (exhibitors) {
+      set(this, 'exhibitors', exhibitors);
+    }
+    return {
+      exhibitors
+    };
+  }
+
+  setupController(controller) {
+    super.setupController(...arguments);
+    set(controller, 'model.exhibitors', get(this, 'exhibitors').toArray());
   }
 }
