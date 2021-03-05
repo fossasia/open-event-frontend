9e17066c87e84449a9301c198745ba948a984bf4
mehakkkk <55916817+mehakkkk@users.noreply.github.com>
2021-03-05 17:22:56 +0530

    Back to top button for quick navigate to top (#6595)

--- app/components/footer-main.js
+++ app/components/footer-main.js
@@ -28,8 +28,7 @@ export default class FooterMain extends Component {
 
   @action
   scrollTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    window.scrollTo({top: 0, behavior: 'smooth'});
   }
 
   async didInsertElement() {
