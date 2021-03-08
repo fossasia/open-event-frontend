f82867004c53de5e536f2a08ac091256009034b0
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2021-03-08 23:47:15 +0530

    fix: add option "Add Related Session" (#6928)

--- app/routes/events/view/exhibitors/edit.js
+++ app/routes/events/view/exhibitors/edit.js
@@ -6,10 +6,32 @@ export default class ExhibitorsEditRoute extends Route {
   }
 
   async model(params) {
+    const filterOptions = [
+      {
+        or: [
+          {
+            name : 'state',
+            op   : 'eq',
+            val  : 'accepted'
+          },
+          {
+            name : 'state',
+            op   : 'eq',
+            val  : 'confirmed'
+          }
+        ]
+      }
+    ];
     const event = this.modelFor('events.view');
     return {
       event,
-      exhibitor: await this.store.findRecord('exhibitor', params.exhibitor_id)
+      sessions: await event.query('sessions', {
+        'page[size]' : 0,
+        filter       : filterOptions
+      }),
+      exhibitor: await this.store.findRecord('exhibitor', params.exhibitor_id, {
+        include: 'sessions'
+      })
     };
   }
 }
