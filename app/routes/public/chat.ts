e764ade55fa34683d3b40ad5f125e3ebbb5b4611
Sachin Chauhan <sachinchauhan2889@gmail.com>
2021-03-13 09:12:59 +0530

    feat: Add Confirmation Modal before Chat page (#6981)
    Co-authored-by: Areeb Jamal <jamal.areeb@gmail.com>

--- app/routes/public/chat.ts
+++ app/routes/public/chat.ts
@@ -7,7 +7,10 @@ import { action }  from '@ember/object';
 export default class PublicStreamViewChat extends Route {
   @service declare l10n: any;
   @service declare loader: Loader;
+  @service router: any;
+  @service confirm: any;
   @service session : any;
+  @service authManager: any;
 
   titleToken(): string {
     return this.l10n.t('Chat')
@@ -15,6 +18,33 @@ export default class PublicStreamViewChat extends Route {
 
   renderTemplate(): void {
     this.render('public/chat', { into: 'root' });
+  }
+
+  async beforeModel(): Promise<void> {
+    if (this.authManager.currentUser?.isRocketChatRegistered) {
+      return;
+    }
+    try {
+      const heading = this.l10n.t('Please confirm that you understand and agree to the conditions of using the chat!');
+
+      const content =  this.l10n.t('If you join the event chat, your profile name and image will be visible to other attendees. Other event attendees can also contact you directly.') + '<br/><br/>'
+        + this.l10n.t('You may change your chat name and chat profile picture by going to account settings on the chat page on the top left.') + ' '
+        + this.l10n.t('You need to minimize the side panel to access it.') + ' '
+        + this.l10n.t('The feature integration is still in Alpha stage and currently your profile on the eventyay account page and on the chat are not linked and can be independently edited.') + ' '
+        + this.l10n.t('When you change the chat settings you may receive additional email confirmations.') + '<br/><br/>'
+        + this.l10n.t('Do you want to use the chat now?');
+
+      const options = {
+        denyText     : 'Cancel',
+        denyColor    : 'red',
+        approveText  : 'OK',
+        approveColor : 'green',
+        extra        : content
+      }
+      await this.confirm.prompt(heading, options);
+    } catch {
+      this.router.transitionTo('public');
+    }
   }
 
   async model(): Promise<{event: Event, success: boolean, token: string}> {
