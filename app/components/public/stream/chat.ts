8827b232f9fb83f6719571c0d991325bc9a997f1
MeghalBisht <mailtomeghu25@gmail.com>
2021-03-27 21:44:08 +0530

    F

--- app/components/public/stream/chat.ts
+++ app/components/public/stream/chat.ts
@@ -2,9 +2,11 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object'
+import Event from 'open-event-frontend/models/event';
 
 interface Args {
-  token: string
+  token: string,
+  event: Event
 }
 
 export default class PublicStreamChat extends Component<Args> {
@@ -13,6 +15,7 @@ export default class PublicStreamChat extends Component<Args> {
 
   @action
   init(): void {
-    this.iframeUrl = this.settings.rocketChatUrl + '/channel/general?resumeToken=' + this.args.token
+    const roomName = this.args.event.name + this.args.event.identifier;
+    this.iframeUrl = this.settings.rocketChatUrl + `/group/${roomName}?resumeToken=${this.args.token}`
   }
 }
