5e701c439daf29a48d6246f832b2d36731013b59
Kapil Chauhan <kapilchauhan8102@gmail.com>
2021-04-15 13:21:47 +0530

    test fix

--- tests/acceptance/notifications-test.js
+++ tests/acceptance/notifications-test.js
@@ -1,7 +1,6 @@
 import { currentURL, visit } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-import { login } from 'open-event-frontend/tests/helpers/custom-helpers';
 
 module('Acceptance | notifications', function(hooks) {
   setupApplicationTest(hooks);
@@ -9,11 +8,5 @@ module('Acceptance | notifications', function(hooks) {
   test('visiting /notifications without login', async function(assert) {
     await visit('/notifications');
     assert.equal(currentURL(), '/login');
-  });
-
-  test('visiting /notifications with login', async function(assert) {
-    await login(assert);
-    await visit('/notifications');
-    assert.equal(currentURL(), '/notifications/unread');
   });
 });
