dca6379d271f524db2e5ec5bc7f8c26ceeedcb5a
maze-runner <dubeysundaram057@gmail.com>
2021-04-15 17:48:14 +0530

    fixing some

--- app/models/users-groups-role.js
+++ app/models/users-groups-role.js
@@ -1,7 +1,9 @@
 import ModelBase from 'open-event-frontend/models/base';
 import { belongsTo } from 'ember-data/relationships';
+import attr from 'ember-data/attr';
 
 export default ModelBase.extend({
+  email : attr('string'),
   group : belongsTo('group'),
   role  : belongsTo('role'),
   user  : belongsTo('user')
