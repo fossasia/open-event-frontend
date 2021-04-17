762bec892cccabaecb5c29e74b165f21423b50c4
maze-runner <dubeysundaram057@gmail.com>
2021-04-17 19:13:55 +0530

    token fix

--- app/models/users-groups-role.js
+++ app/models/users-groups-role.js
@@ -5,7 +5,7 @@ import attr from 'ember-data/attr';
 export default ModelBase.extend({
   email    : attr('string'),
   accepted : attr('boolean'),
-  token : attr('string'),
+  token    : attr('string'),
   group    : belongsTo('group'),
   role     : belongsTo('role'),
   user     : belongsTo('user')
