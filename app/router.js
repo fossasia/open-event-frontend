c3a476acbaf51120c53daa3326cb646b666b9c2e
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2021-04-21 16:51:43 +0530

    feat: adding team section for groups for managing roles (#7134)
    * starting: work in progress
    
    * testing
    
    * waiting for role invite PR to be complete, A lot of things on hold
    
    * how to query role invite for groups
    
    * fixing things
    
    * fixing some
    
    * fix
    
    * add people
    
    * fix
    
    * adding roles section only organizer at time
    
    * seding role invite working though
    
    * adding token
    
    * token fix
    
    * role-invite redirection
    
    * User corresponding to Users Group Role not Found
    
    * some issues
    
    * role-invite accpet for users groups roles
    
    * factor
    
    * fix

--- app/router.js
+++ app/router.js
@@ -83,6 +83,7 @@ Router.map(function() {
     this.route('chat');
   });
   this.route('role-invites');
+  this.route('group-invites');
   this.route('pricing');
   this.route('create');
   this.route('not-found');
@@ -174,6 +175,7 @@ Router.map(function() {
   this.route('groups', function() {
     this.route('list');
     this.route('create');
+    this.route('view', { path: '/:group_id' });
     this.route('edit', { path: '/:group_id/edit' });
   });
   this.route('my-tickets', function() {
