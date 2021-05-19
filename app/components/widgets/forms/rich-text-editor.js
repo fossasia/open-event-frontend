96846c5392f3c6076c335dabcbe69e91a4e6e7b8
maze-runner <dubeysundaram057@gmail.com>
2021-05-19 15:53:07 +0530

    f

--- app/components/widgets/forms/rich-text-editor.js
+++ app/components/widgets/forms/rich-text-editor.js
@@ -62,7 +62,7 @@ export default Component.extend({
 
       const updateValue = () => {
         debounce(this, () => {
-          let value = String(this.editor.getValue()).replace(/<br>*$/g, '');
+          let value = String(this.editor.getValue()).replace(/(<br>)*$/g, '');
           value = value + '<br>';
           let trimmedValue = new String('');
           let i = value.length;
