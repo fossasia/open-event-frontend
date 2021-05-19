6f5c46c47cf51d95dc7eaab47b7d19efe4ddec18
maze-runner <dubeysundaram057@gmail.com>
2021-05-19 07:32:04 +0530

    regex update

--- app/components/widgets/forms/rich-text-editor.js
+++ app/components/widgets/forms/rich-text-editor.js
@@ -62,7 +62,7 @@ export default Component.extend({
 
       const updateValue = () => {
         debounce(this, () => {
-          const value = String(this.editor.getValue()).replace(/(<br>)*$/g, '').replace(/(\\n|\\r)/g, '');
+          const value = String(this.editor.getValue()).replace(/(<br>)*$/g, '').replace(/(\\n|\\r|\\r\\n)/g, '');
           this.setProperties({ _value: value, value });
         }, 200);
       };
