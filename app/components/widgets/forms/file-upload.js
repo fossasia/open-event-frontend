e067f1d7b63906c65461cb6542161e03f79258ee
ranjit <ranjitmaheshshah@gmail.com>
2021-05-27 09:14:33 +0530

    f

--- app/components/widgets/forms/file-upload.js
+++ app/components/widgets/forms/file-upload.js
@@ -82,8 +82,16 @@ export default class FileUpload extends Component {
     }
   }
 
+  @action
+  refreshSlide() {
+    if (document.getElementById('googleFrame')) {
+      document.getElementById('googleFrame').src += '';
+    }
+  }
+
   init() {
     super.init(...arguments);
+    this.refreshSlide();
     this.set('selectedFile', this.fileUrl);
     if (this.selectedFile) {
       this.set('needsConfirmation', true);
