908ffa92b5cb7e21910fba0c54adf919ad210d3d
tanay123456789 <tanaydwivedi456@gmail.com>
2021-06-03 11:15:57 +0530

    feature added language tab

--- app/components/system-form.js
+++ app/components/system-form.js
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
-/*import { tracked } from '@glimmer/tracking';*/
+/* import { tracked } from '@glimmer/tracking';*/
 import { action } from '@ember/object';
 
 export default class SystemFormComponent extends Component {
@@ -8,6 +8,6 @@ export default class SystemFormComponent extends Component {
     @action
   dropDown() {
     const mylist = document.querySelector('myList');
-    document.getElementById('favourite').value = mylist.options[mylist.selectedIndex].text;
+    document.querySelector("favourite").value=mylist.options[mylist.selectedIndex].text;
   }
 }
