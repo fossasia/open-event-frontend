7f34c5d2d836f92b6f476606e237e5501dc38141
maze-runner <dubeysundaram057@gmail.com>
2021-06-04 21:56:11 +0530

    it's all about logic

--- app/components/ui-table/cell/events/view/tickets/attendees/cell-action.js
+++ app/components/ui-table/cell/events/view/tickets/attendees/cell-action.js
@@ -1,5 +1,17 @@
 import classic from 'ember-classic-decorator';
 import Component from '@ember/component';
+import moment from 'moment';
 
 @classic
-export default class CellAction extends Component {}
+export default class CellAction extends Component {
+    didInsertElement() {
+        const attendee = this.store.peekRecord('attendee', this.record, { backgroundReload: false });
+        let checkinTimes =attendee.get('checkinTimes');
+        checkinTimes = checkinTimes.split(',');
+        const lastDay = checkinTimes[checkinTimes.length - 1].split('T')[0];
+        const presentDay =  moment().toISOString().split('T')[0];
+        if(lastDay < presentDay) {
+            attendee.set('isCheckedIn', false);
+        }
+    }
+}
