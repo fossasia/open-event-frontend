9e7fcb374ab4ef99ce3f4c29c33e16d1124d3861
Meghal Bisht <mailtomeghu25@gmail.com>
2021-06-06 20:30:57 +0530

    feat: Session Forms: Provide dropdown field for levels (#7185)
    * show dropdown for level
    
    * query
    
    * f
    
    * f
    
    Co-authored-by: Schin Chauhan <sachinchauhan2889@gmail.com>
    Co-authored-by: ranjit shah <ranjitmaheshshah@gmail.com>

---
+++ app/utils/dictionary/levels.ts
@@ -0,0 +1,18 @@
+export const levels = [
+  {
+    name     : 'Beginner',
+    position : 1
+  },
+  {
+    name     : 'Intermediate',
+    position : 2
+  },
+  {
+    name     : 'Advanced',
+    position : 3
+  },
+  {
+    name     : 'Expert',
+    position : 4
+  }
+];
