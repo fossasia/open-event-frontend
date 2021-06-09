c1959c53d00f8d51e3a5cf424095d78e9813abe0
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2021-06-09 15:09:30 +0530

    f

--- app/controllers/events/view/tickets/attendees/list.js
+++ app/controllers/events/view/tickets/attendees/list.js
@@ -74,7 +74,7 @@ export default class extends Controller.extend(EmberTableControllerMixin) {
     const attendee = this.store.peekRecord('attendee', attendee_id, { backgroundReload: false });
     attendee.toggleProperty('isCheckedIn');
     if (attendee.isCheckedIn) {
-      const newCheckinTimes = attendee.get('checkinTimes') === null ? `${moment().toISOString()}` : `${moment().toISOString()}`;
+      const newCheckinTimes = `${moment().toISOString()}`;
       attendee.set('checkinTimes', newCheckinTimes);
     }
     attendee.save()
