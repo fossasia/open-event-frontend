e67eadd8c61a35d7c74f164152830763cb79aa12
Deepak-032 <dkart032@gmail.com>
2021-06-16 13:46:48 +0530

    dropdown menu top fixed

--- app/components/side-bar.js
+++ app/components/side-bar.js
@@ -58,3 +58,22 @@ export default class SideBar extends Component {
     }
   }
 }
+
+$(function() {
+
+  $(window).bind('scroll', function() {
+    const menuPosition = $('#col-content').offset().top;
+
+    if ($(window).scrollTop() > menuPosition) {
+      $('#public-event-content').addClass('fixed');
+    } else {
+      $('#public-event-content').removeClass('fixed');
+    }
+    const ticketPosition = $('#tickets').offset().top;
+    if ($(window).scrollTop() > ticketPosition) {
+      $('#public-event-content .text').html('<i class="mr-4  ticket icon"></i>Tickets');
+    } else {
+      $('#public-event-content .text').html('<i class="mr-4  info icon"></i>Info');
+    }
+  });
+});
