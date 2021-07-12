17ec53a977c33ca0c197c585056b002f2c02fb18
ranjit <ranjitmaheshshah@gmail.com>
2021-07-12 22:04:13 +0530

    f

--- app/models/admin-statistics-group.js
+++ app/models/admin-statistics-group.js
@@ -2,7 +2,7 @@ import attr from 'ember-data/attr';
 import ModelBase from 'open-event-frontend/models/base';
 
 export default ModelBase.extend({
-  groups : attr('number'),
-  followers  : attr('number'),
-  groupEvents : attr('number'),
+  groups      : attr('number'),
+  followers   : attr('number'),
+  groupEvents : attr('number')
 });
