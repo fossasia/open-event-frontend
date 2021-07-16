cf3e3ed2765ff9f89afdedc7981072be75710451
MeghalBisht <mailtomeghu25@gmail.com>
2021-07-16 15:07:35 +0530

    make create page and edit page alike

--- app/components/forms/group/group-events-form.js
+++ app/components/forms/group/group-events-form.js
@@ -78,7 +78,7 @@ export default class GroupEventsForm extends Component.extend(FormMixin) {
           {
             id: 'group_save'
           });
-        this.router.transitionTo('groups.list');
+        this.router.transitionTo('groups.edit.settings', this.group.id);
       } catch (e) {
         console.error('Error while saving group', e);
         this.errorHandler.handle(e);
