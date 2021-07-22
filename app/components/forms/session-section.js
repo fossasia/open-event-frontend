de575261ebbc11b5c7222775269df425999941ca
ranjit <ranjitmaheshshah@gmail.com>
2021-07-22 17:26:39 +0530

    f

--- app/components/forms/session-section.js
+++ app/components/forms/session-section.js
@@ -6,5 +6,15 @@ import { levels } from 'open-event-frontend/utils/dictionary/levels';
 
 export default Component.extend(FormMixin, {
   languages : orderBy(languages, 'name'),
-  levels    : orderBy(levels, 'position')
+  levels    : orderBy(levels, 'position'),
+
+  actions: {
+    removeDocument(document) {
+      this.data.session.slides = this.data.session.slides.filter(dl => dl !== document);
+    },
+    addSessionDocument() {
+      this.data.session.slides = [...this.data.session.slides, { name: '', link: '' }];
+    }
+  }
+
 });
