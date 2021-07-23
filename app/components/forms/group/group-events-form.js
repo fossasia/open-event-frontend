09cab6aeec42224ebb85ed2a431bb162167e3e0d
Schin Chauhan <sachinchauhan2889@gmail.com>
2021-07-23 21:31:22 +0530

    announcement of event

--- app/components/forms/group/group-events-form.js
+++ app/components/forms/group/group-events-form.js
@@ -1,25 +1,60 @@
 
 import Component from '@ember/component';
 import { action, computed } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import classic from 'ember-classic-decorator';
 import FormMixin from 'open-event-frontend/mixins/form';
 import moment from 'moment';
 import { sortBy } from 'lodash-es';
+import { all } from 'rsvp';
 
 @classic
 export default class GroupEventsForm extends Component.extend(FormMixin) {
 
   @service errorHandler;
 
+  @tracked eventToBeUpdate = [];
+  @tracked unsavedEvents = [];
+  @tracked savedEvents = [];
+
   @action
   addEvent(event) {
+    const eventIds = this.eventToBeUpdate.map(evnt => evnt.identifier);
+    if (this.savedEvents.includes(event.id) && eventIds.includes(event.id)) {
+      event.isAnnounced = true;
+      this.eventToBeUpdate = this.eventToBeUpdate.filter(event => event.identifier === event.id);
+    }
     this.group.events.pushObject(event);
   }
 
   @action
   removeEvent(event) {
+    if (event.isAnnounced) {
+      event.isAnnounced = false;
+      this.eventToBeUpdate.push(event);
+    }
     this.group.events.removeObject(event);
+  }
+
+  @action
+  async announceEvent(event) {
+    this.set('isLoading', true);
+    this.loader.load(`/groups/${this.group.id}/events/${event.identifier}/announce`)
+      .then(() => {
+        event.isAnnounced = true;
+        this.notify.success(this.l10n.t('Event has been announced.'), {
+          id: 'event_announce'
+        });
+      })
+      .catch(() => {
+        this.notify.error(this.l10n.t('Oops something went wrong. Please try again.'), {
+          id: 'event_announce'
+        });
+      })
+      .finally(() => {
+        this.set('isLoading', false);
+      });
   }
 
   @computed('events.[]', 'group.events.[]')
@@ -29,12 +64,12 @@ export default class GroupEventsForm extends Component.extend(FormMixin) {
 
   @computed('events.[]', 'group.events.[]')
   get pastEvents() {
-    return sortBy(this.events.toArray().filter(event => { return moment(event.endsAt) < moment()}), ['startsAt']).reverse();
+    return sortBy(this.remainingEvents.toArray().filter(event => { return moment(event.endsAt) < moment()}), ['startsAt']).reverse();
   }
 
   @computed('events.[]', 'group.events.[]')
   get upcomingEvents() {
-    return sortBy(this.events.toArray().filter(event => { return moment(event.endsAt) > moment()}), ['startsAt']).reverse();
+    return sortBy(this.remainingEvents.toArray().filter(event => { return moment(event.endsAt) > moment()}), ['startsAt']).reverse();
   }
 
   @action
@@ -48,13 +83,17 @@ export default class GroupEventsForm extends Component.extend(FormMixin) {
     event.preventDefault();
     this.onValid(async() => {
       try {
-        this.loading = true;
+        this.set('isLoading', true);
         /* For the first save throws an error -> field may not be null, as social links are added in group-settings-form
         Hence, passing an empty array. */
         if (!this.group.socialLinks) {
           this.group.socialLinks = [];
         }
         await this.group.save();
+        const updatedEvents = this.eventToBeUpdate.map(event => {
+          return event.save();
+        });
+        await all([...updatedEvents]);
         this.notify.success(this.l10n.t('Your group has been saved'),
           {
             id: 'group_save'
@@ -64,8 +103,12 @@ export default class GroupEventsForm extends Component.extend(FormMixin) {
         console.error('Error while saving group', e);
         this.errorHandler.handle(e);
       } finally {
-        this.loading = false;
+        this.set('isLoading', false);
       }
     });
+  }
+
+  didInsertElement() {
+    this.set('savedEvents', [...this.groupEvents.toArray().map(event => event.identifier)]);
   }
 }
--- app/models/event.js
+++ app/models/event.js
@@ -53,6 +53,7 @@ export default class Event extends ModelBase.extend(CustomPrimaryKeyMixin, {
   isChatEnabled             : attr('boolean', { defaultValue: false }),
   isBillingInfoMandatory    : attr('boolean', { defaultValue: false }),
   isDocumentEnabled         : attr('boolean', { defaultValue: false }),
+  isAnnounced               : attr('boolean', { defaultValue: false }),
 
   isTaxEnabled    : attr('boolean', { defaultValue: false }),
   canPayByPaypal  : attr('boolean', { defaultValue: false }),
