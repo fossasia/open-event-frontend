3a608cea03c8e49063af4f0ca0632fe058401d93
Schin Chauhan <sachinchauhan2889@gmail.com>
2021-07-27 17:23:44 +0530

    fix error

--- app/components/forms/events/view/videoroom-form.js
+++ app/components/forms/events/view/videoroom-form.js
@@ -294,11 +294,17 @@ export default class VideoroomForm extends Component.extend(FormMixin) {
     this.onValid(() => {
       const existingEmails = this.data.stream.moderators.map(moderator => moderator.email);
       if (!existingEmails.includes(this.moderatorEmail)) {
-        const moderator = this.store.createRecord('video-stream-moderator', {
-          email       : this.moderatorEmail,
-          videoStream : this.data.stream
-        });
-        this.data.stream.moderators.pushObject(moderator);
+        const existingModerator = this.deletedModerators.filter(moderator => moderator.email === this.moderatorEmail);
+        if (existingModerator.length === 0) {
+          const newModerator = this.store.createRecord('video-stream-moderator', {
+            email       : this.moderatorEmail,
+            videoStream : this.data.stream
+          });
+          this.data.stream.moderators.pushObject(newModerator);
+        } else {
+          const moderator = this.store.peekRecord('video-stream-moderator', existingModerator[0].id);
+          this.data.stream.moderators.pushObject(moderator);
+        }
       }
       this.deletedModerators = this.deletedModerators.filter(moderator => moderator.email !== this.moderatorEmail);
       this.moderatorEmail = '';
