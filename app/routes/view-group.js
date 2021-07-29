a1e101c31e80c2bc29ac786413c3325e0c36d820
Schin Chauhan <sachinchauhan2889@gmail.com>
2021-07-29 18:30:53 +0530

    separate view page of groups

---
+++ app/routes/view-group.js
@@ -0,0 +1,20 @@
+import classic from 'ember-classic-decorator';
+import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
+
+@classic
+export default class ViewRoute extends Route {
+  titleToken(model) {
+    const groupTitle = model.group.name;
+    return groupTitle.concat(' - View');
+  }
+
+  model(params) {
+
+    return hash({
+      group: this.store.findRecord('group', params.group_id, {
+        include: 'events'
+      })
+    });
+  }
+}
