a5fc760bca8d50cb9d4001e117a58da2cd14ef93
maze-runner <dubeysundaram057@gmail.com>
2021-08-12 08:11:36 +0530

    f

--- app/controllers/admin/groups/list.js
+++ app/controllers/admin/groups/list.js
@@ -18,7 +18,7 @@ export default class extends Controller.extend(EmberTableControllerMixin) {
         headerComponent : 'tables/headers/sort',
         cellComponent   : 'ui-table/cell/admin/groups/cell-group-name',
         actions         : {
-          deleteGroup: this.deleteGroup.bind(this),
+          deleteGroup  : this.deleteGroup.bind(this),
           restoreGroup : this.restoreGroup.bind(this)
         }
       },
