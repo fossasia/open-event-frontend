c4da49cbb73eeae21d58b0aa781545391fb7403b
daretobedifferent18 <daretobedifferent10920@gmail.com>
2021-09-22 11:29:43 +0530

    Calculate Tax properly

--- app/models/order.js
+++ app/models/order.js
@@ -1,4 +1,5 @@
 import attr from 'ember-data/attr';
+import { computed } from '@ember/object';
 import ModelBase from 'open-event-frontend/models/base';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 import CustomPrimaryKeyMixin from 'open-event-frontend/mixins/custom-primary-key';
@@ -37,5 +38,26 @@ export default ModelBase.extend(CustomPrimaryKeyMixin, {
   discountCode     : belongsTo('discount-code'),
   accessCode       : belongsTo('access-code'),
   tickets          : hasMany('ticket', { readOnly: true }),
-  attendees        : hasMany('attendee')
+  attendees        : hasMany('attendee'),
+
+  ticketPriceWithTax: computed('amount', 'event.tax.isTaxIncludedInPrice', 'event.tax.rate', function() {
+    const taxType = this.event.get('tax.isTaxIncludedInPrice');
+    if (!taxType) {
+      return ((this.event.get('tax.rate') / 100) * this.amount).toFixed(2);
+    }
+    return this.price;
+  }),
+
+  /**
+   * This attribute computes value added tax amount in the cases
+   * when tax amount is included in ticket price, otherwise return
+   * 0
+   */
+  includedTaxAmount: computed('amount', 'event.tax.isTaxIncludedInPrice', 'event.tax.rate', function() {
+    const taxType = this.event.get('tax.isTaxIncludedInPrice');
+    if (taxType) {
+      return ((this.event.get('tax.rate') / 100) * this.amount).toFixed(2);
+    }
+    return 0;
+  })
 });
