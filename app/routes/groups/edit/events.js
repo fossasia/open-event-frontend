495dac0a66cf5a7e5a97ab5775fe6a3a79c5ef26
Pavnesh Chaturvedi <56963647+pc-beast@users.noreply.github.com>
2022-01-19 18:25:57 +0530

    fix: event sorting logic in group events area (#8272)
    Co-authored-by: Shubham Upreti <shubham.upreti@gmail.com>

--- app/routes/groups/edit/events.js
+++ app/routes/groups/edit/events.js
@@ -12,56 +12,68 @@ export default class EventsRoute extends Route.extend(AuthenticatedRouteMixin) {
   }
 
   async model(params) {
-    const filterOptions = [
-      {
-        name : 'deleted-at',
-        op   : 'eq',
-        val  : null
-      }
 
+    const upcomingEventsFilterOptions = [
+      {
+        and: [
+          {
+            name : 'deleted-at',
+            op   : 'eq',
+            val  : null
+          },
+          {
+            name : 'starts-at',
+            op   : 'gt',
+            val  : moment().toISOString()
+          }
+        ]
+      }
     ];
 
-    const upcomingEventsFilterOptions = [...filterOptions,
-      {
-        name : 'starts-at',
-        op   : 'ge',
-        val  : moment().toISOString()
-      }
-    ];
 
-    const pastEventsFilterOptions = [...filterOptions,
+    const pastEventsFilterOptions = [
       {
-        name : 'ends-at',
-        op   : 'le',
-        val  : moment().toISOString()
+        and: [
+          {
+            name : 'deleted-at',
+            op   : 'eq',
+            val  : null
+          },
+          {
+            name : 'ends-at',
+            op   : 'le',
+            val  : moment().toISOString()
+          }
+        ]
       }
     ];
 
     const group = await this.store.findRecord('group', params.group_id, {
       include: 'events,user'
     });
-
+    const upcomingEvents = await this.infinity.model('events', {
+      filter       : upcomingEventsFilterOptions,
+      perPage      : 9,
+      startingPage : 1,
+      perPageParam : 'page[size]',
+      pageParam    : 'page[number]',
+      store        : this.authManager.currentUser,
+      include      : 'event-topic,event-sub-topic,event-type,speakers-call',
+      sort         : '-starts-at'
+    });
+    const pastEvents = await this.infinity.model('events', {
+      filter       : pastEventsFilterOptions,
+      perPage      : 9,
+      startingPage : 1,
+      perPageParam : 'page[size]',
+      pageParam    : 'page[number]',
+      store        : this.authManager.currentUser,
+      include      : 'event-topic,event-sub-topic,event-type,speakers-call',
+      sort         : '-starts-at'
+    });
     return hash({
-      upcomingEvents: this.infinity.model('events', {
-        filter       : upcomingEventsFilterOptions,
-        perPage      : 9,
-        startingPage : 1,
-        perPageParam : 'page[size]',
-        pageParam    : 'page[number]',
-        store        : this.authManager.currentUser,
-        include      : 'event-topic,event-sub-topic,event-type,speakers-call',
-        sort         : 'name'
-      }),
-      pastEvents: this.infinity.model('events', {
-        filter       : pastEventsFilterOptions,
-        perPage      : 9,
-        startingPage : 1,
-        perPageParam : 'page[size]',
-        pageParam    : 'page[number]',
-        store        : this.authManager.currentUser,
-        include      : 'event-topic,event-sub-topic,event-type,speakers-call',
-        sort         : 'name'
-      }),
+      upcomingEvents,
+      pastEvents,
       group,
       groupEvents: group.query('events', {})
     });
