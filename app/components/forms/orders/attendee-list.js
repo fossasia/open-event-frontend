d9f169010de8caa3b0fcd2615293aa5841886696
pc-beast <impavanesh@gmail.com>
2022-01-21 20:06:31 +0530

    f

--- app/components/forms/orders/attendee-list.js
+++ app/components/forms/orders/attendee-list.js
@@ -64,7 +64,7 @@ export default class AttendeeList extends Component {
         })
         .catch(e => {
           console.error('Error while downloading tickets', e);
-        })
+        });
     } catch (error) {
       console.warn(error);
     }
