fb69fd7a04e5f15962eeb207517c35e987691f54
pc-beast <impavanesh@gmail.com>
2022-02-03 12:33:16 +0530

    fix: chst button points to side chat panel

--- app/components/public/stream/side-panel.ts
+++ app/components/public/stream/side-panel.ts
@@ -19,8 +19,15 @@ export default class PublicStreamSidePanel extends Component<Args> {
   @service loader!: Loader;
   @service declare event: EventService;
   @service declare settings: any;
+  @service authManager: any;
+  @service declare l10n: any;
+  @service confirm: any;
+
 
   @tracked shown = false;
+  @tracked chatPanelShown = false;
+  @tracked token = '';
+  @tracked success = null;
   @tracked loading = true;
   @tracked streams: VideoStream[] = [];
   @tracked showSessions: number | null = null;
@@ -84,9 +91,43 @@ export default class PublicStreamSidePanel extends Component<Args> {
       } catch (e) {
         console.error('Error while loading rooms in video stream', e);
       }
+      const { success, token } = await this.loader.load(`/events/${this.args.event.id}/chat-token`);
+      this.token = token;
+      this.success = success;
+      this.loading = false;
     }
 
     this.loading = false;
     this.streams = [...this.streams];
+  }
+
+  @action
+  async showChatPanel(): Promise<void> {
+    if (this.authManager.currentUser?.isRocketChatRegistered) {
+      this.chatPanelShown = true;
+      return;
+    }
+    try {
+      const heading = this.l10n.t('Please confirm that you understand and agree to the conditions of using the chat!');
+
+      const content =  this.l10n.t('If you join the event chat, your profile name and image will be visible to other attendees. Other event attendees can also contact you directly.') + '<br/><br/>'
+        + this.l10n.t('You may change your chat name and chat profile picture by going to account settings on the chat page on the top left.') + ' '
+        + this.l10n.t('You need to minimize the side panel to access it.') + ' '
+        + this.l10n.t('The feature integration is still in Alpha stage and currently your profile on the {{appName}} account page and on the chat are not linked and can be independently edited.', { appName: this.settings.appName }) + ' '
+        + this.l10n.t('When you change the chat settings you may receive additional email confirmations.') + '<br/><br/>'
+        + this.l10n.t('Do you want to use the chat now?');
+
+      const options = {
+        denyText     : 'Cancel',
+        denyColor    : 'red',
+        approveText  : 'OK',
+        approveColor : 'green',
+        extra        : content
+      }
+      await this.confirm.prompt(heading, options);
+      this.chatPanelShown = true;
+    } catch {
+      this.chatPanelShown = false;
+    }
   }
 }
