1629c4c701691b90e9e4fb7dc6ab3e7ba4623be5
pc-beast <impavanesh@gmail.com>
2022-02-04 19:27:14 +0530

    fix: change over lay to same component

--- app/components/public/stream/side-panel.ts
+++ app/components/public/stream/side-panel.ts
@@ -12,7 +12,8 @@ import EventService from 'open-event-frontend/services/event';
 interface Args {
   videoStream: VideoStream,
   event: Event,
-  shown: boolean
+  shown: boolean,
+  showChatPanel: any
 }
 
 export default class PublicStreamSidePanel extends Component<Args> {
@@ -20,12 +21,9 @@ export default class PublicStreamSidePanel extends Component<Args> {
   @service declare event: EventService;
   @service declare settings: any;
   @service authManager: any;
-  @service declare l10n: any;
   @service confirm: any;
 
-
   @tracked shown = false;
-  @tracked chatPanelShown = false;
   @tracked token = '';
   @tracked success = null;
   @tracked loading = true;
@@ -99,35 +97,5 @@ export default class PublicStreamSidePanel extends Component<Args> {
 
     this.loading = false;
     this.streams = [...this.streams];
-  }
-
-  @action
-  async showChatPanel(): Promise<void> {
-    if (this.authManager.currentUser?.isRocketChatRegistered) {
-      this.chatPanelShown = true;
-      return;
-    }
-    try {
-      const heading = this.l10n.t('Please confirm that you understand and agree to the conditions of using the chat!');
-
-      const content =  this.l10n.t('If you join the event chat, your profile name and image will be visible to other attendees. Other event attendees can also contact you directly.') + '<br/><br/>'
-        + this.l10n.t('You may change your chat name and chat profile picture by going to account settings on the chat page on the top left.') + ' '
-        + this.l10n.t('You need to minimize the side panel to access it.') + ' '
-        + this.l10n.t('The feature integration is still in Alpha stage and currently your profile on the {{appName}} account page and on the chat are not linked and can be independently edited.', { appName: this.settings.appName }) + ' '
-        + this.l10n.t('When you change the chat settings you may receive additional email confirmations.') + '<br/><br/>'
-        + this.l10n.t('Do you want to use the chat now?');
-
-      const options = {
-        denyText     : 'Cancel',
-        denyColor    : 'red',
-        approveText  : 'OK',
-        approveColor : 'green',
-        extra        : content
-      }
-      await this.confirm.prompt(heading, options);
-      this.chatPanelShown = true;
-    } catch {
-      this.chatPanelShown = false;
-    }
   }
 }
