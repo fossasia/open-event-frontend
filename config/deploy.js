339a821a1300f42a4c132230035e22adf9f1dcfc
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2023-05-18 09:27:10 +0530

    Update deploy.js

--- config/deploy.js
+++ config/deploy.js
@@ -1,50 +1,40 @@
 /* jshint node: true */
-
 module.exports = function(deployTarget) {
   const ENV = {
     build: {}
     // include other plugin configuration that applies to all deploy targets here
   };
-
-  ENV['revision-data'] = {
+ENV['revision-data'] = {
     type : 'file-hash',
     scm  : null
   };
-
-  ENV['html-manifest'] = {
+ ENV['html-manifest'] = {
     filename     : 'manifest.appcache',
     prependPath  : '/',
     excludePaths : ['index.html', '/assets/moment-locales/', '/assets/locales/', 'assets/moment-locales', 'assets/locales/'],
     includePaths : ['/'],
     network      : ['*'],
-
-    manifestRoot(context) {
+manifestRoot(context) {
       return context.config['html-manifest'].prependPath;
     }
   };
-
-  ENV.pipeline = {
+ENV.pipeline = {
     disabled: {
       git: true
     }
   };
-
-  if (deployTarget === 'development') {
+if (deployTarget === 'development') {
     ENV.build.environment = 'development';
   }
-
-  if (deployTarget === 'staging') {
+ if (deployTarget === 'staging') {
     ENV.build.environment = 'production';
   }
-
-  if (deployTarget === 'production') {
+if (deployTarget === 'production') {
     ENV.build.environment = 'production';
   }
-
   if (deployTarget === 'gh-pages') {
     ENV['html-manifest'].prependPath = `/${process.env.REPO_SLUG || 'open-event-frontend'}/`;
   }
-
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
   // ENV object synchronously.
