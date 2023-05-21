43c4e9e4eaf969c944563c30394731735879939f
cweitat <cweitat@gmail.com>
2023-05-22 01:09:15 +0800

    Update test-helper.js

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -6,8 +6,9 @@ import { start } from 'ember-qunit';
 import loadEmberExam from 'ember-exam/test-support/load';
 
 QUnit.config.testTimeout = 300000;
-
-setApplication(Application.create(config.APP));
+let application;
+application = setApplication(Application.create(config.APP));
+application.__container__.lookup('service:intl').setLocale('en'); // Set the locale to 'en'
 
 loadEmberExam();
 start();
