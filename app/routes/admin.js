6e2873fe674773854a0c61f788757517fc04c2ea
cweitat <chungweitat@fundigo.co>
2023-05-21 19:37:57 +0800

    update auth

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,18 +1,22 @@
 import classic from 'ember-classic-decorator';
 import Route from '@ember/routing/route';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import { getOwner } from '@ember/application';
+// import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 @classic
-export default class AdminRoute extends Route.extend(AuthenticatedRouteMixin) {
+export default class AdminRoute extends Route {
   beforeModel(transition) {
     super.beforeModel(transition);
-    if (!this.authManager.currentUser.isAdmin) {
-      throw {
-        errors: [
-          { status: 403 }
-        ]
-      };
-    }
+    let isAuthenticated = requireAuthentication(getOwner(this), transition);
+    if (isAuthenticated) {
+      if (!this.authManager.currentUser.isAdmin) {
+        throw {
+          errors: [
+            { status: 403 }
+          ]
+        };
+      }
+    } 
   }
 
   titleToken() {
