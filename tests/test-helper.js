df3a46b9c971a488da8f861ed6d0d95b7368d39b
cweitat <cweitat@gmail.com>
2023-05-22 02:03:05 +0800

    revert

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,15 +4,10 @@ import config from 'open-event-frontend/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import loadEmberExam from 'ember-exam/test-support/load';
-import { getOwner } from '@ember/application';
 
 QUnit.config.testTimeout = 300000;
-let application;
-application = setApplication(Application.create(config.APP));
 
-// Set the locale globally for all tests
-let intlService = getOwner(application).lookup('service:intl');
-intlService.setLocale('en');
+setApplication(Application.create(config.APP));
 
 loadEmberExam();
 start();
