420f88becabe6480e0fd9d55013668e5779c69c2
Hieu Lam - TMA <135117837+lthanhhieu@users.noreply.github.com>
2023-06-22 17:31:47 +0700

    feature-8789: Add option "home wiki" to attendee form (#8805)
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8740: Make attendee form ticket specific
    
    * feature-8740: Make attendee form ticket specific
    
    * feature-8740: Make attendee form ticket specific
    
    * Demo-20230616: Merge code
    
    * Demo-20230616: Merge code
    
    * Demo-20230616: Merge code
    
    * feature-8470: Revert code demo
    
    * feature-8470: Revert code demo
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Merge code development
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8740: Make attendee form ticket specific
    
    * feature-8740: Make attendee form ticket specific
    
    * feature-8740: Make attendee form ticket specific
    
    * feature-8789: Add option 'home wiki' to attendee form
    
    * feature-8789: Add option 'home wiki' to attendee form
    
    * feature-8789:translate
    
    * feature-8789: Merge code development
    
    * feature-8789: Fix git comment
    
    * feature-8789: Fix git comment
    
    ---------
    
    Co-authored-by: Khang On - TMA <135116292+khangon@users.noreply.github.com>
    Co-authored-by: khangon <odkhang@tma.com.vn>

--- app/mixins/custom-form.js
+++ app/mixins/custom-form.js
@@ -627,6 +627,17 @@ export default Mixin.create(MutableArray, {
         isIncluded      : false,
         isPublic        : false,
         event           : parent
+      }),
+      this.store.createRecord('custom-form', {
+        name            : 'homeWiki',
+        fieldIdentifier : 'home_wiki',
+        formID          : _formID,
+        form            : 'attendee',
+        type            : 'select',
+        isRequired      : false,
+        isIncluded      : false,
+        isPublic        : false,
+        event           : parent
       })
     ];
   }
