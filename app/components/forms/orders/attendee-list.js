8b2e932700d30ebd7a0fcf16c43a9569dca1d050
khangon <odkhang@tma.com.vn>
2023-06-27 18:45:16 +0700

    feature-8824 should not map language for second time

--- app/components/forms/orders/attendee-list.js
+++ app/components/forms/orders/attendee-list.js
@@ -30,11 +30,11 @@ export default class AttendeeList extends Component {
   }
 
   languageFormMapCodeToName(attendee, key) {
-    if ( attendee.get(key.concat("_name_mapped")) ) {
-      return
+    if (attendee.get(key.concat('_name_mapped'))) {
+      return;
     }
     const languageFormMap = [];
-    const languageFormList = attendee[key].split(',').map(item => item);
+    const languageFormList = attendee[key].split(',');
     languageForms.forEach(languageForm => {
       languageFormList.forEach(item => {
         if (item === languageForm.code) {
@@ -42,7 +42,7 @@ export default class AttendeeList extends Component {
         }
       });
     });
-    attendee.set(key.concat("_name_mapped"), true);
+    attendee.set(key.concat('_name_mapped'), true);
     return attendee.set(key, languageFormMap.map(select => select).join(', '));
   }
 
