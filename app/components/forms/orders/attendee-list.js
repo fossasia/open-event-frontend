933684dc5ef9656f59b7d5ca2f640360a3eac650
Hieu Lam - TMA <135117837+lthanhhieu@users.noreply.github.com>
2023-06-28 11:57:23 +0700

    feature-8800: Make custom form question translatable by the organizer (#8836)
    * feature-8800: Make custom form question translatable by the organizer
    
    * feature-8800: Make custom form question translatable by the organizer
    
    * feature-8800: Make custom form question translatable by the organizer
    
    * feature-8800: Make custom form question translatable by the organizer
    
    ---------
    
    Co-authored-by: Khang On - TMA <135116292+khangon@users.noreply.github.com>
    Co-authored-by: cweitat <cweitat@gmail.com>

--- app/components/forms/orders/attendee-list.js
+++ app/components/forms/orders/attendee-list.js
@@ -47,7 +47,27 @@ export default class AttendeeList extends Component {
 
   @computed('fields.@each.form')
   get allFields() {
-    return groupBy(this.fields.toArray(), field => field.form);
+    const current_locale = this.cookies.read('current_locale');
+    return groupBy(this.fields.toArray(), field => {
+      const { main_language } = field;
+
+      if ((main_language && main_language.split('-')[0] === current_locale) || !field.translations || !field.translations.length) {
+        field.transName = field.name;
+      } else if (field.translations?.length) {
+
+        const transName = field.translations.filter(trans => trans.language_code.split('-')[0] === current_locale);
+
+        if (transName.length) {
+          field.transName = transName[0].name;
+        } else {
+          field.transName = field.name;
+        }
+      } else {
+        field.transName = field.name;
+      }
+
+      return field.form;
+    });
   }
 
   @action
