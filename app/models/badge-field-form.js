bedd6123fa5ba0c74b2504044b9c3443a5b16e19
Khang On - TMA <135116292+khangon@users.noreply.github.com>
2023-08-03 03:57:25 +0700

    feature-8980: Badge creation in wizard misses default options (#8994)
    * feature-8980: Badge creation in wizard misses default options
    
    * feature-8980: Badge creation in wizard misses default options
    add options for font style
    
    * refactor code
    
    * feature-8980: Badge creation in wizard misses default options
    add font style as multiple choise
    
    * set default font style
    
    ---------
    
    Co-authored-by: nnhathung <nnhathung@tma.com.vn>
    Co-authored-by: lthanhhieu <lthanhhieu@tma.com.vn>

--- app/models/badge-field-form.js
+++ app/models/badge-field-form.js
@@ -15,18 +15,18 @@ export default ModelBase.extend({
   badge_id          : attr('string'),
   field_identifier  : attr('string'),
   custom_field      : attr('string'),
-  sample_text       : attr('string'),
-  font_size         : attr('number'),
-  font_name         : attr('string'),
-  font_color        : attr('string'),
-  text_rotation     : attr('number'),
-  margin_top        : attr('number'),
-  margin_bottom     : attr('number'),
-  margin_left       : attr('number'),
-  margin_right      : attr('number'),
-  font_weight       : attr('number'),
+  sample_text       : attr('string', { defaultValue: 'Sample Text' }),
+  font_size         : attr('number', { defaultValue: 14 }),
+  font_name         : attr('string', { defaultValue: 'Arial' }),
+  font_color        : attr('string', { defaultValue: '#000000' }),
+  text_rotation     : attr('number', { defaultValue: 0 }),
+  margin_top        : attr('number', { defaultValue: 0 }),
+  margin_bottom     : attr('number', { defaultValue: 0 }),
+  margin_left       : attr('number', { defaultValue: 0 }),
+  margin_right      : attr('number', { defaultValue: 0 }),
+  font_weight       : attr(),
   text_alignment    : attr('string', { defaultValue: 'center' }),
-  text_type         : attr('string'),
+  text_type         : attr('string', { defaultValue: 'None' }),
   is_deleted        : attr('boolean'),
   qr_custom_field   : attr(),
   is_field_expanded : attr('boolean', { defaultValue: true }),
@@ -37,12 +37,28 @@ export default ModelBase.extend({
   ticket            : hasMany('ticket'),
 
   getFieldStyle: computed('font_size', 'font_name', 'font_color', 'text_rotation', 'margin_top', 'margin_bottom', 'margin_left'
-    , 'margin_right', 'font_weight', 'text_alignment', 'text_type', 'isDeleted', 'this', function() {
+    , 'margin_right', 'font_weight.@each', 'text_alignment', 'text_type', 'isDeleted', 'this', function() {
       let font_name = fieldFontName.find(item => {  return item.name === this.font_name});
       if (font_name) {
         font_name = font_name.value;
       }
+      const font_weight = [];
+      const font_style = [];
+      const text_decoration = [];
+      if (this.font_weight) {
+        this.font_weight.forEach(element => {
+          if (element.font_weight) {
+            font_weight.addObject(element.font_weight);
+          }
+          if (element.font_style) {
+            font_style.addObject(element.font_style);
+          }
+          if (element.text_decoration) {
+            text_decoration.addObject(element.text_decoration);
+          }
+        });
+      }
       return htmlSafe(
-        'font-family: ' + font_name + '; font-size: ' + this.font_size + 'px; text-align: ' + this.text_alignment + '; text-transform: ' + this.text_type + '; color:' + this.font_color + '; font-weight:' + this.font_weight + '; -webkit-transform: rotate(' + this.text_rotation + 'deg); -moz-transform: rotate(' + this.text_rotation + 'deg); -o-transform: rotate(' + this.text_rotation + 'deg); writing-mode: lr-tb; margin-top:' + this.margin_top + 'mm; margin-bottom:' + this.margin_bottom + 'mm; margin-left:' + this.margin_left + 'mm; margin-right:' + this.margin_right + 'mm;');
+        'font-family: ' + font_name + '; font-size: ' + this.font_size + 'px; text-align: ' + this.text_alignment + '; text-transform: ' + this.text_type + '; color:' + this.font_color + '; font-weight:' + font_weight.join(',') + '; font-style:' + font_style.join(',') + '; text-decoration: ' + text_decoration.join(',') + '; -webkit-transform: rotate(' + this.text_rotation + 'deg); -moz-transform: rotate(' + this.text_rotation + 'deg); -o-transform: rotate(' + this.text_rotation + 'deg); writing-mode: lr-tb; margin-top:' + this.margin_top + 'mm; margin-bottom:' + this.margin_bottom + 'mm; margin-left:' + this.margin_left + 'mm; margin-right:' + this.margin_right + 'mm;');
     })
 });
