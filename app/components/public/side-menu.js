6df6acb6fae42bd63724c49fdf7f52e1c6bf3a43
ntthitrinh <ntthitrinh@tma.com.vn>
2023-08-14 13:40:32 +0700

    feature-9069: Improve mobile top menu and add calendar icon

--- app/components/public/side-menu.js
+++ app/components/public/side-menu.js
@@ -74,6 +74,9 @@ export default class SideMenu extends Component {
 
   @action
   scrollToTarget(section) {
+    if(!section) return;
+    const el = document.querySelector(`#${section}`);
+    if(!el) return;
     this.hideSidebar();
     document.querySelector(`#${section}`).scrollIntoView({
       behavior: 'smooth'
