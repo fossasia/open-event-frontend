8816e075797b0f334227227ed3a253f8b923dbbe
lthanhhieu <lthanhhieu@tma.com.vn>
2023-08-15 17:03:08 +0700

    fix-9080: There was no result for searching with Ticket price 0.0

--- app/mixins/ember-table-route.js
+++ app/mixins/ember-table-route.js
@@ -109,8 +109,8 @@ export default Mixin.create({
           if (isNaN(params.search)) {
             continue;
           }
-          condition = 'eq';
-          value = params.search;
+          condition = params.search && params.search !== 0 ? 'eq' : 'is_';
+          value = params.search && params.search !== 0 ? params.search : null;
         }
         const filter = {
           name : splitField[0],
