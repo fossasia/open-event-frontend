098bc38f06f20b923f3e349086c1c5296c6a7083
ntthitrinh <ntthitrinh@tma.com.vn>
2023-08-16 13:45:40 +0700

    feature-9069: Improve mobile top menu and add calendar icon

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -151,6 +151,7 @@ export default class NavBar extends Component {
     this.showSessions = await this.eventService.hasSessions(this.globalData.idEvent);
   }
 
+
   @action
   logout() {
     this.authManager.logout();
