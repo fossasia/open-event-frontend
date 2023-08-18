378b75a15a2b8d0732d66cdfd66c7dfb788bc7e8
odkhang <odkhang@tma.com.vn>
2023-08-18 16:39:40 +0700

    fix-9126: Delete option to show events on the current day

--- app/routes/public/sessions.js
+++ app/routes/public/sessions.js
@@ -48,7 +48,7 @@ export default class SessionsRoute extends Route {
     const dates = await this.loader.load('/events/' + event.id + '/sessions/dates');
     if (moment().isSameOrAfter(event.startsAt) && moment().isSameOrBefore(event.endsAt) && dates.includes(moment().format('YYYY-MM-DD')) && !isDateFilterActive) {
       isDateFilterActive = true;
-      this.transitionTo('public.sessions', event.get('identifier'), { queryParams: { date: moment().format('YYYY-MM-DD') } });
+      this.transitionTo('public.sessions', event.get('identifier'));
     }
   }
 
