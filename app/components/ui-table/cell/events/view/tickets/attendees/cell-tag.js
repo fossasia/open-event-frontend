c63b50f46beb08d0c422a7daa263b08aefc613a8
lthanhhieu <lthanhhieu@tma.com.vn>
2023-08-25 11:03:53 +0700

    feature-8684: Add option to tag attendees

--- app/components/ui-table/cell/events/view/tickets/attendees/cell-tag.js
+++ app/components/ui-table/cell/events/view/tickets/attendees/cell-tag.js
@@ -5,12 +5,29 @@ import { htmlSafe } from '@ember/string';
 
 @classic
 export default class CellTag extends Component {
+  didInsertElement() {
+    this._super(...arguments)
+    this.$('.removeTag').on('click', () => { this.removeTag() })
+  }
+
+  removeTag() {
+    const attendee = this.props.row;
+    if (attendee) {
+      attendee.set('tagId', '');
+      attendee.save();
+    }
+  }
 
   @computed('record')
   get attendeeTagName() {
     const tag = this.props?.options?.tags?.find(tag => parseInt(tag.id) === this.record);
     if (tag) {
-      return htmlSafe(`<div class="ui inverted segment link" style="background-color: ${tag.color}; ">${tag.name}</div>`);
+      return htmlSafe(
+        `<div class="ui inverted segment link" style="background-color: ${tag.color}; display: inline-flex">
+          <div>${tag.name}</div>
+          <i class="close icon removeTag"}}></i>
+        </div>`
+      );
     }
     return '';
   }
